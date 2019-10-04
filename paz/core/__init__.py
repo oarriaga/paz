70aeb45fb78563665cb61b31b14ad80e5aebd4c1
Octavio Arriaga <octavio.arriaga@dfki.de>
2019-10-04 09:56:00 +0200

    Add video player as import of core

--- paz/core/__init__.py
+++ paz/core/__init__.py
@@ -2,3 +2,4 @@ from .loader import Loader
 from .sequencer import Sequencer
 from .messages import Box2D, Pose6D
 from .processor import Processor, SequentialProcessor, LambdaProcessor
+from .ops import VideoPlayer
