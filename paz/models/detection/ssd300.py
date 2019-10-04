54a1308c3a1041330d12e96a767ff81559ba1495
Octavio Arriaga <octavio.arriaga@dfki.de>
2019-10-04 13:24:38 +0200

    Add check for changing flag for loading fine-tunning models

--- paz/models/detection/ssd300.py
+++ paz/models/detection/ssd300.py
@@ -193,7 +193,9 @@ def SSD300(num_classes=21, base_weights='VOC', head_weights='VOC',
         weights_path = get_file(model_filename, WEIGHT_PATH + model_filename,
                                 cache_subdir='altamira/models')
         print('Loading %s model weights' % weights_path)
-        model.load_weights(weights_path, by_name=True)
-
+        finetunning_model_names = ['SSD300-VGG-None_weights.hdf5',
+                                   'SSD300-VOC-None_weights.hdf5']
+        by_name = True if model_filename in finetunning_model_names else False
+        model.load_weights(weights_path, by_name)
     model.prior_boxes = create_prior_boxes('VOC')
     return model
