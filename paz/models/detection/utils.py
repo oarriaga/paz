cd7915a3b22513a20bd194800354c7c006393246
Octavio Arriaga <octavio.arriaga@dfki.de>
2019-10-04 11:30:05 +0200

    Refactor multibox head creation function

--- paz/models/detection/utils.py
+++ paz/models/detection/utils.py
@@ -1,11 +1,8 @@
-from __future__ import division
 from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import Activation
 from tensorflow.keras.layers import BatchNormalization
 from tensorflow.keras.layers import Flatten
-from tensorflow.keras.layers import Lambda
 from tensorflow.keras.layers import Reshape
-from tensorflow.keras.layers import concatenate
 from tensorflow.keras.layers import Concatenate
 from tensorflow.keras.regularizers import l2
 import tensorflow.keras.backend as K
@@ -16,88 +13,56 @@ import numpy as np
 from itertools import product
 
 
-def create_multibox_head(
-        output_tensors, num_classes, num_priors, with_l2_norm=False,
-        with_batch_norm=False, l2_regularization=0.0005, num_regressions=4,
-        base_name=''):
-
+def create_multibox_head(tensors, num_classes, num_priors, l2_loss=0.0005,
+                         num_regressions=4, l2_norm=False, batch_norm=False):
+    """Adds multibox head with classification and regression output tensors.
+    # Arguments
+        tensors: List of tensors.
+        num_classes: Int. Number of classes.
+        num_priors. List of integers. Length should equal to tensors length.
+            Each integer represents the amount of bounding boxes shapes in
+            each feature map value.
+        l2_loss: Float. L2 loss value to be added to convolutional layers.
+        num_regressions: Number of values to be regressed per prior box.
+            e.g. for 2D bounding boxes we regress 4 coordinates.
+        l2_norm: Boolean. If `True` l2 normalization layer is applied to
+            each before a convolutional layer.
+        batch_norm: Boolean. If `True` batch normalization is applied after
+            each convolutional layer.
+    """
     classification_layers, regression_layers = [], []
-    for layer_arg, base_layer in enumerate(output_tensors):
-
-        if with_l2_norm:
+    for layer_arg, base_layer in enumerate(tensors):
+        if l2_norm:
             base_layer = Conv2DNormalization(20)(base_layer)
-        str_arg = str(layer_arg)
 
-        # classification leaf
-        class_name = 'classification_leaf_' + str(layer_arg)
-        class_leaf = Conv2D(
-            num_priors[layer_arg] * num_classes, (3, 3),
-            padding='same', name=base_name + class_name,
-            kernel_regularizer=l2(l2_regularization))(base_layer)
-
-        if with_batch_norm:
-            class_leaf = BatchNormalization(
-                name=base_name + 'batch_norm_ssd_3_' + str_arg)(class_leaf)
-
-        class_leaf = Flatten(
-            name=base_name + 'flat_classification_' + str_arg)(class_leaf)
+        # classification leaf -------------------------------------------------
+        num_kernels = num_priors[layer_arg] * num_classes
+        class_leaf = Conv2D(num_kernels, 3, padding='same',
+                            kernel_regularizer=l2(l2_loss))(base_layer)
+        if batch_norm:
+            class_leaf = BatchNormalization()(class_leaf)
+        class_leaf = Flatten()(class_leaf)
         classification_layers.append(class_leaf)
 
-        # regression leaf
-        regress_name = 'regression_leaf_' + str(layer_arg)
-        regress_leaf = Conv2D(
-            num_priors[layer_arg] * num_regressions, (3, 3),
-            padding='same', name=base_name + regress_name,
-            kernel_regularizer=l2(l2_regularization))(base_layer)
+        # regression leaf -----------------------------------------------------
+        num_kernels = num_priors[layer_arg] * num_regressions
+        regress_leaf = Conv2D(num_kernels, 3, padding='same',
+                              kernel_regularizer=l2(l2_loss))(base_layer)
+        if batch_norm:
+            regress_leaf = BatchNormalization()(regress_leaf)
 
-        if with_batch_norm:
-            regress_leaf = BatchNormalization(
-                name=base_name + 'batch_norm_ssd_4_' + str_arg)(regress_leaf)
-
-        regress_leaf = Flatten(
-            name=base_name + 'flat_regression_' + str_arg)(regress_leaf)
+        regress_leaf = Flatten()(regress_leaf)
         regression_layers.append(regress_leaf)
 
-    classifications = concatenate(
-        classification_layers, axis=1,
-        name=base_name + 'concatenate_classification_' + str_arg)
-    regressions = concatenate(
-        regression_layers, axis=1,
-        name=base_name + 'concatenate_regression_' + str_arg)
-
-    if hasattr(regressions, '_keras_shape'):
-        num_boxes = regressions._keras_shape[-1] // num_regressions
-        print('_kersa_shape', num_boxes)
-    elif hasattr(regressions, 'int_shape'):
-        num_boxes = K.int_shape(regressions)[-1] // num_regressions
-        print('_int_shape', num_boxes)
+    classifications = Concatenate(axis=1)(classification_layers)
+    regressions = Concatenate(axis=1)(regression_layers)
     num_boxes = K.int_shape(regressions)[-1] // num_regressions
-    print(num_boxes)
-    classifications = Reshape(
-        (num_boxes, num_classes),
-        name=base_name + 'reshape_classification' + str_arg)(classifications)
-
-    classifications = Activation(
-        'softmax',
-        name=base_name + 'softmax_activation_' + str_arg)(classifications)
-
-    regressions = Reshape(
-        (num_boxes, num_regressions),
-        name=base_name + 'reshape_regression_' + str_arg)(regressions)
-
-    # super pose hack
-    if num_regressions == 8:
-        box_reg, pose_reg = Lambda(split_regressions)(regressions)
-        pose_reg = Activation('tanh')(pose_reg)
-        regressions = Concatenate(axis=2)([box_reg, pose_reg])
-
-    boxes_output = concatenate([regressions, classifications],
-                               axis=2, name='predictions')
-    return boxes_output
-
-
-def split_regressions(x):
-    return [x[:, :, :4], x[:, :, 4:8]]
+    classifications = Reshape((num_boxes, num_classes))(classifications)
+    classifications = Activation('softmax')(classifications)
+    regressions = Reshape((num_boxes, num_regressions))(regressions)
+    outputs = Concatenate(
+        axis=2, name='predictions')([regressions, classifications])
+    return outputs
 
 
 def create_prior_boxes(configuration_name='VOC'):
