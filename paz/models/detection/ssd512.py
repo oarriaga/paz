9f79b8554bf6b3a9a568d76b0f26b49b4d30f38c
Octavio Arriaga <octavio.arriaga@dfki.de>
2019-10-04 12:53:29 +0200

    Refactor arguments and documentation

--- paz/models/detection/ssd512.py
+++ paz/models/detection/ssd512.py
@@ -15,13 +15,10 @@ BASE_WEIGHT_PATH = ('https://github.com/oarriaga/altamira-data/'
                     'releases/download/v0.1/')
 
 
-def SSD512(num_classes=81, weights='COCO',
-           input_shape=(512, 512, 3),
-           num_priors=[4, 6, 6, 6, 6, 4, 4],
-           return_base=False, l2_regularization=0.0005,
-           trainable_base=True, batch_norm=False, model_name='SSD512'):
-
-    """Single-shot-multibox detector for 512x512 inputs
+def SSD512(num_classes=81, weights='COCO', input_shape=(512, 512, 3),
+           num_priors=[4, 6, 6, 6, 6, 4, 4], l2_loss=0.0005,
+           return_base=False, trainable_base=True):
+    """Single-shot-multibox detector for 512x512x3 BGR input images.
     # Arguments
         num_classes: Integer. Specifies the number of class labels.
         weights: String or None. If string should be a valid dataset name.
@@ -30,12 +27,11 @@ def SSD512(num_classes=81, weights='COCO',
             spatial and channel resolution e.g. (512, 512, 3).
         num_priors: List of integers. Number of default box shapes
             used in each detection layer.
+        l2_loss: Float. l2 regularization loss for convolutional layers.
         return_base: Boolean. If `True` the model returned is just
             the original base.
         trainable_base: Boolean. If `True` the base model
             weights are also trained.
-        batch_norm: Boolean. If `True` batch normalization layers are
-            added to the head detection layers.
 
     # Reference
         SSD: Single Shot MultiBox Detector: https://arxiv.org/abs/1512.02325
@@ -60,11 +56,11 @@ def SSD512(num_classes=81, weights='COCO',
 
     # Block 1 -----------------------------------------------------------------
     conv1_1 = Conv2D(64, (3, 3), padding='same', activation='relu',
-                     kernel_regularizer=l2(l2_regularization),
+                     kernel_regularizer=l2(l2_loss),
                      trainable=trainable_base,
                      name='conv1_1')(image)
     conv1_2 = Conv2D(64, (3, 3), padding='same', activation='relu',
-                     kernel_regularizer=l2(l2_regularization),
+                     kernel_regularizer=l2(l2_loss),
                      trainable=trainable_base,
                      name='conv1_2')(conv1_1)
     pool1 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2),
@@ -72,11 +68,11 @@ def SSD512(num_classes=81, weights='COCO',
 
     # Block 2 -----------------------------------------------------------------
     conv2_1 = Conv2D(128, (3, 3), padding='same', activation='relu',
-                     kernel_regularizer=l2(l2_regularization),
+                     kernel_regularizer=l2(l2_loss),
                      trainable=trainable_base,
                      name='conv2_1')(pool1)
     conv2_2 = Conv2D(128, (3, 3), padding='same', activation='relu',
-                     kernel_regularizer=l2(l2_regularization),
+                     kernel_regularizer=l2(l2_loss),
                      trainable=trainable_base,
                      name='conv2_2')(conv2_1)
     pool2 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2),
@@ -84,15 +80,15 @@ def SSD512(num_classes=81, weights='COCO',
 
     # Block 3 -----------------------------------------------------------------
     conv3_1 = Conv2D(256, (3, 3), padding='same', activation='relu',
-                     kernel_regularizer=l2(l2_regularization),
+                     kernel_regularizer=l2(l2_loss),
                      trainable=trainable_base,
                      name='conv3_1')(pool2)
     conv3_2 = Conv2D(256, (3, 3), padding='same', activation='relu',
-                     kernel_regularizer=l2(l2_regularization),
+                     kernel_regularizer=l2(l2_loss),
                      trainable=trainable_base,
                      name='conv3_2')(conv3_1)
     conv3_3 = Conv2D(256, (3, 3), padding='same', activation='relu',
-                     kernel_regularizer=l2(l2_regularization),
+                     kernel_regularizer=l2(l2_loss),
                      trainable=trainable_base,
                      name='conv3_3')(conv3_2)
     pool3 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2),
@@ -100,15 +96,15 @@ def SSD512(num_classes=81, weights='COCO',
 
     # Block 4 -----------------------------------------------------------------
     conv4_1 = Conv2D(512, (3, 3), padding='same', activation='relu',
-                     kernel_regularizer=l2(l2_regularization),
+                     kernel_regularizer=l2(l2_loss),
                      trainable=trainable_base,
                      name='conv4_1')(pool3)
     conv4_2 = Conv2D(512, (3, 3), padding='same', activation='relu',
-                     kernel_regularizer=l2(l2_regularization),
+                     kernel_regularizer=l2(l2_loss),
                      trainable=trainable_base,
                      name='conv4_2')(conv4_1)
     conv4_3 = Conv2D(512, (3, 3), padding='same', activation='relu',
-                     kernel_regularizer=l2(l2_regularization),
+                     kernel_regularizer=l2(l2_loss),
                      trainable=trainable_base,
                      name='conv4_3')(conv4_2)
     conv4_3_norm = Conv2DNormalization(20, name='branch_1')(conv4_3)
@@ -117,15 +113,15 @@ def SSD512(num_classes=81, weights='COCO',
 
     # Block 5 -----------------------------------------------------------------
     conv5_1 = Conv2D(512, (3, 3), padding='same', activation='relu',
-                     kernel_regularizer=l2(l2_regularization),
+                     kernel_regularizer=l2(l2_loss),
                      trainable=trainable_base,
                      name='conv5_1')(pool4)
     conv5_2 = Conv2D(512, (3, 3), padding='same', activation='relu',
-                     kernel_regularizer=l2(l2_regularization),
+                     kernel_regularizer=l2(l2_loss),
                      trainable=trainable_base,
                      name='conv5_2')(conv5_1)
     conv5_3 = Conv2D(512, (3, 3), padding='same', activation='relu',
-                     kernel_regularizer=l2(l2_regularization),
+                     kernel_regularizer=l2(l2_loss),
                      trainable=trainable_base,
                      name='conv5_3')(conv5_2)
     pool5 = MaxPooling2D(pool_size=(3, 3), strides=(1, 1),
@@ -135,12 +131,12 @@ def SSD512(num_classes=81, weights='COCO',
     pool5z = ZeroPadding2D(padding=(6, 6))(pool5)
     fc6 = Conv2D(1024, (3, 3), dilation_rate=(6, 6),
                  padding='valid', activation='relu',
-                 kernel_regularizer=l2(l2_regularization),
+                 kernel_regularizer=l2(l2_loss),
                  trainable=trainable_base,
                  name='fc6')(pool5z)
     fc7 = Conv2D(1024, (1, 1), padding='same',
                  activation='relu',
-                 kernel_regularizer=l2(l2_regularization),
+                 kernel_regularizer=l2(l2_loss),
                  trainable=trainable_base,
                  name='branch_2')(fc6)
 
@@ -148,63 +144,62 @@ def SSD512(num_classes=81, weights='COCO',
 
     # Block 6 -----------------------------------------------------------------
     conv6_1 = Conv2D(256, (1, 1), padding='same', activation='relu',
-                     kernel_regularizer=l2(l2_regularization),
+                     kernel_regularizer=l2(l2_loss),
                      name='conv6_1')(fc7)
     conv6_1z = ZeroPadding2D()(conv6_1)
     conv6_2 = Conv2D(512, (3, 3), strides=(2, 2), padding='valid',
                      activation='relu', name='branch_3',
-                     kernel_regularizer=l2(l2_regularization))(conv6_1z)
+                     kernel_regularizer=l2(l2_loss))(conv6_1z)
 
     # Block 7 -----------------------------------------------------------------
     conv7_1 = Conv2D(128, (1, 1), padding='same', activation='relu',
-                     kernel_regularizer=l2(l2_regularization),
+                     kernel_regularizer=l2(l2_loss),
                      name='conv7_1')(conv6_2)
     conv7_1z = ZeroPadding2D()(conv7_1)
     conv7_2 = Conv2D(256, (3, 3), padding='valid', strides=(2, 2),
                      activation='relu', name='branch_4',
-                     kernel_regularizer=l2(l2_regularization))(conv7_1z)
+                     kernel_regularizer=l2(l2_loss))(conv7_1z)
 
     # Block 8 -----------------------------------------------------------------
     conv8_1 = Conv2D(128, (1, 1), padding='same', activation='relu',
-                     kernel_regularizer=l2(l2_regularization),
+                     kernel_regularizer=l2(l2_loss),
                      name='conv8_1')(conv7_2)
     conv8_1z = ZeroPadding2D()(conv8_1)
     conv8_2 = Conv2D(256, (3, 3), padding='valid', strides=(2, 2),
                      activation='relu', name='branch_5',
-                     kernel_regularizer=l2(l2_regularization))(conv8_1z)
+                     kernel_regularizer=l2(l2_loss))(conv8_1z)
 
     # Block 9 -----------------------------------------------------------------
     conv9_1 = Conv2D(128, (1, 1), padding='same', activation='relu',
-                     kernel_regularizer=l2(l2_regularization),
+                     kernel_regularizer=l2(l2_loss),
                      name='conv9_1')(conv8_2)
     conv9_1z = ZeroPadding2D()(conv9_1)
     conv9_2 = Conv2D(256, (3, 3), padding='valid', strides=(2, 2),
                      activation='relu', name='branch_6',
-                     kernel_regularizer=l2(l2_regularization))(conv9_1z)
+                     kernel_regularizer=l2(l2_loss))(conv9_1z)
 
     # Block 10 ----------------------------------------------------------------
     conv10_1 = Conv2D(128, (1, 1), padding='same', activation='relu',
-                      kernel_regularizer=l2(l2_regularization),
+                      kernel_regularizer=l2(l2_loss),
                       name='conv10_1')(conv9_2)
     conv10_1z = ZeroPadding2D()(conv10_1)
     conv10_2 = Conv2D(256, (4, 4), padding='valid', strides=(1, 1),
                       activation='relu', name='branch_7',
-                      kernel_regularizer=l2(l2_regularization))(conv10_1z)
+                      kernel_regularizer=l2(l2_loss))(conv10_1z)
 
-    ssd_tensors = [conv4_3_norm, fc7, conv6_2, conv7_2,
-                   conv8_2, conv9_2, conv10_2]
+    branch_tensors = [conv4_3_norm, fc7, conv6_2, conv7_2,
+                      conv8_2, conv9_2, conv10_2]
     if return_base:
-        output_tensor = ssd_tensors
+        output_tensor = branch_tensors
 
     else:
         output_tensor = create_multibox_head(
-            ssd_tensors, num_classes, num_priors,
-            with_batch_norm=batch_norm, l2_regularization=l2_regularization)
+            branch_tensors, num_classes, num_priors, l2_loss)
 
-    model = Model(inputs=image, outputs=output_tensor, name=model_name)
+    model = Model(inputs=image, outputs=output_tensor, name='SSD512')
 
     if weights is not None:
-        model_name = '_'.join([model_name, weights])
+        model_name = '_'.join(['SSD512', weights])
 
     if weights is not None:
         weights_url = BASE_WEIGHT_PATH + model_name + '_weights.hdf5'
