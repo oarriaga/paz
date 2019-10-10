f4c5e45dbf4da5f8538cb97157d79e4b07724a10
Octavio Arriaga <octavio.arriaga@dfki.de>
2019-10-10 15:35:52 +0200

    Separated DetectBoxes2D into PredictBoxes, DecodeBoxes, DetectBoxes (nms), FilterBoxes

--- paz/processors/detection.py
+++ paz/processors/detection.py
@@ -120,14 +120,18 @@ class ToBoxes2D(Processor):
         class_names: List of class names ordered with respect to the
             class indices from the dataset `boxes`.
     """
-    def __init__(self, class_names):
+    def __init__(self, class_names, one_hot_encoded=False):
         self.arg_to_class = dict(zip(range(len(class_names)), class_names))
+        self.one_hot_encoded = one_hot_encoded
         super(ToBoxes2D, self).__init__()
 
     def call(self, kwargs):
-        numpy_boxes2D, boxes2D = kwargs['boxes2D'], []
+        numpy_boxes2D, boxes2D = kwargs['boxes'], []
         for numpy_box2D in numpy_boxes2D:
-            class_name = self.arg_to_class[numpy_box2D[-1]]
+            if self.one_hot_encoded:
+                class_name = self.arg_to_class[np.argmax(numpy_box2D[4:])]
+            else:
+                class_name = self.arg_to_class[numpy_box2D[-1]]
             boxes2D.append(Box2D(numpy_box2D[:4], 1.0, class_name))
         kwargs['boxes2D'] = boxes2D
         return kwargs
@@ -142,15 +146,95 @@ class MatchBoxes(Processor):
         different than `background`.
         variance: List of two floats.
     """
-    def __init__(self, prior_boxes, iou=.5, variances=[.1, .2]):
+    def __init__(self, prior_boxes, iou=.5):
         self.prior_boxes = prior_boxes
         self.iou = iou
-        self.variances = variances
         super(MatchBoxes, self).__init__()
 
     def call(self, kwargs):
-        kwargs['boxes'] = ops.match(
-            kwargs['boxes'], self.prior_boxes, self.iou, self.variances)
+        boxes = ops.match(kwargs['boxes'], self.prior_boxes, self.iou)
+        kwargs['boxes'] = boxes
+        return kwargs
+
+
+class EncodeBoxes(Processor):
+    """Encodes bounding boxes TODO:
+    """
+    def __init__(self, prior_boxes, variances=[.1, .2]):
+        self.prior_boxes = prior_boxes
+        self.variances = variances
+        super(EncodeBoxes, self).__init__()
+
+    def call(self, kwargs):
+        boxes = kwargs['boxes']
+        encoded_boxes = ops.encode(boxes, self.prior_boxes, self.variances)
+        kwargs['boxes'] = encoded_boxes
+        return kwargs
+
+
+class DecodeBoxes(Processor):
+    """Decodes bounding boxes TODO:
+    """
+    def __init__(self, prior_boxes, variances=[.1, .2]):
+        self.prior_boxes = prior_boxes
+        self.variances = variances
+        super(DecodeBoxes, self).__init__()
+
+    def call(self, kwargs):
+        boxes = kwargs['boxes']
+        encoded_boxes = ops.decode(boxes, self.prior_boxes, self.variances)
+        kwargs['boxes'] = encoded_boxes
+        return kwargs
+
+
+class DetectBoxes(Processor):
+    """Applies detect function TODO:
+    """
+    def __init__(self, prior_boxes, nms_thresh=.45, conf_thresh=0.01):
+        self.prior_boxes = prior_boxes
+        self.nms_thresh = nms_thresh
+        self.conf_thresh = conf_thresh
+        super(DetectBoxes, self).__init__()
+
+    def call(self, kwargs):
+        boxes = kwargs['boxes']
+        # print(boxes.shape)
+        detections = ops.detect(
+            boxes, self.prior_boxes, self.conf_thresh, self.nms_thresh)
+        kwargs['boxes'] = detections
+        return kwargs
+
+
+class FilterBoxes(Processor):
+    """Filters boxes outputted from function ``detect`` as ``Box2D`` messages
+    # Arguments
+        class_names: List of strings.
+        conf_thresh: Float.
+    """
+    def __init__(self, class_names, conf_thresh=0.5):
+        self.class_names = class_names
+        self.conf_thresh = conf_thresh
+        self.arg_to_class = dict(zip(
+            list(range(len(self.class_names))), self.class_names))
+        super(FilterBoxes, self).__init__()
+
+    def call(self, kwargs):
+        detections = kwargs['boxes']
+        kwargs['boxes2D'] = ops.filter_detections(
+            detections, self.arg_to_class, self.conf_thresh)
+        return kwargs
+
+
+class PredictBoxes(Processor):
+    def __init__(self, model, mean=BGR_IMAGENET_MEAN):
+        self.model = model
+        super(PredictBoxes, self).__init__()
+
+    def call(self, kwargs):
+        image = ops.resize_image(kwargs['image'], self.model.input_shape[1:3])
+        image = image - BGR_IMAGENET_MEAN
+        image = np.expand_dims(image, 0)
+        kwargs['boxes'] = np.squeeze(self.model.predict(image))
         return kwargs
 
 
