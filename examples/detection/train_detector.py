9b4cc3c94a8a76d239b5208a8af008e036157ff7
Octavio Arriaga <octavio.arriaga@dfki.de>
2019-10-15 13:45:56 +0200

    Add first training script for VOC dataset

---
+++ examples/detection/train_detector.py
@@ -0,0 +1,89 @@
+import os
+import argparse
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.callbacks import CSVLogger, ModelCheckpoint
+from paz.optimization.callbacks import LearningRateScheduler
+from paz.pipelines import DetectionAugmentation
+from paz.models import SSD300
+from paz.datasets import VOC
+from paz.optimization import MultiboxLoss
+from paz.core import Sequencer
+
+description = 'Training script for single-shot object detection models'
+parser = argparse.ArgumentParser(description=description)
+parser.add_argument('-bs', '--batch_size', default=32, type=int,
+                    help='Batch size for training')
+parser.add_argument('-st', '--steps_per_epoch', default=1000, type=int,
+                    help='Batch size for training')
+parser.add_argument('-lr', '--learning_rate', default=0.001, type=float,
+                    help='Initial learning rate for SGD')
+parser.add_argument('-m', '--momentum', default=0.9, type=float,
+                    help='Momentum for SGD')
+parser.add_argument('-g', '--gamma_decay', default=0.1, type=float,
+                    help='Gamma decay for learning rate scheduler')
+parser.add_argument('-e', '--num_epochs', default=120, type=int,
+                    help='Maximum number of epochs before finishing')
+parser.add_argument('-sp', '--save_path', default='trained_models/',
+                    type=str, help='Path for writing model weights and logs')
+parser.add_argument('-dp', '--data_path', default='/home/username/Vocdevkit/',
+                    type=str, help='Path for writing model weights and logs')
+parser.add_argument('-se', '--scheduled_epochs', nargs='+', type=int,
+                    default=[55, 76],
+                    help='Kernels used in each block e.g. 128 256 512')
+args = parser.parse_args()
+
+optimizer = SGD(args.learning_rate, args.momentum)
+
+data_splits = [['trainval', 'trainval'], 'test']
+data_names = [['VOC2007', 'VOC2012'], 'VOC2007']
+
+# loading datasets
+data_managers, datasets = [], []
+for data_name, data_split in zip(data_names, data_splits):
+    data_manager = VOC(args.data_path, data_split, name=data_name)
+    data_managers.append(data_manager)
+    datasets.append(data_manager.load_data())
+
+# instantiating model
+num_classes = data_managers[0].num_classes
+class_names = data_managers[0].class_names
+model = SSD300(num_classes, base_weights='VGG', head_weights=None)
+model.summary()
+
+# compile model with loss and optimizer
+loss = MultiboxLoss()
+model.compile(optimizer, loss.compute_loss)
+
+# setting data augmentation pipeline
+augmentators = []
+for split in ['train', 'val']:
+    augmentator = DetectionAugmentation(model.prior_boxes, num_classes, split)
+    augmentators.append(augmentator)
+
+# setting sequencers
+sequencers = []
+for data, augmentator in zip(datasets, augmentators):
+    sequencer = Sequencer(data, augmentator, args.batch_size)
+    sequencers.append(sequencer)
+
+# setting callbacks
+model_path = os.path.join(args.save_path, model.name)
+if not os.path.exists(model_path):
+    os.makedirs(model_path)
+log = CSVLogger(os.path.join(model_path, model.name + '-optimization.log'))
+save_path = os.path.join(model_path, 'weights.{epoch:02d}-{val_loss:.2f}.hdf5')
+checkpoint = ModelCheckpoint(save_path, verbose=1, save_weights_only=True)
+schedule = LearningRateScheduler(
+    args.learning_rate, args.gamma_decay, args.scheduled_epochs)
+callbacks = [checkpoint, log, schedule]
+
+# training
+model.fit_generator(
+    sequencers[0],
+    steps_per_epoch=args.steps_per_epoch,
+    epochs=args.num_epochs,
+    verbose=1,
+    callbacks=callbacks,
+    validation_data=sequencers[1],
+    use_multiprocessing=True,
+    workers=6)
