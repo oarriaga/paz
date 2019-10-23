81f14a0f6871d44832510a1c4e0e8c6b45545d21
Octavio Arriaga <octavio.arriaga@dfki.de>
2019-10-23 13:30:47 +0200

    Add pipeline with different split transformations

--- paz/pipelines.py
+++ paz/pipelines.py
@@ -2,45 +2,6 @@ from .core import SequentialProcessor
 from . import processors as pr
 
 
-class KeypointAugmentation(SequentialProcessor):
-    def __init__(self, renderer, projector, keypoints,
-                 image_paths, max_radius_scale, num_occlusions, size, color):
-
-        self.renderer = renderer
-        self.projector = projector
-        self.keypoints = keypoints
-
-        self.add(pr.RenderSample(self.renderer))
-        self.add(pr.ProjectKeypoints(self.projector, self.keypoints))
-        self.add(pr.CastImageToFloat())
-        if image_paths is not None:
-            self.add(pr.AddCroppedBackground(image_paths, size, color))
-            self.add(pr.ConcatenateAlphaMask())
-        for occlusion_arg in range(self.num_occlusions):
-            self.add(pr.AddOcclusion(self.max_radius_scale))
-        self.add(pr.RandomBlur())
-        self.add(pr.RandomContrast())
-        self.add(pr.RandomBrightness())
-        self.add(pr.CastImageToInts())
-        self.add(pr.ConvertColor('BGR', to='HSV'))
-        self.add(pr.CastImageToFloat())
-        self.add(pr.RandomSaturation())
-        self.add(pr.RandomHue())
-        self.add(pr.CastImageToInts())
-        self.add(pr.ConvertColor('HSV', to='BGR'))
-        self.add(pr.RandomLightingNoise())
-        # self.add(pr.Expand(mean=self.mean)) # which position?
-        # self.add(pr.RandomSampleCrop()) # why not use?
-        # self.add(pr.DenormalizeKeypoints()) # TODO
-        # self.add(pr.ApplyRandomTranslation()) # TODO
-        # self.add(pr.ApplyRandomTranslation()) # TODO
-        # self.add(pr.NormalizeKeypoints()) # TODO
-        self.add(pr.Resize(shape=(self.size, self.size)))
-        self.add(pr.CastImageToFloat())
-        self.add(pr.NormalizeImage())
-        self.add(pr.OutputSelector(['image', 'keypoints']))
-
-
 class DetectionAugmentation(SequentialProcessor):
     def __init__(self, prior_boxes, num_classes, split='train', size=300,
                  iou=.5, variances=[.1, .2], mean=pr.BGR_IMAGENET_MEAN):
@@ -105,3 +66,63 @@ class SingleShotInference(SequentialProcessor):
         self.add(pr.DenormalizeBoxes2D())
         self.add(pr.DrawBoxes2D(class_names))
         self.add(pr.CastImageToInts())
+
+
+class KeypointAugmentation(SequentialProcessor):
+    def __init__(self, renderer, projector, keypoints, split='train',
+                 image_paths=None, size=128, num_occlusions=0,
+                 max_radius_scale=0.5, plain_color=[0, 0, 0]):
+
+        if split not in ['train', 'val', 'test']:
+            raise ValueError('Invalid split mode')
+
+        self.renderer = renderer
+        self.projector = projector
+        self.keypoints = keypoints
+        self.image_paths = image_paths
+        self.split = split
+        self.size = self.size
+        self.num_occlusions = num_occlusions
+        self.max_radius_scale = max_radius_scale
+        self.plain_color = plain_color
+
+        self.add(pr.RenderSample(self.renderer))
+        self.add(pr.ConvertColor('RGB', to='BGR'))
+        self.add(pr.ProjectKeypoints(self.projector, self.keypoints))
+
+        if split == 'train':
+            self.add(pr.CastImageToFloat())
+            if image_paths is not None:
+                self.add(pr.AddCroppedBackground(image_paths, size))
+                self.add(pr.ConcatenateAlphaMask())
+            else:
+                self.add(pr.AddPlainBackground(self.plain_color))
+
+            for occlusion_arg in range(self.num_occlusions):
+                self.add(pr.AddOcclusion(self.max_radius_scale))
+
+            self.add(pr.RandomBlur())
+            self.add(pr.RandomContrast())
+            self.add(pr.RandomBrightness())
+            self.add(pr.CastImageToInts())
+            self.add(pr.ConvertColor('BGR', to='HSV'))
+            self.add(pr.CastImageToFloat())
+            self.add(pr.RandomSaturation())
+            self.add(pr.RandomHue())
+            self.add(pr.CastImageToInts())
+            self.add(pr.ConvertColor('HSV', to='BGR'))
+            self.add(pr.RandomLightingNoise())
+            self.add(pr.DenormalizeKeypoints())
+            self.add(pr.ApplyRandomTranslation())
+            self.add(pr.Expand(mean=self.mean))
+            self.add(pr.NormalizeKeypoints())
+
+        self.add(pr.RemoveKeypointsDepth())
+        self.add(pr.Resize(shape=(self.size, self.size)))
+        self.add(pr.CastImageToFloat())
+        self.add(pr.NormalizeImage())
+        self.add(pr.OutputSelector(['image', 'keypoints']))
+
+    @property
+    def output_shapes(self):
+        return [(self.size, self.size, 3), (self.keypoints.shape[:2])]
