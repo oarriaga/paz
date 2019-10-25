9c507c96c6c8e82931d4e1d7b2cc1d1a8b572250
Octavio Arriaga <octavio.arriaga@dfki.de>
2019-10-25 10:46:06 +0200

    Add normalization and denormalization of keypoints

--- paz/core/ops/numpy_ops.py
+++ paz/core/ops/numpy_ops.py
@@ -538,3 +538,37 @@ def make_mosaic(images, shape, border=0):
         mosaic[row * paddedh:row * paddedh + image_shape[0],
                col * paddedw:col * paddedw + image_shape[1], :] = image
     return mosaic
+
+
+def denormalize_keypoints(keypoints, height, width):
+    """Transform normalized keypoint coordinates into image coordinates
+    # Arguments
+        keypoints: Numpy array of shape (num_keypoints, 2)
+        height: Int. Height of the image
+        width: Int. Width of the image
+    """
+    for keypoint_arg, keypoint in enumerate(keypoints):
+        x, y = keypoint[:2]
+        # transform key-point coordinates to image coordinates
+        x = (min(max(x, -1), 1) * width / 2 + width / 2) - 0.5
+        # flip since the image coordinates for y are flipped
+        y = height - 0.5 - (min(max(y, -1), 1) * height / 2 + height / 2)
+        x, y = int(round(x)), int(round(y))
+        keypoints[keypoint_arg][:2] = [x, y]
+    return keypoints
+
+
+def normalize_keypoints(keypoints, height, width):
+    """Transform keypoints in image coordinates to normalized coordinates
+        keypoints: Numpy array of shape (num_keypoints, 2)
+        height: Int. Height of the image
+        width: Int. Width of the image
+    """
+    normalized_keypoints = np.zeros_like(keypoints, dtype=np.float32)
+    for keypoint_arg, keypoint in enumerate(keypoints):
+        x, y = keypoint[:2]
+        # transform key-point coordinates to image coordinates
+        x = (((x + 0.5) - (width / 2.0)) / (width / 2))
+        y = (((height - 0.5 - y) - (height / 2.0)) / (height / 2))
+        normalized_keypoints[keypoint_arg][:2] = [x, y]
+    return normalized_keypoints
