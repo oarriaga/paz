7a154d6837a4485058ddf823fd5d4cf5896a563c
Octavio Arriaga <octavio.arriaga@dfki.de>
2019-10-25 15:51:16 +0200

    Split sequencers into data generators and data processors

--- paz/core/sequencer.py
+++ paz/core/sequencer.py
@@ -2,39 +2,30 @@ from tensorflow.keras.utils import Sequence
 import numpy as np
 
 
-class Sequencer(Sequence):
-    """Abstract base sequencer class for dispatching batches.
-
+class ProcessingSequencer(Sequence):
+    """Base sequencer class for processing or generating batches.
+    If data is ``None`` the sequencer assumes that ``processor``
+        generates the data. If data is not ``None`` the sequencer
+        assumes the ``processor`` works as data processing pipeline.
     # Arguments
+        processor: Function. If data is not ``None``, ``processor``
+            takes a sample (see data) as input and returns a dictionary
+            with keys ``inputs`` and ``labels`` and values dictionaries
+            with keys being the ``layer names`` in which the values
+            (numpy arrays) will be inputted.
+        batch_size: Int.
         data: List of dictionaries. The length of the list corresponds to the
             amount of samples in the data. Inside each sample there should
             be a dictionary with `keys` indicating the data types/topics
             e.g. ``image``, ``depth``, ``boxes`` and as `values` of these
-            `keys` the corresponding data e.g. numpy arrays.
-        processor: Function that takes a sample (dictionary) as input and
-            returns a list of outputs.
-        tensor_shapes: List of lists. Each element of the list contains a list
-            indicating the shape of the data
-            e.g. ((32, 128, 128, 1), (32, 8732, 21))
-            In the case above the first shape could correspond to an image with
-            the shape indicating (batch_shape, height, width, num_channels),
-            while the second shape indicates the bounding boxes in that image.
-            (batch_size, num_bounding_boxes, box_coordinates + num_classes)
-            The shapes must correspond to the output shapes of the processor.
-        tensor_names: List of strings. Each string is the name of the tensor
-            in which that data topic will be given to.
-            The list must have the same order as the processor output types.
-        batch_size: Integer. Batch size.
+            `keys` the corresponding data e.g. strings, numpy arrays, etc.
     """
-    def __init__(self, data, processor, batch_size=32, tensor_names=None):
-
-        self.data = data
-        self.batch_size = batch_size
+    def __init__(self, processor, batch_size, data):
         self.processor = processor
-        self.tensor_names = tensor_names
-        if self.tensor_names is None:
-            self.tensor_names = data[0].keys()
-        self.shapes = self.processor.output_shapes
+        self.input_topics = self.processor.processors[-1].input_topics
+        self.label_topics = self.processor.processors[-1].label_topics
+        self.batch_size = batch_size
+        self.data = data
 
     def __len__(self):
         return int(np.ceil(len(self.data) / float(self.batch_size)))
@@ -42,12 +33,70 @@ class Sequencer(Sequence):
     def __getitem__(self, batch_index):
         batch_arg_A = self.batch_size * (batch_index)
         batch_arg_B = self.batch_size * (batch_index + 1)
-        batch_samples = self.data[batch_arg_A:batch_arg_B]
-        batches = [np.zeros((self.batch_size, *size)) for size in self.shapes]
-        for sample_arg, unprocessed_sample in enumerate(batch_samples):
+        batch = self.data[batch_arg_A:batch_arg_B]
+        inputs_batch = self.get_empty_batch(
+            self.input_topics, self.processor.input_shapes)
+        labels_batch = self.get_empty_batch(
+            self.label_topics, self.processor.label_shapes)
+        for sample_arg, unprocessed_sample in enumerate(batch):
             sample = self.processor(**unprocessed_sample.copy())
-            for data_arg, data in enumerate(sample):
-                batches[data_arg][sample_arg] = data
-        # return dict(zip(self.tensor_names, batches))
-        # dictionaries are not supported as output
-        return batches[0], batches[1]
+            for topic, data in sample['inputs'].items():
+                inputs_batch[topic][sample_arg] = data
+            for topic, data in sample['labels'].items():
+                labels_batch[topic][sample_arg] = data
+        return inputs_batch, labels_batch
+
+    def get_empty_batch(self, topics, shapes):
+        batch = {}
+        for topic, shape in zip(topics, shapes):
+            batch[topic] = np.zeros((self.batch_size, *shape))
+        return batch
+
+
+class GeneratingSequencer(Sequence):
+    """Base sequencer class for processing or generating batches.
+    If data is ``None`` the sequencer assumes that ``processor``
+        generates the data. If data is not ``None`` the sequencer
+        assumes the ``processor`` works as data processing pipeline.
+    # Arguments
+        processor: Function. If data is not ``None``, ``processor``
+            takes a sample (see data) as input and returns a dictionary
+            with keys ``inputs`` and ``labels`` and values dictionaries
+            with keys being the ``layer names`` in which the values
+            (numpy arrays) will be inputted.
+        batch_size: Int.
+        data: List of dictionaries. The length of the list corresponds to the
+            amount of samples in the data. Inside each sample there should
+            be a dictionary with `keys` indicating the data types/topics
+            e.g. ``image``, ``depth``, ``boxes`` and as `values` of these
+            `keys` the corresponding data e.g. strings, numpy arrays, etc.
+    """
+    def __init__(self, processor, batch_size=32, data=None):
+        self.processor = processor
+        self.input_topics = self.processor.processors[-1].input_topics
+        self.label_topics = self.processor.processors[-1].label_topics
+        self.batch_size = batch_size
+        self.data = data
+
+    def __len__(self):
+        # fix this horrendous hack
+        return 1000
+
+    def __getitem__(self, batch_index):
+        inputs_batch = self.get_empty_batch(
+            self.input_topics, self.processor.input_shapes)
+        labels_batch = self.get_empty_batch(
+            self.label_topics, self.processor.label_shapes)
+        for sample_arg in range(self.batch_size):
+            sample = self.processor()
+            for topic, data in sample['inputs'].items():
+                inputs_batch[topic][sample_arg] = data
+            for topic, data in sample['labels'].items():
+                labels_batch[topic][sample_arg] = data
+        return inputs_batch, labels_batch
+
+    def get_empty_batch(self, topics, shapes):
+        batch = {}
+        for topic, shape in zip(topics, shapes):
+            batch[topic] = np.zeros((self.batch_size, *shape))
+        return batch
