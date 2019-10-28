70555621d55245001a640f69bf9bc708f2453cc8
Octavio Arriaga <octavio.arriaga@dfki.de>
2019-10-28 13:39:58 +0100

    Add multi-functionality to draw whenever there is not class given

--- paz/processors/draw.py
+++ paz/processors/draw.py
@@ -7,20 +7,25 @@ class DrawBoxes2D(Processor):
     # Arguments
         class_names: List of strings.
     """
-    def __init__(self, class_names):
+    def __init__(self, class_names=None, colors=None):
         self.class_names = class_names
-        self.num_classes = len(self.class_names)
-        self.colors = ops.lincolor(self.num_classes)
-        self.class_to_color = dict(zip(self.class_names, self.colors))
+        self.colors = colors
+        if self.colors is None:
+            self.colors = ops.lincolor(len(self.class_names))
+        if class_names is not None:
+            self.num_classes = len(self.class_names)
+            self.class_to_color = dict(zip(self.class_names, self.colors))
+        else:
+            self.class_to_color = {None: self.colors, '': self.colors}
         super(DrawBoxes2D, self).__init__()
 
     def call(self, kwargs):
         image, boxes2D = kwargs['image'], kwargs['boxes2D']
         for box2D in boxes2D:
-            class_name = box2D.class_name
-            text = '{:0.2f}, {}'.format(box2D.score, box2D.class_name)
             x_min, y_min, x_max, y_max = box2D.coordinates
+            class_name = box2D.class_name
             color = self.class_to_color[class_name]
+            text = '{:0.2f}, {}'.format(box2D.score, class_name)
             ops.put_text(image, text, (x_min, y_min - 10), .7, color, 1)
             ops.draw_rectangle(image, (x_min, y_min), (x_max, y_max), color, 2)
         return kwargs
