0b75bc05ee5fed3b72e5e399c8a740eec934fd40
Octavio Arriaga <octavio.arriaga@dfki.de>
2019-10-28 13:40:27 +0100

    Extended toBoxes by including case with no class

--- paz/processors/detection.py
+++ paz/processors/detection.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from ..core import Processor, SequentialProcessor, Box2D, ops
+from ..core import Processor, Box2D, ops
 from .image import BGR_IMAGENET_MEAN
 
 
@@ -100,17 +100,19 @@ class CropBoxes2D(Processor):
         topic: String indicating the new key in the data dictionary that
             will contain the list of cropped images.
     """
-    def __init__(self, topic='cropped_images'):
+    def __init__(self, offset_scales, topic='image_crops'):
+        self.offset_scales = offset_scales
         self.topic = topic
         super(CropBoxes2D, self).__init__()
 
     def call(self, kwargs):
-        cropped_images, boxes2D, image = [], kwargs['boxes2D'], kwargs['image']
-        for box2D in boxes2D:
-            x_min, y_min, x_max, y_max = box2D.coordinates
-            cropped_image = image[y_min:y_max, x_min:x_max]
-            cropped_images.append(cropped_image)
-        kwargs[self.topic] = cropped_images
+        image, image_crops = kwargs['image'], []
+        for box2D in kwargs['boxes2D']:
+            coordinates = box2D.coordinates
+            coordinates = ops.apply_offsets(coordinates, self.offset_scales)
+            x_min, y_min, x_max, y_max = coordinates
+            image_crops.append(image[y_min:y_max, x_min:x_max])
+        kwargs[self.topic] = image_crops
         return kwargs
 
 
@@ -120,8 +122,9 @@ class ToBoxes2D(Processor):
         class_names: List of class names ordered with respect to the
             class indices from the dataset `boxes`.
     """
-    def __init__(self, class_names, one_hot_encoded=False):
-        self.arg_to_class = dict(zip(range(len(class_names)), class_names))
+    def __init__(self, class_names=None, one_hot_encoded=False):
+        if class_names is not None:
+            self.arg_to_class = dict(zip(range(len(class_names)), class_names))
         self.one_hot_encoded = one_hot_encoded
         super(ToBoxes2D, self).__init__()
 
@@ -130,8 +133,10 @@ class ToBoxes2D(Processor):
         for numpy_box2D in numpy_boxes2D:
             if self.one_hot_encoded:
                 class_name = self.arg_to_class[np.argmax(numpy_box2D[4:])]
-            else:
+            elif numpy_box2D.shape[-1] == 5:
                 class_name = self.arg_to_class[numpy_box2D[-1]]
+            elif numpy_box2D.shape[-1] == 4:
+                class_name = None
             boxes2D.append(Box2D(numpy_box2D[:4], 1.0, class_name))
         kwargs['boxes2D'] = boxes2D
         return kwargs
@@ -237,77 +242,26 @@ class PredictBoxes(Processor):
         return kwargs
 
 
-class DetectBoxes2D(Processor):
-    """Detects objects from a single shot model.
-    # Arguments
-        model: Tensorflow SSD model with prior_boxes.
-        class_names: List of strings containing the class names.
-        score_thresh: Float between [0, 1]. If the predictions are less
-            than this score they are ignored.
-        nsm_thresh: Non-maximum supression score.
-    """
-    def __init__(self, model, class_names, score_thresh=.6, nms_thresh=.45,
-                 mean=BGR_IMAGENET_MEAN):
-        self.model = model
-        self.class_names = class_names
-        self.num_classes = len(self.class_names)
-        self.score_thresh = score_thresh
-        self.nms_thresh = nms_thresh
-        self.mean = mean
-        self.arg_to_class = dict(zip(
-            list(range(self.num_classes)), self.class_names))
-        super(DetectBoxes2D, self).__init__()
+class ApplyOffsets(Processor):
+    def __init__(self, offsets):
+        super(ApplyOffsets, self).__init__()
+        self.offsets = offsets
 
     def call(self, kwargs):
-        args = (kwargs['image'], self.model, self.nms_thresh, self.mean)
-        detections = self.detect_from_image(*args)
-        args = (detections, self.arg_to_class, self.score_thresh)
-        boxes2D = self.filter_detections(*args)
-        kwargs['boxes2D'] = boxes2D
+        box2D = kwargs['box2D']
+        box2D.coordinates = ops.apply_offsets(kwargs['box2D'], self.offsets)
+        kwargs['box2D'] = box2D
         return kwargs
 
-    def detect_from_image(self, image, model, nms_thresh, mean):
-        input_shape = model.input_shape[1:3]
-        image_array = ops.resize_image(image, input_shape)
-        image_array = ops.substract_mean(image_array, mean)
-        image_array = np.expand_dims(image_array, 0)
-        predictions = model.predict(image_array)
-        prior_boxes = model.prior_boxes
-        detections = ops.detect(
-            predictions, prior_boxes, nms_thresh=nms_thresh)
-        return detections
 
-    def filter_detections(self, detections, arg_to_class, conf_thresh=0.5):
-        num_classes = detections.shape[0]
-        filtered_detections = []
-        for class_arg in range(1, num_classes):
-            class_detections = detections[class_arg, :]
-            confidence_mask = np.squeeze(class_detections[:, -1] > conf_thresh)
-            confident_class_detections = class_detections[confidence_mask]
-
-            if len(confident_class_detections) == 0:
-                continue
-
-            class_name = arg_to_class[class_arg]
-            for confident_class_detection in confident_class_detections:
-                coordinates = confident_class_detection[:4]
-                score = confident_class_detection[4]
-                detection = Box2D(coordinates, score, class_name)
-                filtered_detections.append(detection)
-        return filtered_detections
-
-
-class IterateOverCroppedImages(Processor):
-    def __init__(self, processes):
-        super(IterateOverCroppedImages, self).__init__()
-        self.process = SequentialProcessor()
-        [self.process.add(process) for process in processes]
+class CropImages(Processor):
+    def __init__(self, offsets, output_topic='image_crops'):
+        self.offsets = offsets
+        self.output_topic = output_topic
+        super(CropImages, self).__init__()
 
     def call(self, kwargs):
-        keypoints = []
-        images, boxes2D = kwargs['cropped_images'], kwargs['boxes2D']
-        for image, box2D in zip(images, boxes2D):
-            inferences = self.process(image=image, box2D=box2D)
-            keypoints.append(inferences['keypoints'])
-        kwargs['keypoints'] = keypoints
+        x_min, y_min, x_max, y_max = kwargs['box2D'].coordinates
+        image_crop = kwargs['image'][y_min:y_max, x_min:x_max]
+        kwargs[self.output_topic] = image_crop
         return kwargs
