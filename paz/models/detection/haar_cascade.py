dfe07fbef2295db97745eaf2ca5a7bce57364659
Octavio Arriaga <octavio.arriaga@dfki.de>
2019-10-28 11:39:16 +0100

    Fix bug with coordinates being outputted differently

--- paz/models/detection/haar_cascade.py
+++ paz/models/detection/haar_cascade.py
@@ -1,24 +1,47 @@
+import numpy as np
+from tensorflow.keras.utils import get_file
+
 from ...core import ops
+
+WEIGHT_PATH = ('https://raw.githubusercontent.com/opencv/opencv/'
+               'master/data/haarcascades/')
 
 
 class HaarCascadeDetector(object):
     """Haar cascade face detector.
     # Arguments
-        path: String. Path to default openCV XML format.
-        scale_factor= Float. Scale for image reduction
-        min_neighbors: Int.
+        path: String. Postfix to default openCV haarcascades XML files, see [1]
+            e.g. `eye`, `frontalface_alt2`, `fullbody`
+        scale = Float. Scale for image reduction
+        neighbors: Int. Minimum neighbors
+
     # Methods
         predict()
+
+    # References
+        [1] https://github.com/opencv/opencv/tree/master/data/haarcascades
     """
-    def __init__(self, path, scale_factor=1.3, min_neighbors=5):
-        self.model = ops.cascade_classifier(path)
-        self.scale_factor = scale_factor
-        self.min_neighbors = min_neighbors
+
+    def __init__(self, weights='frontalface_default', scale=1.3, neighbors=5):
+        self.weights = weights
+        self.name = 'haarcascade_' + weights + '.xml'
+        self.url = WEIGHT_PATH + self.name
+        self.path = get_file(self.name, self.url, cache_subdir='paz/models')
+        self.model = ops.cascade_classifier(self.path)
+        self.scale = scale
+        self.neighbors = neighbors
 
     def predict(self, gray_image):
         """ Detects faces from gray images.
         """
         if len(gray_image.shape) != 2:
             raise ValueError('Invalid gray image shape:', gray_image.shape)
-        args = (gray_image, self.scale_factor, self.min_neighbors)
-        return self.model.detectMultiScale(*args)
+        args = (gray_image, self.scale, self.neighbors)
+        boxes = self.model.detectMultiScale(*args)
+        boxes_point_form = np.zeros_like(boxes)
+        if len(boxes) != 0:
+            boxes_point_form[:, 0] = boxes[:, 0]
+            boxes_point_form[:, 1] = boxes[:, 1]
+            boxes_point_form[:, 2] = boxes[:, 0] + boxes[:, 2]
+            boxes_point_form[:, 3] = boxes[:, 1] + boxes[:, 3]
+        return boxes_point_form
