82be7a547e1f5cb45328abb6e7cb78b1ffefaa52
Octavio Arriaga <octavio.arriaga@dfki.de>
2019-10-28 11:40:00 +0100

    Move lambda function to standard processors

--- paz/core/__init__.py
+++ paz/core/__init__.py
@@ -1,5 +1,5 @@
 from .loader import Loader
 from .sequencer import GeneratingSequencer, ProcessingSequencer
 from .messages import Box2D, Pose6D
-from .processor import Processor, SequentialProcessor, LambdaProcessor
+from .processor import Processor, SequentialProcessor
 from .ops import VideoPlayer
