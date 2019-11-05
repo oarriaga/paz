9e2db17da5cd66e7930f8f3635c6ec7aba8b396d
Octavio Arriaga <octavio.arriaga@dfki.de>
2019-11-05 15:20:58 +0100

    Change ferplus to include a master path to read labels and images

--- paz/datasets/ferplus.py
+++ paz/datasets/ferplus.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 
 from .utils import get_class_names
@@ -11,8 +12,8 @@ from ..core import ops
 class FERPlus(Loader):
     """Class for loading FER2013 [1] emotion classification dataset.
     # Arguments
-        paths: List of strings. Full path to image and labels files e.g.
-            ['/fullpath/fer2013.csv', '/fullpath/fer2013new.csv']
+        path: String. Path to directory that has inside the files:
+            `fer2013.csv` and  `fer2013new.csv`
         split: String. Valid option contain 'train', 'val' or 'test'.
         class_names: String or list: If 'all' then it loads all default
             class names.
@@ -23,15 +24,17 @@ class FERPlus(Loader):
     [1] kaggle.com/c/challenges-in-representation-learning-facial-\
             expression-recognition-challenge
     """
-    def __init__(self, paths, split='train', class_names='all',
+    def __init__(self, path, split='train', class_names='all',
                  image_size=(48, 48)):
 
         if class_names == 'all':
             class_names = get_class_names('FERPlus')
-        super(FERPlus, self).__init__(paths, split, class_names, 'FERPlus')
+
+        super(FERPlus, self).__init__(path, split, class_names, 'FERPlus')
 
         self.image_size = image_size
-        self.images_path, self.labels_path = self.paths
+        self.images_path = os.path.join(self.path, 'fer2013.csv')
+        self.labels_path = os.path.join(self.path, 'fer2013new.csv')
         self.split_to_filter = {
             'train': 'Training', 'val': 'PublicTest', 'test': 'PrivateTest'}
 
