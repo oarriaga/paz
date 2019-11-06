30a6ad68248ed4cc77d0cc196e9d1db9c8f43a0a
Octavio Arriaga <octavio.arriaga@dfki.de>
2019-11-06 16:18:28 +0100

    Add keypointnet model

--- paz/models/__init__.py
+++ paz/models/__init__.py
@@ -2,3 +2,5 @@ from .detection import SSD300
 from .detection import SSD512
 from .detection import HaarCascadeDetector
 from .keypoint.projector import Projector
+from .keypoint.keypointnet import KeypointNet
+from .keypoint.keypointnet import KeypointNetShared
