5c16e72ea85f30cff5820070bc294b347fbdaa8d
Octavio Arriaga <octavio.arriaga@dfki.de>
2019-11-07 14:50:30 +0100

    Add discovery of latent keypoints tested script

--- examples/discovery_of_latent_keypoints/discover_latent_keypoints.py
+++ examples/discovery_of_latent_keypoints/discover_latent_keypoints.py
@@ -7,7 +7,6 @@ import json
 import argparse
 from paz.models import KeypointNetShared
 from paz.models import Projector
-from paz.pipelines import DrawMosaic
 from paz.pipelines import KeypointInference
 from paz.pipelines import KeypointSharedAugmentation
 from paz.core.sequencer import GeneratingSequencer
@@ -26,11 +25,11 @@ description = 'Training script for learning latent 3D keypoints'
 parser = argparse.ArgumentParser(description=description)
 parser.add_argument('-cl', '--class_name', default='035_power_drill', type=str,
                     help='``FERPlus``, ``FER`` or ``IMDB``')
-parser.add_argument('-nk', '--num_keypoints', default=20, type=int,
+parser.add_argument('-nk', '--num_keypoints', default=10, type=int,
                     help='Number of keypoints to be learned')
 parser.add_argument('-f', '--filters', default=64, type=int,
                     help='Number of filters in convolutional blocks')
-parser.add_argument('-bs', '--batch_size', default=3, type=int,
+parser.add_argument('-bs', '--batch_size', default=5, type=int,
                     help='Batch size for training')
 parser.add_argument('-lr', '--learning_rate', default=0.001, type=float,
                     help='Initial learning rate for Adam')
@@ -40,9 +39,9 @@ parser.add_argument('-sp', '--stop_patience', default=7, type=int,
                     help='Number of epochs before doing early stopping')
 parser.add_argument('-pp', '--plateau_patience', default=3, type=int,
                     help='Number of epochs before reducing learning rate')
-parser.add_argument('-e', '--max_num_epochs', default=10000, type=int,
+parser.add_argument('-e', '--max_num_epochs', default=2, type=int,
                     help='Maximum number of epochs before finishing')
-parser.add_argument('-st', '--steps_per_epoch', default=1000, type=int,
+parser.add_argument('-st', '--steps_per_epoch', default=3, type=int,
                     help='Steps per epoch')
 parser.add_argument('-sa', '--save_path', default='trained_models/',
                     type=str, help='Path for writing model weights and logs')
@@ -71,7 +70,7 @@ args = parser.parse_args()
 class_name = '035_power_drill'
 OBJ_filepath = '.keras/altamira/datasets/models/%s/textured.obj' % class_name
 OBJ_filepath = os.path.join(os.path.expanduser('~'), OBJ_filepath)
-save_path = os.path.join(os.path.expanduser('~'), '.keras/altamira/models/')
+save_path = os.path.join(os.path.expanduser('~'), '.keras/paz/models/')
 
 # setting scene
 scene = MultiView(OBJ_filepath, (args.image_size, args.image_size),
@@ -102,8 +101,6 @@ metrics = {'uvz_points-shared': uvz_point_losses,
 # model compilation
 optimizer = Adam(args.learning_rate, amsgrad=True)
 model.compile(optimizer, losses, metrics)
-# metrics_names = ['loss', 'uvz', 'uv', 'cons', 'sep', 'rel_pose', 'sil', 'var']
-# model.metrics_names = metrics_names
 model_name = '_'.join([model.name, str(args.num_keypoints), class_name])
 model.summary()
 
@@ -112,16 +109,19 @@ save_path = os.path.join(save_path, model_name)
 if not os.path.exists(save_path):
     os.makedirs(save_path)
 
-# callbacks
+# setting callbacks
 log = CSVLogger(os.path.join(save_path, '%s.log' % model_name))
 stop = EarlyStopping('loss', patience=args.stop_patience, verbose=1)
 plateau = ReduceLROnPlateau('loss', patience=args.plateau_patience, verbose=1)
 inferencer = KeypointInference(
-    model.get_layer('keypointnet'), args.num_keypoints)
-drawer = DrawMosaic(inferencer, (5, 5))
-draw = DrawInferences(save_path, sequencer, drawer, 'images')
+    model.get_layer('keypointnet'), args.num_keypoints, to_BGR=True)
+images = (sequencer.__getitem__(0)[0][0] * 255).astype('uint8')
+draw = DrawInferences(save_path, images, inferencer)
 model_path = os.path.join(save_path, '%s_weights.hdf5' % model_name)
-save = ModelCheckpoint(model_path, 'loss', verbose=1, save_best_only=True)
+save = ModelCheckpoint(model_path, 'loss', verbose=1,
+                       save_best_only=True, save_weights_only=True)
+
+# saving hyper-parameters and model summary as text files
 with open(os.path.join(save_path, 'hyperparameters.json'), 'w') as filer:
     json.dump(args.__dict__, filer, indent=4)
 with open(os.path.join(save_path, 'model_summary.txt'), 'w') as filer:
@@ -130,6 +130,7 @@ with open(os.path.join(save_path, 'model_summary.txt'), 'w') as filer:
 # model optimization
 model.fit_generator(
     sequencer,
+    steps_per_epoch=args.steps_per_epoch,
     epochs=args.max_num_epochs,
     callbacks=[stop, log, save, plateau, draw],
     verbose=1,
@@ -140,7 +141,7 @@ model.load_weights(model_path)
 model = model.get_layer('keypointnet')
 
 # rendering multiple scenes for forward passing
-keypoints_set, projector = [], Projector(input_shape, focal_length, True)
+keypoints_set, projector = [], Projector(focal_length, True)
 print('Calculating mean and variance of discovered keypoints...')
 num_forward_passes = 1000  # number of samples for calculating the mean
 sequencer.batch_size = 1   # changing batch size of sequencer
