becc548046ec444012eaab665604d1c001e5f38a
Octavio Arriaga <octavio.arriaga@dfki.de>
2019-11-13 11:09:48 +0100

    Change name of keypointnet inference pipelien

--- examples/discovery_of_latent_keypoints/discover_latent_keypoints.py
+++ examples/discovery_of_latent_keypoints/discover_latent_keypoints.py
@@ -8,7 +8,7 @@ import argparse
 
 from paz.models import KeypointNetShared
 from paz.models import Projector
-from paz.pipelines import KeypointInference
+from paz.pipelines import KeypointNetInference
 from paz.pipelines import KeypointSharedAugmentation
 from paz.core.sequencer import GeneratingSequencer
 from paz.optimization import KeypointNetLoss
@@ -119,7 +119,7 @@ if not os.path.exists(save_path):
 log = CSVLogger(os.path.join(save_path, '%s.log' % model_name))
 stop = EarlyStopping('loss', patience=args.stop_patience, verbose=1)
 plateau = ReduceLROnPlateau('loss', patience=args.plateau_patience, verbose=1)
-inferencer = KeypointInference(
+inferencer = KeypointNetInference(
     model.get_layer('keypointnet'), args.num_keypoints)
 images = (sequencer.__getitem__(0)[0][0] * 255).astype('uint8')
 draw = DrawInferences(save_path, images, inferencer)
