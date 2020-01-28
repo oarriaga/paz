b5e0613e493ec9803d3e4affe32ca1f4f05f2ad1
Octavio Arriaga <octavio.arriaga@dfki.de>
2020-01-28 15:14:24 +0100

    Refactor evaluation to use sequencer

--- examples/unsupervised_difficulty_score/evaluation.py
+++ examples/unsupervised_difficulty_score/evaluation.py
@@ -6,18 +6,18 @@ from mnist import MNIST
 
 
 top_k = 10
-x_test, y_test = MNIST('test').load()
-x_test, y_test = x_test, y_test
+data = MNIST('test').load()
 
-metrics = h5py.File('data/metrics.hdf5')
-metrics = np.asarray(metrics['metrics'])[..., 0]
+metrics = h5py.File('evaluations.hdf5', 'r')
+
+metrics = np.asarray(metrics['evaluations'])[..., 0]
 loss = np.sum(metrics, axis=0)
 loss_args = np.argsort(loss)
 
 print('Displaying easiest samples')
 for arg in loss_args[:top_k]:
-    ops.show_image((x_test[arg] * 255).astype('uint8'))
+    ops.show_image((data[arg]['image']).astype('uint8'))
 
 print('Displaying most difficult samples')
 for arg in loss_args[::-1][:top_k]:
-    ops.show_image((x_test[arg] * 255).astype('uint8'))
+    ops.show_image((data[arg]['image']).astype('uint8'))
