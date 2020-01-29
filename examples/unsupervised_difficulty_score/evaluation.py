92d592e91ed90417a669a6329b29f40e06de31e1
Octavio Arriaga <octavio.arriaga@dfki.de>
2020-01-29 11:20:37 +0100

    Add quick mosaic display of action scores

--- examples/unsupervised_difficulty_score/evaluation.py
+++ examples/unsupervised_difficulty_score/evaluation.py
@@ -1,28 +1,58 @@
 import h5py
 import numpy as np
-from paz.core import ops
+import matplotlib.pyplot as plt
 
-from mnist import MNIST
+from datasets import MNIST
 
 
-top_k = 10
+def make_mosaic(images, titles=None, rows=1):
+    assert((titles is None) or (len(images) == len(titles)))
+    num_images = len(images)
+    figure = plt.figure()
+    for image_arg, (image, title) in enumerate(zip(images, titles)):
+        cols = np.ceil(num_images / float(rows))
+        axis = figure.add_subplot(rows, cols, image_arg + 1)
+        if image.ndim == 2:
+            plt.gray()
+        plt.imshow(image)
+        plt.axis('off')
+        if titles is not None:
+            axis.set_title(title)
+    figure.set_size_inches(np.array(figure.get_size_inches()) * num_images)
+    return figure
+
+
+top_k, evaluation_arg = 9, 0
 data = MNIST('test').load()
 
-metrics = h5py.File('evaluations.hdf5', 'r')
+evaluations = h5py.File('evaluations/MNIST.hdf5', 'r')
+evaluations = np.asarray(evaluations['evaluations'])
+evaluations = evaluations[..., :len(data), evaluation_arg]
 
-metrics = np.asarray(metrics['evaluations'])[..., :len(data), 0]
-loss = np.sum(metrics, axis=0)
-loss = np.sort(loss)
-loss_args = np.argsort(loss)
+action_scores = np.sum(evaluations, axis=0)
+sorted_scores = np.argsort(action_scores)
 
 print('Displaying easiest samples')
-for arg in loss_args[:top_k]:
-    print(loss[arg])
-    print(arg)
-    ops.show_image((data[arg]['image']).astype('uint8'))
+images, titles = [], []
+for arg in sorted_scores[:top_k]:
+    print(action_scores[arg], arg)
+    images.append(data[arg]['image'])
+    action_score = round(action_scores[arg], 2)
+    titles.append(str(action_score))
 
-print('Displaying most difficult samples')
-for arg in loss_args[-top_k:]:
-    print(loss[arg])
-    print(arg)
-    ops.show_image((data[arg]['image']).astype('uint8'))
+figure = make_mosaic(images, titles, rows=3)
+figure.suptitle('Easiest examples', fontsize=16)
+plt.show()
+
+print('Displaying easiest samples')
+images, titles = [], []
+for arg in sorted_scores[-top_k:]:
+    print(action_scores[arg], arg)
+    images.append(data[arg]['image'])
+    action_score = round(action_scores[arg], 2)
+    titles.append(str(action_score))
+
+
+figure = make_mosaic(images, titles, rows=3)
+figure.suptitle('Most difficult samples', fontsize=16)
+plt.show()
