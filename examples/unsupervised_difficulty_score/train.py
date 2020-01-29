6d683c38e32ac4d046362b35068496f55c2f4972
Octavio Arriaga <octavio.arriaga@dfki.de>
2020-01-29 11:21:47 +0100

    Refactor training script for general dataset and model instantiation

--- examples/unsupervised_difficulty_score/train.py
+++ examples/unsupervised_difficulty_score/train.py
@@ -1,47 +1,60 @@
+import os
+import argparse
 from tensorflow import keras
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Dropout, Flatten, Input
-from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D
 
 from paz.core import ProcessingSequencer
 
-
+from datasets import MNIST, CIFAR10
+from models import CNN
+from pipelines import ImageAugmentation
 from callbacks import Evaluator
-from mnist import MNIST, ImageAugmentation
 
-size, num_classes, batch_size, epochs, save_path = 28, 10, 32, 10, 'data'
-splits, evaluations_filename = ['train', 'test'], 'evaluations.hdf5'
+splits = ['train', 'test']
+
+description = 'Unsupervised difficulty estimation for classification'
+parser = argparse.ArgumentParser(description=description)
+parser.add_argument('-p', '--save_path', default='evaluations', type=str,
+                    help='Path for saving evaluations')
+parser.add_argument('-m', '--model', default='CNN', type=str,
+                    choices=['CNN'])
+parser.add_argument('-d', '--dataset', default='MNIST', type=str,
+                    choices=['MNIST', 'CIFAR10'])
+parser.add_argument('-b', '--batch_size', default=32, type=int,
+                    help='Batch size used during optimization')
+parser.add_argument('-e', '--epochs', default=20, type=int,
+                    help='Number of epochs before finishing')
+args = parser.parse_args()
+
+name_to_manager = {'MNIST': MNIST, 'CIFAR10': CIFAR10}
+data_managers, datasets = {}, {}
+for split in splits:
+    data_manager = name_to_manager[args.dataset](split)
+    data = data_manager.load()
+    data_managers[split], datasets[split] = data_manager, data
 
 sequencers = {}
 for split in splits:
-    processor = ImageAugmentation(size, num_classes)
-    sequencer = ProcessingSequencer(processor, batch_size, MNIST(split).load())
+    manager = data_managers[split]
+    processor = ImageAugmentation(manager.image_size[0], manager.num_classes)
+    sequencer = ProcessingSequencer(processor, args.batch_size, data)
     sequencers[split] = sequencer
-input_shape = sequencers['train'].processor.input_shapes[0]
 
-
-inputs = Input(input_shape, name='image')
-x = Conv2D(32, (3, 3), activation='relu')(inputs)
-x = Conv2D(64, (3, 3), activation='relu')(x)
-x = MaxPooling2D(pool_size=(2, 2))(x)
-x = Dropout(0.25)(x)
-x = Flatten()(x)
-x = Dense(128, activation='relu')(x)
-x = Dropout(0.5)(x)
-outputs = Dense(num_classes, activation='softmax', name='label')(x)
-model = Model(inputs, outputs, name='CNN')
-
+name_to_model = {'CNN': CNN}
+Model = name_to_model[args.model]
+model = Model(sequencers['train'].processor.input_shapes[0],
+              data_managers['train'].num_classes)
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adam(),
               metrics=['accuracy'])
 
-evaluators = [keras.losses.CategoricalCrossentropy('none')]
+evaluations_filepath = os.path.join(args.save_path, args.dataset + '.hdf5')
+evaluators = [keras.losses.CategoricalCrossentropy(reduction='none')]
 evaluate = Evaluator(sequencers['test'], 'label', evaluators,
-                     epochs, evaluations_filename)
+                     args.epochs, evaluations_filepath)
 
 model.fit_generator(
     sequencers['train'],
-    epochs=epochs,
+    steps_per_epoch=10,
+    epochs=args.epochs,
     callbacks=[evaluate],
-    verbose=1,
-    validation_data=sequencers['test'])
+    verbose=1)
