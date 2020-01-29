2f304acb1cf0271aef33f36c44b2ba182ef4675d
Octavio Arriaga <octavio.arriaga@dfki.de>
2020-01-29 16:01:52 +0100

    Refactor training script for multiple models, datasets and experiments

--- examples/unsupervised_difficulty_score/train.py
+++ examples/unsupervised_difficulty_score/train.py
@@ -3,13 +3,13 @@ import argparse
 from tensorflow import keras
 
 from paz.core import ProcessingSequencer
+from paz.datasets import FERPlus
 
 from datasets import MNIST, CIFAR10
 from models import CNN
 from pipelines import ImageAugmentation
 from callbacks import Evaluator
 
-splits = ['train', 'test']
 
 description = 'Unsupervised difficulty estimation for classification'
 parser = argparse.ArgumentParser(description=description)
@@ -18,24 +18,39 @@ parser.add_argument('-p', '--save_path', default='evaluations', type=str,
 parser.add_argument('-m', '--model', default='CNN', type=str,
                     choices=['CNN'])
 parser.add_argument('-d', '--dataset', default='MNIST', type=str,
-                    choices=['MNIST', 'CIFAR10'])
+                    choices=['MNIST', 'CIFAR10', 'FERPlus'])
 parser.add_argument('-b', '--batch_size', default=32, type=int,
                     help='Batch size used during optimization')
 parser.add_argument('-e', '--epochs', default=20, type=int,
                     help='Number of epochs before finishing')
+parser.add_argument('-l', '--experiment_label', default='00', type=str,
+                    help='Label used to distinguish between different runs')
+parser.add_argument('-s', '--evaluation_splits', nargs='+', type=str,
+                    default=['test'], help='Splits used for evaluation')
+parser.add_argument('-r', '--data_path', type=str,
+                    default=os.path.join(
+                        os.path.expanduser('~'), '.keras/paz/datasets/'),
+                    help='Default root data path')
 args = parser.parse_args()
 
-name_to_manager = {'MNIST': MNIST, 'CIFAR10': CIFAR10}
+splits = ['train'] + args.evaluation_splits
+name_to_manager = {'MNIST': MNIST, 'CIFAR10': CIFAR10, 'FERPlus': FERPlus}
 data_managers, datasets = {}, {}
+
 for split in splits:
-    data_manager = name_to_manager[args.dataset](split)
+    data_path = os.path.join(args.data_path, args.dataset)
+    kwargs = {'path': data_path} if args.dataset in ['FERPlus'] else {}
+    print(kwargs)
+    data_manager = name_to_manager[args.dataset](split=split, **kwargs)
     data = data_manager.load()
     data_managers[split], datasets[split] = data_manager, data
 
 sequencers = {}
 for split in splits:
-    manager = data_managers[split]
-    processor = ImageAugmentation(manager.image_size[0], manager.num_classes)
+    data_manager = data_managers[split]
+    size, num_classes = data_manager.image_size[0], data_manager.num_classes
+    grayscale = True if args.dataset in ['MNIST', 'FERPlus'] else False
+    processor = ImageAugmentation(size, num_classes, grayscale)
     sequencer = ProcessingSequencer(processor, args.batch_size, data)
     sequencers[split] = sequencer
 
@@ -47,14 +62,17 @@ model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adam(),
               metrics=['accuracy'])
 
-evaluations_filepath = os.path.join(args.save_path, args.dataset + '.hdf5')
-evaluators = [keras.losses.CategoricalCrossentropy(reduction='none')]
-evaluate = Evaluator(sequencers['test'], 'label', evaluators,
-                     args.epochs, evaluations_filepath)
+callbacks = []
+for split in args.evaluation_splits:
+    filename = '_'.join([args.dataset, split, args.experiment_label])
+    evaluations_filepath = os.path.join(args.save_path, filename + '.hdf5')
+    evaluators = [keras.losses.CategoricalCrossentropy(reduction='none')]
+    callbacks.append(Evaluator(sequencers[split], 'label',
+                     evaluators, args.epochs, evaluations_filepath))
 
 model.fit_generator(
     sequencers['train'],
     steps_per_epoch=10,
     epochs=args.epochs,
-    callbacks=[evaluate],
+    callbacks=callbacks,
     verbose=1)
