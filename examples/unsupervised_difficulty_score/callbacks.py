5562e1546a011adb0d564b25f3675a46d721947b
Octavio Arriaga <octavio.arriaga@dfki.de>
2020-01-29 11:18:17 +0100

    Fix bug with callback not working when directory was not made before

--- examples/unsupervised_difficulty_score/callbacks.py
+++ examples/unsupervised_difficulty_score/callbacks.py
@@ -5,13 +5,18 @@ from tensorflow.keras.utils import Progbar
 
 
 class Evaluator(Callback):
-    def __init__(self, sequencer, topic, evaluators, epochs, filename):
+    def __init__(self, sequencer, topic, evaluators, epochs, filepath):
         self.sequencer = sequencer
         self.topic = topic
         self.evaluators = evaluators
         self.epochs = epochs
-        self.filename = filename
-        self.write_file = h5py.File(self.filename, 'w')
+        self.filepath = filepath
+
+        directory_name = os.path.dirname(self.filepath)
+        if not os.path.exists(directory_name):
+            os.makedirs(directory_name)
+
+        self.write_file = h5py.File(self.filepath, 'w')
         self.evaluations = self.write_file.create_dataset(
             'evaluations',
             (self.epochs, self.num_samples, self.num_evaluators))
@@ -42,11 +47,8 @@ class Evaluator(Callback):
                 self.evaluations[
                     epoch, batch_arg_A:batch_arg_B, eval_arg] = evaluation
             progress_bar.update(batch_index + 1)
-        print('\n Finish per-sample evaluaiton for epoch', epoch)
         self.evaluations.flush()
 
     def on_train_end(self, logs=None):
-        if not os.path.exists(self.filename):
-            os.makedirs(self.filename)
-        print('\n Saving metric in ', self.filename)
+        print('\n Closing writing file in ', self.filepath)
         self.write_file.close()
