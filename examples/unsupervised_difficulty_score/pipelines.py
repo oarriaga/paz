5c7ea93a9c509747de3977991cb2b48ea2bace1d
Octavio Arriaga <octavio.arriaga@dfki.de>
2020-01-29 16:00:19 +0100

    Add grayscale argument to pipeline to be used with RGB and grayscale values

--- examples/unsupervised_difficulty_score/pipelines.py
+++ examples/unsupervised_difficulty_score/pipelines.py
@@ -3,24 +3,27 @@ from paz import processors as pr
 
 
 class ImageAugmentation(SequentialProcessor):
-    def __init__(self, size, num_classes, split='train'):
+    def __init__(self, size, num_classes, grayscale=True, split='train'):
         super(ImageAugmentation, self).__init__()
         if split not in ['train', 'val', 'test']:
             raise ValueError('Invalid split mode')
 
         self.size = size
         self.num_classes = num_classes
+        self.grayscale = grayscale
         self.split = split
 
         self.add(pr.CastImageToFloat())
         self.add(pr.ResizeImage((self.size, self.size)))
-        self.add(pr.ExpandDims(axis=-1, topic='image'))
+        if self.grayscale:
+            self.add(pr.ExpandDims(axis=-1, topic='image'))
         self.add(pr.NormalizeImage())
         self.add(pr.OutputSelector(['image'], ['label']))
 
     @property
     def input_shapes(self):
-        return [(self.size, self.size, 1)]
+        num_channels = 1 if self.grayscale else 3
+        return [(self.size, self.size, num_channels)]
 
     @property
     def label_shapes(self):
