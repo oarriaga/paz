8627c60f43d56209d775bcee932320b5f8734bd2
Octavio Arriaga <octavio.arriaga@dfki.de>
2020-02-04 19:33:49 +0100

    Clean example directory

--- examples/unsupervised_difficulty_score/datasets/__init__.py
+++
@@ -1,2 +0,0 @@
-from .mnist import MNIST
-from .cifar10 import CIFAR10
--- examples/unsupervised_difficulty_score/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .cnn import CNN_KERAS_A
-from .cnn import CNN_KERAS_B
-from .xception import XCEPTION_MINI
-from .resnet import RESNET_V2
