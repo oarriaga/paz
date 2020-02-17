4779527e116cf34ff6ffd0d47a492fd3cd5278c3
Octavio Arriaga <octavio.arriaga@dfki.de>
2020-02-17 14:07:46 +0100

    Add camera instantiation as well as device id to parameters

--- examples/haar_cascade_detectors/haar_cascade_detectors.py
+++ examples/haar_cascade_detectors/haar_cascade_detectors.py
@@ -6,12 +6,14 @@ from paz.processors import DrawBoxes2D
 from paz.processors import ConvertColor
 from paz.processors import CastImageToInts
 from paz.models import HaarCascadeDetector
-from paz.core import SequentialProcessor, VideoPlayer
+from paz.core import SequentialProcessor, VideoPlayer, Camera
 
 parser = argparse.ArgumentParser(description='Haarcascade detectors example')
 parser.add_argument('-m', '--models', nargs='+', type=str,
                     default=['frontalface_default', 'eye'],
                     help='Model name postfix of openCV xml file')
+parser.add_argument('-d', '--device_id', type=int, default=0,
+                    help='Camera device ID')
 args = parser.parse_args()
 
 detectors = []
@@ -25,4 +27,6 @@ for class_arg, model_name in enumerate(args.models):
 pipeline = SequentialProcessor(detectors)
 pipeline.add(DrawBoxes2D(args.models))
 pipeline.add(CastImageToInts())
-VideoPlayer((1280, 960), pipeline).run()
+camera = Camera(device_id=0)
+player = VideoPlayer((1280, 960), pipeline, camera)
+player.run()
