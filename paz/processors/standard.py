f745da5b724b95dac28255bebe3c8b79e35158c3
Octavio Arriaga <arriaga.camargo@gmail.com>
2020-04-02 14:50:58 +0200

    Refactor to not use dictionary with topics

--- paz/processors/standard.py
+++ paz/processors/standard.py
@@ -4,26 +4,22 @@ from ..core import ops
 import numpy as np
 
 
-class ToOneHotVector(Processor):
+class BoxClassToOneHotVector(Processor):
     """Transform from class index to a one-hot encoded vector.
     # Arguments
         num_classes: Integer. Total number of classes.
         topic: String. Currently valid topics: `boxes`
     """
-    def __init__(self, num_classes, topic='boxes'):
+    def __init__(self, num_classes):
         self.num_classes = num_classes
-        self.topic = topic
-        super(ToOneHotVector, self).__init__()
+        super(BoxClassToOneHotVector, self).__init__()
 
-    def call(self, kwargs):
-        if self.topic == 'boxes':
-            boxes = kwargs[self.topic]
-            class_indices = boxes[:, 4].astype('int')
-            one_hot_vectors = ops.to_one_hot(class_indices, self.num_classes)
-            one_hot_vectors = one_hot_vectors.reshape(-1, self.num_classes)
-            boxes = np.hstack([boxes[:, :4], one_hot_vectors.astype('float')])
-            kwargs[self.topic] = boxes
-        return kwargs
+    def call(self, boxes):
+        class_indices = boxes[:, 4].astype('int')
+        one_hot_vectors = ops.to_one_hot(class_indices, self.num_classes)
+        one_hot_vectors = one_hot_vectors.reshape(-1, self.num_classes)
+        boxes = np.hstack([boxes[:, :4], one_hot_vectors.astype('float')])
+        return boxes
 
 
 class OutputSelector(Processor):
@@ -50,17 +46,6 @@ class OutputSelector(Processor):
         for topic in self.label_topics:
             labels[topic] = kwargs[topic]
         return {'inputs': inputs, 'labels': labels}
-
-
-class PrintTopics(Processor):
-    def __init__(self, topics):
-        self.topics = topics
-        super(PrintTopics, self).__init__()
-
-    def call(self, kwargs):
-        for topic in self.topics:
-            print(topic, kwargs[topic])
-        return kwargs
 
 
 class Predict(Processor):
