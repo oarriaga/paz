d9d82cfd10539dedb05fda88cf439699ad527eee
Octavio Arriaga <arriaga.camargo@gmail.com>
2020-04-02 17:01:32 +0200

    Move tensor ops to numpy ops based on which ops are used after inference

--- paz/core/ops/tensor_ops.py
+++ paz/core/ops/tensor_ops.py
@@ -1,9 +1,4 @@
-from __future__ import division
-
-import numpy as np
 import backend as pz
-
-from ..messages import Box2D
 
 
 def compute_iou(box, boxes):
@@ -57,7 +52,10 @@ def compute_ious(boxes_A, boxes_B):
     # Returns
         Numpy array of shape (num_boxes_A, num_boxes_B)
     """
-    return np.apply_along_axis(compute_iou, 1, boxes_A, boxes_B)
+    IOUs = pz.zeros(len(boxes_A), len(boxes_B))
+    for box_A_arg, box_A in enumerate(boxes_A):
+        IOUs[box_A_arg, :] = compute_iou(box_A, boxes_B)
+    return IOUs
 
 
 def to_point_form(boxes):
@@ -71,10 +69,10 @@ def to_point_form(boxes):
     """
     center_x, center_y = boxes[:, 0], boxes[:, 1]
     width, height = boxes[:, 2], boxes[:, 3]
-    x_min = center_x - (width / 2.)
-    x_max = center_x + (width / 2.)
-    y_min = center_y - (height / 2.)
-    y_max = center_y + (height / 2.)
+    x_min = center_x - (width / 2.0)
+    x_max = center_x + (width / 2.0)
+    y_min = center_y - (height / 2.0)
+    y_max = center_y + (height / 2.0)
     return pz.concatenate([x_min[:, None], y_min[:, None],
                            x_max[:, None], y_max[:, None]], axis=1)
 
@@ -98,25 +96,6 @@ def to_center_form(boxes):
                            width[:, None], height[:, None]], axis=1)
 
 
-def denormalize_box(box, image_shape):
-    """Scales corner box coordinates from normalized values to image dimensions.
-
-    # Arguments
-        box: Numpy array containing corner box coordinates.
-        image_shape: List of integers with (height, width).
-
-    # Returns
-        returns: box corner coordinates in image dimensions
-    """
-    x_min, y_min, x_max, y_max = box[:4]
-    height, width = image_shape
-    x_min = int(x_min * width)
-    y_min = int(y_min * height)
-    x_max = int(x_max * width)
-    y_max = int(y_max * height)
-    return (x_min, y_min, x_max, y_max)
-
-
 def encode(matched, priors, variances):
     """Encode the variances from the priorbox layers into the ground truth boxes
     we have matched (based on jaccard overlap) with the prior boxes.
@@ -131,7 +110,7 @@ def encode(matched, priors, variances):
     """
 
     # dist b/t match center and prior's center
-    g_cxcy = (matched[:, :2] + matched[:, 2:4]) / 2 - priors[:, :2]
+    g_cxcy = (matched[:, :2] + matched[:, 2:4]) / 2.0 - priors[:, :2]
     # encode variance
     g_cxcy /= (variances[0] * priors[:, 2:4])
     # match wh / prior wh
@@ -156,7 +135,7 @@ def decode(predictions, priors, variances):
     boxes = pz.concatenate((
         priors[:, :2] + predictions[:, :2] * variances[0] * priors[:, 2:4],
         priors[:, 2:4] * pz.exp(predictions[:, 2:4] * variances[1])), 1)
-    boxes[:, :2] = boxes[:, :2] - (boxes[:, 2:4] / 2)
+    boxes[:, :2] = boxes[:, :2] - (boxes[:, 2:4] / 2.0)
     boxes[:, 2:4] = boxes[:, 2:4] + boxes[:, :2]
     return pz.concatenate([boxes, predictions[:, 4:]], 1)
     return boxes
@@ -172,7 +151,7 @@ def reversed_argmax(array, axis):
         axis : int, argmax operation along this specified axis
     # Returns: index_array : Numpy array of ints
     """
-    array_flip = np.flip(array, axis=axis)
+    array_flip = pz.flip(array, axis=axis)
     return array.shape[axis] - pz.argmax(array_flip, axis=axis) - 1
 
 
@@ -211,167 +190,6 @@ def match(boxes, prior_boxes, iou_threshold=0.5):
     # setting class value to 0 (background argument)
     matches[best_box_iou_per_prior_box < iou_threshold, 4] = 0
     return matches
-
-
-def make_box_square(box, offset_scale=0.05):
-    """Makes box coordinates square.
-
-    # Arguments
-        box: Numpy array with shape (4) with point corner coordinates.
-        offset_scale: Float, scale of the addition applied box sizes.
-
-    # Returns
-        returns: Numpy array with shape (4).
-    """
-
-    x_min, y_min, x_max, y_max = box[:4]
-    center_x = (x_max + x_min) / 2.
-    center_y = (y_max + y_min) / 2.
-    width = x_max - x_min
-    height = y_max - y_min
-
-    if height >= width:
-        half_box = height / 2.
-        x_min = center_x - half_box
-        x_max = center_x + half_box
-    if width > height:
-        half_box = width / 2.
-        y_min = center_y - half_box
-        y_max = center_y + half_box
-
-    box_side_lenght = (x_max + x_min) / 2.
-    offset = offset_scale * box_side_lenght
-    x_min = x_min - offset
-    x_max = x_max + offset
-    y_min = y_min - offset
-    y_max = y_max + offset
-    return (int(x_min), int(y_min), int(x_max), int(y_max))
-
-
-def filter_detections(detections, arg_to_class, conf_thresh=0.5):
-    """Filters boxes outputted from function ``nms_per_class`` as ``Box2D``
-        messages depending on their confidence threshold.
-    # Arguments
-        detections. Numpy array of shape (num_classes, num_boxes, 5)
-    """
-    num_classes = detections.shape[0]
-    filtered_detections = []
-    for class_arg in range(1, num_classes):
-        class_detections = detections[class_arg, :]
-        confidence_mask = pz.squeeze(class_detections[:, -1] >= conf_thresh)
-        confident_class_detections = class_detections[confidence_mask]
-        if len(confident_class_detections) == 0:
-            continue
-        class_name = arg_to_class[class_arg]
-        for confident_class_detection in confident_class_detections:
-            coordinates = confident_class_detection[:4]
-            score = confident_class_detection[4]
-            detection = Box2D(coordinates, score, class_name)
-            filtered_detections.append(detection)
-    return filtered_detections
-
-
-def apply_non_max_suppression(boxes, scores, iou_thresh=.45, top_k=200):
-    """Apply non maximum suppression.
-
-    # Arguments
-        boxes: Numpy array, box coordinates of shape (num_boxes, 4)
-            where each columns corresponds to x_min, y_min, x_max, y_max
-        scores: Numpy array, of scores given for each box in 'boxes'
-        iou_thresh : float, intersection over union threshold
-            for removing boxes.
-        top_k: int, number of maximum objects per class
-
-    # Returns
-        selected_indices: Numpy array, selected indices of kept boxes.
-        num_selected_boxes: int, number of selected boxes.
-    """
-
-    selected_indices = pz.zeros(shape=len(scores))
-    if boxes is None or len(boxes) == 0:
-        return selected_indices
-    x_min = boxes[:, 0]
-    y_min = boxes[:, 1]
-    x_max = boxes[:, 2]
-    y_max = boxes[:, 3]
-    areas = (x_max - x_min) * (y_max - y_min)
-    remaining_sorted_box_indices = pz.argsort(scores)
-    remaining_sorted_box_indices = remaining_sorted_box_indices[-top_k:]
-
-    num_selected_boxes = 0
-    while len(remaining_sorted_box_indices) > 0:
-        best_score_args = remaining_sorted_box_indices[-1]
-        selected_indices[num_selected_boxes] = best_score_args
-        num_selected_boxes = num_selected_boxes + 1
-        if len(remaining_sorted_box_indices) == 1:
-            break
-
-        remaining_sorted_box_indices = remaining_sorted_box_indices[:-1]
-
-        best_x_min = x_min[best_score_args]
-        best_y_min = y_min[best_score_args]
-        best_x_max = x_max[best_score_args]
-        best_y_max = y_max[best_score_args]
-
-        remaining_x_min = x_min[remaining_sorted_box_indices]
-        remaining_y_min = y_min[remaining_sorted_box_indices]
-        remaining_x_max = x_max[remaining_sorted_box_indices]
-        remaining_y_max = y_max[remaining_sorted_box_indices]
-
-        inner_x_min = pz.maximum(remaining_x_min, best_x_min)
-        inner_y_min = pz.maximum(remaining_y_min, best_y_min)
-        inner_x_max = pz.minimum(remaining_x_max, best_x_max)
-        inner_y_max = pz.minimum(remaining_y_max, best_y_max)
-
-        inner_box_widths = inner_x_max - inner_x_min
-        inner_box_heights = inner_y_max - inner_y_min
-
-        inner_box_widths = pz.maximum(inner_box_widths, 0.0)
-        inner_box_heights = pz.maximum(inner_box_heights, 0.0)
-
-        intersections = inner_box_widths * inner_box_heights
-        remaining_box_areas = areas[remaining_sorted_box_indices]
-        best_area = areas[best_score_args]
-        unions = remaining_box_areas + best_area - intersections
-        intersec_over_union = intersections / unions
-        intersec_over_union_mask = intersec_over_union <= iou_thresh
-        remaining_sorted_box_indices = remaining_sorted_box_indices[
-            intersec_over_union_mask]
-
-    return selected_indices.astype(int), num_selected_boxes
-
-
-def nms_per_class(box_data, nms_thresh=.45, conf_thresh=0.01, top_k=200):
-    """Applies non-maximum-suppression per class.
-    # Arguments
-        box_data: Numpy array of shape (num_prior_boxes, 4 + num_classes)
-        nsm_thresh: Float. Non-maximum suppression threshold.
-        conf_thresh: Float. Filter scores with a lower confidence value before
-            performing non-maximum supression.
-        top_k: Integer. Maximum number of boxes per class outputted by nms.
-
-    Returns
-        Numpy array of shape (num_classes, top_k, 5)
-    """
-    decoded_boxes, class_predictions = box_data[:, :4], box_data[:, 4:]
-    num_classes = class_predictions.shape[1]
-    output = pz.zeros((num_classes, top_k, 5))
-
-    # skip the background class (start counter in 1)
-    for class_arg in range(1, num_classes):
-        conf_mask = class_predictions[:, class_arg] >= conf_thresh
-        scores = class_predictions[:, class_arg][conf_mask]
-        if len(scores) == 0:
-            continue
-        boxes = decoded_boxes[conf_mask]
-        indices, count = apply_non_max_suppression(
-            boxes, scores, nms_thresh, top_k)
-        scores = pz.expand_dims(scores, -1)
-        selected_indices = indices[:count]
-        selections = pz.concatenate(
-            (boxes[selected_indices], scores[selected_indices]), axis=1)
-        output[class_arg, :count, :] = selections
-    return output
 
 
 def to_one_hot(class_indices, num_classes):
@@ -437,22 +255,3 @@ def normalize_keypoints(keypoints, height, width):
         y = (((height - 0.5 - y) - (height / 2.0)) / (height / 2))
         normalized_keypoints[keypoint_arg][:2] = [x, y]
     return normalized_keypoints
-
-
-def apply_offsets(coordinates, offset_scales):
-    """Apply offsets to coordinates
-    #Arguments
-        coordinates: List of floats containing coordinates in point form.
-        offset_scales: List of floats having x and y scales respectively.
-    #Returns
-        coordinates: List of floats containing coordinates in point form.
-    """
-    x_min, y_min, x_max, y_max = coordinates
-    x_offset_scale, y_offset_scale = offset_scales
-    x_offset = (x_max - x_min) * x_offset_scale
-    y_offset = (y_max - y_min) * y_offset_scale
-    x_min = int(x_min - x_offset)
-    y_max = int(y_max + x_offset)
-    y_min = int(y_min - y_offset)
-    x_max = int(x_max + y_offset)
-    return (x_min, y_min, x_max, y_max)
