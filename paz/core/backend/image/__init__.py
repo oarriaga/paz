be55a55c60f785adaecb1a974a043839bf89bc7f
Octavio Arriaga <arriaga.camargo@gmail.com>
2020-04-09 13:44:33 +0200

    Add selected imports from both openCV and tensorflow backends

---
+++ paz/core/backend/image/__init__.py
@@ -0,0 +1,22 @@
+from .tensorflow_image import RGB2BGR
+from .tensorflow_image import BGR2RGB
+from .tensorflow_image import RGB2HSV
+from .tensorflow_image import HSV2RGB
+from .tensorflow_image import RGB2GRAY
+
+from .tensorflow_image import cast_image
+from .tensorflow_image import load_image
+from .tensorflow_image import resize
+from .tensorflow_image import random_saturation
+from .tensorflow_image import random_brightness
+from .tensorflow_image import random_contrast
+from .tensorflow_image import random_hue
+from .tensorflow_image import random_image_quality
+from .tensorflow_image import convert_color_space
+from .tensorflow_image import random_plain_background
+from .tensorflow_image import random_crop
+from .tensorflow_image import random_cropped_background
+
+from .opencv_image import show_image
+
+from .draw import draw_random_polygon
