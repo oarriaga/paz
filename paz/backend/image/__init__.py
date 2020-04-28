ba1ef8e5208148a5c20bdb9d80cdf316c422fb5a
Octavio Arriaga <octavio.arriaga@dfki.de>
2020-04-28 14:53:27 +0200

    Fall back to openCV functionality

--- paz/backend/image/__init__.py
+++ paz/backend/image/__init__.py
@@ -1,22 +1,24 @@
-from .tensorflow_image import RGB2BGR
-from .tensorflow_image import BGR2RGB
-from .tensorflow_image import RGB2HSV
-from .tensorflow_image import HSV2RGB
-from .tensorflow_image import RGB2GRAY
+from .opencv_image import RGB2BGR
+from .opencv_image import BGR2RGB
+from .opencv_image import RGB2HSV
+from .opencv_image import HSV2RGB
+from .opencv_image import RGB2GRAY
 
-from .tensorflow_image import cast_image
-from .tensorflow_image import load_image
-from .tensorflow_image import resize
-from .tensorflow_image import random_saturation
-from .tensorflow_image import random_brightness
-from .tensorflow_image import random_contrast
-from .tensorflow_image import random_hue
-from .tensorflow_image import random_image_quality
-from .tensorflow_image import convert_color_space
-from .tensorflow_image import random_plain_background
-from .tensorflow_image import random_crop
-from .tensorflow_image import random_cropped_background
-from .tensorflow_image import random_flip_left_right
+from .opencv_image import cast_image
+from .opencv_image import load_image
+from .opencv_image import resize
+from .opencv_image import convert_color_space
+from .opencv_image import random_saturation
+from .opencv_image import random_brightness
+from .opencv_image import random_contrast
+from .opencv_image import random_hue
+from .opencv_image import random_image_quality
+from .opencv_image import random_plain_background
+from .opencv_image import random_flip_left_right
+
+# TODO implement the following
+# from .opencv_image import random_crop
+# from .opencv_image import random_cropped_background
 
 from .opencv_image import show_image
 from .opencv_image import save_image
