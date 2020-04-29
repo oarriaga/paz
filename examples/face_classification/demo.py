ae554bd4b0a0beb04bc5bae9e32bfab3795fe515
Octavio Arriaga <octavio.arriaga@dfki.de>
2020-04-29 12:17:25 +0200

    Refactor demo to work with new API

--- examples/face_classification/demo.py
+++ examples/face_classification/demo.py
@@ -1,51 +1,47 @@
 from tensorflow.keras.models import load_model
 from paz.models import HaarCascadeDetector
-from paz.core import Processor
-from paz import processors as pr
 from paz.datasets import get_class_names
-from paz.core import VideoPlayer
-from paz.core import Camera
+from paz.backend.camera import VideoPlayer
+from paz.backend.camera import Camera
+from paz.abstract import Processor, SequentialProcessor
+import paz.processors as pr
 import numpy as np
 
 
 class FaceClassifier(Processor):
-    def __init__(self, detector, classifier, offsets, class_names):
+    def __init__(self, detector, classifier, labels, offsets):
         super(FaceClassifier, self).__init__()
-        processors = [pr.ConvertColor('BGR', 'GRAY')]
-        self.detect = pr.Predict(detector, 'image', 'boxes', processors)
-        self.to_boxes = pr.ToBoxes2D()
-        self.crop_images = pr.CropBoxes2D(offsets)
-        self.to_gray = pr.ConvertColor('BGR', 'GRAY')
-        self.normalize = pr.NormalizeImage()
-        self.resize = pr.Resize(classifier.input_shape[1:3])
-        self.classify = pr.Predict(classifier, 'image')
-        self.to_label = pr.ToLabel(class_names)
-        self.draw_boxes = pr.DrawBoxes2D(class_names)
-        self.to_int8 = pr.CastImageToInts()
+        RGB2GRAY = pr.ConvertColorSpace(pr.RGB2GRAY)
+        self.detect = pr.Predict(detector, RGB2GRAY, pr.ToBoxes2D())
+        preprocess = SequentialProcessor([
+            pr.ConvertColorSpace(pr.RGB2GRAY),
+            pr.NormalizeImage(),
+            pr.ResizeImage(classifier.input_shape[1:3]),
+            pr.ExpandDims(0),
+            pr.ExpandDims(3)])
+        postprocessor = pr.ToClassName(labels)
+        self.classify = pr.Predict(classifier, preprocess, postprocessor)
+        self.crop_boxes2D = pr.CropBoxes2D(offsets)
+        self.draw_boxes2D = pr.DrawBoxes2D(labels)
+        self.to_uint8 = pr.CastImage(np.uint8)
 
-    def call(self, kwargs):
-        x = self.detect(kwargs)
-        x = self.to_boxes(x)
-        x = self.crop_images(x)
-        for image, box2D in zip(x['image_crops'], x['boxes2D']):
-            i = self.to_gray({'image': image})
-            i = self.normalize(i)
-            i = self.resize(i)
-            i['image'] = np.expand_dims(np.expand_dims(i['image'], 0), 3)
-            i = self.classify(i)
-            i = self.to_label(i)
-            box2D.class_name = i['predictions']
-        x = self.draw_boxes(x)
-        x = self.to_int8(x)
-        return x
+    def call(self, image):
+        boxes2D = self.detect(image)
+        images = self.crop_boxes2D(image, boxes2D)
+        for cropped_image, box2D in zip(images, boxes2D):
+            box2D.class_name = self.classify(cropped_image)
+        image = self.draw_boxes2D(image, boxes2D)
+        image = self.to_uint8(image)
+        return image
 
 
-detector = HaarCascadeDetector('frontalface_default')
-class_names = get_class_names('FER')
-classifier = load_model('fer2013_mini_XCEPTION.119-0.65.hdf5')
-offsets = (0, 0)
+if __name__ == "__main__":
+    detector = HaarCascadeDetector('frontalface_default')
+    labels = get_class_names('FER')
+    classifier = load_model('fer2013_mini_XCEPTION.119-0.65.hdf5')
+    offsets = (0, 0)
 
-pipeline = FaceClassifier(detector, classifier, offsets, class_names)
-camera = Camera(device_id=0)
-video_player = VideoPlayer((1280, 960), pipeline, camera)
-video_player.run()
+    pipeline = FaceClassifier(detector, classifier, labels, offsets)
+    camera = Camera(device_id=0)
+    video_player = VideoPlayer((1280, 960), pipeline, camera)
+    video_player.run()
