9fb18f70b16d038d2561c70c3e8e7a519302ba1b
Octavio Arriaga <octavio.arriaga@dfki.de>
2020-04-29 12:17:01 +0200

    Include image conversion flags in module

--- paz/processors/__init__.py
+++ paz/processors/__init__.py
@@ -5,3 +5,8 @@ from .geometric import *
 from .standard import *
 from .keypoints import *
 from .pose import *
+from ..backend.image.opencv_image import RGB2BGR
+from ..backend.image.opencv_image import BGR2RGB
+from ..backend.image.opencv_image import RGB2GRAY
+from ..backend.image.opencv_image import RGB2HSV
+from ..backend.image.opencv_image import HSV2RGB
