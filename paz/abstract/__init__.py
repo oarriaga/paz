28e5110a5afac9850b27cbf3f514691b0cb098b6
Octavio Arriaga <octavio.arriaga@dfki.de>
2020-05-06 10:51:54 +0200

    Change name of Sequence batch dispatchers for keras-like compatibility

--- paz/abstract/__init__.py
+++ paz/abstract/__init__.py
@@ -1,4 +1,4 @@
 from .loader import Loader
-from .sequencer import GeneratingSequencer, ProcessingSequencer
+from .sequencer import GeneratingSequence, ProcessingSequence
 from .messages import Box2D, Pose6D
 from .processor import Processor, SequentialProcessor
