b972d37ea06b718aa0adea36304057584e64d79f
Octavio Arriaga <octavio.arriaga@dfki.de>
2020-05-07 16:29:33 +0200

    Refactor Sequence into modular functions

--- paz/abstract/sequencer.py
+++ paz/abstract/sequencer.py
@@ -1,108 +1,82 @@
 from tensorflow.keras.utils import Sequence
 import numpy as np
+# from ..processors.standard import OutputWrapper
+from .processor import SequentialProcessor
 
 
-class ProcessingSequencer(Sequence):
-    """Base sequencer class for processing or generating batches.
-    If data is ``None`` the sequencer assumes that ``processor``
-        generates the data. If data is not ``None`` the sequencer
-        assumes the ``processor`` works as data processing pipeline.
-    # Arguments
-        processor: Function. If data is not ``None``, ``processor``
-            takes a sample (see data) as input and returns a dictionary
-            with keys ``inputs`` and ``labels`` and values dictionaries
-            with keys being the ``layer names`` in which the values
-            (numpy arrays) will be inputted.
-        batch_size: Int.
-        data: List of dictionaries. The length of the list corresponds to the
-            amount of samples in the data. Inside each sample there should
-            be a dictionary with `keys` indicating the data types/topics
-            e.g. ``image``, ``depth``, ``boxes`` and as `values` of these
-            `keys` the corresponding data e.g. strings, numpy arrays, etc.
-    """
-    def __init__(self, processor, batch_size, data):
-        self.processor = processor
-        self.input_topics = self.processor.processors[-1].input_topics
-        self.label_topics = self.processor.processors[-1].label_topics
+class SequenceExtra(Sequence):
+    def __init__(self, pipeline, batch_size, as_list=False):
+        if not isinstance(pipeline, SequentialProcessor):
+            raise ValueError('``processor`` must be a ``SequentialProcessor``')
+        output_wrapper = pipeline.processors[-1]
+        self.pipeline = pipeline
+        self.inputs_name_to_shape = output_wrapper.inputs_name_to_shape
+        self.labels_name_to_shape = output_wrapper.labels_name_to_shape
         self.batch_size = batch_size
+        self.as_list = as_list
+
+    def make_empty_batches(self, name_to_shape):
+        batch = {}
+        for name, shape in name_to_shape.items():
+            batch[name] = np.zeros((self.batch_size, *shape))
+        return batch
+
+    def _to_list(self, batch, names):
+        return [batch[name] for name in names]
+
+    def _place_sample(self, sample, sample_arg, batch):
+        for name, data in sample.items():
+            batch[name][sample_arg] = data
+
+    def _get_unprocessed_batch(self, data, batch_index):
+        batch_arg_A = self.batch_size * (batch_index)
+        batch_arg_B = self.batch_size * (batch_index + 1)
+        unprocessed_batch = data[batch_arg_A:batch_arg_B]
+        return unprocessed_batch
+
+    def __getitem__(self, batch_index):
+        inputs = self.make_empty_batches(self.inputs_name_to_shape)
+        labels = self.make_empty_batches(self.labels_name_to_shape)
+        inputs, labels = self.process_batch(inputs, labels, batch_index)
+        if self.as_list:
+            inputs = self._to_list(inputs, self.input_names)
+            labels = self._to_list(labels, self.label_names)
+        return inputs, labels
+
+    def process_batch(self, inputs, labels, batch_index=None):
+        raise NotImplementedError
+
+
+class ProcessingSequence(SequenceExtra):
+    def __init__(self, processor, batch_size, data, as_list=False):
         self.data = data
+        super(ProcessingSequence, self).__init__(
+            processor, batch_size, as_list)
 
     def __len__(self):
         return int(np.ceil(len(self.data) / float(self.batch_size)))
 
-    def __getitem__(self, batch_index):
-        batch_arg_A = self.batch_size * (batch_index)
-        batch_arg_B = self.batch_size * (batch_index + 1)
-        batch = self.data[batch_arg_A:batch_arg_B]
-        inputs_batch = self.get_empty_batch(
-            self.input_topics, self.processor.input_shapes)
-        labels_batch = self.get_empty_batch(
-            self.label_topics, self.processor.label_shapes)
-        for sample_arg, unprocessed_sample in enumerate(batch):
-            sample = self.processor(unprocessed_sample.copy())
-            for topic, data in sample['inputs'].items():
-                inputs_batch[topic][sample_arg] = data
-            for topic, data in sample['labels'].items():
-                labels_batch[topic][sample_arg] = data
-        return inputs_batch, labels_batch
-
-    def get_empty_batch(self, topics, shapes):
-        batch = {}
-        for topic, shape in zip(topics, shapes):
-            batch[topic] = np.zeros((self.batch_size, *shape))
-        return batch
+    def process_batch(self, inputs, labels, batch_index):
+        unprocessed_batch = self._get_unprocessed_batch(self.data, batch_index)
+        for sample_arg, unprocessed_sample in enumerate(unprocessed_batch):
+            sample = self.pipeline(unprocessed_sample.copy())
+            self._place_sample(sample['inputs'], sample_arg, inputs)
+            self._place_sample(sample['labels'], sample_arg, labels)
+        return inputs, labels
 
 
-class GeneratingSequencer(Sequence):
-    """Base sequencer class for processing or generating batches.
-    If data is ``None`` the sequencer assumes that ``processor``
-        generates the data. If data is not ``None`` the sequencer
-        assumes the ``processor`` works as data processing pipeline.
-    # Arguments
-        processor: Function. If data is not ``None``, ``processor``
-            takes a sample (see data) as input and returns a dictionary
-            with keys ``inputs`` and ``labels`` and values dictionaries
-            with keys being the ``layer names`` in which the values
-            (numpy arrays) will be inputted.
-        batch_size: Int.
-        data: List of dictionaries. The length of the list corresponds to the
-            amount of samples in the data. Inside each sample there should
-            be a dictionary with `keys` indicating the data types/topics
-            e.g. ``image``, ``depth``, ``boxes`` and as `values` of these
-            `keys` the corresponding data e.g. strings, numpy arrays, etc.
-    """
-    def __init__(self, processor, batch_size=32, as_list=False, num_steps=100):
-        self.processor = processor
-        self.input_topics = self.processor.processors[-1].input_topics
-        self.label_topics = self.processor.processors[-1].label_topics
-        self.batch_size = batch_size
-        self.as_list = as_list
+class GeneratingSequence(SequenceExtra):
+    def __init__(self, processor, batch_size, num_steps, as_list=False):
         self.num_steps = num_steps
+        super(GeneratingSequence, self).__init__(
+            processor, batch_size, as_list)
 
     def __len__(self):
         return self.num_steps
 
-    def __getitem__(self, batch_index):
-        inputs_batch = self.get_empty_batch(
-            self.input_topics, self.processor.input_shapes)
-        labels_batch = self.get_empty_batch(
-            self.label_topics, self.processor.label_shapes)
+    def _process_batch(self, inputs, labels, batch_index):
         for sample_arg in range(self.batch_size):
-            sample = self.processor({'image': None})
-            for topic, data in sample['inputs'].items():
-                inputs_batch[topic][sample_arg] = data
-            for topic, data in sample['labels'].items():
-                labels_batch[topic][sample_arg] = data
-        if self.as_list:
-            inputs_batch = self.to_list(inputs_batch, self.input_topics)
-            labels_batch = self.to_list(labels_batch, self.label_topics)
-        return inputs_batch, labels_batch
-
-    def get_empty_batch(self, topics, shapes):
-        batch = {}
-        for topic, shape in zip(topics, shapes):
-            batch[topic] = np.zeros((self.batch_size, *shape))
-        return batch
-
-    def to_list(self, batch, topics):
-        return [batch[topic] for topic in topics]
+            sample = self.pipeline()
+            self._place_sample(sample['inputs'], sample_arg, inputs)
+            self._place_sample(sample['labels'], sample_arg, labels)
+        return inputs, labels
