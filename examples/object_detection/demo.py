83c8f28e050b00d96247a061249cfe12dff014b2
Octavio Arriaga <octavio.arriaga@dfki.de>
2020-05-11 14:40:36 +0200

    Add multiple arguments for shell parsing

--- examples/object_detection/demo.py
+++ examples/object_detection/demo.py
@@ -1,12 +1,25 @@
+import argparse
+
 from paz.backend.camera import VideoPlayer, Camera
 from paz.models import SSD300
 from paz.datasets import get_class_names
 from paz.pipelines import SingleShotInference
 
 
-score_thresh, nms_thresh, labels = .6, .45, get_class_names('VOC')
-model = SSD300(len(labels))
-detector = SingleShotInference(model, labels, score_thresh, nms_thresh)
-camera = Camera(device_id=0)
-video_player = VideoPlayer((1280, 960), detector, camera)
-video_player.run()
+parser = argparse.ArgumentParser(description='MultiHaarCascadeDetectors')
+parser.add_argument('-c', '--camera_id', type=int, default=0,
+                    help='Camera device ID')
+parser.add_argument('-s', '--score_thresh', type=float, default=0.6,
+                    help='Box/class score threshold')
+parser.add_argument('-n', '--nms_thresh', type=float, default=0.45,
+                    help='non-maximum suppression threshold')
+parser.add_argument('-d', '--dataset', type=str, default='VOC',
+                    help='Dataset name')
+args = parser.parse_args()
+
+names = get_class_names(args.dataset)
+model = SSD300(len(names))
+detect = SingleShotInference(model, names, args.score_thresh, args.nms_thresh)
+camera = Camera(args.camera_id)
+player = VideoPlayer((1280, 960), detect, camera)
+player.run()
