57fd75e088cecf1fdba0a6b081829d90a85799f5
Octavio Arriaga <octavio.arriaga@dfki.de>
2020-05-11 14:35:15 +0200

    Refactor demo to include multiple args

--- examples/face_classification/demo.py
+++ examples/face_classification/demo.py
@@ -1,47 +1,26 @@
+import argparse
 from tensorflow.keras.models import load_model
 from paz.models import HaarCascadeDetector
 from paz.datasets import get_class_names
 from paz.backend.camera import VideoPlayer
 from paz.backend.camera import Camera
-from paz.abstract import Processor, SequentialProcessor
-import paz.processors as pr
-import numpy as np
-
-
-class FaceClassifier(Processor):
-    def __init__(self, detector, classifier, labels, offsets):
-        super(FaceClassifier, self).__init__()
-        RGB2GRAY = pr.ConvertColorSpace(pr.RGB2GRAY)
-        self.detect = pr.Predict(detector, RGB2GRAY, pr.ToBoxes2D())
-        preprocess = SequentialProcessor([
-            pr.ConvertColorSpace(pr.RGB2GRAY),
-            pr.NormalizeImage(),
-            pr.ResizeImage(classifier.input_shape[1:3]),
-            pr.ExpandDims(0),
-            pr.ExpandDims(3)])
-        postprocessor = pr.ToClassName(labels)
-        self.classify = pr.Predict(classifier, preprocess, postprocessor)
-        self.crop_boxes2D = pr.CropBoxes2D(offsets)
-        self.draw_boxes2D = pr.DrawBoxes2D(labels)
-        self.to_uint8 = pr.CastImage(np.uint8)
-
-    def call(self, image):
-        boxes2D = self.detect(image)
-        images = self.crop_boxes2D(image, boxes2D)
-        for cropped_image, box2D in zip(images, boxes2D):
-            box2D.class_name = self.classify(cropped_image)
-        image = self.draw_boxes2D(image, boxes2D)
-        image = self.to_uint8(image)
-        return image
+from pipelines import FaceClassifier
 
 
 if __name__ == "__main__":
-    detector = HaarCascadeDetector('frontalface_default')
+    parser = argparse.ArgumentParser(description='Real-time face classifier')
+    parser.add_argument('-c', '--camera_id', type=int, default=0,
+                        help='Camera device ID')
+    parser.add_argument('-d', '--detector_name', type=str,
+                        default='frontalface_default')
+    args = parser.parse_args()
+
+    detector = HaarCascadeDetector(args.detector_name)
     labels = get_class_names('FER')
     classifier = load_model('fer2013_mini_XCEPTION.119-0.65.hdf5')
     offsets = (0, 0)
 
     pipeline = FaceClassifier(detector, classifier, labels, offsets)
-    camera = Camera(device_id=0)
-    video_player = VideoPlayer((1280, 960), pipeline, camera)
-    video_player.run()
+    camera = Camera(args.camera_id)
+    player = VideoPlayer((1280, 960), pipeline, camera)
+    player.run()
