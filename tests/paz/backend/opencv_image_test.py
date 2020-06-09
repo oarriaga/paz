d9c325ac24a9d286d1487f792c7e96f9a7644c77
SushmaDG <sushma.devaramani@gmail.com>
2020-06-09 11:33:28 +0200

    Add test for alpha_blend

--- tests/paz/backend/opencv_image_test.py
+++ tests/paz/backend/opencv_image_test.py
@@ -16,11 +16,6 @@ def test_convert_color_space():
     rgb_to_bgr = test_image[..., ::-1]
     assert np.all(converted_colorspace == rgb_to_bgr)
 
-def test_load_image():
-    load_image = opencv_image.load_image(file_path)
-    #convert the image to BGR
-    assert np.all(load_image == test_image[..., ::-1])
-
 def test_random_saturation():
     saturated_img = opencv_image.random_saturation(test_image)
     # cv2.imwrite('saturated.png', saturated_img)
@@ -54,8 +49,8 @@ def test_split_alpha_channel():
     assert np.all(split_alpha_img == test_image)
 
 def test_alpha_blend():
-    alpha = np.ones((150, 150), dtype=b_channel.dtype) * 50
-    alpha_blend_image = opencv_image.alpha_blend(foreground, background, alpha)
+    alpha_blend_image = opencv_image.alpha_blend(foreground, background, alpha_mask)
+    assert np.all(alpha_blend_image == test_image)
 
 def test_random_plain_background():
     random_background_img = opencv_image.random_plain_background(masked_image)
@@ -63,41 +58,48 @@ def test_random_plain_background():
 def test_show_image():
     img_ = opencv_image.show_image(test_image)
 
+def test_warp_affine():
+    rows, cols = test_image.shape[:2]
+    warp_image = opencv_image.warp_affine(test_image, affine_matrix)
+    test_warp = warp_affine(test_image, affine_matrix, cols, rows)
+    assert np.all(warp_image == test_warp)
+
+def warp_affine(image, matrix, width, height):
+    dst = np.zeros((width, height, 3), dtype=np.float32)
+    oldh, oldw = image.shape[:2]
+    for u in range(width):
+        for v in range(height):
+            x = u * matrix[0, 0] + v * matrix[0, 1] + matrix[0, 2]
+            y = u * matrix[1, 0] + v * matrix[1, 1] + matrix[1, 2]
+            intx, inty = int(x), int(y)
+            if 0 < x < oldw and 0 < y < oldh:
+                dst[u, v] = image[intx, inty]
+    return dst
+
 def get_image(shape, r_channel, g_channel, b_channel):
     image = np.ones(shape)
     image[:, :, 0] = r_channel
     image[:, :, 1] = g_channel
     image[:, :, 2] = b_channel
-    image = image.astype(np.uint8)
     return image
 
-def extract_foreground(image):
-    mask = np.zeros(image.shape[:2], np.uint8)
-    backgroundModel = np.zeros((1, 65), np.float64) 
-    foregroundModel = np.zeros((1, 65), np.float64)
-    rectangle = (50,50,450,290)
-    cv2.grabCut(image_, mask, rectangle,   
-            backgroundModel, foregroundModel, 
-            3, cv2.GC_INIT_WITH_RECT) 
-    mask2 = np.where((mask == 2)|(mask == 0), 0, 1).astype('uint8')
-    extracted_image = image * mask2[:, :, np.newaxis]
-    alpha_blend_image = opencv_image.alpha_blend(extracted_image, backgroundModel, mask2[:, :, np.newaxis])
+image_shape = (128, 128, 3)
+test_image = get_image(image_shape, 50, 120, 201)
+test_image[10:50, 50:120] = 100
+test_image = test_image.astype(np.uint8)
 
+alpha_mask = get_image(image_shape, 0, 0, 0)
+alpha_mask[10:50, 50:120] = 255
+alpha_mask = alpha_mask.astype(float)/255.
 
-test_image = get_image((128, 128, 3), 50, 120, 201)
-test_image[10:25, 30:50] = 1
+background = get_image(image_shape, 50, 120, 201).astype(float)
 
-foreground = get_image((50, 50, 3), 210, 120, 30)
-background = get_image((150, 150, 3), 200, 30, 30)
-
-cv2.imwrite('foreground.png', foreground)
-cv2.imwrite('backgroun.png', background)
-
-cv2.imwrite('image.png', test_image)
-file_path = 'image.png'
+foreground = get_image(image_shape, 0, 0, 0)
+foreground[10:50, 50:120] = 100
+foreground = foreground.astype(float)
 
 b_channel, g_channel, r_channel = cv2.split(test_image)
-alpha_channel = np.ones(b_channel.shape, dtype=b_channel.dtype) * 50 
+alpha_channel = np.ones(b_channel.shape, dtype=b_channel.dtype) * 50
 masked_image = cv2.merge((b_channel, g_channel, r_channel, alpha_channel))
 
 flipped_image = test_image[:, ::-1]
@@ -107,11 +109,12 @@ width = int(test_image.shape[1] * scale_percent / 100)
 height = int(test_image.shape[0] * scale_percent / 100)
 size = width * height * 3
 
-# extract_foreground(test_image)
+affine_matrix = np.array([[2., 1., 0.],
+                          [0., 3., 4.]])
+
 test_cast_image()
 test_resize_image()
 test_convert_color_space()
-test_load_image()
 # test_random_saturation()
 test_flip_left_right()
 test_gaussian_blur_output_shape()
@@ -120,3 +123,4 @@ test_split_alpha_channel()
 test_random_plain_background()
 # test_show_image()
 test_alpha_blend()
+test_warp_affine()
