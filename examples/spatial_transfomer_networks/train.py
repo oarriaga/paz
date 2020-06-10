0c29a7e6d4615517d028a054c67d2b9e3823f2f6
Octavio Arriaga <octavio.arriaga@dfki.de>
2020-06-10 11:13:54 +0200

    Add training script for STNs

---
+++ examples/spatial_transfomer_networks/train.py
@@ -0,0 +1,32 @@
+from cluttered_mnist import ClutteredMNIST
+from STN import STN
+from paz.backend.image import save_image
+
+
+dataset_path = "mnist_cluttered_60x60_6distortions.npz"
+batch_size = 256
+num_epochs = 30
+save_path = ''
+
+data_manager = ClutteredMNIST(dataset_path)
+train_data, val_data, test_data = data_manager.load()
+x_train, y_train = train_data
+
+
+model = STN()
+model.compile(loss={'label': 'categorical_crossentropy'}, optimizer='adam')
+model.summary()
+
+
+def plot_predictions(samples):
+    (lables, interpolations) = model.predict(samples)
+    for arg, images in enumerate(zip(interpolations, samples)):
+        interpolated, image = images
+        interpolated = (interpolated * 255).astype('uint8')
+        image = (image * 255).astype('uint8')
+        save_image('images/interpolated_image_%03d.png' % arg, interpolated)
+        save_image('images/original_image_%03d.png' % arg, image)
+
+
+plot_predictions(test_data[0][:9])
+model.fit(x_train, y_train, batch_size, num_epochs, validation_data=val_data)
