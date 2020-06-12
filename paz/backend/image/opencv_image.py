e1279a60fed88f0413b70e0a828054f7ea05bf22
Octavio Arriaga <octavio.arriaga@dfki.de>
2020-06-12 13:08:14 +0200

    Refactor image with show_image and added random occlusion core functions

--- paz/backend/image/opencv_image.py
+++ paz/backend/image/opencv_image.py
@@ -69,32 +69,6 @@ def random_hue(image, delta=18):
     return image
 
 
-def split_alpha_channel(image):
-    if image.shape[-1] != 4:
-        raise ValueError('Provided image does not contain alpha mask.')
-    image, alpha_channel = np.split(image, [3], -1)
-    alpha_channel = alpha_channel / 255.0
-    return image, alpha_channel
-
-
-def alpha_blend(foreground, background, alpha_channel):
-    return (alpha_channel * foreground) + ((1.0 - alpha_channel) * background)
-
-
-def random_plain_background(image):
-    """Adds random plain background to image using a normalized alpha channel
-    # Arguments
-        image: Float array-like with shape (H, W, 4).
-        alpha_channel: Float array-like. Normalized alpha channel for blending.
-    """
-    image, alpha_channel = split_alpha_channel(image)
-    random_color = np.random.uniform(0, 255, [3])
-    random_color = np.reshape(random_color, [1, 1, 3])
-    H, W = image.shape[:2]
-    background = np.tile(random_color, [H, W, 1])
-    return alpha_blend(image, background, alpha_channel)
-
-
 def flip_left_right(image):
     return image[:, ::-1]
 
@@ -105,35 +79,14 @@ def random_flip_left_right(image):
     return image
 
 
-def gaussian_blur(image, kernel_size=(5, 5)):
-    return cv2.GaussianBlur(image, kernel_size, 0)
-
-
-def median_blur(image, apperture=5):
-    return cv2.medianBlur(image, apperture)
-
-
-def random_image_quality(image):
-    blur = np.random.choice([gaussian_blur, median_blur])
-    return blur(image)
-
-
-def random_crop(image, size):
-    raise NotImplementedError
-
-
-def random_cropped_background(image, background):
-    image, alpha_channel = split_alpha_channel(image)
-    background = random_crop(background, size=image.shape)
-    return alpha_blend(image, background, alpha_channel)
-
-
 def show_image(image, name='image', wait=True):
     """Shows RGB image in an external window.
     # Arguments
         image: Numpy array
         name: String indicating the window name.
     """
+    if image.dtype != np.uint8:
+        raise ValueError('``image`` must be of type ``uint8``')
     # openCV default color space is BGR
     image = convert_color_space(image, RGB2BGR)
     cv2.imshow(name, image)
@@ -165,14 +118,53 @@ def save_image(filepath, image):
     return cv2.imwrite(filepath, image)
 
 
-def save_images(save_path, images):
-    """Saves multiple images in a directory
-    # Arguments
-        save_path: String. Path to directory. If path does not exist it will
-        be created.
-        images: List of numpy arrays.
-    """
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
-    for arg, image in enumerate(images):
-        save_image(os.path.join(save_path, 'image_%03d.png' % arg), image)
+def random_image_crop(image, size):
+    H, W = image.shape[:2]
+    if (size >= H) or (size >= W):
+        print('WARNING: Image is smaller than crop size')
+        return None
+    x_min = np.random.randint(0, (W - 1) - size)
+    y_min = np.random.randint(0, (H - 1) - size)
+    x_max = int(x_min + size)
+    y_max = int(y_min + size)
+    cropped_image = image[y_min:y_max, x_min:x_max]
+    return cropped_image
+
+
+def make_random_plain_image(shape):
+    if len(shape) != 3:
+        raise ValueError('``shape`` must have three values')
+    return (np.ones(shape) * np.random.randint(0, 256, shape[-1]))
+
+
+def blend_alpha_channel(image, background):
+    if image.shape[-1] != 4:
+        raise ValueError('``image`` does not contain an alpha mask.')
+    foreground, alpha = np.split(image, [3], -1)
+    return (1.0 - (alpha / 255.0)) * background.astype(float)
+
+
+def concatenate_alpha_mask(image, alpha_mask):
+    alpha_mask = np.expand_dims(alpha_mask, axis=-1)
+    return np.concatenate([image, alpha_mask], axis=2)
+
+
+def split_and_normalize_alpha_channel(image):
+    if image.shape[-1] != 4:
+        raise ValueError('Provided image does not contain alpha mask.')
+    image, alpha_channel = np.split(image, [3], -1)
+    alpha_channel = alpha_channel / 255.0
+    return image, alpha_channel
+
+
+def gaussian_image_blur(image, kernel_size=(5, 5)):
+    return cv2.GaussianBlur(image, kernel_size, 0)
+
+
+def median_image_blur(image, apperture=5):
+    return cv2.medianBlur(image, apperture)
+
+
+def random_image_blur(image):
+    blur = np.random.choice([gaussian_image_blur, median_image_blur])
+    return blur(image)
