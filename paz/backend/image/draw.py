24be8cbce3765e09c40d2aabf51ae99255aff18c
Octavio Arriaga <octavio.arriaga@dfki.de>
2020-06-22 13:44:16 +0200

    Refactor documentation of draw module

--- paz/backend/image/draw.py
+++ paz/backend/image/draw.py
@@ -9,69 +9,94 @@ LINE = cv2.LINE_AA
 
 
 def draw_circle(image, point, color=GREEN, radius=5):
-    """ Draws a cirle in image.
+    """ Draws a circle in image.
+
     # Arguments
-        image: Numpy array of shape [H, W, 3].
-        point: List/tuple of length two indicating (y,x) openCV coordinates.
-        color: List of length three indicating BGR color of point.
+        image: Numpy array of shape ``[H, W, 3]``.
+        point: List of length two indicating ``(y, x)``
+            openCV coordinates.
+        color: List of length three indicating RGB color of point.
         radius: Integer indicating the radius of the point to be drawn.
+
+    # Returns
+        Numpy array with shape ``[H, W, 3]``. Image with circle.
     """
     cv2.circle(image, tuple(point), radius, (0, 0, 0), cv2.FILLED)
     inner_radius = int(.8 * radius)
+    color = color[::-1]  # transform to BGR for openCV
     cv2.circle(image, tuple(point), inner_radius, tuple(color), cv2.FILLED)
 
 
 def put_text(image, text, point, scale, color, thickness):
     """Draws text in image.
+
     # Arguments
         image: Numpy array.
         text: String. Text to be drawn.
         point: Tuple of coordinates indicating the top corner of the text.
         scale: Float. Scale of text.
-        color: Tuple of integers. BGR color coordinates.
+        color: Tuple of integers. RGB color coordinates.
         thickness: Integer. Thickness of the lines used for drawing text.
+
+    # Returns
+        Numpy array with shape ``[H, W, 3]``. Image with text.
     """
     return cv2.putText(image, text, point, FONT, scale, color, thickness, LINE)
 
 
 def draw_line(image, point_A, point_B, color=GREEN, thickness=5):
-    """ Draws a line in image from point_A to point_B.
+    """ Draws a line in image from ``point_A`` to ``point_B``.
+
     # Arguments
-        image: Numpy array of shape [H, W, 3].
-        point_A: List/tuple of length two indicating (y,x) openCV coordinates.
-        point_B: List/tuple of length two indicating (y,x) openCV coordinates.
-        color: List of length three indicating BGR color of point.
+        image: Numpy array of shape ``[H, W, 3]``.
+        point_A: List of length two indicating ``(y, x)`` openCV coordinates.
+        point_B: List of length two indicating ``(y, x)`` openCV coordinates.
+        color: List of length three indicating RGB color of point.
         thickness: Integer indicating the thickness of the line to be drawn.
+
+    # Returns
+        Numpy array with shape ``[H, W, 3]``. Image with line.
     """
+    color = color[::-1]  # transform to BGR for openCV
     cv2.line(image, tuple(point_A), tuple(point_B), tuple(color), thickness)
 
 
 def draw_rectangle(image, corner_A, corner_B, color, thickness):
-    """ Draws a filled rectangle from corner_A to corner_B.
+    """ Draws a filled rectangle from ``corner_A`` to ``corner_B``.
+
     # Arguments
-        image: Numpy array of shape [H, W, 3].
-        corner_A: List/tuple of length two indicating (y,x) openCV coordinates.
-        corner_B: List/tuple of length two indicating (y,x) openCV coordinates.
-        color: List of length three indicating BGR color of point.
+        image: Numpy array of shape ``[H, W, 3]``.
+        corner_A: List of length two indicating ``(y, x)`` openCV coordinates.
+        corner_B: List of length two indicating ``(y, x)`` openCV coordinates.
+        color: List of length three indicating RGB color of point.
         thickness: Integer/openCV Flag. Thickness of rectangle line.
             or for filled use cv2.FILLED flag.
+
+    # Returns
+        Numpy array with shape ``[H, W, 3]``. Image with rectangle.
     """
+    color = color[::-1]  # transform to BGR for openCV
     return cv2.rectangle(
         image, tuple(corner_A), tuple(corner_B), tuple(color), thickness)
 
 
 def draw_dot(image, point, color=GREEN, radius=5, filled=True):
     """ Draws a dot (small rectangle) in image.
+
     # Arguments
-        image: Numpy array of shape [H, W, 3].
-        point: List/tuple of length two indicating (y,x) openCV coordinates.
-        color: List of length three indicating BGR color of point.
+        image: Numpy array of shape ``[H, W, 3]``.
+        point: List of length two indicating ``(y, x)`` openCV coordinates.
+        color: List of length three indicating RGB color of point.
         radius: Integer indicating the radius of the point to be drawn.
         filled: Boolean. If `True` rectangle is filled with `color`.
+
+    # Returns
+        Numpy array with shape ``[H, W, 3]``. Image with dot.
     """
     # drawing outer black rectangle
     point_A = (point[0] - radius, point[1] - radius)
     point_B = (point[0] + radius, point[1] + radius)
+    color = color[::-1]  # transform to BGR for openCV
     draw_rectangle(image, tuple(point_A), tuple(point_B), (0, 0, 0), filled)
 
     # drawing innner rectangle with given `color`
@@ -83,13 +108,19 @@ def draw_dot(image, point, color=GREEN, radius=5, filled=True):
 
 def draw_cube(image, points, color=GREEN, thickness=2):
     """ Draws a cube in image.
+
     # Arguments
-        image: Numpy array of shape [H, W, 3].
+        image: Numpy array of shape ``[H, W, 3]``.
         points: List of length 8  having each element a list
-            of length two indicating (y,x) openCV coordinates.
-        color: List of length three indicating BGR color of point.
+            of length two indicating ``(y, x)`` openCV coordinates.
+        color: List of length three indicating RGB color of point.
         thickness: Integer indicating the thickness of the line to be drawn.
+
+    # Returns
+        Numpy array with shape ``[H, W, 3]``. Image with cube.
     """
+    color = color[::-1]  # transform to BGR for openCV
+
     # draw bottom
     draw_line(image, points[0][0], points[1][0], color, thickness)
     draw_line(image, points[1][0], points[2][0], color, thickness)
@@ -114,21 +145,36 @@ def draw_cube(image, points, color=GREEN, thickness=2):
 
     # draw dots
     # [draw_dot(image, point, color, point_radii) for point in points]
+    return image
 
 
 def draw_filled_polygon(image, vertices, color):
     """ Draws filled polygon
+
     # Arguments
         image: Numpy array.
         vertices: List of elements each having a list
-            of length two indicating (y,x) openCV coordinates.
-        color: Numpy array specifying BGR color of the polygon.
+            of length two indicating ``(y, x)`` openCV coordinates.
+        color: Numpy array specifying RGB color of the polygon.
+
+    # Returns
+        Numpy array with shape ``[H, W, 3]``. Image with polygon.
     """
+    color = color[::-1]  # transform to BGR for openCV
     cv2.fillPoly(image, [vertices], color)
     return image
 
 
 def draw_random_polygon(image, max_radius_scale=.5):
+    """Draw random polygon image.
+
+    # Arguments
+        image: Numpy array with shape ``[H, W, 3]``.
+        max_radius_scale: Float between [0, 1].
+
+    # Returns
+        Numpy array with shape ``[H, W, 3]``. Image with polygon.
+    """
     height, width = image.shape[:2]
     max_distance = np.max((height, width)) * max_radius_scale
     num_vertices = np.random.randint(3, 7)
@@ -148,7 +194,7 @@ def draw_random_polygon(image, max_radius_scale=.5):
 
 def lincolor(num_colors, saturation=1, value=1, normalized=False):
     """Creates a list of RGB colors linearly sampled from HSV space with
-    randomised Saturation and Value
+        randomised Saturation and Value.
 
     # Arguments
         num_colors: Int.
@@ -157,13 +203,10 @@ def lincolor(num_colors, saturation=1, value=1, normalized=False):
         value: Float or `None`. If float indicates value.
             If `None` it samples a random value.
         normalized: Bool. If True, RGB colors are returned between [0, 1]
-            if False, RGB colros are between [0, 255].
+            if False, RGB colors are between [0, 255].
 
     # Returns
         List, for which each element contains a list with RGB color
-
-    # References
-        [Original implementation](https://github.com/jutanke/cselect)
     """
     RGB_colors = []
     hues = [value / num_colors for value in range(0, num_colors)]
