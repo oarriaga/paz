e79b7ec48eed3be6bb2057d5cda9300878f23b56
Octavio Arriaga <octavio.arriaga@dfki.de>
2020-06-25 12:12:47 +0200

    Add documentation to renderer

--- paz/processors/renderer.py
+++ paz/processors/renderer.py
@@ -3,6 +3,12 @@ from ..abstract import Processor
 
 
 class Render(Processor):
+    """Render images and labels.
+
+    # Arguments
+        renderer: Object that renders images and labels using a method
+            ''render_sample()''.
+    """
     def __init__(self, renderer):
         super(Render, self).__init__()
         self.renderer = renderer
