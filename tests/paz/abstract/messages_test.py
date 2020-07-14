d4eab99ff7eee4c2c869837adb2b5b61823c8ca2
SushmaDG <sushma.devaramani@gmail.com>
2020-07-14 19:35:10 +0200

    Refactor messages unit_test with pytest fixtures

--- tests/paz/abstract/messages_test.py
+++ tests/paz/abstract/messages_test.py
@@ -1,25 +1,54 @@
 import numpy as np
-from paz.abstract.messages import Box2D, Pose6D, Keypoint3D
+from paz.abstract.messages import Box2D, Pose6D
+import pytest
 
-def test_Box2D():
+
+@pytest.fixture(params=[[219, 49, 319, 179]])
+def box_coordinates(request):
+    return (request.param)
+
+
+@pytest.fixture(params=[60.0, 80.0])
+def score(request):
+    return request.param
+
+
+@pytest.fixture
+def quaternion():
+    _quaternion = np.array([-0.4732069, 0.5253096, 0.4732069, 0.5255476])
+    return _quaternion
+
+
+@pytest.fixture
+def translation():
+    _translation = np.array([1.0, 0.765, 0])
+    return _translation
+
+
+@pytest.fixture
+def rotation_vector():
+    vector = np.array([1., -0.994522, 0.104528])
+    return vector
+
+
+@pytest.fixture
+def quaternion_result():
+    result = np.array([0.45936268, -0.45684629, 0.04801626, 0.76024458])
+    return result
+
+
+@pytest.mark.parametrize("test_point", [[256, 60], [219, 49]])
+def test_Box2D(box_coordinates, score, test_point):
     """ Unit test for Bounding box 2D with class
     and score
     """
-    coordinates = [219, 49, 319, 179]
-    score = 10.0
-    point = [250, 60]
-    Box2D_class = Box2D(coordinates, score)
-    assert (Box2D_class.contains(point) == True)
+    box2D = Box2D(box_coordinates, score)
+    assert (box2D.contains(test_point))
 
-def test_Pose6D():
-    """Unit test for Pose estimation 
+
+def test_Pose6D(quaternion, translation, rotation_vector, quaternion_result):
+    """Unit test for Pose estimation
     """
-    quaternion = np.array([-0.4732069, 0.5253096, 0.4732069, 0.5255476])
-    translation = np.array([1.0, 0.765, 0])
-    rotation_vector = np.array([1., -0.994522, 0.104528])
-    Pose6D_class = Pose6D(quaternion, translation)
-    result = Pose6D_class.from_rotation_vector(rotation_vector, translation)
-    assert(result.translation.all() == translation.all())
-
-test_Box2D()
-test_Pose6D()
+    pose6D = Pose6D(quaternion, translation)
+    result = pose6D.from_rotation_vector(rotation_vector, translation)
+    assert(result.quaternion.all() == quaternion_result.all())
