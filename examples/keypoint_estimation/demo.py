0ae3000374e8045782f6c91d407503c547757974
Octavio Arriaga <octavio.arriaga@dfki.de>
2020-07-22 15:26:52 +0200

    Add class-arg to detector for printing class-names

--- examples/keypoint_estimation/demo.py
+++ examples/keypoint_estimation/demo.py
@@ -5,23 +5,19 @@ import numpy as np
 from paz.backend.camera import Camera
 from paz.backend.camera import VideoPlayer
 from paz.models import HaarCascadeDetector
+from paz.models import KeypointNet2D
 
-from model import GaussianMixtureModel
-from pipelines import ProbabilisticKeypointPrediction
-
-# TODO change camera id
+from pipelines import PredictMultipleKeypoints2D
 
 description = 'Demo script for running 2D probabilistic keypoints'
 parser = argparse.ArgumentParser(description=description)
 parser.add_argument('-f', '--filters', default=8, type=int,
                     help='Number of filters in convolutional blocks')
-parser.add_argument('-b', '--batch_size', default=10, type=int,
-                    help='Batch size for training')
 parser.add_argument('-nk', '--num_keypoints', default=15, type=int,
                     help='Number of keypoints')
 parser.add_argument('-is', '--image_size', default=96, type=int,
-                    help='Image size')
-parser.add_argument('-c', '--camera_id', type=int, default=2,
+                    help='Model image size')
+parser.add_argument('-c', '--camera_id', type=int, default=0,
                     help='Camera device ID')
 parser.add_argument('-d', '--detector_name', type=str,
                     default='frontalface_default')
@@ -32,8 +28,8 @@ parser.add_argument('-s', '--save_path',
 args = parser.parse_args()
 
 # instantiate model
-batch_shape = (1, args.image_size, args.image_size, 1)
-model = GaussianMixtureModel(batch_shape, args.num_keypoints, args.filters)
+input_shape = (args.image_size, args.image_size, 1)
+model = KeypointNet2D(input_shape, args.num_keypoints, args.filters)
 model.summary()
 
 # loading weights
@@ -45,12 +41,12 @@ model.compile(run_eagerly=False)
 
 model.predict(np.zeros((1, 96, 96, 1)))  # first prediction takes a while...
 # setting detector
-detector = HaarCascadeDetector(args.detector_name)
+detector = HaarCascadeDetector(args.detector_name, 0)
 
 # setting prediction pipeline
-pipeline = ProbabilisticKeypointPrediction(detector, model)
+pipeline = PredictMultipleKeypoints2D(detector, model)
 
 # setting camera and video player
 camera = Camera(args.camera_id)
-player = VideoPlayer((1280, 960), pipeline, camera)
+player = VideoPlayer((640, 480), pipeline, camera)
 player.run()
