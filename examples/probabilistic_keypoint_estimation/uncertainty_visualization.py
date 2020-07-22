9f83075cf2634a2e6ceb6ef31349ac9c7c3a0307
Octavio Arriaga <octavio.arriaga@dfki.de>
2020-07-22 12:40:53 +0200

    Change uncertainty drawing to plot distribution over image

--- examples/probabilistic_keypoint_estimation/uncertainty_visualization.py
+++ examples/probabilistic_keypoint_estimation/uncertainty_visualization.py
@@ -2,19 +2,17 @@ import os
 import argparse
 import numpy as np
 import tensorflow as tf
-from paz.backend.image import lincolor
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-
 from paz.abstract import ProcessingSequence
+from paz.backend.keypoints import denormalize_keypoints
+from paz.backend.image import lincolor
 
 from facial_keypoints import FacialKeypoints
 from pipelines import AugmentKeypoints
 from model import GaussianMixtureModel
-from paz.backend.image import show_image
-from paz.backend.keypoints import denormalize_keypoints
-from processors import draw_circles
+
 
 description = 'Training script for learning 2D probabilistic keypoints'
 parser = argparse.ArgumentParser(description=description)
@@ -48,16 +46,12 @@ datasets = {'train': data[:num_train_samples],
 kwargs = {'with_partition': True, 'num_keypoints': args.num_keypoints}
 processor = AugmentKeypoints('validation', **kwargs)
 
-# creating sequencers
-sequence = ProcessingSequence(processor, args.batch_size, data, True)
-
 # instantiate model
 batch_shape = (1, args.image_size, args.image_size, 1)
 model = GaussianMixtureModel(batch_shape, args.num_keypoints, args.filters)
 model.summary()
 
-
-# making directory for saving model weights and logs
+# loading weights
 model_name = '_'.join(['FaceKP', model.name, str(args.num_keypoints)])
 save_path = os.path.join(args.save_path, model_name)
 if not os.path.exists(save_path):
@@ -65,29 +59,31 @@ if not os.path.exists(save_path):
 model_path = os.path.join(save_path, '%s_weights.hdf5' % model_name)
 
 model.load_weights(model_path)
-"""
+
+
 for arg in range(1, 100):
-    # arg = 0
     sample = datasets['validation'][arg]
     sample = processor(sample)
     image = sample['inputs']['image']
     image = np.expand_dims(image, axis=0)
-    x = np.linspace(-1, 1, 90).astype('float32')
-    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(4, 4))
+
+    x = np.linspace(-1, 1, 96).astype('float32')
     grid = tf.stack(np.meshgrid(x, x), axis=2)
-    predictions = model(image)
+
+    distributions = model(image)
+
     keypoints = np.zeros((15, 2))
     colors = lincolor(args.num_keypoints, normalized=True)
-    for keypoint_arg, distribution in enumerate(predictions):
+    fig, axis = plt.subplots(nrows=1, ncols=1, figsize=(4, 4))
+    for keypoint_arg, distribution in enumerate(distributions):
         keypoints[keypoint_arg] = distribution.mean()
-        # probability = distribution.prob(grid).numpy()
+        probability = distribution.prob(grid).numpy()
         color = colors[keypoint_arg]
         cmap = sns.light_palette(color, input='hsl', as_cmap=True)
-        # axes[0].contour(probability, cmap=cmap)
+        probability = probability[::-1, :]
+        axis.contour(probability, cmap=cmap)
     keypoints = denormalize_keypoints(keypoints, 96, 96)
     keypoints = keypoints.astype('int')
     image = (image[0] * 255).astype('uint8')
-    image = draw_circles(image, keypoints)
-    axes[1].imshow(image[..., 0])
+    axis.imshow(image[..., 0], cmap='gray')
     plt.show()
-"""
