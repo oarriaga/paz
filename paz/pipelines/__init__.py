a6590f37c9d588759b1fb29f0f90b8afdf343dec
Octavio Arriaga <octavio.arriaga@dfki.de>
2020-07-24 22:47:22 +0200

    Add high-level object detection API

--- paz/pipelines/__init__.py
+++ paz/pipelines/__init__.py
@@ -7,7 +7,11 @@ from .image import DecoderPredictor
 from .detection import AugmentBoxes
 from .detection import PreprocessBoxes
 from .detection import AugmentDetection
-from .detection import SingleShotInference
+from .detection import SingleShotPrediction
+from .detection import SSD512COCO
+from .detection import SSD512YCBVideo
+from .detection import SSD300VOC
+from .detection import SSD300FAT
 
 from .keypoints import KeypointNetSharedAugmentation
 from .keypoints import KeypointNetInference
--- paz/pipelines/detection.py
+++ paz/pipelines/detection.py
@@ -1,5 +1,7 @@
 from ..abstract import SequentialProcessor, Processor
 from .. import processors as pr
+from ..models import SSD512, SSD300
+from ..datasets import get_class_names
 
 from .image import AugmentImage, PreprocessImage
 
@@ -75,19 +77,19 @@ class AugmentDetection(SequentialProcessor):
             {1: {'boxes': [len(prior_boxes), 4 + num_classes]}}))
 
 
-class SingleShotInference(Processor):
+class SingleShotPrediction(Processor):
     """Single-shot object detection prediction.
 
     # Arguments
         model: Keras model.
         class_names: List of strings indicating the class names.
-        score_thresh: Float.
+        score_thresh: Float between [0, 1]
         nms_thresh: Float between [0, 1].
         mean: List of three elements indicating the per channel mean.
     """
     def __init__(self, model, class_names, score_thresh, nms_thresh,
                  mean=pr.BGR_IMAGENET_MEAN):
-        super(SingleShotInference, self).__init__()
+        super(SingleShotPrediction, self).__init__()
         preprocessing = SequentialProcessor(
             [pr.ResizeImage(model.input_shape[1:3]),
              pr.ConvertColorSpace(pr.RGB2BGR),
@@ -110,3 +112,49 @@ class SingleShotInference(Processor):
         boxes2D = self.denormalize(image, boxes2D)
         image = self.draw(image, boxes2D)
         return self.wrap(image, boxes2D)
+
+
+class SSD512COCO(SingleShotPrediction):
+    def __init__(self, score_thresh=0.60, nms_thresh=0.45):
+        """Single-shot inference pipeline with SSD512 trained on COCO.
+        score_thresh: Float between [0, 1]
+        nms_thresh: Float between [0, 1].
+        """
+        model = SSD512()
+        names = get_class_names('COCO')
+        super(SSD512COCO, self).__init__(
+            model, names, score_thresh, nms_thresh)
+
+
+class SSD512YCBVideo(SingleShotPrediction):
+    def __init__(self, score_thresh=0.60, nms_thresh=0.45):
+        """Single-shot inference pipeline with SSD512 trained on YCBVideo.
+        score_thresh: Float between [0, 1]
+        nms_thresh: Float between [0, 1].
+        """
+        model = SSD512(weights='YCBVideo')
+        names = get_class_names('YCBVideo')
+        super(SSD512YCBVideo, self).__init__(
+            model, names, score_thresh, nms_thresh)
+
+
+class SSD300VOC(SingleShotPrediction):
+    def __init__(self, score_thresh=0.60, nms_thresh=0.45):
+        """Single-shot inference pipeline with SSD300 trained on VOC.
+        score_thresh: Float between [0, 1]
+        nms_thresh: Float between [0, 1].
+        """
+        model = SSD300()
+        names = get_class_names('VOC')
+        super(SSD300VOC, self).__init__(model, names, score_thresh, nms_thresh)
+
+
+class SSD300FAT(SingleShotPrediction):
+    def __init__(self, score_thresh=0.60, nms_thresh=0.45):
+        """Single-shot inference pipeline with SSD300 trained on FAT.
+        score_thresh: Float between [0, 1]
+        nms_thresh: Float between [0, 1].
+        """
+        model = SSD300(22, 'FAT', 'FAT')
+        names = get_class_names('FAT')
+        super(SSD300FAT, self).__init__(model, names, score_thresh, nms_thresh)
