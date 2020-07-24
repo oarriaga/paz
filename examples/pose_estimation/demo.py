c372e59735c28a2d2fa5eefa19ba7fe9fc94569d
Octavio Arriaga <octavio.arriaga@dfki.de>
2020-07-24 13:15:56 +0200

    Change script for automatic approximation in case camera intrinsics are not given

--- examples/pose_estimation/demo.py
+++ examples/pose_estimation/demo.py
@@ -8,8 +8,9 @@ from paz.models import HaarCascadeDetector
 from paz.models import KeypointNet2D
 
 from pipelines import HeadPose6DEstimation
-# TODO change camera id to 0
-description = 'Demo script for running 2D probabilistic keypoints'
+from pipelines import model_data
+
+description = 'Demo script for estimating 6D pose-heads from face-keypoints'
 parser = argparse.ArgumentParser(description=description)
 parser.add_argument('-f', '--filters', default=32, type=int,
                     help='Number of filters in convolutional blocks')
@@ -17,51 +18,43 @@ parser.add_argument('-nk', '--num_keypoints', default=15, type=int,
                     help='Number of keypoints')
 parser.add_argument('-is', '--image_size', default=96, type=int,
                     help='Model image size')
-parser.add_argument('-c', '--camera_id', type=int, default=2,
+parser.add_argument('-c', '--camera_id', type=int, default=0,
                     help='Camera device ID')
 parser.add_argument('-d', '--detector_name', type=str,
                     default='frontalface_default')
+parser.add_argument('-fl', '--focal_length', type=float, default=None,
+                    help="Focal length in pixels. If ''None'' it's"
+                    "approximated using the image width")
+parser.add_argument('-ic', '--image_center', nargs='+', type=float,
+                    default=None, help="Image center in pixels for internal"
+                    "camera matrix. If ''None'' it's approximated using the"
+                    "image center from an extracted frame.")
 parser.add_argument('-s', '--save_path',
                     default=os.path.join(
                         os.path.expanduser('~'), '.keras/paz/models'),
                     type=str, help='Path for writing model weights and logs')
 args = parser.parse_args()
 
-keypoints3D = np.array([
-    [-220, 678, 1138],  # left--center-eye
-    [+220, 678, 1138],  # right-center-eye
-    [-131, 676, 1107],  # left--eye close to nose
-    [-294, 610, 1123],  # left--eye close to ear
-    [+131, 676, 1107],  # right-eye close to nose
-    [+294, 610, 1123],  # right-eye close to ear
-    [-106, 758, 1224],  # left--eyebrow close to nose
-    [-375, 585, 1208],  # left--eyebrow close to ear
-    [+106, 758, 1224],  # right-eyebrow close to nose
-    [+375, 585, 1208],  # right-eyebrow close to ear
-    [0.0, 909, 919],  # nose
-    [-183, 691, 683],  # lefty-lip
-    [+183, 691, 683],  # right-lip
-    [0.0, 826, 754],  # up---lip
-    [0.0, 815, 645],  # down-lip
-])
-
-keypoints3D = keypoints3D - np.mean(keypoints3D, axis=0)
-model_points = {'keypoints3D': keypoints3D,
-                'dimensions': {None: [900.0, 600.0]}}
-
+# obtaining a frame to perform focal-length and camera center approximation
 camera = Camera(args.camera_id)
 camera.start()
-frame = camera.read()
+image_size = camera.read().shape[0:2]
 camera.stop()
 
-image_size = frame.shape[0:2]
-focal_length = image_size[1]
-#  center = (image_size[1] / 2.0, image_size[0] / 2.0)
-center = (300, 200)
-focal_length = 832.0
+# loading focal length or approximating it
+focal_length = args.focal_length
+if focal_length is None:
+    focal_length = image_size[1]
+
+# loading image/sensor center or approximating it
+image_center = args.image_center
+if args.image_center is None:
+    image_center = (image_size[1] / 2.0, image_size[0] / 2.0)
+
+# building camera parameters
 camera.distortion = np.zeros((4, 1))
-camera.intrinsics = np.array([[focal_length, 0, center[0]],
-                              [0, focal_length, center[1]],
+camera.intrinsics = np.array([[focal_length, 0, image_center[0]],
+                              [0, focal_length, image_center[1]],
                               [0, 0, 1]])
 
 # instantiate model
@@ -81,7 +74,7 @@ model.compile(run_eagerly=False)
 detector = HaarCascadeDetector(args.detector_name, 0)
 
 # setting prediction pipeline
-pipeline = HeadPose6DEstimation(detector, model, model_points, camera)
+pipeline = HeadPose6DEstimation(detector, model, model_data, camera)
 
 # setting camera and video player
 player = VideoPlayer((640, 480), pipeline, camera)
