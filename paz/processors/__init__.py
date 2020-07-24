652108aae8c5d870d76c5206f5e0f8575b028885
Octavio Arriaga <octavio.arriaga@dfki.de>
2020-07-24 13:17:42 +0200

    Add solvers to module imports

--- paz/processors/__init__.py
+++ paz/processors/__init__.py
@@ -86,13 +86,16 @@ from .standard import Concatenate
 from .standard import SelectElement
 
 from .pose import SolvePNP
-from .pose import UPNP
+
 
 from ..backend.image.opencv_image import RGB2BGR
 from ..backend.image.opencv_image import BGR2RGB
 from ..backend.image.opencv_image import RGB2GRAY
 from ..backend.image.opencv_image import RGB2HSV
 from ..backend.image.opencv_image import HSV2RGB
+
+from ..backend.keypoints import UPNP
+from ..backend.keypoints import LEVENBERG_MARQUARDT
 
 TRAIN = 0
 VAL = 1
