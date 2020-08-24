538917e224f945125947b75852c275c450251487
SushmaDG <sushma.devaramani@gmail.com>
2020-08-24 14:40:09 +0200

    Modify mask RCNN inference test

--- examples/mask_rcnn/tests/inference_test.py
+++ examples/mask_rcnn/tests/inference_test.py
@@ -5,25 +5,26 @@ import cv2
 from mask_rcnn import inference
 
 @pytest.fixture
-def image():
+def test_results():
     file_path = '/home/incendio/Documents/Thesis/test_images/elephant.jpg'
-    return cv2.imread(file_path)
+    image = cv2.imread(file_path)
+    return inference.test(image)[0]
 
 @pytest.mark.parametrize('box', [np.array([34, 55, 359, 592])])
-def test_bounding_box(image, box):
-    boxes = inference.test(image)[0]['rois']
+def test_bounding_box(test_results, box):
+    boxes = test_results['rois']
     assert (boxes[0].all() == box.all())
 
 
 @pytest.mark.parametrize('mask_shape', [(426, 640)])
-def test_mask_shape(image, mask_shape):
-    masks = inference.test(image)[0]['masks']
+def test_mask_shape(test_results, mask_shape):
+    masks = test_results['masks']
     masks = np.array(masks)[:, :, 0]
     assert (mask_shape == masks.shape)
 
 
 @pytest.mark.parametrize('ones', [79848])
-def test_mask(image, ones):
-    masks = inference.test(image)[0]['masks']
+def test_mask(test_results, ones):
+    masks = test_results['masks']
     masks = np.array(masks)[:, :, 0]
     assert(ones == np.sum(masks))
