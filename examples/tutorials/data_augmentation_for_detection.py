3244544e3b65fd69665e5fe761b360d9242f9c55
Octavio Arriaga <octavio.arriaga@dfki.de>
2020-09-07 17:16:44 +0200

    Add complete tutorial on how to create a data augmentation pipeline for object detection

--- examples/tutorials/data_augmentation_for_detection.py
+++ examples/tutorials/data_augmentation_for_detection.py
@@ -4,7 +4,7 @@
 import os
 import numpy as np
 from paz.abstract import SequentialProcessor
-from paz.backend.image import show_image, load_image
+from paz.backend.image import show_image, load_image, resize_image
 import paz.processors as pr
 from paz.models.detection.utils import create_prior_boxes
 from paz.backend.image import convert_color_space
@@ -16,116 +16,134 @@ IMAGE_URL = ('https://github.com/oarriaga/altamira-data/releases/download'
 image_filename = os.path.basename(IMAGE_URL)
 image_fullpath = get_file(image_filename, IMAGE_URL, cache_subdir='paz/data')
 
+
+# Images
+
+# We can also create sequential pipelines by inheriting ``SequentialProcessor``
+class AugmentImage(SequentialProcessor):
+    def __init__(self):
+        super(AugmentImage, self).__init__()
+        self.add(pr.RandomContrast())
+        self.add(pr.RandomBrightness())
+        self.add(pr.RandomSaturation())
+        self.add(pr.RandomHue())
+
+
+class PreprocessImage(SequentialProcessor):
+    def __init__(self, shape, mean=pr.BGR_IMAGENET_MEAN):
+        super(PreprocessImage, self).__init__()
+        self.add(pr.ResizeImage(shape))
+        self.add(pr.CastImage(float))
+        if mean is None:
+            self.add(pr.NormalizeImage())
+        else:
+            self.add(pr.SubtractMeanImage(mean))
+
+
+# Let's see who it works:
+preprocess_image, augment_image = PreprocessImage((300, 300)), AugmentImage()
+
 '''
-# We construct a pre-processing pipeline for **ONLY** our image:
-preprocess_image = SequentialProcessor()
-preprocess_image.add(pr.ConvertColorSpace(pr.RGB2BGR))
-preprocess_image.add(pr.ResizeImage((300, 300)))
-preprocess_image.add(pr.CastImage(float))
-preprocess_image.add(pr.SubtractMeanImage(pr.BGR_IMAGENET_MEAN))
-
-# Let's see who it works:
-image = load_image(image_fullpath)
-image = preprocess_image(image)
-show_image(image.astype('uint8'))
-
-
-# Now let's build data augmentation for our image:
-augment_image = SequentialProcessor()
-augment_image.add(pr.RandomContrast())
-augment_image.add(pr.RandomBrightness())
-augment_image.add(pr.RandomSaturation())
-augment_image.add(pr.RandomHue())
-
-# Let's see who it works:
 for _ in range(10):
     image = load_image(image_fullpath)
-    image = augment_image(image)
-    show_image(image.astype('uint8'))
-
-
-# Let's put together our preprocessing and our augmentation for our image:
-augment_image.add(preprocess_image)
-for _ in range(10):
-    image = load_image(image_fullpath)
-    image = augment_image(image)
+    image = preprocess_image(augment_image(image))
     show_image(image.astype('uint8'))
 '''
 
-# Now lets' work out the boxes part.
-# Let's first build our labels:
-# Keep in mind that the origin of our images is located at the **top-left**.
+# Boxes
 
-# The x_min, y_min are the **noramlized** coordinates
-# of **top-left** bounding-box corner.
-height, width = load_image(image_fullpath).shape[:2]
-x_min_human, y_min_human = 200 / width, 60 / height
-x_min_horse, y_min_horse = 100 / width, 90 / height
-
-# The x_max, y_max are the **normalized** coordinates
-# of **bottom-right** bounding-box corner.
-x_max_human, y_max_human = 300 / width, 200 / height
-x_max_horse, y_max_horse = 400 / width, 300 / height
-
-# Our image has 1 + 2 classes. The **first** class is the background-class.
-# The other 2 classes correspond to each object i.e. person (human), horse.
-num_classes = 3
-background_class, human_class, horse_class = 0, 1, 2
+# Let's first build our box labels:
+# For a tutorial on how to build your box labels check here:
+# paz/examples/tutorials/bounding_boxes.py
+H, W = load_image(image_fullpath).shape[:2]
 class_names = ['background', 'human', 'horse']
+box_data = np.array([[200 / W, 60 / H, 300 / W, 200 / H, 1],
+                     [100 / W, 90 / H, 400 / W, 300 / H, 2]])
 
-box_data = np.array(
-    [[x_min_human, y_min_human, x_max_human, y_max_human, human_class],
-     [x_min_horse, y_min_horse, x_max_horse, y_max_horse, horse_class]])
 
-# Let's visualize our boxes!
+# The augment boxes pipeline
+class AugmentBoxes(SequentialProcessor):
+    def __init__(self, mean=pr.BGR_IMAGENET_MEAN):
+        super(AugmentBoxes, self).__init__()
+        self.add(pr.ToAbsoluteBoxCoordinates())
+        self.add(pr.Expand(mean=mean))
+        self.add(pr.RandomSampleCrop())
+        self.add(pr.RandomFlipBoxesLeftRight())
+        self.add(pr.ToNormalizedBoxCoordinates())
 
-# first we transform our numpy array into our built-in ``Box2D`` messages
-to_boxes2D = pr.ToBoxes2D(class_names)
-denormalize = pr.DenormalizeBoxes2D()
 
-boxes2D = to_boxes2D(box_data)
-image = load_image(image_fullpath)
-boxes2D = denormalize(image, boxes2D)
-# then we load, draw and show our image:
-draw_boxes2D = pr.DrawBoxes2D(class_names)
-show_image(draw_boxes2D(image, boxes2D))
+# We now visualize our current box augmentation
+# For that we build a quick pipeline for drawing our boxes
+draw_boxes = SequentialProcessor([
+    pr.ControlMap(pr.ToBoxes2D(class_names, True), [1], [1]),
+    pr.ControlMap(pr.DenormalizeBoxes2D(), [0, 1], [1], {0: 0}),
+    pr.DrawBoxes2D(class_names),
+    pr.ShowImage()])
 
-# As you see were not able to put everything as a ``SequentialProcessor``
-# This is because we are dealing with 2 inputs: ``box_data`` and ``image``.
-# We can join them into a single processor using ``pr.ControlMap`` wrap.
-# ``pr.ControlMap`` allows you to select which arguments (``intro_indices``)
-# are passed to your processor, and also where you should put the output
-# of your processor (``outro_indices``).
-draw_boxes = SequentialProcessor()
-draw_boxes.add(pr.ControlMap(to_boxes2D, intro_indices=[1], outro_indices=[1]))
-draw_boxes.add(pr.ControlMap(pr.LoadImage(), [0], [0]))
-draw_boxes.add(pr.ControlMap(pr.DenormalizeBoxes2D(), [0, 1], [1]))
-# draw_boxes.add(pr.DrawBoxes2D(class_names))
-# draw_boxes.add(pr.ShowImage())
-
-# now you have everything in a single packed function that loads and draws!
-a = draw_boxes(image_fullpath, box_data)
-
+# Let's test it our box data augmentation pipeline
+augment_boxes = AugmentBoxes()
 '''
-# Let's now pre-process our boxes:
+for _ in range(10):
+    image = load_image(image_fullpath)
+    image, boxes = augment_boxes(image, box_data.copy())
+    draw_boxes(resize_image(image, (300, 300)), boxes)
+'''
 
-# Some object detectors create **positive boxes** by matching the ground
-# truths with a set of **prior boxes**. Let's use for our case the prior
-# boxes from the SSD300 detector
+# There is also some box-preprocessing that is required.
+# Mostly we must match our boxes to a set of default (prior) boxes.
+# Then we must encode them and expand the class label to a one-hot vector.
+class PreprocessBoxes(SequentialProcessor):
+    def __init__(self, num_classes, prior_boxes, IOU, variances):
+        super(PreprocessBoxes, self).__init__()
+        self.add(pr.MatchBoxes(prior_boxes, IOU),)
+        self.add(pr.EncodeBoxes(prior_boxes, variances))
+        self.add(pr.BoxClassToOneHotVector(num_classes))
+
+
+# Putting everything together in a single processor:
+class AugmentDetection(SequentialProcessor):
+    def __init__(self, prior_boxes, split=pr.TRAIN, num_classes=21, size=300,
+                 mean=pr.BGR_IMAGENET_MEAN, IOU=.5, variances=[.1, .2]):
+        super(AugmentDetection, self).__init__()
+
+        # image processors
+        self.augment_image = AugmentImage()
+        self.augment_image.add(pr.ConvertColorSpace(pr.RGB2BGR))
+        self.preprocess_image = PreprocessImage((size, size), mean)
+
+        # box processors
+        self.augment_boxes = AugmentBoxes()
+        args = (num_classes, prior_boxes, IOU, variances)
+        self.preprocess_boxes = PreprocessBoxes(*args)
+
+        # pipeline
+        self.add(pr.UnpackDictionary(['image', 'boxes']))
+        self.add(pr.ControlMap(pr.LoadImage(), [0], [0]))
+        if split == pr.TRAIN:
+            self.add(pr.ControlMap(self.augment_image, [0], [0]))
+            self.add(pr.ControlMap(self.augment_boxes, [0, 1], [0, 1]))
+        self.add(pr.ControlMap(self.preprocess_image, [0], [0]))
+        self.add(pr.ControlMap(self.preprocess_boxes, [1], [1]))
+        self.add(pr.SequenceWrapper(
+            {0: {'image': [size, size, 3]}},
+            {1: {'boxes': [len(prior_boxes), 4 + num_classes]}}))
+
+
 prior_boxes = create_prior_boxes()
+draw_boxes.insert(0, pr.ControlMap(pr.DecodeBoxes(prior_boxes), [1], [1]))
+draw_boxes.insert(2, pr.ControlMap(
+    pr.FilterClassBoxes2D(class_names[1:]), [1], [1]))
 
-# We begin by pre-processing our boxes
-preprocess_boxes = SequentialProcessor()
-preprocess_boxes.add(pr.MatchBoxes(prior_boxes))
-preprocess_boxes.add(pr.EncodeBoxes(prior_boxes))
-preprocess_boxes.add(pr.BoxClassToOneHotVector(num_classes))
 
-draw_boxes.insert(0, pr.ControlMap(preprocess_boxes, [1], [1]))
-draw_boxes.get_processor('ControlMap-ToBoxes2D').processor.one_hot_encoded = True
-draw_boxes(image_fullpath, box_data)
-
-def undo_preprocessing(image):
+def deprocess_image(image):
     image = (image + pr.BGR_IMAGENET_MEAN).astype('uint8')
-    image = convert_color_space(image, pr.BGR2RGB)
-    return image
-'''
+    return convert_color_space(image, pr.BGR2RGB)
+
+
+augment_detection = AugmentDetection(prior_boxes)
+for _ in range(10):
+    sample = {'image': image_fullpath, 'boxes': box_data.copy()}
+    data = augment_detection(sample)
+    image, boxes = data['inputs']['image'], data['labels']['boxes']
+    image = deprocess_image(image)
+    draw_boxes(image, boxes)
