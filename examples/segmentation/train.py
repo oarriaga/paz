f028ddc4feee387b9491a0957ae6be0f9417582a
Octavio Arriaga <arriaga.camargo@gmail.com>
2021-02-03 23:08:23 +0100

    Add basic callbacks

--- examples/segmentation/train.py
+++ examples/segmentation/train.py
@@ -1,12 +1,18 @@
+import os
 import tensorflow as tf
 gpus = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(gpus[0], True)
 
-from shapes import Shapes
-from model import UNET_VGG16
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import CSVLogger, EarlyStopping
+from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 from paz.abstract import ProcessingSequence, SequentialProcessor
 from paz import processors as pr
+
+from shapes import Shapes
+from model import UNET_VGG16
+from pipelines import PreprocessSegmentation
+from pipelines import PostprocessSegmentation
 
 num_classes = 3
 input_shape = (128, 128, 3)
@@ -20,23 +26,35 @@ H, W = image_shape = input_shape[:2]
 batch_size = 5
 epochs = 10
 freeze = True
+stop_patience = 5
+reduce_patience = 2
+experiment_path = 'experiments/'
 
 data_manager = Shapes(num_samples, image_shape, iou_thresh=iou_thresh,
                       max_num_shapes=max_num_shapes)
 num_classes = data_manager.num_classes - 1
 data = data_manager.load_data()
-preprocess_image = SequentialProcessor()
-preprocess_image.add(pr.ConvertColorSpace(pr.RGB2BGR))
-preprocess_image.add(pr.SubtractMeanImage(pr.BGR_IMAGENET_MEAN))
-processor = SequentialProcessor()
-processor.add(pr.UnpackDictionary(['image', 'masks']))
-processor.add(pr.ControlMap(preprocess_image, [0], [0]))
-processor.add(pr.SequenceWrapper({0: {'input_1': [H, W, 3]}},
-                                 {1: {'masks': [H, W, num_classes]}}))
+processor = PreprocessSegmentation(image_shape, num_classes)
+
+# setting additional callbacks
+callbacks = []
+log_filename = os.path.join(experiment_path, 'optimization.log')
+log = CSVLogger(log_filename)
+stop = EarlyStopping('loss', patience=stop_patience)
+save_filename = os.path.join(experiment_path, 'model.hdf5')
+save = ModelCheckpoint(save_filename, 'loss', save_best_only=True)
+plateau = ReduceLROnPlateau('loss', patience=reduce_patience)
+callbacks.extend([log, stop, save, plateau])
 
 model = UNET_VGG16(num_classes, input_shape, 'imagenet', freeze, activation)
 sequence = ProcessingSequence(processor, batch_size, data)
 optimizer = Adam()
 model.compile(optimizer, loss, metrics)
 model.summary()
-model.fit(sequence, batch_size=batch_size, epochs=epochs, callbacks=None)
+model.fit(sequence, batch_size=batch_size, epochs=epochs, callbacks=callbacks)
+
+
+postprocess = PostprocessSegmentation(model)
+for sample in data:
+    image = sample['image']
+    postprocess(image)
