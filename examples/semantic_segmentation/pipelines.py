057d0bcd2628967f45dacaaf4929a7effcb86ec9
Octavio Arriaga <arriaga.camargo@gmail.com>
2021-02-09 15:45:37 +0100

    Move pipeline

--- examples/semantic_segmentation/pipelines.py
+++ examples/semantic_segmentation/pipelines.py
@@ -15,3 +15,14 @@ class PostprocessSegmentation(pr.SequentialProcessor):
         self.add(pr.DenormalizeImage())
         self.add(pr.CastImage('uint8'))
         self.add(pr.ShowImage())
+
+
+class PreprocessSegmentation(pr.SequentialProcessor):
+    def __init__(self, image_shape, num_classes, input_name='input_1'):
+        super(PreprocessSegmentation, self).__init__()
+        H, W = image_shape
+        preprocess_image = PreprocessImage()
+        self.add(pr.UnpackDictionary(['image', 'masks']))
+        self.add(pr.ControlMap(preprocess_image, [0], [0]))
+        self.add(pr.SequenceWrapper({0: {input_name: [H, W, 3]}},
+                                    {1: {'masks': [H, W, num_classes]}}))
--- examples/semantic_segmentation/processors.py
+++ examples/semantic_segmentation/processors.py
@@ -10,17 +10,6 @@ class PreprocessImage(pr.SequentialProcessor):
         self.add(pr.SubtractMeanImage(mean))
 
 
-class PreprocessSegmentation(pr.SequentialProcessor):
-    def __init__(self, image_shape, num_classes, input_name='input_1'):
-        super(PreprocessSegmentation, self).__init__()
-        H, W = image_shape
-        preprocess_image = PreprocessImage()
-        self.add(pr.UnpackDictionary(['image', 'masks']))
-        self.add(pr.ControlMap(preprocess_image, [0], [0]))
-        self.add(pr.SequenceWrapper({0: {input_name: [H, W, 3]}},
-                                    {1: {'masks': [H, W, num_classes]}}))
-
-
 class MasksToColors(pr.Processor):
     def __init__(self, num_classes, colors=None):
         super(MasksToColors, self).__init__()
