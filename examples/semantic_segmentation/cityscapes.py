89e26426cfd1b1f54d539adc6c0fd5817b8802d2
Octavio Arriaga <arriaga.camargo@gmail.com>
2021-02-09 19:48:53 +0100

    Add basic loader for the CityScapes dataset

---
+++ examples/semantic_segmentation/cityscapes.py
@@ -0,0 +1,31 @@
+import os
+import glob
+from paz.abstract import Loader
+
+
+class CityScapes(Loader):
+    def __init__(self, image_path, label_path, split, class_names):
+        self.image_path = os.path.join(image_path, split)
+        self.label_path = os.path.join(label_path, split)
+        super(CityScapes, self).__init__(
+            None, split, class_names, 'CityScapes')
+
+    def load_data(self):
+        image_path = os.path.join(self.image_path, '*/*.png')
+        label_path = os.path.join(self.label_path, '*/*color.png')
+        image_paths = glob.glob(image_path)
+        label_paths = glob.glob(label_path)
+        image_paths = sorted(image_paths)
+        label_paths = sorted(label_paths)
+        assert len(image_paths) == len(label_paths)
+        dataset = []
+        for image_path, label_path in zip(image_paths, label_paths):
+            sample = {'image_path': image_path, 'label_path': label_path}
+            dataset.append(sample)
+        return dataset
+
+
+label_path = '/home/octavio/Downloads/dummy/gtCoarse/'
+image_path = '/home/octavio/Downloads/dummy/RGB_images/leftImg8bit/'
+data_manager = CityScapes(image_path, label_path, 'train', None)
+dataset = data_manager.load_data()
