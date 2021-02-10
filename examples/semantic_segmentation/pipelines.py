9c444bce87c445bf42a481cfae1dfbcea521005b
Octavio Arriaga <arriaga.camargo@gmail.com>
2021-02-10 14:06:31 +0100

    Add postprocess pipelines

--- examples/semantic_segmentation/pipelines.py
+++ examples/semantic_segmentation/pipelines.py
@@ -1,6 +1,7 @@
 from paz import processors as pr
 
 from processors import PreprocessImage, Round, MasksToColors
+from processors import FromIdToMask, ResizeImageWithNearestNeighbors
 
 
 class PostprocessSegmentation(pr.SequentialProcessor):
@@ -26,3 +27,41 @@ class PreprocessSegmentation(pr.SequentialProcessor):
         self.add(pr.ControlMap(preprocess_image, [0], [0]))
         self.add(pr.SequenceWrapper({0: {input_name: [H, W, 3]}},
                                     {1: {'masks': [H, W, num_classes]}}))
+
+
+class PreprocessSegmentationIds(pr.SequentialProcessor):
+    def __init__(self, image_shape, num_classes, input_name='input_1'):
+        super(PreprocessSegmentationIds, self).__init__()
+        self.add(pr.UnpackDictionary(['image_path', 'label_path']))
+        preprocess_image = pr.SequentialProcessor()
+        preprocess_image.add(pr.LoadImage())
+        preprocess_image.add(pr.ResizeImage(image_shape))
+        preprocess_image.add(pr.ConvertColorSpace(pr.RGB2BGR))
+        preprocess_image.add(pr.SubtractMeanImage(pr.BGR_IMAGENET_MEAN))
+
+        preprocess_label = pr.SequentialProcessor()
+        preprocess_label.add(pr.LoadImage())
+        preprocess_label.add(ResizeImageWithNearestNeighbors(image_shape))
+        preprocess_label.add(FromIdToMask())
+
+        self.add(pr.ControlMap(preprocess_image, [0], [0]))
+        self.add(pr.ControlMap(preprocess_label, [1], [1]))
+        H, W = image_shape[:2]
+        self.add(pr.SequenceWrapper({0: {input_name: [H, W, 3]}},
+                                    {1: {'masks': [H, W, num_classes]}}))
+
+
+class PostprocessSegmentationIds(pr.SequentialProcessor):
+    def __init__(self, num_classes, colors=None):
+        super(PostprocessSegmentationIds, self).__init__()
+        self.add(MasksToColors(num_classes, colors))
+        self.add(pr.DenormalizeImage())
+        self.add(pr.CastImage('uint8'))
+
+
+class PostProcessImage(pr.SequentialProcessor):
+    def __init__(self):
+        super(PostProcessImage, self).__init__()
+        self.add(pr.AddMeanImage(pr.BGR_IMAGENET_MEAN))
+        self.add(pr.CastImage('uint8'))
+        self.add(pr.ConvertColorSpace(pr.BGR2RGB))
