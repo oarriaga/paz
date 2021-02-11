3cfa5c992b75d244cb8c047109c20bddd647c4b0
Octavio Arriaga <arriaga.camargo@gmail.com>
2021-02-11 10:48:54 +0100

    Integrated cityscapes dataset into module

--- paz/datasets/cityscapes.py
+++ paz/datasets/cityscapes.py
@@ -1,16 +1,9 @@
 import os
 import glob
-import numpy as np
+
 from paz.abstract import Loader
-from pipelines import PreprocessSegmentationIds
-from pipelines import PostprocessSegmentationIds
-from pipelines import PostProcessImage
 
-
-def get_class_names(dataset_name):
-    if dataset_name == 'CityScapes':
-        return ['void', 'flat', 'construction',
-                'object', 'nature', 'sky', 'human', 'vehicle']
+from .utils import get_class_names
 
 
 class CityScapes(Loader):
@@ -51,26 +44,3 @@ class CityScapes(Loader):
             sample = {'image_path': image_path, 'label_path': label_path}
             dataset.append(sample)
         return dataset
-
-
-if __name__ == "__main__":
-    from paz.backend.image import show_image
-
-    label_path = '/home/octavio/Downloads/dummy/gtFine/'
-    # label_path = '/home/octavio/Downloads/dummy/gtCoarse/'
-    image_path = '/home/octavio/Downloads/dummy/RGB_images/leftImg8bit/'
-    data_manager = CityScapes(image_path, label_path, 'train')
-    dataset = data_manager.load_data()
-    class_names = data_manager.class_names
-    num_classes = len(class_names)
-    preprocess = PreprocessSegmentationIds((128, 128), num_classes)
-    postprocess_masks = PostprocessSegmentationIds(num_classes)
-    postprocess_image = PostProcessImage()
-    for sample in dataset:
-        preprocessed_sample = preprocess(sample)
-        image = preprocessed_sample['inputs']['input_1']
-        image = postprocess_image(image)
-        masks = preprocessed_sample['labels']['masks']
-        masks = postprocess_masks(masks)
-        mask_and_image = np.concatenate([masks, image], axis=1)
-        show_image(mask_and_image)
