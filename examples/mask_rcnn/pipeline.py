3bd20ce67254b73f2968c649ab67574ad4a8ec14
Sushma Devaramani <sdevaramani@hpc-dnode03.dfki.uni-bremen.de>
2021-02-12 13:14:35 +0100

    Extend and test DetectionPipeline

--- examples/mask_rcnn/pipeline.py
+++ examples/mask_rcnn/pipeline.py
@@ -1,20 +1,23 @@
 import paz.processors as pr
 from paz.abstract import Processor, SequentialProcessor
 from paz.pipelines import AugmentImage, PreprocessImage
-from paz.pipelines import AugmentBoxes, PreprocessBoxes
+from paz.pipelines import AugmentBoxes
 from mask_rcnn.utils import build_rpn_targets
 
 
 class MatchRPNBoxes(Processor):
-    def __init__(self, config):
+    def __init__(self, anchors, config):
         super(MatchRPNBoxes, self).__init__()
         self.config = config
+        self.anchors = anchors
         self.image_shape = config.IMAGE_SHAPE
 
-    def call(self, boxes, anchors):
-        matches, boxes = build_rpn_targets(self.image_shape,
-                                           anchors, boxes, self.config)
-        return boxes
+    def call(self, boxes):
+        class_ids = boxes[:, 4]
+        matches, box_deltas = build_rpn_targets(self.image_shape,
+                                                self.anchors, class_ids,
+                                                boxes, self.config)
+        return box_deltas, matches
 
 
 class PreprocessMask(SequentialProcessor):
@@ -23,9 +26,15 @@ class PreprocessMask(SequentialProcessor):
         self.add(pr.ResizeImage(shape))
 
 
+class ComputeBoxDeltas(SequentialProcessor):
+    def __init__(self, config, prior_boxes):
+        super(ComputeBoxDeltas, self).__init__()
+        self.add(MatchRPNBoxes(prior_boxes, config),)
+
+
 class DetectionPipeline(SequentialProcessor):
-    def __init__(self, prior_boxes, split='TRAIN', num_classes=4, size=320, 
-                 mean=pr.BGR_IMAGENET_MEAN, IOU=.5, variances=[.1, .2]):
+    def __init__(self, config, prior_boxes, split='TRAIN', num_classes=4,
+                 size=320, mean=pr.BGR_IMAGENET_MEAN, IOU=.5, variances=[.1, .2]):
         super(DetectionPipeline, self).__init__()
 
         self.augment_image = AugmentImage()
@@ -33,8 +42,8 @@ class DetectionPipeline(SequentialProcessor):
         self.preprocess_image = PreprocessImage((size, size), mean)
 
         self.augment_boxes = AugmentBoxes()
-        args = (num_classes, prior_boxes, IOU, variances)
-        self.preprocess_boxes = PreprocessBoxes(*args)
+        self.compute_box_deltas = ComputeBoxDeltas(config, prior_boxes)
+        self.num_anchors_per_image = config.RPN_TRAIN_ANCHORS_PER_IMAGE
 
         self.preprocess_mask = PreprocessMask((size, size))
         
@@ -43,10 +52,12 @@ class DetectionPipeline(SequentialProcessor):
             self.add(pr.ControlMap(self.augment_image, [0], [0]))
             self.add(pr.ControlMap(self.preprocess_boxes, [0, 1], [1]))
         self.add(pr.ControlMap(self.preprocess_image, [0], [0]))
-        self.add(pr.ControlMap(self.preprocess_boxes, [1], [1]))
-        self.add(pr.ControlMap(self.preprocess_mask, [2], [2]))
+        self.add(pr.ControlMap(self.compute_box_deltas, [1], [1, 2], {1: 3}))
+        self.add(pr.ControlMap(self.preprocess_mask, [4], [4]))
         self.add(pr.SequenceWrapper(
             {0: {'image': [size, size, 3]}},
-            {1: {'boxes': [len(prior_boxes), 4 + num_classes]},
-             2: {'mask': [size, size, (num_classes - 1)]}}))
+            {1: {'box_deltas': [self.num_anchors_per_image, 4]},
+             2: {'matches': [len(prior_boxes)]},
+             3: {'boxes': [(num_classes - 1), 5]},
+             4: {'mask': [size, size, (num_classes - 1)]}}))
 
--- examples/mask_rcnn/tests/pipeline_test.py
+++ examples/mask_rcnn/tests/pipeline_test.py
@@ -1,52 +1,75 @@
 import pytest
-import cv2
-import numpy as np
-from paz.models.detection.utils import create_prior_boxes
 from mask_rcnn.pipeline import DetectionPipeline
+from mask_rcnn.shapes_loader import Shapes
+from mask_rcnn.config import Config
+from mask_rcnn.utils import compute_backbone_shapes
+from mask_rcnn.utils import generate_pyramid_anchors
+from paz.abstract import ProcessingSequence
+
+
+class ShapesConfig(Config):
+    """Configuration for training on the toy shapes dataset.
+    """
+    NAME = 'shapes'
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+    NUM_CLASSES = 1 + 3
+    IMAGE_MIN_DIM = 320
+    IMAGE_MAX_DIM = 320
+    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)
+    TRAIN_ROIS_PER_IMAGE = 32
 
 
 @pytest.fixture
-def image():
-    image = cv2.imread('../example/image.png')
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    return image
+def data():
+    config = ShapesConfig()
+    size = (320, 320)
+    shapes = Shapes(1, size)
+    dataset = shapes.load_data()
+    image = dataset[0]['image']
+    mask = dataset[0]['mask']
+    box_data = dataset[0]['box_data']
+    data = [{'image': image, 'boxes': box_data, 'mask': mask}]
+    return data
 
 
 @pytest.fixture
-def boxes():
-    box_data = np.array([[0., 0.5343, 0.2062, 0.8062, 2.],
-                         [0.3249, 0.4718, 0.625, 0.7312, 3.],
-                         [0.5531, 0.4218, 0.8093, 0.6437, 3.]])
-    return box_data
+def anchors():
+    config = ShapesConfig()
+    backbone_shapes = compute_backbone_shapes(config, config.IMAGE_SHAPE)
+    anchors = generate_pyramid_anchors(config.RPN_ANCHOR_SCALES,
+                                       config.RPN_ANCHOR_RATIOS,
+                                       backbone_shapes,
+                                       config.BACKBONE_STRIDES,
+                                       config.RPN_ANCHOR_STRIDE)
+    return anchors
 
 
 @pytest.fixture
-def masks():
-    mask_path = ['../example/mask_0.png',
-                 '../example/mask_1.png',
-                 '../example/mask_2.png']
-    masks = []
-    for path in mask_path:
-        masks.append(cv2.imread(path)[:, :, 0])
-    masks = np.stack(masks, axis=2)
-    return masks
+def pipeline(anchors, num_classes=4):
+    config = ShapesConfig()
+    augmentator = DetectionPipeline(config, anchors,
+                                    num_classes=num_classes)
+    return augmentator
 
 
 @pytest.fixture
-def class_names():
-    classes = ['background', 'square', 'circle',
-               'triangle']
-    return classes
+def sequencer(data, pipeline):
+    batch_size = 1
+    sequencer = ProcessingSequence(pipeline, batch_size, data)
+    return sequencer
 
 
-@pytest.fixture
-def pipeline(class_names):
-    return DetectionPipeline(create_prior_boxes(),
-                             num_classes=len(class_names))
+def test_pipeline(sequencer, anchors):
+    size = (320, 320, 3)
+    config = ShapesConfig()
+    batch = sequencer.__getitem__(0)
+    batch_images, batch_boxes = batch[0]['image'], batch[1]['boxes']
+    batch_masks = batch[1]['mask']
+    batch_box_deltas, batch_matches = batch[1]['box_deltas'], batch[1]['matches']
+    image, boxes, masks = batch_images[0], batch_boxes[0], batch_masks[0]
+    assert image.shape == size == masks.shape
+    assert boxes.shape == (3, 5)
+    assert batch_box_deltas.shape[1:] == (config.RPN_TRAIN_ANCHORS_PER_IMAGE, 4)
+    assert batch_matches.shape[1:][0] == anchors.shape[0]
 
-
-def test_pipeline(pipeline, image, boxes, masks):
-    sample = {'image': image, 'boxes': boxes, 'mask': masks}
-    data = pipeline(sample)
-    assert (image.shape == data['inputs']['image'].shape)
-    assert masks.shape == data['labels']['mask'].shape
