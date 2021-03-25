25311f8aa2bb5a169ae45cf81fafeb774a701df2
Octavio Arriaga <arriaga.camargo@gmail.com>
2021-03-25 10:46:14 +0100

    Update semantic segmentation demo

--- examples/semantic_segmentation/demo.py
+++ examples/semantic_segmentation/demo.py
@@ -1,16 +1,25 @@
+import numpy as np
 import tensorflow as tf
+import argparse
 gpus = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(gpus[0], True)
 
-from cityscapes import CityScapes
+from paz.datasets import CityScapes
 from paz import processors as pr
+from paz.backend.image import resize_image, load_image, show_image
 from processors import Round, MasksToColors
-from model import UNET_VGG16
+from processors import ResizeImageWithNearestNeighbors
+from paz.models import UNET_VGG16
 
 
-label_path = '/home/octavio/Downloads/dummy/gtFine/'
-image_path = '/home/octavio/Downloads/dummy/RGB_images/leftImg8bit/'
-data_manager = CityScapes(image_path, label_path, 'test')
+description = 'Training script for semantic segmentation'
+parser = argparse.ArgumentParser(description=description)
+parser.add_argument('--label_path', type=str, help='Path to labels')
+parser.add_argument('--image_path', type=str, help='Path to images')
+parser.add_argument('--weights_path', type=str, help='Path to weights')
+args = parser.parse_args()
+
+data_manager = CityScapes(args.image_path, args.label_path, 'test')
 data = data_manager.load_data()
 
 
@@ -29,16 +38,20 @@ class PostprocessSegmentation(pr.SequentialProcessor):
         self.add(MasksToColors(model.output_shape[-1], colors))
         self.add(pr.DenormalizeImage())
         self.add(pr.CastImage('uint8'))
-        self.add(pr.ShowImage())
+        self.add(ResizeImageWithNearestNeighbors((1024, 512)))
+        # self.add(pr.ShowImage())
 
 
 num_classes = len(data_manager.class_names)
 input_shape = (128, 128, 3)
-activation = 'softmax'
-freeze = True
-model = UNET_VGG16(num_classes, input_shape, 'imagenet', freeze, activation)
+model = UNET_VGG16(num_classes, input_shape, 'imagenet', activation='softmax')
 post_process = PostprocessSegmentation(model)
-model.load_weights('experiments/model.tf')
+model.load_weights(args.weights_path)
 
 for sample in data:
-    image = post_process(sample)
+    masks = post_process(sample)
+    image = load_image(sample['image_path'])
+    image = resize_image(image, (1024, 512))
+    image_with_masks = ((0.6 * image) + (0.4 * masks)).astype("uint8")
+    # image_and_masks = np.concatenate([image, masks], axis=1)
+    show_image(image_with_masks)
