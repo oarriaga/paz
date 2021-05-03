42041d056ac17336bae1e9fde960a35cbfe045bb
jaswanth <jaswanthbjk@gmail.com>
2021-05-03 23:17:59 +0200

    Added the required function for processing the required data

--- examples/hand_pose_estimation/backend.py
+++ examples/hand_pose_estimation/backend.py
@@ -1,19 +1,26 @@
 import numpy as np
+import cv2
+
+
+def get_one_hot(targets, nb_classes):
+    res = np.eye(nb_classes)[np.array(targets).reshape(-1)]
+    res = res.astype(int)
+    return res.reshape(list(targets.shape) + [nb_classes])
 
 
 def normalize_keypoints(keypoints):
     keypoint_coord_xyz_root = keypoints[0, :]
     keypoint_coord_xyz21_rel = keypoints - keypoint_coord_xyz_root
-    index_root_bone_length = np.linalg.norm(keypoint_coord_xyz21_rel[12, :] -
-                                            keypoint_coord_xyz21_rel[11, :])
-    keypoint_scale = index_root_bone_length
-    keypoint_normalized = keypoint_coord_xyz21_rel / index_root_bone_length
+    keypoint_scale = np.sqrt(np.sum(
+        np.square(keypoint_coord_xyz21_rel[12, :] -
+                  keypoint_coord_xyz21_rel[11, :])))
+    keypoint_normalized = keypoint_coord_xyz21_rel / keypoint_scale
     return keypoint_scale, keypoint_normalized
 
 
 def to_homogeneous_coordinates(vector):
-    vector = np.reshape(vector, [1, -1, 1])
-    vector = np.concat([vector, np.ones((1, 1, 1))], 1)
+    vector = np.append(vector, 1)
+    vector = np.reshape(vector, [-1, 1])
     return vector
 
 
@@ -64,9 +71,13 @@ def get_rotation_matrix_y(angle):
     return rotation_matrix
 
 
-def get_rotation_matrix_z(self, angle):
+def get_rotation_matrix_z(angle):
     rotation_matrix_z = np.array([np.cos(angle), np.sin(angle), 0,
                                   -np.sin(angle), np.cos(angle), 0,
                                   0, 0, 1])
     rotation_matrix = np.reshape(rotation_matrix_z, [3, 3])
     return rotation_matrix
+
+
+def crop_resize_image(image, box, new_size):
+    crooped_image = image[box[1]:box[3], box[0]:box[2]]
