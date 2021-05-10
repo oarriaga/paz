4376a80b036d2b9b76867aec0b03e17ea975904b
Octavio Arriaga <octavio.arriaga@dfki.de>
2021-05-10 21:20:08 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 import paz
 
 setup(name='pypaz',
-      version='0.1.5',
+      version='0.1.6',
       description='Perception for Autonomous Systems',
       author='Octavio Arriaga',
       author_email='octavio.arriaga@dfki.de',
