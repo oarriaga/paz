4dcb579588bd96527c8c77ef478de9fd7e83126f
jaswanthbjk <>
2021-05-26 11:51:59 +0200

    Modified model after testing

--- examples/hand_pose_estimation/HandPoseEstimation.py
+++ examples/hand_pose_estimation/HandPoseEstimation.py
@@ -1,3 +1,6 @@
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 from tensorflow.keras.layers import Concatenate, Dense, Dropout, Reshape, Input
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, UpSampling2D, \
