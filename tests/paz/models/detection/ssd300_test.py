6ce59b27217248433f6c21f25a5a6db8478d3c23
deepan <deepangrad@gmail.com>
2021-06-22 20:32:06 +0200

    refactor code using linter

--- tests/paz/models/detection/ssd300_test.py
+++ tests/paz/models/detection/ssd300_test.py
@@ -4,26 +4,26 @@ from paz.models import SSD300
 
 def test_SSD300_VOC_VOC():
     try:
-        model = SSD300(num_classes=21,
-                       base_weights='VOC',
-                       head_weights='VOC')
-    except:
-        pytest.fail("SSD VOC-VOC loading failed")
+        SSD300(num_classes=21,
+               base_weights='VOC',
+               head_weights='VOC')
+    except ValueError as valuerror:
+        pytest.fail("SSD VOC-VOC loading failed: {}". format(valuerror))
 
 
 def test_SSD300_VOC_None():
     try:
-        model = SSD300(num_classes=2,
-                       base_weights='VOC',
-                       head_weights=None)
-    except:
-        pytest.fail("SSD VOC-None loading failed")
+        SSD300(num_classes=2,
+               base_weights='VOC',
+               head_weights=None)
+    except ValueError as valuerror:
+        pytest.fail("SSD VOC-None loading failed: {}". format(valuerror))
 
 
 def test_SSD300_VGG_None():
     try:
-        model = SSD300(num_classes=21,
-                       base_weights='VGG',
-                       head_weights=None)
-    except:
-        pytest.fail("SSD VGG-None loading failed")
+        SSD300(num_classes=21,
+               base_weights='VGG',
+               head_weights=None)
+    except ValueError as valuerror:
+        pytest.fail("SSD VGG-None loading failed: {}". format(valuerror))
