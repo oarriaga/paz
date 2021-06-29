c26d01340be0561eb8613e896c1d924bebacac7b
proneetsharma <sproneet@gmail.com>
2021-06-29 14:35:05 +0200

    Refactor the tests using pytest

--- tests/paz/processors/draw_test.py
+++ tests/paz/processors/draw_test.py
@@ -1,29 +1,14 @@
-import unittest
+import pytest
 from paz import processors as pr
 
-
-class TestDrawBoxes2D(unittest.TestCase):
-    """Test the type of the parameters for DrawBoxes2D class.
-        class_names: List of strings.
-        colors: List of lists containing the color values
-    """
-    def test_parameters_type(self):
-        # class_name is not of required type
-        self.class_names = 'Face'
-        self.colors = [[255, 0, 0]]
-        with self.assertRaises(TypeError) as context:
-            pr.DrawBoxes2D(self.class_names, self.colors)
-        self.assertEqual("Class name should be of type 'List of strings'",
-                         str(context.exception))
-
-        # colors is not of required type
-        self.class_names = ['Face']
-        self.colors = [255, 0, 0]
-        with self.assertRaises(TypeError) as context:
-            pr.DrawBoxes2D(self.class_names, self.colors)
-        self.assertEqual("Colors should be of type 'List of lists'",
-                         str(context.exception))
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_DrawBoxes2D_with_invalid_class_names_type():
+    with pytest.raises(TypeError):
+        class_names = 'Face'
+        colors = [[255, 0, 0]]
+        pr.DrawBoxes2D(class_names, colors)
+ 
+def test_DrawBoxes2D_with_invalid_colors_type():
+    with pytest.raises(TypeError):
+        class_names = ['Face']
+        colors = [255, 0, 0]
+        pr.DrawBoxes2D(class_names, colors)
