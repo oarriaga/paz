5e3fb76478f2447946009d7eff3e8a520e71fd11
deepan <deepangrad@gmail.com>
2021-07-04 08:26:40 +0200

    add tests to merge outputs and default params

--- examples/efficientdet/efficientdet_test.py
+++ examples/efficientdet/efficientdet_test.py
@@ -1,57 +1,23 @@
-from PIL import Image
-import numpy as np
 import tensorflow as tf
 from efficientdet_model import EfficientDet
 from config import get_efficientdet_default_params
+from postprocess import merge_class_box_level_outputs
 
 
-def efficient_model_configuration():
-    config = dict()
-    config['model_name'] = 'efficientdet-d0'
-    config['backbone_name'] = 'efficientnet-b0'
-    config['backbone_weight'] = 'imagenet'
-    config['act_type'] = 'swish'
-    config['min_level'] = 3
-    config['max_level'] = 7
-    config['fpn_name'] = 'BiFPN'
-    config['fpn_weight_method'] = 'fastattn'
-    config['fpn_num_filters'] = 64
-    config['fpn_cell_repeats'] = 3
-    config['use_batchnorm_for_sampling'] = True
-    config['conv_after_downsample'] = False
-    config['conv_batchnorm_act_pattern'] = False
-    config['separable_conv'] = True
-    config['aspect_ratios'] = [1.0, 2.0, 0.5]
-    config['survival_prob'] = None
-    config['num_classes'] = 90
-    config['num_scales'] = 3
-    config['anchor_scale'] = 4
-    config['image_size'] = 512
-    config['box_class_repeats'] = 3
-    config['feature_only'] = False
-    return config
-
-
-def get_test_image():
-    image = tf.zeros((1, 512, 512, 3), dtype=tf.float32)
+def get_test_image(image_size):
+    image = tf.zeros((1, image_size, image_size, 3), dtype=tf.float32)
     return image
 
 
-def assert_model_outputs(detector, image, config):
-    class_out, det_out = detector(image)
-    assert len(class_out) == (config['max_level'] - config['min_level'] + 1),\
-        'Class outputs length fail'
-    assert len(det_out) == (config['max_level'] - config['min_level'] + 1), \
-        'Class outputs length fail'
-
-
 def test_efficientdet_model():
-    config = efficient_model_configuration()
-    image = get_test_image()
-    detector = EfficientDet()
-    assert_model_outputs(detector,
-                         image,
-                         config)
+    detector = EfficientDet(model_name='efficientdet-d0')
+    params = get_efficientdet_default_params('efficientdet-d0')
+    image_size = params['image_size']
+    num_levels = params['max_level'] - params['min_level'] + 1
+    image = get_test_image(image_size)
+    class_outs, det_outs = detector(image)
+    assert len(class_outs) == num_levels, 'Class outputs length fail'
+    assert len(det_outs) == num_levels, 'Box outputs length fail'
 
 
 def test_efficientdet_default_params():
@@ -61,8 +27,29 @@ def test_efficientdet_default_params():
                    'min_level', 'max_level', 'fpn_weight_method'
                    ]
     params = get_efficientdet_default_params('efficientdet-d5')
-    assert list(params.keys()) == param_names
+    assert list(params.keys()) == param_names, 'Default params not matching'
+
+
+def test_efficientdet_merge_outputs():
+    detector = EfficientDet(model_name='efficientdet-d0')
+    params = get_efficientdet_default_params('efficientdet-d0')
+    image_size = params['image_size']
+    num_levels = params['max_level'] - params['min_level'] + 1
+    num_classes = 90
+    image = get_test_image(image_size)
+    class_outs, box_outs = detector(image)
+    class_outs_expected = tf.zeros((1, 49104, 90), dtype=tf.float32)
+    box_outs_expected = tf.zeros((1, 49104, 4), dtype=tf.float32)
+    class_outs, box_outs = merge_class_box_level_outputs(class_outs,
+                                                         box_outs,
+                                                         num_levels,
+                                                         num_classes)
+    assert class_outs_expected.shape == class_outs.shape, \
+        'Merged class outputs not matching'
+    assert box_outs_expected.shape == box_outs.shape,\
+        'Merged box outputs not matching'
 
 
 test_efficientdet_model()
 test_efficientdet_default_params()
+test_efficientdet_merge_outputs()
