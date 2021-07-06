d4fce4574fcdc68a7cb624e3cbaea15ee5c7348f
proneetsharma <sproneet@gmail.com>
2021-07-06 10:41:40 +0200

    Refactor code with internal changes

--- examples/eigenfaces/demo_pipeline.py
+++ examples/eigenfaces/demo_pipeline.py
@@ -1,17 +1,18 @@
 import numpy as np
-import processors as pe
 from paz import processors as pr
+from paz.backend.image import lincolor
 from paz.pipelines import HaarCascadeFrontalFace
 from paz.abstract import SequentialProcessor, Processor
+from pipelines import CalculateFaceWeights
 
 
 class DetectEigenFaces(Processor):
-    def __init__(self, weights_database, parameters, project,
+    def __init__(self, weights, norm_order, thresh, eigenfaces,
                  mean_face, offsets=[0, 0]):
         super(DetectEigenFaces, self).__init__()
         self.offsets = offsets
-        self.colors = parameters['colors']
-        self.class_names = parameters['class_names']
+        self.class_names = list(weights.keys()) + ['Face not found']
+        self.colors = lincolor(len(self.class_names))
         self.croped_images = None
         # detection
         self.detect = HaarCascadeFrontalFace()
@@ -20,10 +21,11 @@ class DetectEigenFaces(Processor):
         self.square.add(pr.OffsetBoxes2D(offsets))
         self.clip = pr.ClipBoxes2D()
         self.crop = pr.CropBoxes2D()
-        self.face_detector = EigenFaceDetector(weights_database, parameters,
-                                               project, mean_face)
+        self.face_detector = EigenFaceDetector(weights, norm_order, thresh,
+                                               eigenfaces, mean_face)
         # drawing and wrapping
-        self.draw = pr.DrawBoxes2D(self.class_names, self.colors, True)
+        self.draw = pr.DrawBoxes2D(self.class_names, self.colors,
+                                   weighted=True, with_score=False)
         self.wrap = pr.WrapOutput(['image', 'boxes2D'])
 
     def call(self, image):
@@ -32,39 +34,34 @@ class DetectEigenFaces(Processor):
         boxes2D = self.clip(image, boxes2D)
         self.cropped_images = self.crop(image, boxes2D)
         for cropped_image, box2D in zip(self.cropped_images, boxes2D):
-            self.face_detector.store_each_frame(cropped_image)
-            self.face_detector()
-            box2D.class_name = self.face_detector()
-            # box2D.score = np.amax(predictions['scores'])
+            box2D.class_name = self.face_detector(cropped_image)
         image = self.draw(image, boxes2D)
         return self.wrap(image, boxes2D)
 
 
 class EigenFaceDetector(Processor):
-    def __init__(self, weights_data_base, parameters, project, mean_face):
+    def __init__(self, weights_data_base, norm_order, thresh,
+                 eigenfaces, mean_face):
         self.weights_data_base = weights_data_base
-        self.calculate_weights = CalculateTestFaceWeights(project, mean_face)
-        self.query = QueryFace(parameters)
+        self.calculate_weights = CalculateFaceWeights(eigenfaces, mean_face,
+                                                      with_crop=False)
+        self.query = QueryFace(norm_order, thresh)
         super(EigenFaceDetector, self).__init__()
 
-    def store_each_frame(self, test_data):
-        self.test_data = test_data
-
-    def call(self):
-        test_weight = self.calculate_weights(self.test_data)
+    def call(self, image):
+        test_weight = self.calculate_weights(image)
         similar_face = self.query(test_weight, self.weights_data_base)
         return similar_face
 
 
 class QueryFace(Processor):
-    def __init__(self, parameters):
+    def __init__(self, norm_order, thresh):
         self.norm = np.linalg.norm
-        self.norm_order = parameters['norm_order']
-        self.threshold = parameters['threshold']
+        self.norm_order = norm_order
+        self.threshold = thresh
         super(QueryFace).__init__()
 
     def call(self, test_face_weight, database):
-        # you could also get a none
         self.database = database
         weights_difference = []
         for sample in database:
@@ -73,30 +70,8 @@ class QueryFace(Processor):
                                     ord=self.norm_order, axis=1)
             weights_difference.append(np.min(weight_norm))
 
-        if np.min(weights_difference) < self.threshold:
-            return None
+        if np.min(weights_difference) > self.threshold:
+            return 'Face not found'
         else:
             most_similar_face_arg = np.argmin(weights_difference)
         return list(database.keys())[most_similar_face_arg]
-
-
-class CalculateTestFaceWeights(pr.Processor):
-    def __init__(self, project, mean_face, shape=(48, 48)):
-        super(CalculateTestFaceWeights, self).__init__()
-        self.project = project
-        self.mean_face = mean_face
-        self.convert_to_gray = pr.ConvertColorSpace(pr.RGB2GRAY)
-        self.preprocess = pr.SequentialProcessor()
-        self.preprocess.add(pr.ResizeImage(shape))
-        self.preprocess.add(pr.ExpandDims(-1))
-        self.subtract = pe.SubtractMeanFace()
-
-    def call(self, face):
-        if len(face.shape) != 3:
-            raise ValueError('input should have shape [H, W, num_channels]')
-        if face.shape[-1] == 3:
-            face = self.convert_to_gray(face)
-        face = self.preprocess(face)
-        face = self.subtract(face, self.mean_face)
-        weights = self.project(face)
-        return weights
