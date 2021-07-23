204e4fb46388e0080b461d9f6efe3e93b36f2eea
Kashmira Shinde <51785966+shin-ka@users.noreply.github.com>
2021-07-23 11:44:42 +0200

    Add files via upload

---
+++ examples/human_pose_2d/lib/model/__init__.py
@@ -0,0 +1,11 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) Microsoft
+# Licensed under the MIT License.
+# Written by Bin Xiao (Bin.Xiao@microsoft.com)
+# ------------------------------------------------------------------------------
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import model.model_higher_hrnet
