Fix rotation

examples/pix2pose/coloring.py
@@ -93,7 +93,13 @@ def calculate_canonical_pose_cylinder(mesh):
 
     angle_backrotation = np.arctan2(rotation_matrix[0, 2]-rotation_matrix[2, 0], rotation_matrix[0, 0]+rotation_matrix[2, 2])
     angle_backrotation = np.round(angle_backrotation, 4)
-    quaternion = quaternion_multiply(np.array([0, -np.sin(angle_backrotation / 2), 0, np.cos(angle_backrotation / 2)]), mesh.rotation)
+    rotation_matrix_around_y = np.array([[np.cos(angle_backrotation), 0, np.sin(angle_backrotation)],
+                                         [0, 1, 0],
+                                         [-np.sin(angle_backrotation), 0, np.cos(angle_backrotation)]])
+    rotation_matrix_around_y = np.linalg.inv(rotation_matrix_around_y)
+    quaternion_rotation_around_y = trimesh.transformations.quaternion_from_matrix(rotation_matrix_around_y)
+    quaternion_rotation_around_y = quaternion_rotation_around_y[[1, 2, 3, 0]]
+    quaternion = quaternion_multiply(mesh.rotation, quaternion_rotation_around_y)
     mesh.rotation = quaternion
 
 
@@ -133,17 +139,28 @@ if __name__ == "__main__":
     scene.set_pose(camera, camera_to_world)
     scene.set_pose(light, camera_to_world)
 
-    #mesh = trimesh.load("/home/fabian/.keras/datasets/custom_objects/symmetry_z_2_object.obj")
-    mesh, mesh_rotated = color_object("/home/fabian/.keras/datasets/tless_obj/obj_000005.obj")
-
+    mesh, _ = color_object("/home/fabian/.keras/datasets/tless_obj/obj_000014.obj")
     mesh = scene.add(Mesh.from_trimesh(mesh, smooth=False))
-    #mesh_rotated = scene.add(Mesh.from_trimesh(mesh_rotated, smooth=False))
 
-    angle = -np.pi
-    mesh.rotation = np.array([0, np.sin(angle/2), 0, np.cos(angle/2)])
-    #angle = -np.pi/4 + np.pi
-    #mesh_rotated.rotation = np.array([0, np.sin(angle/2), 0, np.cos(angle/2)])
-    calculate_canonical_pose_two_symmetries(mesh)
+    angle = np.pi*1.4
+    #rotation_matrix_around_y = np.array([[np.cos(angle), 0, np.sin(angle)], [0, 1, 0], [-np.sin(angle), 0, np.cos(angle)]])
+    #rotation = trimesh.transformations.quaternion_from_matrix(rotation_matrix_around_y)
+    #print("Quaternion: {}".format(rotation))
+    #rotation = rotation[[1, 2, 3, 0]]
+    #rotation = np.array([0, np.random.random()*2, 0, np.cos(angle/2)])
+    #rotation = rotation/np.linalg.norm(rotation)
+    #mesh.rotation = rotation
+    random_quaternion = trimesh.transformations.random_quaternion()
+    random_quaternion = random_quaternion[[1, 2, 3, 0]]
+    mesh.rotation = random_quaternion
+
+    renderer = OffscreenRenderer(size[0], size[1])
+    image_original, depth_original = renderer.render(scene, flags=RenderFlags.FLAT)
+
+    plt.imshow(image_original)
+    plt.show()
+
+    calculate_canonical_pose_cylinder(mesh)
     print("Mesh rotation: {}".format(mesh.rotation))
 
     renderer = OffscreenRenderer(size[0], size[1])

examples/pix2pose/model.py
@@ -32,33 +32,51 @@ def transformer_loss(real_depth_image, predicted_depth_image):
     return K.mean(K.square(predicted_depth_image - real_depth_image), axis=-1)
 
 
+def rotate_image(image, rotation_matrix):
+    mask_image = (np.sum(image, axis=-1) != 0).astype(float)
+    mask_image = np.repeat(mask_image[..., np.newaxis], 3, axis=-1)
+
+    image_colors = (image * 2) - 1
+
+    image_colors_rotated = image_colors + np.ones_like(image_colors) * 0.0001
+    image_colors_rotated = np.einsum('ij,klj->kli', rotation_matrix, image_colors_rotated)
+    image_colors_rotated = (image_colors_rotated + 1) / 2
+    image_colors_rotated = np.clip(image_colors_rotated, a_min=0.0, a_max=1.0)
+    image_colors_rotated *= mask_image
+
+    return image_colors_rotated
+
+
 def loss_color_wrapped(rotation_matrices):
     def loss_color_unwrapped(color_image, predicted_color_image):
         min_loss = tf.float32.max
+
+        # Bring the image in the range between 0 and 1
+        color_image = (color_image + 1) * 0.5
+
+        # Calculate masks for the object and the background (they are independent of the rotation)
+        mask_object = tf.repeat(tf.expand_dims(tf.math.reduce_max(tf.math.ceil(color_image), axis=-1), axis=-1),
+                                repeats=3, axis=-1)
+        mask_background = tf.ones(tf.shape(mask_object)) - mask_object
+
+        # Bring the image again in the range between -1 and 1
+        color_image = (color_image * 2) - 1
 
         # Iterate over all possible rotations
         for rotation_matrix in rotation_matrices:
 
             real_color_image = tf.identity(color_image)
 
-            # Bring the image in the range between 0 and 1
-            real_color_image = (real_color_image+1)*0.5
-
-            # Calculate masks for the object and the background (they are independent of the rotation)
-            mask_object = tf.repeat(tf.expand_dims(tf.math.reduce_max(tf.math.ceil(real_color_image), axis=-1), axis=-1), repeats=3, axis=-1)
-            mask_background = tf.ones(tf.shape(mask_object)) - mask_object
-
             # Add a small epsilon value to avoid the discontinuity problem
             real_color_image = real_color_image + tf.ones_like(real_color_image) * 0.0001
 
             # Rotate the object
             real_color_image = tf.einsum('ij,mklj->mkli', tf.convert_to_tensor(np.array(rotation_matrix), dtype=tf.float32), real_color_image)
-            real_color_image = tf.where(tf.math.less(real_color_image, 0), tf.ones_like(real_color_image) + real_color_image, real_color_image)
+            #real_color_image = tf.where(tf.math.less(real_color_image, 0), tf.ones_like(real_color_image) + real_color_image, real_color_image)
 
-            real_color_image = real_color_image*mask_object
-
-            # Bring the image again in the range between -1 and 1
-            real_color_image = (real_color_image*2)-1
+            # Set the background to be all -1
+            real_color_image *= mask_object
+            real_color_image += (mask_background*tf.constant(-1.))
 
             # Get the number of pixels
             num_pixels = tf.math.reduce_prod(tf.shape(real_color_image)[1:3])
@@ -107,12 +125,12 @@ class PlotImagesCallback(Callback):
         self.processor = processor
 
     def on_epoch_end(self, epoch_index, logs=None):
-        renderer = SingleView(self.obj_path, (self.image_size, self.image_size),
-                              self.y_fov, self.depth, self.light, bool(self.top_only),
-                              self.roll, self.shift)
+        #renderer = SingleView(self.obj_path, (self.image_size, self.image_size),
+        #                      self.y_fov, self.depth, self.light, bool(self.top_only),
+        #                      self.roll, self.shift)
 
         # creating sequencer
-        image_paths = glob.glob(os.path.join(self.images_directory, '*.jpg'))
+        #image_paths = glob.glob(os.path.join(self.images_directory, '*.jpg'))
         #processor = DepthImageGenerator(renderer, self.image_size, image_paths, num_occlusions=0)
         sequence = GeneratingSequencePix2Pose(self.processor, self.model, self.batch_size, self.steps_per_epoch * 2, rotation_matrices=self.rotation_matrices)
 
@@ -154,7 +172,7 @@ class PlotImagesCallback(Callback):
 
         plt.tight_layout()
         plt.show()
-        plt.savefig(os.path.join(self.save_path, "images/plot-epoch-{}.png".format(epoch_index)))
+        #plt.savefig(os.path.join(self.save_path, "images/plot-epoch-{}.png".format(epoch_index)))
 
         if self.neptune_logging:
             neptune.log_image('plot', fig, image_name="epoch_{}.png".format(epoch_index))
@@ -176,7 +194,7 @@ class NeptuneLogger(Callback):
 
         if epoch%self.log_interval == 0:
             self.model.save(os.path.join(self.save_path, 'pix2pose_dcgan_{}.h5'.format(epoch)))
-            neptune.log_artifact('pix2pose_dcgan_{}.h5'.format(epoch))
+            #neptune.log_artifact('pix2pose_dcgan_{}.h5'.format(epoch))
 
 
 def Generator():

examples/pix2pose/rotation.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from copy import deepcopy
 from paz.backend.quaternion import quarternion_to_rotation_matrix
 
 import tensorflow as tf
@@ -58,9 +59,23 @@ def loss_color(real_color_image, predicted_color_image, rotation_matrix):
 
     return loss_colors
 
+
+def generate_rotation_matrices_continuous_symmetry(num_matrices=10):
+    angles = np.linspace(0, np.pi*2, num_matrices)
+    rotation_matrices = list()
+
+    for angle in angles:
+        rotation_matrix = np.array([[np.cos(angle), 0, np.sin(angle)],
+                                    [0, 1, 0],
+                                    [-np.sin(angle), 0, np.cos(angle)]])
+        rotation_matrices.append(rotation_matrix)
+
+    return rotation_matrices
+
+
 num_samples = 5
 list_images = list()
-view = SingleView(filepath="/home/fabian/.keras/datasets/custom_objects/simple_symmetry_object.obj")
+view = SingleView(filepath="/home/fabian/.keras/datasets/tless_obj/obj_000014.obj")
 
 image_original, image_colors, alpha_original = view.render()
 image_colors = image_colors.copy()
@@ -70,26 +85,42 @@ image_colors = image_colors/255.
 epsilon = 0.0001
 #rotation_matrix = np.array([[-1, 0, 0], [0, 1, 0], [0, 0, -1]])
 #rotation_matrix = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
-rotation_matrices = np.load("./rotation_matrices/simple_symmetry_object_two_matrices.npy")
+rotation_matrices = np.load("./rotation_matrices/2_fold_symmetry_rotation_matrices.npy")
 
 image_colors_copy = np.expand_dims(image_colors, axis=0)
-#loss_color(tf.convert_to_tensor(image_colors_copy/255., dtype=tf.float32), tf.convert_to_tensor(image_colors_copy/255., dtype=tf.float32), tf.convert_to_tensor(rotation_matrices[0], dtype=tf.float32))
 
 # Applying the rotation to the image (easy way)
-plt.imshow(image_colors)
-plt.show()
+num_matrices = 10
+images_rotated = list()
+rotation_matrices = generate_rotation_matrices_continuous_symmetry(num_matrices)
 mask_image = (np.sum(image_colors, axis=-1) != 0).astype(float)
-mask_image = np.repeat(mask_image[...,np.newaxis], 3, axis=-1)
-image_colors_rotated = image_colors + np.ones_like(image_colors)*0.0001
-image_colors_rotated = np.einsum('ij,klj->kli', rotation_matrices[0], image_colors_rotated)
-image_colors_rotated = np.where(np.less(image_colors_rotated, 0), 1 * np.ones_like(image_colors_rotated) + image_colors_rotated, image_colors_rotated)
-image_colors_rotated = np.clip(image_colors_rotated, a_min=0.0, a_max=1.0)
-image_colors_rotated = image_colors_rotated*mask_image
+mask_image = np.repeat(mask_image[..., np.newaxis], 3, axis=-1)
 
-plt.imshow(mask_image)
-plt.show()
+image_colors = (image_colors*2)-1
 
-plt.imshow(image_colors_rotated)
+print(rotation_matrices)
+for rotation_matrix in rotation_matrices:
+    image_colors_rotated = image_colors + np.ones_like(image_colors)*0.0001
+    image_colors_rotated = np.einsum('ij,klj->kli', rotation_matrix, image_colors_rotated)
+    #images_rotated.append(image_colors_rotated[:, :, 0])
+    #image_colors_rotated = np.where(np.less(image_colors_rotated, 0), 1 * np.ones_like(image_colors_rotated) + image_colors_rotated, image_colors_rotated)
+    #image_colors_rotated = np.abs(image_colors_rotated)
+    image_colors_rotated = (image_colors_rotated + 1) / 2
+    image_colors_rotated = np.clip(image_colors_rotated, a_min=0.0, a_max=1.0)
+    image_colors_rotated *= mask_image
+    images_rotated.append(image_colors_rotated)
+
+    #image_colors = deepcopy(image_colors_rotated)
+
+f, axs = plt.subplots(2, 5)
+#f, axs = plt.subplots(1, 2)
+axs = axs.flatten()
+for i, ax in enumerate(axs):
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    ax.imshow(images_rotated[i])
+
 plt.show()
 
 # Applying the rotation to the image (complicated way)

examples/pix2pose/scenes.py
@@ -66,7 +66,7 @@ class SingleView():
         roll: Float, to sample [-roll, roll] rolls of the Z OpenGL camera axis.
         shift: Float, to sample [-shift, shift] to move in X, Y OpenGL axes.
     """
-    def __init__(self, filepath, filepath_half_object, viewport_size=(128, 128), y_fov=3.14159 / 4.0,
+    def __init__(self, filepath, filepath_half_object="", viewport_size=(128, 128), y_fov=3.14159 / 4.0,
                  distance=[0.3, 0.5], light_bounds=[0.5, 30], top_only=False,
                  roll=None, shift=None):
         self.distance, self.roll, self.shift = distance, roll, shift
@@ -146,6 +146,7 @@ class SingleView():
         #camera_translation_z = 0.69
         # Random rotation and translation for the object
         rotation = trimesh.transformations.random_quaternion()
+        #rotation = np.array([0, 0, 0, 1])
 
         translation = get_random_translation(0.05)
 

paz/abstract/sequence.py
@@ -129,21 +129,18 @@ class GeneratingSequencePix2Pose(SequenceExtra):
         return self.num_steps
 
     def rotate_image(self, image, rotation_matrix):
-        # Bring the image in the range between 0 and 1
-        image = (image + 1) * 0.5
-
         mask_image = (np.sum(image, axis=-1) != 0).astype(float)
         mask_image = np.repeat(mask_image[..., np.newaxis], 3, axis=-1)
-        image_colors_rotated = image + np.ones_like(image) * 0.0001
-        image_colors_rotated = np.einsum('ij,klj->kli', rotation_matrix, image_colors_rotated)
-        image_colors_rotated = np.where(np.less(image_colors_rotated, 0),
-                                        np.ones_like(image_colors_rotated) + image_colors_rotated, image_colors_rotated)
-        image_colors_rotated = np.clip(image_colors_rotated, a_min=0.0, a_max=1.0)
-        image_colors_rotated = image_colors_rotated * mask_image
+        mask_background = np.ones_like(mask_image) - mask_image
 
-        # Bring the image again in the range between -1 and 1
-        image_colors_rotated = (image_colors_rotated * 2) - 1
-        return image_colors_rotated
+        image = image + np.ones_like(image) * 0.0001
+
+        # Rotate the object
+        image_rotated = np.einsum('ij,klj->kli', rotation_matrix, image)
+        image_rotated *= mask_image
+        image_rotated += (mask_background * -1.)
+
+        return image_rotated
 
     def process_batch(self, inputs, labels, batch_index):
         start = time.time()