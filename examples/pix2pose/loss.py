524ecfc5e8da28d8c2ee0f532756e6fa9735c406
Octavio Arriaga <arriaga.camargo@gmail.com>
2021-10-21 08:55:54 +0200

    Fix bug with predict weighted foreground loss

--- examples/pix2pose/loss.py
+++ examples/pix2pose/loss.py
@@ -14,13 +14,23 @@ class WeightedForeground(Loss):
 
     def call(self, RGBA_mask_true, RGB_mask_pred):
         RGB_mask_true, alpha_mask = self._extract_alpha_mask(RGBA_mask_true)
+
         foreground_true = RGB_mask_true * alpha_mask
         foreground_pred = RGB_mask_pred * alpha_mask
-        background_true = RGB_mask_true * (1.0 - alpha_mask)
-        background_pred = RGB_mask_true * (1.0 - alpha_mask)
         foreground_loss = tf.abs(foreground_true - foreground_pred)
+
+        background_true = RGB_mask_true * (1.0 - alpha_mask)
+        background_pred = RGB_mask_pred * (1.0 - alpha_mask)
         background_loss = tf.abs(background_true - background_pred)
+
         loss = (self.beta * foreground_loss) + background_loss
+
         loss = tf.reduce_mean(loss, axis=[1, 2, 3])
-        loss = tf.math.minimum(loss, tf.float32.max)
+        # loss = tf.math.minimum(loss, tf.float32.max)
+        # loss = tf.losses.mean_squared_error(RGB_mask_true, RGB_mask_pred)
         return loss
+
+
+def MSE_with_alpha_channel(y_true, y_pred):
+    squared_difference = tf.square(y_true[:, :, :, 0:3] - y_pred)
+    return tf.reduce_mean(squared_difference, axis=-1)  # Note the `axis=-1`
