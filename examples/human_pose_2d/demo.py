ef1a90fe2ba1a4497c5807a1e4997a9ce85de559
proneetsharma <sproneet@gmail.com>
2021-11-09 17:31:20 +0100

    Add documentations

--- examples/human_pose_2d/demo.py
+++ examples/human_pose_2d/demo.py
@@ -1,26 +1,28 @@
 import os
 import argparse
-import processors as pe
 from pipelines import DetectHumanPose2D
 from tensorflow.keras.models import load_model
 from paz.backend.image import write_image, load_image
 from dataset import JOINT_CONFIG, FLIP_CONFIG
 
 
+# check for numpy implementation of Munkers
+# images directory name should be consistant with the readme
 parser = argparse.ArgumentParser(description='Test keypoints network')
-parser.add_argument('-i', '--image_path', default='image',
+parser.add_argument('-i', '--image_path', default='images',
                     help='Path to the image')
 parser.add_argument('-m', '--model_weights_path', default='models_weights_tf',
                     help='Path to the model weights')
 args = parser.parse_args()
 
-image_path = os.path.join(args.image_path, 'image3.jpg')
+image_path = os.path.join(args.image_path, 'image1.jpg')
 model_path = os.path.join(args.model_weights_path, 'HigherHRNet')
 model = load_model(model_path)
 print("\n==> Model loaded!\n")
 image = load_image(image_path)
 
 dataset = 'COCO'
+# data_with_center - needed for 3D pose estimation
 data_with_center = False
 if data_with_center:
     joint_order = JOINT_CONFIG[dataset + '_WITH_CENTER']
@@ -31,9 +33,6 @@ else:
 
 
 detect = DetectHumanPose2D(model, joint_order, fliped_joint_order,
-                           data_with_center)
-draw_skeleton = pe.DrawSkeleton(dataset)
-
-joints, scores = detect(image)
-image = draw_skeleton(image, joints)
-write_image('output/result.jpg', image)
+                           dataset, data_with_center)
+output = detect(image)
+write_image('output/result.jpg', output['image'])
--- examples/human_pose_2d/pipelines.py
+++ examples/human_pose_2d/pipelines.py
@@ -4,46 +4,99 @@ import processors as pe
 
 
 class DetectHumanPose2D(pr.Processor):
-    def __init__(self, model, joint_order, fliped_joint_order,
-                 data_with_center, max_num_people=30, with_flip=True):
+    """Detectect human jonts in a image and draw a skeleton over the image.
+    # Arguments
+        model: Modle weights trained on HigherHRNet model.
+        joint_order: List of length 17 (number of joints).
+            where the joints are listed order wise.
+        fliped_joint_order: List of length 17 (number of joints).
+            Flipped list of joint order.
+        dataset: String. Name of the dataset used for training the model.
+        data_with_center: Boolean. True is the model is trained using the
+            center.
+        image: Numpy array. Input image
+
+    # Returns
+        dictonary with the following keys:
+            image: contains the image with skeleton drawn on it.
+            joints: location of joints
+            score: score of detection
+    """
+    def __init__(self, model, joint_order, fliped_joint_order, dataset,
+                 data_with_center, max_num_people=30, with_flip=True,
+                 draw=True):
         super(DetectHumanPose2D, self).__init__()
         self.with_flip = with_flip
+        self.draw = draw
         self.preprocess_image = PreprocessImage()
-        self.get_output = pr.SequentialProcessor(
-            [GetMultiStageOutputs(model, fliped_joint_order, data_with_center),
-             AggregateResults(with_flip=self.with_flip)])
+        self.predict_multi_stage_output = pr.SequentialProcessor(
+            [GetMultiStageOutputs(model, fliped_joint_order, data_with_center,
+             with_flip), AggregateResults(with_flip=self.with_flip)])
         self.heatmaps_parser = HeatmapsParser(max_num_people, joint_order)
         self.transform_joints = TransformJoints()
+        self.draw_skeleton = pe.DrawSkeleton(dataset)
+        self.wrap = pr.WrapOutput(['image', 'joints', 'scores'])
 
     def call(self, image):
         resized_image, center, scale = self.preprocess_image(image)
-        heatmaps, tags = self.get_output(resized_image, self.with_flip)
+        heatmaps, tags = self.predict_multi_stage_output(resized_image)
         grouped_joints, scores = self.heatmaps_parser(heatmaps, tags)
         joints = self.transform_joints(grouped_joints, center, scale, heatmaps)
-        return joints, scores
+        if self.draw:
+            image = self.draw_skeleton(image, joints)
+        return self.wrap(image, joints, scores)
 
 
 class PreprocessImage(pr.Processor):
-    def __init__(self, min_scale=1, input_size=512, inverse=False):
+    """Preprocess the image accourding to the model input requirement.
+    # Arguments
+        scaling_factor: Int. scale factor for image dimensions.
+        input_size: Int. resize the first dimension of image to input size.
+        inverse: Boolean. Reverse the affine transform input.
+        image: Numpy array. Input image
+
+    # Returns
+        image: resized and transformed image
+        center: center of the image
+        scale: scaled image dimensions
+    """
+    def __init__(self, scaling_factor=200, input_size=512, inverse=False):
         super(PreprocessImage, self).__init__()
         self.get_image_center = pe.GetImageCenter()
-        self.get_multi_scale_size = pe.GetMultiScaleSize(min_scale, input_size)
+        self.resize_dims = pe.ResizeDims(input_size)
+        self.scale_dims = pe.ScaleDims(scaling_factor)
         self.get_affine_transform = pe.GetAffineTransform(inverse)
         self.transform_image = pr.SequentialProcessor(
             [pe.WarpAffine(), pe.ImagenetPreprocessInput(), pr.ExpandDims(0)])
 
     def call(self, image):
         center = self.get_image_center(image)
-        scale, size = self.get_multi_scale_size(image)
+        size = self.resize_dims(image)
+        scale = self.scale_dims(image, size)
         transform = self.get_affine_transform(center, scale, size)
         image = self.transform_image(image, transform, size)
         return image, center, scale
 
 
 class GetMultiStageOutputs(pr.Processor):
-    def __init__(self, model, fliped_joint_order, data_with_center,
+    """Get Heatmaps and Tags from the model output.
+    # Arguments
+        model: Modle weights trained on HigherHRNet model.
+        fliped_joint_order: List of length 17 (number of joints).
+            Flipped list of joint order.
+        data_with_center: Boolean. True is the model is trained using the
+            center.
+        image: Numpy array. Input image of shape (H, W)
+
+    # Returns
+        heatmaps: Numpy array of shape (1, H/2, W/2, num_joints)
+        Tgas: Numpy array of shape (1, H/2, W/2, num_joints)
+    """
+    def __init__(self, model, fliped_joint_order, data_with_center, with_flip,
                  tag_per_joint=True, project2image=True):
         super(GetMultiStageOutputs, self).__init__()
+        self.with_flip = with_flip
+        self.heatmaps, self.tags = [], []
         self.predict = pr.SequentialProcessor(
             [pr.Predict(model), pe.ScaleOutput(2)])
         self.get_heatmaps = pr.SequentialProcessor(
@@ -55,20 +108,32 @@ class GetMultiStageOutputs(pr.Processor):
         if project2image:
             self.postprocess.add(pe.ScaleImage(2))
 
-    def call(self, image, with_flip):
+    # move with flip to __init__.
+    def call(self, image):
         outputs = self.predict(image)
-        heatmaps = self.get_heatmaps(outputs, heatmaps=[], with_flip=False)
-        tags = self.get_tags(outputs, tags=[], with_flip=False)
-        if with_flip:
+        heatmaps = self.get_heatmaps(outputs, self.heatmaps, with_flip=False)
+        tags = self.get_tags(outputs, self.tags, with_flip=False)
+        if self.with_flip:
             outputs = self.predict(np.flip(image, [2]))
-            heatmaps = self.get_heatmaps(outputs, heatmaps, with_flip)
-            tags = self.get_tags(outputs, tags, with_flip)
+            heatmaps = self.get_heatmaps(outputs, heatmaps, self.with_flip)
+            tags = self.get_tags(outputs, tags, self.with_flip)
         heatmaps = self.postprocess(heatmaps)
         tags = self.postprocess(tags)
         return heatmaps, tags
 
 
 class AggregateResults(pr.Processor):
+    """Aggregate heatmaps and tags to get final heatmaps and tags for
+       processing.
+    # Arguments
+        heatmaps: Numpy array of shape (1, H/2, W/2, num_joints)
+        Tgas: Numpy array of shape (1, H/2, W/2, num_joints)
+
+    # Returns
+        heatmaps: Numpy array of shape (1, H/2, W/2, num_joints, 1)
+        Tgas: Numpy array of shape (1, H/2, W/2, num_joints, 1)
+    """
+
     def __init__(self, project2image=True, with_flip=False):
         super(AggregateResults, self).__init__()
         self.aggregate_tags = pr.SequentialProcessor(
@@ -84,6 +149,19 @@ class AggregateResults(pr.Processor):
 
 
 class HeatmapsParser(pr.Processor):
+    """Extract out the top k heatmaps and group the joints with their
+       respective tags value. Adjust and refine the joint locations by
+       removing the margins.
+    # Arguments
+        max_num_people: Int. Maximum number of person to be detected.
+        joint_order: List of length 17 (number of joints).
+        heatmaps: Numpy array of shape (1, H/2, W/2, num_joints)
+        Tgas: Numpy array of shape (1, H/2, W/2, num_joints)
+
+    # Returns
+        grouped_joints: numpy array. joints grouped by tag
+        scores: int: score for the joint
+    """
     def __init__(self, max_num_people, joint_order, detection_thresh=0.2,
                  tag_thresh=1, tag_per_joint=True):
         super(HeatmapsParser, self).__init__()
@@ -108,6 +186,17 @@ class HeatmapsParser(pr.Processor):
 
 
 class TransformJoints(pr.Processor):
+    """inverse the affine transform to get the joint location with respect to
+       the input image.
+    # Arguments
+        grouped_joints: Numpy array. joints grouped by tag
+        center: Tuple. center of the imput image
+        scale: Float. scaled imput image dimension
+        heatmaps: Numpy array of shape (1, H/2, W/2, num_joints)
+
+    # Returns
+        transformed_joints: joint location with respect to the input image
+    """
     def __init__(self):
         super(TransformJoints, self).__init__()
         self.transformed_joints = []
