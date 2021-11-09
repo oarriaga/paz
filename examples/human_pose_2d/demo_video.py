7fefe9a414dad004d2cc6a6cbf12b191c23b3c52
proneetsharma <sproneet@gmail.com>
2021-11-09 17:31:57 +0100

    Change video pipeline according to paz

--- examples/human_pose_2d/demo_video.py
+++ examples/human_pose_2d/demo_video.py
@@ -1,28 +1,22 @@
 import os
 import argparse
-import numpy as np
-import cv2
-import processors as pe
 from pipelines import DetectHumanPose2D
+from paz.backend.camera import VideoPlayer
+from paz.backend.camera import Camera
 from tensorflow.keras.models import load_model
 from dataset import JOINT_CONFIG, FLIP_CONFIG
 
 
 parser = argparse.ArgumentParser(description='Test keypoints network')
-parser.add_argument('-i', '--image_path', default='image',
-                    help='Path to the image')
+parser.add_argument('-c', '--camera_id', type=int, default=0,
+                    help='Camera device ID')
 parser.add_argument('-m', '--model_weights_path', default='models_weights_tf',
                     help='Path to the model weights')
 args = parser.parse_args()
 
-image_path = os.path.join(args.image_path, 'image4.jpg')
 model_path = os.path.join(args.model_weights_path, 'HigherHRNet')
 model = load_model(model_path)
 print("\n==> Model loaded!\n")
-
-data_info = {'data': 'COCO',
-             'data_with_center': False}
-
 
 dataset = 'COCO'
 data_with_center = False
@@ -33,25 +27,9 @@ else:
     joint_order = JOINT_CONFIG[dataset]
     fliped_joint_order = FLIP_CONFIG[dataset]
 
-detect = DetectHumanPose2D(model, joint_order, fliped_joint_order,
-                           data_with_center)
-draw_skeleton = pe.DrawSkeleton(dataset)
-video_capture = cv2.VideoCapture(0)
 
-while True:
-    final_heatmaps = None
-    ret, image = video_capture.read()
-    image = np.array(image).astype(np.uint8)
-
-    joints, scores = detect(image)
-    image = draw_skeleton(image, joints)
-    cv2.imshow('Video', image)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-video_capture.release()
-cv2.destroyAllWindows()
-
-
-# replace it with object_detection's demo.py
+pipeline = DetectHumanPose2D(model, joint_order, fliped_joint_order,
+                             dataset, data_with_center)
+camera = Camera(args.camera_id)
+player = VideoPlayer((640, 480), pipeline, camera)
+player.run()
