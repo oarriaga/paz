74c4888970b5dfb362ef664c2c29d237f3acc168
Octavio Arriaga <arriaga.camargo@gmail.com>
2021-11-17 11:22:58 +0100

    Change demo for image processing

--- examples/pix2pose/demo.py
+++ examples/pix2pose/demo.py
@@ -15,10 +15,10 @@ model = UNET_VGG16(num_classes, image_shape, freeze_backbone=True)
 model.load_weights('UNET_weights_epochs-10_beta-3.hdf5')
 
 # approximating intrinsic camera parameters
-camera = Camera(device_id=4)
-# camera.start()
-# image_size = camera.read().shape[0:2]
-# camera.stop()
+camera = Camera(device_id=0)
+camera.start()
+image_size = camera.read().shape[0:2]
+camera.stop()
 
 image = load_image('test_image.jpg')
 image_size = image.shape[0:2]
@@ -30,8 +30,9 @@ camera.intrinsics = np.array([[focal_length, 0, image_center[0]],
                               [0, 0, 1]])
 object_sizes = np.array([0.184, 0.187, 0.052])
 epsilon = 0.15
-detect = SSD300FAT(draw=False)
-offsets = [0.1, 0.1]
+score_thresh = 0.50
+detect = SSD300FAT(score_thresh, draw=False)
+offsets = [0.2, 0.2]
 estimate_keypoints = Pix2Pose(model, object_sizes)
 pipeline = EstimatePoseMasks(detect, estimate_keypoints, camera, offsets, None)
 
@@ -39,8 +40,6 @@ results = pipeline(image)
 predicted_image = results['image']
 show_image(predicted_image)
 
-"""
-image_size = (640, 480)
-player = VideoPlayer(image_size, pipeline, camera)
-player.run()
-"""
+# image_size = (640, 480)
+# player = VideoPlayer(image_size, pipeline, camera)
+# player.run()
