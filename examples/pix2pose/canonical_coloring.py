86bec271787a6af0a229435a2e78a42386d50be1
Octavio Arriaga <arriaga.camargo@gmail.com>
2021-11-25 19:58:45 +0100

    Add training script for canonical pose estimation

--- examples/pix2pose/canonical_coloring.py
+++ examples/pix2pose/canonical_coloring.py
@@ -193,8 +193,8 @@ class CanonicalScene():
             RGB_masks.append(RGB_mask[..., 0:3])
         images = np.concatenate(images, axis=1)
         RGB_masks = np.concatenate(RGB_masks, axis=1)
-        print(images.shape)
-        print(RGB_masks.shape)
+        # print(images.shape)
+        # print(RGB_masks.shape)
         images = np.concatenate([images, RGB_masks], axis=0)
         return images
 
@@ -208,7 +208,7 @@ def compute_norm_SO3(rotation_mesh, rotation):
 def calculate_canonical_rotation(rotation_mesh, rotations):
     norms = [compute_norm_SO3(rotation_mesh, R) for R in rotations]
     closest_rotation_arg = np.argmin(norms)
-    print(closest_rotation_arg)
+    # print(closest_rotation_arg)
     closest_rotation = rotations[closest_rotation_arg]
     canonical_rotation = np.linalg.inv(closest_rotation)
     return canonical_rotation
@@ -219,7 +219,7 @@ if __name__ == "__main__":
     from paz.backend.image import show_image
     from backend import build_rotation_matrix_z
     from backend import build_rotation_matrix_x
-    from backend import build_rotation_matrix_y
+    # from backend import build_rotation_matrix_y
     path_OBJ = 'single_solar_panel_02.obj'
     root_path = os.path.expanduser('~')
     path_OBJ = os.path.join(root_path, path_OBJ)
@@ -233,7 +233,8 @@ if __name__ == "__main__":
     # min_corner = [-0.1, -0.1, -0.0]
     # max_corner = [+0.1, +0.1, +0.4]
     angles = np.linspace(0, 2 * np.pi, 7)[:6]
-    symmetric_rotations = np.array([build_rotation_matrix_z(angle) for angle in angles])
+    symmetric_rotations = np.array(
+        [build_rotation_matrix_z(angle) for angle in angles])
     min_corner = [0.0, 0.0, -0.4]
     max_corner = [0.0, 0.0, +0.0]
     # translation = np.array([0.0, 0.0, 1.0])
@@ -242,13 +243,63 @@ if __name__ == "__main__":
     camera_rotation = build_rotation_matrix_x(np.pi)
     translation = np.array([0.0, 0.0, -1.0])
     camera_pose = to_affine_matrix(camera_rotation, translation)
-    scene = CanonicalScene(path_OBJ, camera_pose, min_corner, max_corner, symmetric_rotations)
-    from pyrender import Viewer
-    Viewer(scene.scene)
-    scene.scene.ambient_light = [1.0, 1.0, 1.0]
-    image = scene.render_symmetries()
+    renderer = CanonicalScene(path_OBJ, camera_pose, min_corner,
+                              max_corner, symmetric_rotations)
+    # from pyrender import Viewer
+    # Viewer(scene.scene)
+    renderer.scene.ambient_light = [1.0, 1.0, 1.0]
+    image = renderer.render_symmetries()
     show_image(image)
-    for _ in range(100):
-        image, alpha, RGB_mask = scene.render()
+    for _ in range(0):
+        image, alpha, RGB_mask = renderer.render()
         show_image(image)
         show_image(RGB_mask[:, :, 0:3])
+
+    from pipelines import DomainRandomization
+    from paz.abstract.sequence import GeneratingSequence
+    from loss import WeightedReconstruction
+    from paz.models import UNET_VGG16
+    from tensorflow.keras.optimizers import Adam
+    from metrics import mean_squared_error
+    import glob
+
+    background_wildcard = '.keras/paz/datasets/voc-backgrounds/*.png'
+    background_wildcard = os.path.join(root_path, background_wildcard)
+    image_paths = glob.glob(background_wildcard)
+
+    H, W, num_channels = image_shape
+    batch_size = 32
+    steps_per_epoch = 1000
+    beta = 3.0
+    num_classes = 3
+    learning_rate = 0.001
+    max_num_epochs = 5
+
+    inputs_to_shape = {'input_1': [H, W, num_channels]}
+    labels_to_shape = {'masks': [H, W, 4]}
+
+    processor = DomainRandomization(
+        renderer, image_shape, image_paths, inputs_to_shape,
+        labels_to_shape, num_occlusions)
+
+    sequence = GeneratingSequence(processor, batch_size, steps_per_epoch)
+
+    # build all symmetric rotations for solar pannel
+    angles = np.linspace(0, 2 * np.pi, 7)[:6]
+    rotations = np.array([build_rotation_matrix_z(angle) for angle in angles])
+
+    # loss = WeightedSymmetricReconstruction(rotations, beta)
+    loss = WeightedReconstruction(beta)
+
+    model = UNET_VGG16(num_classes, image_shape, freeze_backbone=True)
+    optimizer = Adam(learning_rate)
+
+    model.compile(optimizer, loss, mean_squared_error)
+
+    model.fit(
+        sequence,
+        epochs=max_num_epochs,
+        verbose=1,
+        workers=0)
+    model.save_weights('UNET-VGG_solar_panel_canonical.hdf5')
+
