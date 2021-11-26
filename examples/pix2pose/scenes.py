6dac02b5ca26a515c26d9aaecc3bd200cca04ffa
Octavio Arriaga <arriaga.camargo@gmail.com>
2021-11-26 19:44:49 +0100

    Add scene for canonical discrete transformations

--- examples/pix2pose/scenes.py
+++ examples/pix2pose/scenes.py
@@ -6,6 +6,9 @@ from pyrender import (PerspectiveCamera, OffscreenRenderer, DirectionalLight,
                       RenderFlags, Mesh, Scene)
 import trimesh
 from coloring import color_object
+from backend import to_affine_matrix
+from backend import sample_affine_transform
+from backend import calculate_canonical_rotation
 
 
 class PixelMaskRenderer():
@@ -65,3 +68,82 @@ class PixelMaskRenderer():
         RGB_mask, _ = self.renderer.render(self.scene, self.flags_FLAT)
         self.mesh.mesh.is_visible = True
         return image, alpha, RGB_mask
+
+
+class CanonicalScene():
+    def __init__(self, path_OBJ, camera_pose, min_corner, max_corner,
+                 symmetric_transforms, viewport_size=(128, 128),
+                 y_fov=3.14159 / 4.0, light_intensity=[0.5, 30]):
+        self.light_intensity = light_intensity
+        self.symmetric_transforms = symmetric_transforms
+        self.min_corner, self.max_corner = min_corner, max_corner
+        self.scene = Scene(bg_color=[0, 0, 0, 0])
+        self.light = self._build_light(light_intensity, camera_pose)
+        self.camera = self._build_camera(y_fov, viewport_size, camera_pose)
+        self.pixel_mesh = self.scene.add(color_object(path_OBJ))
+        self.mesh = self.scene.add(
+            Mesh.from_trimesh(trimesh.load(path_OBJ), smooth=True))
+
+        self.renderer = OffscreenRenderer(viewport_size[0], viewport_size[1])
+
+        self.flags_RGBA = RenderFlags.RGBA
+        self.flags_FLAT = RenderFlags.RGBA | RenderFlags.FLAT
+
+    def _build_light(self, light, pose):
+        directional_light = DirectionalLight([1.0, 1.0, 1.0], np.mean(light))
+        directional_light = self.scene.add(directional_light, pose=pose)
+        return directional_light
+
+    def _build_camera(self, y_fov, viewport_size, pose):
+        aspect_ratio = np.divide(*viewport_size)
+        camera = PerspectiveCamera(y_fov, aspectRatio=aspect_ratio)
+        camera = self.scene.add(camera, pose=pose)
+        return camera
+
+    def _sample_parameters(self, min_corner, max_corner):
+        mesh_transform = sample_affine_transform(min_corner, max_corner)
+        light_intensity = sample_uniformly(self.light_intensity)
+        return mesh_transform, light_intensity
+
+    def render(self):
+        mesh_transform, light_intensity = self._sample_parameters(
+            self.min_corner, self.max_corner)
+        mesh_rotation = mesh_transform[0:3, 0:3]
+        canonical_rotation = calculate_canonical_rotation(
+            mesh_rotation, self.symmetric_transforms)
+        # mesh_rotation[0:3, 0:3] = canonical_rotation
+        canonical_rotation = np.dot(mesh_rotation, canonical_rotation)
+        mesh_rotation[0:3, 0:3] = canonical_rotation
+        self.scene.set_pose(self.mesh, mesh_transform)
+        self.scene.set_pose(self.pixel_mesh, mesh_transform)
+        self.light.light.intensity = light_intensity
+
+        self.pixel_mesh.mesh.is_visible = False
+        image, depth = self.renderer.render(self.scene, self.flags_RGBA)
+        self.pixel_mesh.mesh.is_visible = True
+        image, alpha = split_alpha_channel(image)
+        self.mesh.mesh.is_visible = False
+        RGB_mask, _ = self.renderer.render(self.scene, self.flags_FLAT)
+        self.mesh.mesh.is_visible = True
+        return image, alpha, RGB_mask
+
+    def render_symmetries(self):
+        images, alphas, RGB_masks = [], [], []
+        for rotation in self.symmetric_transforms:
+            symmetric_transform = to_affine_matrix(rotation, np.zeros(3))
+            self.scene.set_pose(self.mesh, symmetric_transform)
+            self.scene.set_pose(self.pixel_mesh, symmetric_transform)
+            self.pixel_mesh.mesh.is_visible = False
+            image, depth = self.renderer.render(self.scene, self.flags_RGBA)
+            self.pixel_mesh.mesh.is_visible = True
+            image, alpha = split_alpha_channel(image)
+            self.mesh.mesh.is_visible = False
+            RGB_mask, _ = self.renderer.render(self.scene, self.flags_FLAT)
+            self.mesh.mesh.is_visible = True
+            images.append(image)
+            alphas.append(alpha)
+            RGB_masks.append(RGB_mask[..., 0:3])
+        images = np.concatenate(images, axis=1)
+        RGB_masks = np.concatenate(RGB_masks, axis=1)
+        images = np.concatenate([images, RGB_masks], axis=0)
+        return images
