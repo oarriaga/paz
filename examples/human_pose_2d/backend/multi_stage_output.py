0be3244167904eafef91ed3e1e4aa45e961a995b
proneetsharma <sproneet@gmail.com>
2022-01-19 16:33:24 +0100

    Add separate functions for flip cases

--- examples/human_pose_2d/backend/multi_stage_output.py
+++ examples/human_pose_2d/backend/multi_stage_output.py
@@ -1,18 +1,25 @@
 import numpy as np
 
 
-def get_heatmaps_average(output, num_joints, with_flip, indices):
-    heatmaps_average = 0
-    if with_flip:
-        temp = output[:, :num_joints, :, :]
-        heatmaps_average += np.take(temp, indices, axis=1)
-    else:
-        heatmaps_average += output[:, :num_joints, :, :]
-    return heatmaps_average
+def get_heatmap_sum(output, num_joints, heatmap_sum):
+    heatmap_sum = heatmap_sum + output[:, :num_joints, :, :]
+    return heatmap_sum
 
 
-def get_tags(output, tags, offset, indices, tag_per_joint, with_flip=False):
-    tags.append(output[:, offset:, :, :])
-    if with_flip and tag_per_joint:
-        tags[-1] = np.take(tags[-1], indices, axis=1)
+def get_heatmap_sum_with_flip(output, num_joints, indices, heatmap_sum):
+    output = np.flip(output, [3])
+    heatmaps = output[:, :num_joints, :, :]
+    heatmap_sum = heatmap_sum + np.take(heatmaps, indices, axis=1)
+    return heatmap_sum
+
+
+def get_tags(output, num_joint):
+    tags = output[:, num_joint:, :, :]
+    return tags
+
+
+def get_tags_with_flip(output, num_joint, indices):
+    output = np.flip(output, [3])
+    tags = output[:, num_joint:, :, :]
+    tags = np.take(tags, indices, axis=1)
     return tags
