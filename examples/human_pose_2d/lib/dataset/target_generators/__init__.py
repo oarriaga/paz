788784de9c003b4667be4ce698da96717ca29d55
proneetsharma <sproneet@gmail.com>
2022-01-19 16:09:04 +0100

    Delete unnecessary files

--- examples/human_pose_2d/lib/dataset/target_generators/__init__.py
+++
@@ -1,5 +0,0 @@
-from .target_generators import HeatmapGenerator
-from .target_generators import ScaleAwareHeatmapGenerator
-from .target_generators import JointsGenerator
-
-__all__ = ['HeatmapGenerator', 'ScaleAwareHeatmapGenerator', 'JointsGenerator']
--- examples/human_pose_2d/lib/dataset/transforms/__init__.py
+++
@@ -1,8 +0,0 @@
-from .transforms import Compose
-from .transforms import RandomAffineTransform
-from .transforms import ToTensor
-from .transforms import Normalize
-from .transforms import RandomHorizontalFlip
-
-from .build import build_transforms
-from .build import FLIP_CONFIG
