24db9d04ade3a6cd0b68b97b129e5cc9bb88d8e2
Octavio Arriaga <arriaga.camargo@gmail.com>
2022-02-01 12:52:04 +0100

    Move scene to legacy

--- examples/pix2pose/scenes.py
+++ examples/pix2pose/scenes.py
@@ -1,74 +1,13 @@
 import numpy as np
-from paz.backend.render import (sample_uniformly, split_alpha_channel,
-                                random_perturbation, sample_point_in_sphere,
-                                compute_modelview_matrices)
+from paz.backend.render import sample_uniformly, split_alpha_channel
 from pyrender import (PerspectiveCamera, OffscreenRenderer, DirectionalLight,
                       RenderFlags, Mesh, Scene)
 import trimesh
+
 from coloring import color_object
 from backend import to_affine_matrix
 from backend import sample_affine_transform
 from backend import calculate_canonical_rotation
-from paz.models import UNET_VGG16
-
-
-class PixelMaskRenderer():
-    """Render-ready scene composed of a single object and a single moving camera.
-
-    # Arguments
-        path_OBJ: String containing the path to an OBJ file.
-        viewport_size: List, specifying [H, W] of rendered image.
-        y_fov: Float indicating the vertical field of view in radians.
-        distance: List of floats indicating [max_distance, min_distance]
-        light: List of floats indicating [max_light, min_light]
-        top_only: Boolean. If True images are only take from the top.
-        roll: Float, to sample [-roll, roll] rolls of the Z OpenGL camera axis.
-        shift: Float, to sample [-shift, shift] to move in X, Y OpenGL axes.
-    """
-    def __init__(self, path_OBJ, viewport_size=(128, 128), y_fov=3.14159 / 4.0,
-                 distance=[0.3, 0.5], light=[0.5, 30], top_only=False,
-                 roll=None, shift=None):
-        self.distance, self.roll, self.shift = distance, roll, shift
-        self.light_intensity, self.top_only = light, top_only
-        self._build_scene(path_OBJ, viewport_size, light, y_fov)
-        self.renderer = OffscreenRenderer(viewport_size[0], viewport_size[1])
-        self.flags_RGBA = RenderFlags.RGBA
-        self.flags_FLAT = RenderFlags.RGBA | RenderFlags.FLAT
-        self.epsilon = 0.01
-
-    def _build_scene(self, path, size, light, y_fov):
-        self.scene = Scene(bg_color=[0, 0, 0, 0])
-        self.light = self.scene.add(
-            DirectionalLight([1.0, 1.0, 1.0], np.mean(light)))
-        self.camera = self.scene.add(
-            PerspectiveCamera(y_fov, aspectRatio=np.divide(*size)))
-        self.pixel_mesh = self.scene.add(color_object(path))
-        self.mesh = self.scene.add(
-            Mesh.from_trimesh(trimesh.load(path), smooth=True))
-        self.world_origin = self.mesh.mesh.centroid
-
-    def _sample_parameters(self):
-        distance = sample_uniformly(self.distance)
-        camera_origin = sample_point_in_sphere(distance, self.top_only)
-        camera_origin = random_perturbation(camera_origin, self.epsilon)
-        light_intensity = sample_uniformly(self.light_intensity)
-        return camera_origin, light_intensity
-
-    def render(self):
-        camera_origin, intensity = self._sample_parameters()
-        camera_to_world, world_to_camera = compute_modelview_matrices(
-            camera_origin, self.world_origin, self.roll, self.shift)
-        self.light.light.intensity = intensity
-        self.scene.set_pose(self.camera, camera_to_world)
-        self.scene.set_pose(self.light, camera_to_world)
-        self.pixel_mesh.mesh.is_visible = False
-        image, depth = self.renderer.render(self.scene, self.flags_RGBA)
-        self.pixel_mesh.mesh.is_visible = True
-        image, alpha = split_alpha_channel(image)
-        self.mesh.mesh.is_visible = False
-        RGB_mask, _ = self.renderer.render(self.scene, self.flags_FLAT)
-        self.mesh.mesh.is_visible = True
-        return image, alpha, RGB_mask
 
 
 class CanonicalScene():
@@ -155,10 +94,6 @@ if __name__ == "__main__":
     from paz.backend.image import show_image
     from backend import build_rotation_matrix_x
     from backend import build_rotation_matrix_z
-    from backend import build_rotation_matrix_y
-    from paz.backend.render import compute_modelview_matrices
-    from pipelines import DomainRandomization
-    import glob
 
     # generic parameters
     root_path = os.path.expanduser('~')
