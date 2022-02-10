1dff6e084a0e570b0642c51c7a8270398ea47dd0
Octavio Arriaga <arriaga.camargo@gmail.com>
2022-02-10 19:06:30 +0100

    Add function to approximate camera internals

--- examples/pix2pose/demo.py
+++ examples/pix2pose/demo.py
@@ -10,14 +10,20 @@ camera = Camera(device_id=0)
 
 image = load_image('images/test_image2.jpg')
 # image = load_image('images/lab_condition.png')
-image_size = image.shape[0:2]
-focal_length = image_size[1]
-image_center = (image_size[1] / 2.0, image_size[0] / 2.0)
-camera.distortion = np.zeros((4))
-camera.intrinsics = np.array([[focal_length, 0, image_center[0]],
-                              [0, focal_length, image_center[1]],
-                              [0, 0, 1]])
 
+
+def approximate_intrincs(image):
+    image_size = image.shape[0:2]
+    focal_length = image_size[1]
+    image_center = (image_size[1] / 2.0, image_size[0] / 2.0)
+    camera_intrinsics = np.array([[focal_length, 0, image_center[0]],
+                                  [0, focal_length, image_center[1]],
+                                  [0, 0, 1]])
+    return camera_intrinsics
+
+
+camera.intrinsics = approximate_intrincs(image)
+camera.distortion = np.zeros((4))
 pipeline = Pix2PosePowerDrill(camera)
 predicted_image = pipeline(image)['image']
 show_image(predicted_image)
