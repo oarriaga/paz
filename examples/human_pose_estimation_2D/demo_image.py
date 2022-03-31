2daf1c83af61d2749c88ea6db43935d9bf58d5c2
Octavio Arriaga <arriaga.camargo@gmail.com>
2022-03-31 12:50:26 +0200

    Change demo to show image instead of writing in disk

--- examples/human_pose_estimation_2D/demo_image.py
+++ examples/human_pose_estimation_2D/demo_image.py
@@ -1,7 +1,7 @@
 import os
 from tensorflow.keras.utils import get_file
 from paz.applications import DetectHumanPose2D
-from paz.backend.image import load_image, write_image
+from paz.backend.image import load_image, show_image
 
 
 URL = ('https://github.com/oarriaga/altamira-data/releases/download'
@@ -14,4 +14,4 @@ detect = DetectHumanPose2D()
 inferences = detect(image)
 
 image = inferences['image']
-write_image('./output/result.jpg', image)
+show_image(image)
