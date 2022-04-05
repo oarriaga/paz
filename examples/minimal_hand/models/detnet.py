1b26dad2e4fdbb57bd37a5e21513e6957fd9d90d
proneetsharma <sproneet@gmail.com>
2022-04-06 01:31:42 +0200

    Migrate model to TF2

--- examples/minimal_hand/models/detnet.py
+++ examples/minimal_hand/models/detnet.py
@@ -1,7 +1,24 @@
 
+from pickle import FALSE
 import numpy as np
 import tensorflow as tf
 from config import DETECTION_MODEL_PATH
+
+from tensorflow.keras.layers import MaxPool2D
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import ReLU
+from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import Conv2DTranspose
+from tensorflow.keras.layers import UpSampling2D
+from tensorflow.keras.layers import Add
+from tensorflow.keras.layers import concatenate
+from tensorflow.keras.layers import ZeroPadding2D
+from tensorflow.keras.regularizers import l2 
+from tensorflow.keras.initializers import VarianceScaling 
+from tensorflow.keras.initializers import truncated_normal 
+from tensorflow.keras import backend as K
+from tensorflow.keras.models import Model
 
 
 def zero_padding(inputs, pad_1, pad_2):
@@ -10,104 +27,96 @@ def zero_padding(inputs, pad_1, pad_2):
 
 
 def conv_bn(inputs, oc, ks, st, scope, training, rate=1):
-    with tf.compat.v1.variable_scope(scope):
-        if st == 1:
-            layer = tf.compat.v1.layers.conv2d(
-            inputs, oc, ks, strides=st, padding='SAME', use_bias=False,
+    if st == 1:
+        layer = Conv2D(oc, ks, strides=st, padding='SAME', use_bias=False,
+                        dilation_rate=rate,
+                        kernel_regularizer=l2(0.5 * (1.0)),
+                        name=scope + '/conv2d',
+                        kernel_initializer=VarianceScaling(scale=1.0, mode="fan_avg", distribution="uniform")
+    )(inputs)
+    else:
+        pad_total = ks - 1
+        pad_1 = pad_total // 2
+        pad_2 = pad_total - pad_1
+        padded_inputs = zero_padding(inputs, pad_1, pad_2)
+        layer = Conv2D(
+            oc, ks, strides=st, padding='VALID', use_bias=False,
             dilation_rate=rate,
-            kernel_regularizer=tf.keras.regularizers.l2(0.5 * (1.0)),
-            kernel_initializer=tf.compat.v1.keras.initializers.VarianceScaling(scale=1.0, mode="fan_avg", distribution="uniform")
-        )
-        else:
-            pad_total = ks - 1
-            pad_1 = pad_total // 2
-            pad_2 = pad_total - pad_1
-            padded_inputs = zero_padding(inputs, pad_1, pad_2)
-            layer = tf.compat.v1.layers.conv2d(
-                padded_inputs, oc, ks, strides=st, padding='VALID', use_bias=False,
-                dilation_rate=rate,
-                kernel_regularizer=tf.keras.regularizers.l2(0.5 * (1.0)),
-                kernel_initializer=tf.compat.v1.keras.initializers.VarianceScaling(scale=1.0, mode="fan_avg", distribution="uniform")
-            )
-        layer = tf.compat.v1.layers.batch_normalization(layer, training=training)
+            name=scope+ '/conv2d',
+            kernel_regularizer=l2(0.5 * (1.0)),
+            kernel_initializer=VarianceScaling(scale=1.0, mode="fan_avg", distribution="uniform")
+        )(padded_inputs)
+    layer = BatchNormalization(name=scope+'/batch_normalization')(layer, training=training)
     return layer
+
 
 
 def conv_bn_relu(inputs, oc, ks, st, scope, training, rate=1):
     layer = conv_bn(inputs, oc, ks, st, scope, training, rate=rate)
-    layer = tf.nn.relu(layer)
+    layer = ReLU()(layer)
     return layer
 
 
-
 def bottleneck(inputs, oc, st, scope, training, rate=1):
-    with tf.compat.v1.variable_scope(scope):
-        ic = inputs.get_shape().as_list()[-1]
-        if ic == oc:
-            if st == 1:
-                shortcut = inputs
-            else:
-                shortcut = \
-                tf.nn.max_pool2d(inputs, [1, st, st, 1], [1, st, st, 1], 'SAME')
+    ic = inputs.get_shape().as_list()[-1]
+    if ic == oc:
+        if st == 1:
+            shortcut = inputs
         else:
-            shortcut = conv_bn(inputs, oc, 1, st, 'shortcut', training)
+            shortcut = MaxPool2D((st, st), (st, st), 'SAME')(inputs)
+    else:
+        shortcut = conv_bn(inputs, oc, 1, st, scope + '/shortcut', training)
 
-        residual = conv_bn_relu(inputs, oc//4, 1, 1, 'conv1', training)
-        residual = conv_bn_relu(residual, oc//4, 3, st, 'conv2', training, rate)
-        residual = conv_bn(residual, oc, 1, 1, 'conv3', training)
-        output = tf.nn.relu(shortcut + residual)
+    residual = conv_bn_relu(inputs, oc//4, 1, 1, scope + '/conv1', training)
+    residual = conv_bn_relu(residual, oc//4, 3, st, scope + '/conv2', training, rate)
+    residual = conv_bn(residual, oc, 1, 1, scope + '/conv3', training)
+    output = ReLU()(shortcut + residual)
 
     return output
 
 
 def resnet50(inputs, scope, training):
-    with tf.compat.v1.variable_scope(scope):
-        layer = conv_bn_relu(inputs, 64, 7, 2, 'conv1', training)
+    layer = conv_bn_relu(inputs, 64, 7, 2, scope + '/conv1', training)
 
-        with tf.compat.v1.variable_scope('block1'):
-            for unit in range(2):
-                layer = bottleneck(layer, 256, 1, 'unit%d' % (unit+1), training)
-            layer = bottleneck(layer, 256, 2, 'unit3', training)
+    for unit in range(2):
+        layer = bottleneck(layer, 256, 1, scope + '/block1/unit%d' % (unit+1), training)
+    layer = bottleneck(layer, 256, 2, scope + '/block1/unit3', training)
 
-        with tf.compat.v1.variable_scope('block2'):
-            for unit in range(4):
-                layer = bottleneck(layer, 512, 1, 'unit%d' % (unit+1), training, 2)
+    for unit in range(4):
+        layer = bottleneck(layer, 512, 1, scope + '/block2/unit%d' % (unit+1), training, 2)
 
-        with tf.compat.v1.variable_scope('block3'):
-            for unit in range(6):
-                layer = bottleneck(layer, 1024, 1, 'unit%d' % (unit+1), training, 4)
+    for unit in range(6):
+        layer = bottleneck(layer, 1024, 1, scope + '/block3/unit%d' % (unit+1), training, 4)
 
-        layer = conv_bn_relu(layer, 256, 3, 1, 'squeeze', training)
+    layer = conv_bn_relu(layer, 256, 3, 1, scope + '/squeeze', training)
 
     return layer
 
 
 def net_2d(features, training, scope, n_out):
-    with tf.compat.v1.variable_scope(scope):
-        layer = conv_bn_relu(features, 256, 3, 1, 'project', training)
-        with tf.compat.v1.variable_scope('prediction'):
-            hmap = tf.compat.v1.layers.conv2d(
-            layer, n_out, 1, strides=1, padding='SAME',
-            activation=tf.nn.sigmoid,
-            kernel_initializer=tf.compat.v1.initializers.truncated_normal(stddev=0.01)
-        )
+    layer = conv_bn_relu(features, 256, 3, 1, scope + '/project', training)
+    hmap = Conv2D(
+    n_out, 1, strides=1, padding='SAME',
+    activation=tf.nn.sigmoid,
+    name=scope + '/prediction/conv2d',
+    kernel_initializer=truncated_normal(stddev=0.01)
+    )(layer)
     return hmap
 
 
 def net_3d(features, training, scope, n_out, need_norm):
-    with tf.compat.v1.variable_scope(scope):
-        layer = conv_bn_relu(features, 256, 3, 1, 'project', training)
-        with tf.compat.v1.variable_scope('prediction'):
-            dmap_raw = tf.compat.v1.layers.conv2d(
-            layer, n_out * 3, 1, strides=1, padding='SAME',
-            activation=None,
-            kernel_initializer=tf.compat.v1.initializers.truncated_normal(stddev=0.01)
-        )
-        if need_norm:
-            dmap_norm = tf.norm(tensor=dmap_raw, axis=-1, keepdims=True)
-            dmap = dmap_raw / tf.maximum(dmap_norm, 1e-6)
-        else:
-            dmap = dmap_raw
+    layer = conv_bn_relu(features, 256, 3, 1, scope + '/project', training)
+    dmap_raw = Conv2D(
+    n_out * 3, 1, strides=1, padding='SAME',
+    activation=None,
+    name=scope + '/prediction/conv2d',
+    kernel_initializer=truncated_normal(stddev=0.01)
+    )(layer)
+    if need_norm:
+        dmap_norm = tf.norm(tensor=dmap_raw, axis=-1, keepdims=True)
+        dmap = dmap_raw / tf.maximum(dmap_norm, 1e-6)
+    else:
+        dmap = dmap_raw
 
     h, w = features.get_shape().as_list()[1:3]
     dmap = tf.reshape(dmap, [-1, h, w, n_out, 3])
@@ -134,8 +143,8 @@ def get_pose_tile(N):
     return pos_tile
 
 
-def detnet(img, n_stack, training):
-    features = resnet50(img, 'resnet', training)
+def detnet(img, n_stack, scope, training):
+    features = resnet50(img, scope + '/resnet', training)
     pos_tile = get_pose_tile(tf.shape(input=img)[0])
     features = tf.concat([features, pos_tile], -1)
 
@@ -143,15 +152,15 @@ def detnet(img, n_stack, training):
     dmaps = []
     lmaps = []
     for i in range(n_stack):
-        hmap = net_2d(features, training, 'hmap_%d' % i, 21)
+        hmap = net_2d(features, training, scope + '/hmap_%d' % i, 21)
         features = tf.concat([features, hmap], axis=-1)
         hmaps.append(hmap)
 
-        dmap = net_3d(features, training, 'dmap_%d' % i, 21, False)
+        dmap = net_3d(features, training, scope + '/dmap_%d' % i, 21, False)
         features = tf.concat([features, tf.reshape(dmap, [-1, 32, 32, 21 * 3])], -1)
         dmaps.append(dmap)
 
-        lmap = net_3d(features, training, 'lmap_%d' % i, 21, False)
+        lmap = net_3d(features, training, scope + '/lmap_%d' % i, 21, False)
         features = tf.concat([features, tf.reshape(lmap, [-1, 32, 32, 21 * 3])], -1)
         lmaps.append(lmap)
 
@@ -168,46 +177,47 @@ def tf_hmap_to_uv(hmap):
     return uv
 
 
-
-
-class ModelDet:
+class DetNet:
     """
     DetNet: estimating 3D keypoint positions from input color image.
     """
 
-    def __init__(self, model_path):
+    def __init__(self):
         """
     Parameters
     ----------
     model_path : str
       Path to the trained model.
     """
+        model_path = DETECTION_MODEL_PATH
         self.graph = tf.Graph()
         with self.graph.as_default():
-            with tf.compat.v1.variable_scope('prior_based_hand'):
-                config = tf.compat.v1.ConfigProto()
-                config.gpu_options.allow_growth = True
-                self.sess = tf.compat.v1.Session(config=config)
-                self.input_ph = tf.compat.v1.placeholder(tf.uint8, [128, 128, 3])
-                self.feed_img = \
-                    tf.cast(tf.expand_dims(self.input_ph, 0), tf.float32) / 255
-                self.hmaps, self.dmaps, self.lmaps = \
-                    detnet(self.feed_img, 1, False)
+            config = tf.compat.v1.ConfigProto()
+            config.gpu_options.allow_growth = True
+            self.sess = tf.compat.v1.Session(config=config)
+            self.input_ph = tf.keras.Input(shape=(128, 128, 3), dtype=tf.uint8)
+            self.input_ph = tf.squeeze(self.input_ph, 0)
+            self.feed_img = \
+                tf.cast(tf.expand_dims(self.input_ph, 0), tf.float32) / 255
+            self.hmaps, self.dmaps, self.lmaps = \
+                detnet(self.feed_img, 1, 'prior_based_hand', False)
 
-                self.hmap = self.hmaps[-1]
-                self.dmap = self.dmaps[-1]
-                self.lmap = self.lmaps[-1]
+            self.hmap = self.hmaps[-1]
+            self.dmap = self.dmaps[-1]
+            self.lmap = self.lmaps[-1]
 
-                self.uv = tf_hmap_to_uv(self.hmap)
-                self.delta = tf.gather_nd(
-                    tf.transpose(a=self.dmap, perm=[0, 3, 1, 2, 4]), self.uv, batch_dims=2
-                )[0]
-                self.xyz = tf.gather_nd(
-                    tf.transpose(a=self.lmap, perm=[0, 3, 1, 2, 4]), self.uv, batch_dims=2
-                )[0]
+            self.uv = tf_hmap_to_uv(self.hmap)
+            self.delta = tf.gather_nd(
+                tf.transpose(a=self.dmap, perm=[0, 3, 1, 2, 4]), self.uv, batch_dims=2
+            )[0]
+            self.xyz = tf.gather_nd(
+                tf.transpose(a=self.lmap, perm=[0, 3, 1, 2, 4]), self.uv, batch_dims=2
+            )[0]
 
-                self.uv = self.uv[0]
+            self.uv = self.uv[0]
             tf.compat.v1.train.Saver().restore(self.sess, model_path)
+            # print(tf.train.list_variables(model_path))
+
 
     def process(self, img):
         """
@@ -230,31 +240,3 @@ class ModelDet:
     """
         results = self.sess.run([self.xyz, self.uv], {self.input_ph: img})
         return results
-
-
-class ModelPipeline:
-    """
-  A wrapper that puts DetNet and IKNet together.
-  """
-
-    def __init__(self, left=True):
-        self.det_model = ModelDet(DETECTION_MODEL_PATH)
-
-    def process(self, frame):
-        """
-        Process a single frame.
-
-        Parameters
-        ----------
-        frame : np.ndarray, shape [128, 128, 3], dtype np.uint8.
-          Frame to be processed.
-
-        Returns
-        -------
-        np.ndarray, shape [21, 3]
-          Joint locations.
-        np.ndarray, shape [21, 4]
-          Joint rotations.
-        """
-        xyz, uv = self.det_model.process(frame)
-        return xyz, uv
