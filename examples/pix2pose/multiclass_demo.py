ea5585f79bdbb66e8fd584bb397d6ecf95d2275e
Octavio Arriaga <arriaga.camargo@gmail.com>
2022-04-19 09:56:11 +0200

    Add basic video player demo

--- examples/pix2pose/multiclass_demo.py
+++ examples/pix2pose/multiclass_demo.py
@@ -2,7 +2,7 @@ from paz.backend.image import show_image, load_image, resize_image
 from paz.backend.camera import Camera
 from pipelines import PIX2Tools6D
 
-
+"""
 image = load_image('images/unit_test.png')
 # image = load_image('images/group_photo_onefourth.png')
 print(image.shape)
@@ -12,6 +12,14 @@ pipeline = PIX2Tools6D(camera)
 
 inferences = pipeline(image)
 predicted_image = inferences['image']
-# H, W = predicted_image.shape[:2]
-# predicted_image = resize_image(predicted_image, (W * 3, H * 3))
+H, W = predicted_image.shape[:2]
+predicted_image = resize_image(predicted_image, (W * 3, H * 3))
 show_image(predicted_image)
+"""
+
+from paz.backend.camera import VideoPlayer
+camera = Camera(4)
+camera.intrinsics_from_HFOV(70)
+pipeline = PIX2Tools6D(camera, resize=True)
+player = VideoPlayer((640 * 3, 480 * 3), pipeline, camera)
+player.run()
