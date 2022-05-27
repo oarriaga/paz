b048e61bfeacb3e002a99cd96b8444a08292b153
proneetsharma <sproneet@gmail.com>
2022-05-27 02:01:55 +0200

    Fix right hand keypoints detection

--- examples/minimal_hand/pipelines.py
+++ examples/minimal_hand/pipelines.py
@@ -5,33 +5,34 @@ from paz.models import IKNet
 from paz.backend.image import flip_left_right
 from paz.datasets import MPII_REF_JOINTS
 from paz.datasets import MPIIHandJoints
-from backend import calculate_relative_angle
+from backend import compute_relative_angle
 from backend import transform_column_to_negative
+from backend import flip_keypoints_wrt_image
 
 
 class MinimalHandPoseEstimator(pr.Processor):
-    def __init__(self, draw=True, left_hand=False):
+    def __init__(self, draw=True, right_hand=False):
         super(MinimalHandPoseEstimator, self).__init__()
         self.keypoints_estimator = DetNetHandKeypoint(draw=draw,
-                                                      left_hand=left_hand)
-        self.angle_estimator = IKNetHandJointAngles(left_hand)
+                                                      right_hand=right_hand)
+        self.angle_estimator = IKNetHandJointAngles(right_hand)
         self.wrap = pr.WrapOutput(['image', 'keypoints3D', 'keypoints2D',
-                                   'absolute_angle', 'relative_angles'])
+                                   'absolute_angles', 'relative_angles'])
 
     def call(self, image):
         keypoints = self.keypoints_estimator(image)
         angles = self.angle_estimator(keypoints['keypoints3D'])
-        print(angles)
-        return self.wrap(keypoints['image'], keypoints['keypoints3D'], 
+        print(keypoints, angles)
+        return self.wrap(keypoints['image'], keypoints['keypoints3D'],
                          keypoints['keypoints2D'], angles['absolute_angles'],
                          angles['relative_angles'])
 
 
 class DetNetHandKeypoint(pr.Processor):
-    def __init__(self, shape=(128, 128), draw=True, left_hand=False):
+    def __init__(self, shape=(128, 128), draw=True, right_hand=False):
         super(DetNetHandKeypoint).__init__()
         self.draw = draw
-        self.left_hand = left_hand
+        self.right_hand = right_hand
         self.preprocess = pr.SequentialProcessor(
             [pr.ResizeImage(shape), pr.ExpandDims(axis=0)])
         self.hand_estimator = DetNet()
@@ -41,10 +42,12 @@ class DetNetHandKeypoint(pr.Processor):
 
     def call(self, input_image):
         image = self.preprocess(input_image)
-        if self.left_hand:
+        if self.right_hand:
             image = flip_left_right(image)
         keypoints3D, keypoints2D = self.hand_estimator.predict(image)
         keypoints2D = flip_left_right(keypoints2D)
+        if self.right_hand:
+            keypoints2D = flip_keypoints_wrt_image(keypoints2D)
         keypoints2D = self.scale_keypoints(keypoints2D, input_image)
         if self.draw:
             image = self.draw_skeleton(input_image, keypoints2D)
@@ -52,14 +55,15 @@ class DetNetHandKeypoint(pr.Processor):
 
 
 class IKNetHandJointAngles(pr.Processor):
-    def __init__(self, left_hand=False):
+    def __init__(self, right_hand=False, config_xyz=MPII_REF_JOINTS):
         super(IKNetHandJointAngles, self).__init__()
         self.calculate_orientation = pe.CalculateOrientationFromCoordinates(
             MPIIHandJoints)
-        self.mpii_ref_xyz = MPII_REF_JOINTS
-        if left_hand:
-            self.mpii_ref_xyz = transform_column_to_negative(self.mpii_ref_xyz)
-        self.mpii_ref_delta = self.calculate_orientation(self.mpii_ref_xyz)
+        self.ref_xyz = config_xyz
+        self.right_hand = right_hand
+        if right_hand:
+            self.ref_xyz = transform_column_to_negative(self.ref_xyz)
+        self.ref_delta = self.calculate_orientation(self.ref_xyz)
         self.concatenate = pr.Concatenate(0)
         self.compute_absolute_angle = pr.SequentialProcessor(
             [pr.ExpandDims(0), IKNet(), pr.Squeeze(0)])
@@ -68,7 +72,8 @@ class IKNetHandJointAngles(pr.Processor):
     def call(self, keypoints3D):
         delta = self.calculate_orientation(keypoints3D)
         pack = self.concatenate(
-            [keypoints3D, delta, self.mpii_ref_xyz, self.mpii_ref_delta])
+            [keypoints3D, delta, self.ref_xyz, self.ref_delta])
         absolute_angle = self.compute_absolute_angle(pack)
-        relative_angles = calculate_relative_angle(absolute_angle)
+        relative_angles = compute_relative_angle(
+            absolute_angle, self.right_hand)
         return self.wrap(absolute_angle, relative_angles)
