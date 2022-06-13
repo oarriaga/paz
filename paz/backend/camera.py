1c4fe414de79a7642edb5e375b1694816bf62063
proneetsharma <sproneet@gmail.com>
2022-06-13 23:02:44 +0200

    Add a functionality to record inference from a video file

--- paz/backend/camera.py
+++ paz/backend/camera.py
@@ -211,3 +211,40 @@ class VideoPlayer(object):
         self.camera.stop()
         writer.release()
         cv2.destroyAllWindows()
+
+    def record_from_file(self, video_file_path, name='video.avi',
+                         fps=20, fourCC='XVID'):
+        """Load video and records continuous inference using ``pipeline``.
+
+        # Arguments
+            video_file_path: String. Path to the video file.
+            name: String. Output video name. Must include the postfix .avi.
+            fps: Int. Frames per second.
+            fourCC: String. Indicates the four character code of the video.
+            e.g. XVID, MJPG, X264.
+        """
+
+        fourCC = cv2.VideoWriter_fourcc(*fourCC)
+        writer = cv2.VideoWriter(name, fourCC, fps, self.image_size)
+
+        video = cv2.VideoCapture(video_file_path)
+        if (video.isOpened() is False):
+            print("Error opening video  file")
+
+        while(video.isOpened()):
+            is_frame_received, frame = video.read()
+            if not is_frame_received:
+                print("Frame not received. Exiting ...")
+                break
+            if is_frame_received is True:
+                output = self.pipeline(frame)
+                if output is None:
+                    continue
+                image = resize_image(output['image'], tuple(self.image_size))
+                show_image(image, 'inference', wait=False)
+                writer.write(image)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+
+        writer.release()
+        cv2.destroyAllWindows()
