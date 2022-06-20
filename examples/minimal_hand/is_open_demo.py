31a226f7d0e9fe72d870673aa546a56b5cd80b30
proneetsharma <sproneet@gmail.com>
2022-06-20 18:39:46 +0200

    Update the pipeline with hand closure status dtype

--- examples/minimal_hand/is_open_demo.py
+++ examples/minimal_hand/is_open_demo.py
@@ -15,6 +15,7 @@ pipeline = SequentialProcessor()
 pipeline.add(MinimalHandPoseEstimation(right_hand=False))
 pipeline.add(pr.UnpackDictionary(['image', 'relative_angles']))
 pipeline.add(pr.ControlMap(pr.IsHandOpen(), [1], [1]))
+pipeline.add(pr.ControlMap(pr.BooleanToTextMessage('OPEN', 'CLOSE'), [1], [1]))
 pipeline.add(pr.ControlMap(pr.DrawText(), [0, 1], [1]))
 pipeline.add(pr.WrapOutput(['image', 'status']))
 
