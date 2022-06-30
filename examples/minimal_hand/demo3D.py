112ca3a895ad0298b667e21ebbe006a80286cfbf
Proneet Sharma <sproneet@gmail.com>
2022-07-01 01:22:36 +0200

    Refactor minimal hand 3D demo

--- examples/minimal_hand/demo3D.py
+++ examples/minimal_hand/demo3D.py
@@ -1,13 +1,11 @@
 import argparse
-from paz.backend.camera import VideoPlayer
-from paz.backend.camera import Camera
-from paz.applications import MinimalHandPoseEstimation
-from paz.backend.image import resize_image, show_image
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from paz.backend.camera import Camera, VideoPlayer
+from paz.applications import MinimalHandPoseEstimation
+from paz.backend.image import resize_image, show_image, plot_3D_keypoints_link
 from paz.datasets import MINIMAL_HAND_CONFIG
-from paz.backend.image import plot_3D_keypoints_link
 
 
 parser = argparse.ArgumentParser(description='Minimal hand keypoint detection')
@@ -23,15 +21,16 @@ player = VideoPlayer((640, 480), pipeline, camera)
 link_orders = MINIMAL_HAND_CONFIG['part_orders']
 link_args = MINIMAL_HAND_CONFIG['part_arg']
 link_colors = MINIMAL_HAND_CONFIG['part_color']
-link_colors = np.array(link_colors)/255
+link_colors = np.array(link_colors) / 255
 joint_colors = MINIMAL_HAND_CONFIG['joint_color']
-joint_colors = np.array(joint_colors)/255
+joint_colors = np.array(joint_colors) / 255
+
 
 def animate(player):
     """Opens camera and starts continuous inference using ``pipeline``,
         until the user presses ``q`` inside the opened window. Plot the
         3D keypoints on pyplot.
-    """  
+    """
     player.camera.start()
     ax = plt.axes(projection='3d')
     ax.view_init(-160, -80)
@@ -41,10 +40,10 @@ def animate(player):
         output = player.step()
         image = resize_image(output[player.topic], tuple(player.image_size))
         show_image(image, 'inference', wait=False)
-        
+
         keypoints3D = output['keypoints3D']
         xs, ys, zs = np.split(keypoints3D, 3, axis=1)
-           
+
         plt.cla()
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
@@ -52,12 +51,9 @@ def animate(player):
         ax.scatter3D(xs, ys, zs, c = joint_colors)
         plot_3D_keypoints_link(ax, keypoints3D, link_args, link_orders,
                                link_colors)
-    return wrapped_animate        
-        
+    return wrapped_animate
+
 
 animation = FuncAnimation(plt.gcf(), animate(player), interval=1)
 plt.tight_layout()
 plt.show()
-
-
-            
