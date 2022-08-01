c2216ad3ce328474e9ce9702d12d70a97ec8aac4
Proneet Sharma <sproneet@gmail.com>
2022-08-01 19:35:47 +0200

    Add minimal hand detection pipeline

--- paz/applications.py
+++ paz/applications.py
@@ -16,3 +16,4 @@ from .pipelines import PIX2YCBTools6D
 from .pipelines import HigherHRNetHumanPose2D
 from .pipelines import DetNetHandKeypoints
 from .pipelines import MinimalHandPoseEstimation
+from .pipelines import DetectMinimalHand
