647ec66fb24c6629ccfea68bf11c868f84950897
Octavio Arriaga <arriaga.camargo@gmail.com>
2022-08-31 11:39:13 +0200

    Change to use internal camera methods

--- examples/minimal_hand/hand_tracking.py
+++ examples/minimal_hand/hand_tracking.py
@@ -11,7 +11,7 @@ parser = argparse.ArgumentParser(description='Minimal hand keypoint detection')
 parser.add_argument('-c', '--camera_id', type=int, default=0,
                     help='Camera device ID')
 parser.add_argument('-HFOV', '--horizontal_field_of_view', type=float,
-                    default=70, help='Horizontal field of view in degrees')
+                    default=75, help='Horizontal field of view in degrees')
 args = parser.parse_args()
 
 camera = Camera(args.camera_id)
@@ -20,18 +20,23 @@ focal_length = camera.intrinsics[0, 0]
 
 
 class EstimateHandPosition(pr.Processor):
-    def __init__(self, focal_length, hand_width=10):
+    def __init__(self, camera, hand_width=10):
         super(EstimateHandPosition, self).__init__()
-        self.focal_length = focal_length
+        self.camera = camera
+        self.focal_length = camera.intrinsics[0, 0]
         self.hand_width = hand_width
+        # TODO FIX THIS
+        self.u_camera_center = 1280 / 2.0
+        self.v_camera_center = 960 / 2.0
 
     def call(self, boxes2D):
         hands_center = []
         for box in boxes2D:
-            x_min, y_min, x_max, y_max = box.coordinates
-            x_center = (x_min + x_max) / (2 * self.focal_length)
-            y_center = (y_min + y_max) / (2 * self.focal_length)
-            z_center = (self.hand_width * focal_length) / (y_max - y_min)
+            u_box_center, v_box_center = box.center
+            # print('BOX CENTER', u_box_center, v_box_center)
+            x_center = (u_box_center - self.u_camera_center) / self.focal_length
+            y_center = (v_box_center - self.v_camera_center) / self.focal_length
+            z_center = (self.hand_width * focal_length) / box.height
             hands_center.append([x_center, y_center, z_center])
         print(hands_center)
         return hands_center
@@ -41,7 +46,7 @@ pipeline = SequentialProcessor()
 pipeline.add(DetectMinimalHand(SSD512HandDetection(),
                                MinimalHandPoseEstimation(right_hand=False)))
 pipeline.add(pr.UnpackDictionary(['image', 'boxes2D', 'keypoints2D']))
-pipeline.add(pr.ControlMap(EstimateHandPosition(focal_length), [1], [1]))
+pipeline.add(pr.ControlMap(EstimateHandPosition(camera), [1], [1]))
 pipeline.add(pr.WrapOutput(['image', 'position']))
 
 
