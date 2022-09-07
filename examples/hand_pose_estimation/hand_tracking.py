cb8cac047c00cff2bd9c6d40bba7e1def5411288
Octavio Arriaga <arriaga.camargo@gmail.com>
2022-09-07 09:26:23 +0200

    Pack example into a single processor function

--- examples/hand_pose_estimation/hand_tracking.py
+++ examples/hand_pose_estimation/hand_tracking.py
@@ -6,7 +6,7 @@ from paz import processors as pr
 
 
 parser = argparse.ArgumentParser(description='Minimal hand keypoint detection')
-parser.add_argument('-c', '--camera_id', type=int, default=0,
+parser.add_argument('-c', '--camera_id', type=int, default=4,
                     help='Camera device ID')
 parser.add_argument('-HFOV', '--horizontal_field_of_view', type=float,
                     default=75, help='Horizontal field of view in degrees')
@@ -17,9 +17,9 @@ camera.intrinsics_from_HFOV(args.horizontal_field_of_view)
 focal_length = camera.intrinsics[0, 0]
 
 
-class EstimateTranslation3D(pr.Processor):
-    def __init__(self, camera, real_width=10):
-        super(EstimateTranslation3D, self).__init__()
+class Translation3DFromBoxWidth(pr.Processor):
+    def __init__(self, camera, real_width=30):
+        super(Translation3DFromBoxWidth, self).__init__()
         self.camera = camera
         self.real_width = real_width
         self.focal_length = self.camera.intrinsics[0, 0]
@@ -36,16 +36,30 @@ class EstimateTranslation3D(pr.Processor):
             x_center = (z_center * u) / self.focal_length
             y_center = (z_center * v) / self.focal_length
             hands_center.append([x_center, y_center, z_center])
-        print(hands_center)
         return hands_center
 
 
-pipeline = SequentialProcessor()
-pipeline.add(SSD512MinimalHandPose())
-pipeline.add(pr.UnpackDictionary(['image', 'boxes2D', 'keypoints2D']))
-pipeline.add(pr.ControlMap(EstimateTranslation3D(camera), [1], [1]))
-pipeline.add(pr.WrapOutput(['image', 'position']))
+class PrintTopics(pr.Processor):
+    def __init__(self, topics):
+        super(PrintTopics, self).__init__()
+        self.topics = topics
+
+    def call(self, dictionary):
+        [print(dictionary[topic]) for topic in self.topics]
+        return dictionary
 
 
+class HandStateEstimation(SequentialProcessor):
+    def __init__(self, camera):
+        super(HandStateEstimation, self).__init__()
+        intro_topics = ['image', 'boxes2D', 'keypoints2D', 'keypoints3D']
+        self.add(SSD512MinimalHandPose())
+        self.add(pr.UnpackDictionary(intro_topics))
+        self.add(pr.ControlMap(Translation3DFromBoxWidth(camera), [1], [4], {1: 1}))
+        outro_topics = intro_topics + ['translation3D']
+        self.add(pr.WrapOutput(outro_topics))
+
+
+pipeline = HandStateEstimation(camera)
 player = VideoPlayer((640, 480), pipeline, camera)
 player.run()
