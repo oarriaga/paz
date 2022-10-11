ee32417a0ca3596b2bf93184ebaf8956527f88ad
ManojKumarMuru <manummk95@gmail.com>
2022-10-11 21:57:41 +0200

    generate_configurations function split into several simpler functions and docstrings added

--- examples/efficientdet/anchors.py
+++ examples/efficientdet/anchors.py
@@ -5,6 +5,7 @@ from paz.backend.boxes import to_center_form
 def compute_feature_sizes(image_size, max_level):
     """Computes the size of features that are given as input to each of
         EfficientNet layers.
+
     # Arguments
         image_size: Tuple, (Height, Width) of the input image.
         max_level: Int, maximum level for features.
@@ -26,7 +27,7 @@ def generate_configurations(feature_sizes, min_level, max_level,
                             num_scales, aspect_ratios, anchor_scale):
     """Generates configurations or in other words different combinations of
         strides, octave scales, aspect ratios and anchor scales for each level
-        of the EfficientNet layers that feeds BiFPN layers.
+        of the EfficientNet layers that feeds BiFPN network.
 
     # Arguments:
         feature_sizes: Numpy array representing the feature sizes of inputs to
@@ -45,27 +46,119 @@ def generate_configurations(feature_sizes, min_level, max_level,
             ratios and anchor scales.
     """
     num_levels = max_level + 1 - min_level
+    scale_aspect_ratio_combinations = (
+        len(range(num_scales)) * len(aspect_ratios))
+    features_H, features_W = build_features(
+        feature_sizes, min_level, max_level, scale_aspect_ratio_combinations)
+    octave_scales = build_octaves(num_scales, aspect_ratios, num_levels)
+    aspects = build_aspects(aspect_ratios, num_scales, num_levels)
+    strides_y, strides_x = build_strides(
+        feature_sizes, features_H, features_W, num_levels)
+    anchor_scales = build_scales(
+        anchor_scale, scale_aspect_ratio_combinations, num_levels)
+    return ((strides_y, strides_x, octave_scales, aspects, anchor_scales),
+            num_levels, scale_aspect_ratio_combinations)
+
+
+def build_strides(feature_sizes, features_H, features_W, num_levels):
+    """Generates strides for each EfficientNet layer that feeds BiFPN network.
+
+    # Arguments:
+        feature_sizes: Numpy array representing the feature sizes of inputs to
+            EfficientNet layers.
+        features_H: Numpy array representing the height of the input features.
+        features_W: Numpy array representing the width of the input features.
+        num_levels: Int, representing the number of feature levels.
+
+    # Returns:
+        Tuple: Containing strides in y and x direction.
+    """
     base_feature_H, base_feature_W = feature_sizes[0]
-    scale_aspect_ratio_combinations = (len(range(num_scales))
-                                       * len(aspect_ratios))
+    strides_y = np.reshape(
+        base_feature_H*np.reciprocal(features_H), (num_levels, -1))
+    strides_x = np.reshape(
+        base_feature_W*np.reciprocal(features_W), (num_levels, -1))
+    return strides_y, strides_x
+
+
+def build_features(feature_sizes, min_level, max_level,
+                   scale_aspect_ratio_combinations):
+    """Calculates feature height and width for each EfficientNet layer that
+    feeds BiFPN network.
+
+    # Arguments:
+        feature_sizes: Numpy array representing the feature sizes of inputs to
+            EfficientNet layers.
+        min_level: Int, Number representing the index of the earliest
+            EfficientNet layer that feeds the BiFPN layers.
+        max_level: Int, Number representing the index of the last EfficientNet
+            layer that feeds the BiFPN layers.
+        scale_aspect_ratio_combinations: Int, representing the number of
+        possible combinations of scales and aspect ratios.
+
+    # Returns:
+        Tuple: Containing feature height and width.
+    """
     feature_H = feature_sizes[min_level: max_level + 1][:, 0]
     feature_W = feature_sizes[min_level: max_level + 1][:, 1]
     features_H = np.repeat(feature_H, scale_aspect_ratio_combinations)
     features_W = np.repeat(feature_W, scale_aspect_ratio_combinations)
+    return features_H, features_W
+
+
+def build_octaves(num_scales, aspect_ratios, num_levels):
+    """Calculates octave scales for each EfficientNet layer that feeds
+    BiFPN network.
+
+    # Arguments:
+        num_scales: Int, specifying the number of scales in the
+            anchor boxes.
+        aspect_ratios: List, specifying the aspect rations of the anchor boxes.
+        num_levels: Int, representing the number of feature levels.
+
+    # Returns:
+        octave_scales: Numpy array of shape ``(5, 9)``.
+    """
     scale_octaves = np.repeat(list(range(num_scales)), len(aspect_ratios))
-    aspect = np.tile(aspect_ratios, len(range(num_scales)))
-    strides_y = np.reshape(base_feature_H*np.reciprocal(features_H),
-                           (num_levels, -1))
-    strides_x = np.reshape(base_feature_W*np.reciprocal(features_W),
-                           (num_levels, -1))
     octave_scales = (np.tile(scale_octaves, num_levels)
                      / float(num_scales)).reshape(num_levels, -1)
+    return octave_scales
+
+
+def build_aspects(aspect_ratios, num_scales, num_levels):
+    """Calculates aspect ratios for each EfficientNet layer that feeds
+    BiFPN network.
+
+    # Arguments:
+        aspect_ratios: List, specifying the aspect rations of the anchor boxes.
+        num_scales: Int, specifying the number of scales in the anchor boxes.
+        num_levels: Int, representing the number of feature levels.
+
+    # Returns:
+        aspects: Numpy array of shape ``(5, 9)``.
+    """
+    aspect = np.tile(aspect_ratios, len(range(num_scales)))
     aspects = np.tile(aspect, num_levels).reshape(num_levels, -1)
-    anchor_scales = np.reshape(np.repeat(anchor_scale,
-                               scale_aspect_ratio_combinations),
-                               (num_levels, -1))
-    return ((strides_y, strides_x, octave_scales, aspects, anchor_scales),
-            num_levels, scale_aspect_ratio_combinations)
+    return aspects
+
+
+def build_scales(anchor_scale, scale_aspect_ratio_combinations, num_levels):
+    """Calculates anchor scales for each EfficientNet layer that feeds
+    BiFPN network.
+
+    # Arguments:
+        anchor_scale: Numpy array representing the scales of the anchor box.
+        scale_aspect_ratio_combinations: Int, representing the number of
+            possible combinations of scales and aspect ratios.
+        num_levels: Int, representing the number of feature levels.
+
+    # Returns:
+        anchor_scales: Numpy array of shape ``(5, 9)``.
+    """
+    anchor_scales = np.reshape(
+        np.repeat(anchor_scale, scale_aspect_ratio_combinations),
+        (num_levels, -1))
+    return anchor_scales
 
 
 def compute_aspect_ratio(aspect):
@@ -135,12 +228,10 @@ def generate_level_boxes(strides_y, strides_x, octave_scales, aspects,
     """
     boxes_level = []
     for combination in range(scale_aspect_ratio_combinations):
-        box_coordinates = compute_box_coordinates(strides_y[combination],
-                                                  strides_x[combination],
-                                                  octave_scales[combination],
-                                                  aspects[combination],
-                                                  anchor_scales[combination],
-                                                  image_size)
+        box_coordinates = compute_box_coordinates(
+            strides_y[combination], strides_x[combination],
+            octave_scales[combination], aspects[combination],
+            anchor_scales[combination], image_size)
         center_x, center_y, anchor_x, anchor_y = box_coordinates
         boxes = np.vstack((center_x - anchor_x, center_y - anchor_y,
                            center_x + anchor_x, center_y + anchor_y))
@@ -169,20 +260,15 @@ def generate_anchors(feature_sizes, min_level, max_level, num_scales,
         anchors: Numpy array of shape ``(49104, 4)``.
     """
     ((strides_y, strides_x, octave_scales, aspects, anchor_scales), num_levels,
-     scale_aspect_ratio_combinations) = generate_configurations(feature_sizes,
-                                                                min_level,
-                                                                max_level,
-                                                                num_scales,
-                                                                aspect_ratios,
-                                                                anchor_scales)
+     scale_aspect_ratio_combinations) = generate_configurations(
+        feature_sizes, min_level, max_level, num_scales,
+        aspect_ratios, anchor_scales)
     boxes_all = []
     for level in range(num_levels):
-        boxes_level = generate_level_boxes(strides_y[level], strides_x[level],
-                                           octave_scales[level],
-                                           aspects[level],
-                                           anchor_scales[level],
-                                           image_size,
-                                           scale_aspect_ratio_combinations)
+        boxes_level = generate_level_boxes(
+            strides_y[level], strides_x[level], octave_scales[level],
+            aspects[level], anchor_scales[level], image_size,
+            scale_aspect_ratio_combinations)
         boxes_level = np.concatenate(boxes_level, axis=1)
         boxes_all.append(boxes_level.reshape([-1, 4]))
     anchors = np.vstack(boxes_all).astype('float32')
@@ -211,8 +297,8 @@ def build_prior_boxes(min_level, max_level, num_scales,
     """
     anchor_scales = np.repeat(anchor_scale, max_level - min_level + 1)
     feature_sizes = compute_feature_sizes(image_size, max_level)
-    prior_boxes = generate_anchors(feature_sizes, min_level, max_level,
-                                   num_scales, aspect_ratios, image_size,
-                                   anchor_scales)
+    prior_boxes = generate_anchors(
+        feature_sizes, min_level, max_level, num_scales, aspect_ratios,
+        image_size, anchor_scales)
     prior_boxes = to_center_form(prior_boxes)
     return prior_boxes
