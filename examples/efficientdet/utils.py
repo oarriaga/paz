95582ed3a5e2157395d2ff632f3672be0dcd07f1
ManojKumarMuru <manummk95@gmail.com>
2022-11-30 21:12:31 +0100

    Unused modules removed

--- examples/efficientdet/utils.py
+++ examples/efficientdet/utils.py
@@ -1,53 +1,14 @@
-import paz.processors as pr
 import tensorflow as tf
 import tensorflow.keras.backend as K
-from paz.abstract import SequentialProcessor
-from paz.processors.image import RGB_IMAGENET_MEAN, LoadImage
+from paz.processors.image import LoadImage
 from tensorflow import keras
 from tensorflow.keras.layers import Activation, Concatenate, Flatten, Reshape
-
-import necessary_imports as ni
-from necessary_imports import RGB_IMAGENET_STDEV
 
 # Mock input image.
 file_name = ('/home/manummk95/Desktop/efficientdet_BKP/paz/'
              'examples/efficientdet/000132.jpg')
 loader = LoadImage()
 raw_images = loader(file_name)
-
-
-def get_class_name_efficientdet(dataset_name):
-    """Returns COCO and VOC dataset class names.
-
-    # Arguments:
-        dataset_name: Str, dataset name.
-
-    # Returns:
-        List: class names.
-    """
-    if dataset_name == 'COCO':
-        return ['person', 'bicycle', 'car', 'motorcycle',
-                'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
-                'fire hydrant', '0', 'stop sign', 'parking meter', 'bench',
-                'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant',
-                'bear', 'zebra', 'giraffe', '0', 'backpack', 'umbrella', '0',
-                '0', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis',
-                'snowboard', 'sports ball', 'kite', 'baseball bat',
-                'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
-                'bottle', '0', 'wine glass', 'cup', 'fork', 'knife', 'spoon',
-                'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli',
-                'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair',
-                'couch', 'potted plant', 'bed', '0', 'dining table', '0', '0',
-                'toilet', '0', 'tv', 'laptop', 'mouse', 'remote', 'keyboard',
-                'cell phone', 'microwave', 'oven', 'toaster', 'sink',
-                'refrigerator', '0', 'book', 'clock', 'vase', 'scissors',
-                'teddy bear', 'hair drier', 'toothbrush']
-
-    elif dataset_name == 'VOC':
-        return ['background', 'aeroplane', 'bicycle', 'bird', 'boat',
-                'bottle', 'bus', 'car', 'cat', 'chair', 'cow',
-                'diningtable', 'dog', 'horse', 'motorbike', 'person',
-                'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor']
 
 
 class GetDropConnect(keras.layers.Layer):
@@ -69,28 +30,6 @@ class GetDropConnect(keras.layers.Layer):
             return output
         else:
             return features
-
-
-def efficientdet_preprocess(image, image_size):
-    """Preprocess image for EfficientDet model.
-
-    # Arguments
-        image: Tensor, raw input image.
-        image_size: Tensor, size to resize raw image.
-
-    # Returns
-        image: Array, resized and preprocessed image
-        image_scale: Array, scale to reconstruct raw image.
-    """
-
-    preprocessing = SequentialProcessor([
-        pr.CastImage(float),
-        pr.SubtractMeanImage(mean=RGB_IMAGENET_MEAN),
-        ni.DivideStandardDeviationImage(standard_deviation=RGB_IMAGENET_STDEV),
-        ni.ScaledResize(image_size=image_size),
-        ])
-    image, image_scale = preprocessing(image)
-    return image, image_scale
 
 
 def create_multibox_head(branch_tensors, num_levels, num_classes,
