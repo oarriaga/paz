984732f9bb7f69cf847ad472642833b30eb836c8
ManojKumarMuru <manummk95@gmail.com>
2022-12-21 03:30:47 +0100

    Unsure commit i.e refactoring of anchors.py needs review

--- examples/efficientdet/anchors.py
+++ examples/efficientdet/anchors.py
@@ -15,7 +15,10 @@ def build_prior_boxes(model, *args):
     boxes_all = []
     for level_arg in range(len(model.branches)):
         level_config = build_level_configuration(model, *args, level_arg)
-        boxes_level = generate_level_boxes(model, *level_config)
+        boxes_level = []
+        for arg in zip(*level_config):
+            boxes = compute_box_coordinates(model, *arg)
+            boxes_level.append(np.expand_dims(boxes.T, axis=1))
         boxes_level = np.concatenate(boxes_level, axis=1)
         boxes_all.append(boxes_level.reshape([-1, 4]))
     prior_boxes = np.concatenate(boxes_all, axis=0).astype('float32')
@@ -45,27 +48,6 @@ def build_level_configuration(model, num_scales, aspect_ratios,
     return stride_y, stride_x, octave, aspect, scales
 
 
-def generate_level_boxes(model, *args):
-    """Generates anchor box in centre form per feature level.
-
-    # Arguments:
-        args:
-        image_size: Tuple, being input image size.
-
-    # Returns:
-        boxes_level: List containing anchor boxes in centre form.
-    """
-    boxes_level = []
-    for arg in zip(*args):
-        box_coordinates = compute_box_coordinates(model, *arg)
-        center_x, center_y, anchor_x, anchor_y = box_coordinates
-        boxes = np.concatenate(([center_x - anchor_x], [center_y - anchor_y],
-                                [center_x + anchor_x], [center_y + anchor_y]),
-                               axis=0)
-        boxes_level.append(np.expand_dims(boxes.T, axis=1))
-    return boxes_level
-
-
 def build_strides(model, num_scale_aspect, level_arg):
     """Generates layer-wise EfficientNet anchor box strides.
 
@@ -89,8 +71,8 @@ def build_strides(model, num_scale_aspect, level_arg):
     return strides_y, strides_x
 
 
-def compute_box_coordinates(model, stride_y, stride_x, octave_scale, aspect,
-                            anchor_scale):
+def compute_box_coordinates(model, stride_y, stride_x, octave_scale,
+                            aspect, anchor_scale):
     """Calculates anchor box coordinates in centre form.
 
     # Arguments:
@@ -105,14 +87,17 @@ def compute_box_coordinates(model, stride_y, stride_x, octave_scale, aspect,
         Tuple: holding anchor box centre, width and height.
     """
     W, H = model.input.shape[1:3]
-    base_anchor_x = anchor_scale * stride_x * (2 ** octave_scale)
-    base_anchor_y = anchor_scale * stride_y * (2 ** octave_scale)
-    aspect_x, aspect_y = np.sqrt(aspect), 1/np.sqrt(aspect)
-    anchor_x = (base_anchor_x * aspect_x / 2.0) / H
-    anchor_y = (base_anchor_y * aspect_y / 2.0) / W
+    base_anchor_W = anchor_scale * stride_x * (2 ** octave_scale)
+    base_anchor_H = anchor_scale * stride_y * (2 ** octave_scale)
+    aspect_W, aspect_H = np.sqrt(aspect), 1/np.sqrt(aspect)
+    anchor_W = (base_anchor_W * aspect_W / 2.0) / H
+    anchor_H = (base_anchor_H * aspect_H / 2.0) / W
     x = np.arange(stride_x / 2, H, stride_x)
     y = np.arange(stride_y / 2, W, stride_y)
     center_x, center_y = np.meshgrid(x, y)
     center_x = center_x.reshape(-1) / H
     center_y = center_y.reshape(-1) / W
-    return center_x, center_y, anchor_x, anchor_y
+    arg = ([center_x - anchor_W], [center_y - anchor_H],
+           [center_x + anchor_W], [center_y + anchor_H])
+    box_coordinates = np.concatenate(arg, axis=0)
+    return box_coordinates
