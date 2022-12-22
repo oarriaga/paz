a576c22d5f839f6e85063a7f11a44bac7704b314
ManojKumarMuru <manummk95@gmail.com>
2022-12-22 01:04:04 +0100

    compute_box_coordinates() function split into smaller functions

--- examples/efficientdet/anchors.py
+++ examples/efficientdet/anchors.py
@@ -108,14 +108,12 @@ def build_strides(branch_arg, image_shape, branches, num_scale_aspect):
     # Returns:
         Tuple: Containing strides in y and x direction.
     """
-    base_feature_H, base_feature_W = image_shape
+    H_image, W_image = image_shape
     feature_H, feature_W = branches[branch_arg].shape[1:3]
     features_H = np.repeat(feature_H, num_scale_aspect).astype('float32')
     features_W = np.repeat(feature_W, num_scale_aspect).astype('float32')
-    H_inverse = np.reciprocal(features_H)
-    W_inverse = np.reciprocal(features_W)
-    strides_y = base_feature_H * H_inverse
-    strides_x = base_feature_W * W_inverse
+    strides_y = H_image / features_H
+    strides_x = W_image / features_W
     return strides_y, strides_x
 
 
@@ -135,17 +133,82 @@ def compute_box_coordinates(image_shape, stride_y, stride_x, octave_scale,
         Tuple: holding anchor box centre, width and height.
     """
     W, H = image_shape
+    base_anchor = build_base_anchor(scale, stride_x, stride_y, octave_scale)
+    base_anchor_W, base_anchor_H = base_anchor
+    aspect_W, aspect_H = compute_aspect_ratio(aspect)
+    anchor_half_W, anchor_half_H = compute_anchor_dims(
+        base_anchor_W, base_anchor_H, aspect_W, aspect_H, image_shape)
+    center_x, center_y = compute_anchor_centres(
+        stride_x, stride_y, image_shape)
+    x_min, y_min = [center_x - anchor_half_W], [center_y - anchor_half_H]
+    x_max, y_max = [center_x + anchor_half_W], [center_y + anchor_half_H]
+    box_coordinates = np.concatenate((x_min, y_min, x_max, y_max), axis=0)
+    return box_coordinates
+
+
+def build_base_anchor(scale, stride_x, stride_y, octave_scale):
+    """_summary_
+
+    Args:
+        scale (_type_): _description_
+        stride_x (_type_): _description_
+        stride_y (_type_): _description_
+        octave_scale (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     base_anchor_W = scale * stride_x * (2 ** octave_scale)
     base_anchor_H = scale * stride_y * (2 ** octave_scale)
+    return base_anchor_W, base_anchor_H
+
+
+def compute_aspect_ratio(aspect):
+    """_summary_
+
+    Args:
+        aspect (_type_): _description_
+    """
     aspect_W, aspect_H = np.sqrt(aspect), 1/np.sqrt(aspect)
-    anchor_W = (base_anchor_W * aspect_W / 2.0) / H
-    anchor_H = (base_anchor_H * aspect_H / 2.0) / W
+    return aspect_W, aspect_H
+
+
+def compute_anchor_dims(base_anchor_W, base_anchor_H, aspect_W, aspect_H,
+                        image_shape):
+    """_summary_
+
+    Args:
+        base_anchor_W (_type_): _description_
+        base_anchor_H (_type_): _description_
+        aspect_W (_type_): _description_
+        aspect_H (_type_): _description_
+        W (_type_): _description_
+        H (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    W, H = image_shape
+    anchor_half_W = (base_anchor_W * aspect_W / 2.0) / H
+    anchor_half_H = (base_anchor_H * aspect_H / 2.0) / W
+    return anchor_half_W, anchor_half_H
+
+
+def compute_anchor_centres(stride_x, stride_y, image_shape):
+    """_summary_
+
+    Args:
+        stride_x (_type_): _description_
+        stride_y (_type_): _description_
+        image_shape (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    W, H = image_shape
     x = np.arange(stride_x / 2, H, stride_x)
     y = np.arange(stride_y / 2, W, stride_y)
     center_x, center_y = np.meshgrid(x, y)
-    center_x = center_x.reshape(-1) / H
-    center_y = center_y.reshape(-1) / W
-    arg = ([center_x - anchor_W], [center_y - anchor_H],
-           [center_x + anchor_W], [center_y + anchor_H])
-    box_coordinates = np.concatenate(arg, axis=0)
-    return box_coordinates
+    normalized_center_x = center_x.flatten() / H
+    normalized_center_y = center_y.flatten() / W
+    return normalized_center_x, normalized_center_y
