f982055ee8fa23dd9da33fb36e7f82878dc6d7a2
Octavio Arriaga <arriaga.camargo@gmail.com>
2023-01-21 16:45:43 +0100

    Refactor random sample crop and fix bug with low default probability

--- tests/paz/processors/geometric_test.py
+++ tests/paz/processors/geometric_test.py
@@ -1,7 +1,10 @@
+import os
 import pytest
 import numpy as np
 
+from tensorflow.keras.utils import get_file
 from paz import processors as pr
+from paz.backend.image import load_image
 
 
 @pytest.fixture
@@ -26,3 +29,28 @@ def test_random_sample_crop_pass_by_reference(boxes_with_label):
     crop = pr.RandomSampleCrop(probability=1.0)
     crop(np.ones((300, 300, 3)), boxes_with_label)
     assert np.all(initial_boxes_with_label == boxes_with_label)
+
+
+def test_random_sample_crop():
+    URL = ('https://github.com/oarriaga/altamira-data/releases/download'
+           '/v0.9/object_detection_augmentation.png')
+    filename = os.path.basename(URL)
+    image_fullpath = get_file(filename, URL, cache_subdir='paz/tutorials')
+    true_image = load_image(image_fullpath)
+    H, W = true_image.shape[:2]
+    true_boxes = np.array([[200 / W, 60 / H, 300 / W, 200 / H, 1],
+                           [100 / W, 90 / H, 400 / W, 300 / H, 2]])
+
+    class AugmentBoxes(pr.SequentialProcessor):
+        def __init__(self, mean=pr.BGR_IMAGENET_MEAN):
+            super(AugmentBoxes, self).__init__()
+            self.add(pr.ToImageBoxCoordinates())
+            self.add(pr.RandomSampleCrop())
+            self.add(pr.ToNormalizedBoxCoordinates())
+
+    augment_boxes = AugmentBoxes()
+    for _ in range(1000):
+        crop_image, crop_boxes = augment_boxes(true_image, true_boxes)
+        assert len(crop_boxes.shape) == 2
+        assert np.alltrue(crop_boxes[:, 0] < crop_boxes[:, 2])
+        assert np.alltrue(crop_boxes[:, 1] < crop_boxes[:, 3])
