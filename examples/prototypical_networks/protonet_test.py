1b577f908db0d0c1e5d8b0743db09eb68acd07b2
Octavio Arriaga <arriaga.camargo@gmail.com>
2023-01-23 11:44:28 +0100

    Add basic test for metric checking

--- examples/prototypical_networks/protonet_test.py
+++ examples/prototypical_networks/protonet_test.py
@@ -1,6 +1,7 @@
 import pytest
 from keras.utils.layer_utils import count_params
-from protonet import Embedding, PROTONET
+from protonet import Embedding, PROTONET, compute_pairwise_distances
+import numpy as np
 
 
 def parametrize(Model, model_args, output_shape, params, static_params):
@@ -40,3 +41,51 @@ test_embedding = parametrize(Embedding,
 test_protonet = parametrize(PROTONET,
                             [Embedding((28, 28, 1), 4), 20, 5, 1, (28, 28, 1)],
                             [(20, 20)], [111_936], [512])
+
+
+def test_pairwise_distances_zero():
+    a = np.random.rand(10, 32)
+    distances = compute_pairwise_distances(a, a).numpy()
+    assert np.allclose(np.diag(distances), 0.0)
+    assert distances.shape == (10, 10)
+
+
+def test_pairwise_distances_positivity():
+    a = np.random.rand(7, 100)
+    b = np.random.rand(8, 100)
+    distances = compute_pairwise_distances(a, b).numpy()
+    assert np.alltrue(distances > 0)
+    assert distances.shape == (7, 8)
+
+
+def test_pairwise_distances_symmetry():
+    a = np.random.rand(30, 5)
+    b = np.random.rand(15, 5)
+    distances_AB = compute_pairwise_distances(a, b).numpy()
+    distances_BA = compute_pairwise_distances(b, a).numpy()
+    assert np.allclose(distances_AB, distances_BA.T)
+    assert distances_AB.shape == (30, 15)
+    assert distances_BA.shape == (15, 30)
+
+
+def test_pairwise_distances_orthogonal():
+    a = np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    b = np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    distances = compute_pairwise_distances(a, b).numpy()
+    assert np.allclose(distances, 0)
+
+
+def test_pairwise_distances_with_values_to_zero():
+    a = np.array([[1.0, 1.0, 1.0], [1.0, 0.0, 0.0], [1.0, 1.0, 0.0]])
+    b = np.array([[0.0, 0.0, 0.0]])
+    values = compute_pairwise_distances(a, b).numpy()
+    distances = np.array([[1.0], [1.0 / 3.0], [2.0 / 3.0]])
+    assert np.allclose(values, distances)
+
+
+def test_pairwise_distances_with_values_to_negative():
+    a = np.array([[1.0, 1.0, 1.0], [1.0, 0.0, 0.0], [1.0, 1.0, 0.0]])
+    b = np.array([[-1.0, -1.0, -1.0]])
+    values = compute_pairwise_distances(a, b).numpy()
+    distances = np.array([[4.0], [2.0], [3.0]])
+    assert np.allclose(values, distances)
