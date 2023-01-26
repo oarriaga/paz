74fa38ceefbc86bdaa8e1533e9796bbf79cda580
ManojKumarMuru <manummk95@gmail.com>
2023-01-26 03:18:45 +0100

    Bounding box visualization reverted to old version

--- examples/efficientdet/demo.py
+++ examples/efficientdet/demo.py
@@ -3,7 +3,6 @@ from paz.backend.image import show_image, write_image
 from paz.datasets import get_class_names
 from paz.pipelines.detection import DetectSingleShot
 from paz.processors.image import LoadImage
-from detection import DrawBoxes2D
 from efficientdet import EFFICIENTDETD0
 
 IMAGE_PATH = ('/home/manummk95/Desktop/efficientdet_BKP/paz/'
@@ -21,7 +20,6 @@ if __name__ == "__main__":
     model.load_weights(weights_path)
 
     detect = DetectSingleShot(model, get_class_names('VOC'), 0.5, 0.45)
-    detect.draw_boxes2D = DrawBoxes2D(get_class_names('VOC'))
     detections = detect(raw_image)
 
     show_image(detections['image'])
