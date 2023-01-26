987b0009135630112b4651df823235dd15360898
ManojKumarMuru <manummk95@gmail.com>
2023-01-26 23:22:08 +0100

    Basic cleanup

--- examples/efficientdet/demo.py
+++ examples/efficientdet/demo.py
@@ -1,14 +1,10 @@
-from paz.backend.image import show_image, write_image
 from paz.processors.image import LoadImage
+from paz.backend.image import show_image
 from detection import EFFICIENTDETD0COCO
 
-IMAGE_PATH = ('/home/manummk95/Desktop/efficientdet_BKP/paz/examples/'
-              'efficientdet/img.jpg')
-
-if __name__ == "__main__":
-    raw_image = LoadImage()(IMAGE_PATH)
-    detect = EFFICIENTDETD0COCO()
-    detections = detect(raw_image)
-    show_image(detections['image'])
-    write_image('detections.png', detections['image'])
-    print('task completed')
+IMAGE_PATH = ('/home/manummk95/Desktop/efficientdet_BKP/'
+              'paz/examples/efficientdet/img.jpg')
+image = LoadImage()(IMAGE_PATH)
+detect = EFFICIENTDETD0COCO()
+detections = detect(image)
+show_image(detections['image'])
