55941b957d59e8030e5bb46dbc55ef63c6ac7398
Alexander Fabisch <alexander.fabisch@dfki.de>
2023-02-17 10:00:19 +0100

    Extend setup.py (e.g., by long description)

--- setup.py
+++ setup.py
@@ -2,11 +2,17 @@ from setuptools import setup
 from setuptools import find_packages
 import paz
 
-setup(name='pypaz',
-      version=paz.__version__,
-      description='Perception for Autonomous Systems',
-      author='Octavio Arriaga',
-      author_email='octavio.arriaga@dfki.de',
-      license='MIT',
-      install_requires=['opencv-python', 'tensorflow', 'numpy'],
-      packages=find_packages())
+
+if __name__ == "__main__":
+    with open("README.md", "r") as f:
+        long_description = f.read()
+    setup(name='pypaz',
+          version=paz.__version__,
+          description='Perception for Autonomous Systems',
+          long_description=long_description,
+          author='Octavio Arriaga',
+          author_email='octavio.arriaga@dfki.de',
+          url='https://github.com/oarriaga/paz/',
+          license='MIT',
+          install_requires=['opencv-python', 'tensorflow', 'numpy'],
+          packages=find_packages())
