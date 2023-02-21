a7b516e35f2048a4669bc911245527dace2ef7c2
amine789 <amine.bahlouli1993@gmail.com>
2023-02-21 13:15:10 +0100

    Human pose estimation (create keypoints processor)

--- examples/human_pose_estimation_3d/viz.py
+++ examples/human_pose_estimation_3d/viz.py
@@ -2,7 +2,7 @@
 import numpy as np
 
 
-def show3Dpose(channels, ax, lcolor="#3498db", rcolor="#e74c3c",
+def show3Dpose(keypoints, ax, lcolor="#3498db", rcolor="#e74c3c",
                add_labels=False):  # blue, orange
     """Visualize a 3d skeleton
 
@@ -13,12 +13,12 @@ def show3Dpose(channels, ax, lcolor="#3498db", rcolor="#e74c3c",
         rcolor: color for right part of the body
         add_labels: whether to add coordinate labels   
     """
-    if channels.shape[1] == 48 or channels.shape[1]==16:
-        vals = np.reshape(channels, (channels.shape[0], 16, -1))
+    if keypoints.shape[1] == 48 or keypoints.shape[1]==16:
+        keypoints = np.reshape(keypoints, (keypoints.shape[0], 16, -1))
         I = np.array([1, 2, 3, 1, 5, 6, 1, 8, 9, 9, 11, 12, 9, 14, 15]) - 1
         J = np.array([2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]) - 1
     else:
-        vals = np.reshape(channels, (channels.shape[0], 32, -1))
+        keypoints = np.reshape(keypoints, (keypoints.shape[0], 32, -1))
         I = np.array([1, 2, 3, 1, 7, 8, 1, 13, 14, 14, 18, 19, 14, 26, 27]) - 1
         J = np.array(
             [2, 3, 4, 7, 8, 9, 13, 14, 16, 18, 19, 20, 26, 27, 28]) - 1
@@ -31,14 +31,14 @@ def show3Dpose(channels, ax, lcolor="#3498db", rcolor="#e74c3c",
             ax.plot(x, y, z, lw=2, c=lcolor if LR[i] else rcolor)
 
     i = 0
-    for person in vals:
+    for person in keypoints:
         if i == 1:
             plot_person(person, lcolor="#9b59b6", rcolor="#2ecc71")
         else:
             plot_person(person)
         i += 1
     RADIUS = 750
-    xroot, yroot, zroot = vals[0, 0, 0], vals[0, 0, 1], vals[0, 0, 2]
+    xroot, yroot, zroot = keypoints[0, 0, 0], keypoints[0, 0, 1], keypoints[0, 0, 2]
     ax.set_xlim3d([-RADIUS + xroot, RADIUS + xroot])
     ax.set_zlim3d([-RADIUS + zroot, RADIUS + zroot])
     ax.set_ylim3d([-RADIUS + yroot, RADIUS + yroot])
@@ -51,12 +51,12 @@ def show3Dpose(channels, ax, lcolor="#3498db", rcolor="#e74c3c",
     ax.w_zaxis.set_pane_color(white)
 
 
-def show2Dpose(channels, ax, lcolor="#3498db", rcolor="#e74c3c",
+def show2Dpose(keypoints, ax, lcolor="#3498db", rcolor="#e74c3c",
                add_labels=False):
     """Visualize a 2d skeleton
 
     # Arguments
-        channels: nx64 vector. n is num persons detected to plot.
+        keypoints: nx64 vector. n is num persons detected to plot.
         ax: matplotlib axis to draw on
         lcolor: color for left part of the body
         rcolor: color for right part of the body
@@ -64,12 +64,12 @@ def show2Dpose(channels, ax, lcolor="#3498db", rcolor="#e74c3c",
     # Returns
         Nothing. Draws on ax.
     """
-    if channels.shape[1] == 32:
-        vals = np.reshape(channels, (channels.shape[0], 16, -1))
+    if keypoints.shape[1] == 32:
+        keypoints = np.reshape(keypoints, (keypoints.shape[0], 16, -1))
         I = np.array([1, 2, 3, 1, 5, 6, 1, 8, 9, 9, 11, 12, 9, 14, 15]) - 1
         J = np.array([2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]) - 1
     else:
-        vals = np.reshape(channels, (channels.shape[0], 32, -1))
+        keypoints = np.reshape(keypoints, (keypoints.shape[0], 32, -1))
         I = np.array([1, 2, 3, 1, 7, 8, 1, 13, 14, 14, 18, 19, 14, 26, 27]) - 1
         J = np.array(
             [2, 3, 4, 7, 8, 9, 13, 14, 16, 18, 19, 20, 26, 27, 28]) - 1
@@ -81,10 +81,10 @@ def show2Dpose(channels, ax, lcolor="#3498db", rcolor="#e74c3c",
                     range(2)]
             ax.plot(x, y, lw=2, c=lcolor if LR[i] else rcolor)
 
-    for person in vals:
+    for person in keypoints:
         plot_person(person)
     RADIUS = 350  # space around the subject
-    xroot, yroot = vals[0, 0, 0], vals[0, 0, 1]
+    xroot, yroot = keypoints[0, 0, 0], keypoints[0, 0, 1]
     ax.set_xlim([-RADIUS + xroot, RADIUS + xroot])
     ax.set_ylim([-RADIUS + yroot, RADIUS + yroot])
     if add_labels:
