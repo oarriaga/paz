905204845edcfe9b5dc166e76971c28d3d89936a
Octavio Arriaga <arriaga.camargo@gmail.com>
2023-06-10 15:21:36 +0200

    Remove long description due to parsing errors with pypi

--- setup.py
+++ setup.py
@@ -4,12 +4,10 @@ import paz
 
 
 if __name__ == "__main__":
-    with open("README.md", "r") as f:
-        long_description = f.read()
+
     setup(name='pypaz',
           version=paz.__version__,
           description='Perception for Autonomous Systems',
-          long_description=long_description,
           author='Octavio Arriaga',
           author_email='octavio.arriaga@dfki.de',
           url='https://github.com/oarriaga/paz/',
