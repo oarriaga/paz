fd278f850732e21461e95318330715cbd671ff39
Cedric Carl-Franek Kränzle <cedric_carl-franek.kraenzle@dfki.de>
2023-08-18 10:12:10 +0200

    Fixed line spacing

--- examples/visual_voice_activity_detection/training/train_v2.py
+++ examples/visual_voice_activity_detection/training/train_v2.py
@@ -68,21 +68,21 @@ model = None
 
 # Python 3.8 does not support switch case statements :(
 if args.model == "VVAD_LRS3":
-  model = VVAD_LRS3_LSTM()
+    model = VVAD_LRS3_LSTM()
 
-  # TODO UserWarning: "`binary_crossentropy` received `from_logits=True`, but the `output` argument was produced by a Sigmoid activation and thus does not represent logits. Was this intended?
-  #   output, from_logits = _get_logits(
-  loss = BinaryCrossentropy()
-  optimizer = SGD()
+    # TODO UserWarning: "`binary_crossentropy` received `from_logits=True`, but the `output` argument was produced by a Sigmoid activation and thus does not represent logits. Was this intended?
+    #   output, from_logits = _get_logits(
+    loss = BinaryCrossentropy()
+    optimizer = SGD()
 
-  model.compile(loss=loss, optimizer=optimizer, metrics=[tf.keras.metrics.BinaryAccuracy(threshold=0.5), 'TrueNegatives', 'TruePositives', 'FalseNegatives', 'FalsePositives'])
+    model.compile(loss=loss, optimizer=optimizer, metrics=[tf.keras.metrics.BinaryAccuracy(threshold=0.5), 'TrueNegatives', 'TruePositives', 'FalseNegatives', 'FalsePositives'])
 elif args.model == "CNN2Plus1D":
-  model = CNN2Plus1D()
+    model = CNN2Plus1D()
 
-  loss = BinaryCrossentropy(from_logits=False)  # Alternative for two label Classifications: Hinge Loss or Squared Hinge Loss
-  optimizer = Adam(learning_rate=0.0001)
+    loss = BinaryCrossentropy(from_logits=False)  # Alternative for two label Classifications: Hinge Loss or Squared Hinge Loss
+    optimizer = Adam(learning_rate=0.0001)
 
-  model.compile(loss="binary_crossentropy", optimizer="rmsprop", metrics=['accuracy'])
+    model.compile(loss="binary_crossentropy", optimizer="rmsprop", metrics=['accuracy'])
 elif args.model == 'MoViNets':
     # model = MoViNets()
     raise NotImplementedError
