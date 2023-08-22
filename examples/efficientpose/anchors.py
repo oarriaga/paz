d9bdd1207e21ba290e9b2af884e9326f35b36588
ManojKumarMuru <manummk95@gmail.com>
2023-08-22 17:55:45 +0200

    Minor refactoring

--- examples/efficientpose/anchors.py
+++ examples/efficientpose/anchors.py
@@ -36,14 +36,14 @@ def build_translation_anchors(image_shape, branches,
 
 def make_branch_translation_boxes(strides_y, strides_x, branch_arg,
                                   branches, num_scale_aspect):
-    centers = compute_translation_centers(strides_y, strides_x, branch_arg,
-                                          branches, num_scale_aspect)
-    translation_anchors = append_stride_to_centre(centers, strides_x)
+    centers = compute_translation_centers(
+        strides_y, strides_x, branch_arg, branches)
+    translation_anchors = append_stride_to_centre(
+        centers, strides_x, num_scale_aspect)
     return translation_anchors
 
 
-def compute_translation_centers(strides_y, strides_x, branch_arg,
-                                branches, num_scale_aspect):
+def compute_translation_centers(strides_y, strides_x, branch_arg, branches):
     feature_H, feature_W = branches[branch_arg].shape[1:3]
     center_x = (np.arange(0, feature_H) + 0.5) * strides_x[0]
     center_y = (np.arange(0, feature_W) + 0.5) * strides_y[0]
@@ -51,11 +51,11 @@ def compute_translation_centers(strides_y, strides_x, branch_arg,
     center_x_flattened = center_x.reshape(-1, 1)
     center_y_flattened = center_y.reshape(-1, 1)
     centers = np.concatenate((center_x_flattened, center_y_flattened), axis=1)
-    centers = np.repeat(centers, num_scale_aspect, axis=0)
     return centers
 
 
-def append_stride_to_centre(centers, strides_x):
+def append_stride_to_centre(centers, strides_x, num_scale_aspect):
+    centers = np.repeat(centers, num_scale_aspect, axis=0)
     num_translation_anchors = centers.shape[0]
     stride_array = np.full((num_translation_anchors, 1), strides_x[0])
     translation_anchors = np.concatenate((centers, stride_array), axis=-1)
