2a2160d10226d759bd296ee1d92210b61001ede1
ManojKumarMuru <manummk95@gmail.com>
2023-08-23 22:01:47 +0200

    Minor refactoring

--- examples/efficientpose/detection.py
+++ examples/efficientpose/detection.py
@@ -138,10 +138,10 @@ class EfficientPosePostprocess(Processor):
 
     def call(self, output, image_scale):
         box_data = self.postprocess(output)
-        box_data = self.scale(box_data, 1 / image_scale)
         box_data, class_labels = self.nms_per_class(box_data)
         box_data = self.merge_box_and_class(box_data, class_labels)
         box_data = self.filter_boxes(box_data)
+        box_data = self.scale(box_data, 1 / image_scale)
         boxes2D = self.to_boxes2D(box_data)
         boxes2D = self.round_boxes(boxes2D)
         return boxes2D
