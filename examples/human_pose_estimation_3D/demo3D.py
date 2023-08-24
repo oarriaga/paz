63b87629898f3c5c9e3da73a63049a1591346f00
proneetsharma <sproneet@gmail.com>
2023-08-24 15:46:42 +0200

    Add demo for Human pose

--- examples/human_pose_estimation_3D/demo3D.py
+++ examples/human_pose_estimation_3D/demo3D.py
@@ -1,19 +1,11 @@
-import os
-from paz.backend.camera import Camera
-from paz.backend.image import load_image
-from scipy.optimize import least_squares
-from tensorflow.keras.utils import get_file
-from paz.pipelines import EstimateHumanPose
-from paz.processors import OptimizeHumanPose3D
-from paz.datasets.human36m import args_to_joints3D
-from viz import visualize, show3Dpose
-
 import argparse
-from paz.backend.camera import VideoPlayer
+from viz import show3Dpose
 import matplotlib.pyplot as plt
-from paz.backend.image import resize_image, show_image
-import numpy as np
+from scipy.optimize import least_squares
+from paz.applications import EstimateHumanPose
 from matplotlib.animation import FuncAnimation
+from paz.backend.camera import Camera, VideoPlayer
+from paz.backend.image import resize_image, show_image
 
 
 parser = argparse.ArgumentParser(description='Human3D visualization')
@@ -23,17 +15,15 @@ args = parser.parse_args()
 
 camera = Camera()
 camera.intrinsics_from_HFOV(HFOV=70, image_shape=(640, 480))
-pipeline = EstimateHumanPose()
-optimize = OptimizeHumanPose3D(args_to_joints3D,
-                               least_squares, camera.intrinsics)
+pipeline = EstimateHumanPose(least_squares, camera.intrinsics)
 camera = Camera(args.camera_id)
 player = VideoPlayer((640, 480), pipeline, camera)
 
 
 def animate(player):
     """Opens camera and starts continuous inference using ``pipeline``,
-        until the user presses ``q`` inside the opened window. Plot the
-        3D keypoints on pyplot.
+       until the user presses ``q`` inside the opened window. Plot the
+       3D keypoints on pyplot.
     """
     player.camera.start()
     ax = plt.axes(projection='3d')
@@ -45,20 +35,15 @@ def animate(player):
         image = resize_image(output[player.topic], tuple(player.image_size))
         show_image(image, 'inference', wait=False)
 
-        keypoints2D = output['keypoints2D']
         keypoints3D = output['keypoints3D']
-        pose = optimize(keypoints3D, keypoints2D)
         if len(keypoints3D) == 0:
             return
-        keypoints3D = keypoints3D[0]  # TAKE ONLY THE FIRST PREDICTION
-        xs, ys, zs = np.split(keypoints3D, 3, axis=1)
 
         plt.cla()
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
         ax.set_zlabel('Z')
-        # ax.scatter3D(xs, ys, zs)
-        show3Dpose(pose[2], ax)
+        show3Dpose(keypoints3D, ax)
     return wrapped_animate
 
 
