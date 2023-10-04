5d031aeee5e8e9a8fd7088914872bd161a8d711c
ManojKumarMuru <manummk95@gmail.com>
2023-10-04 20:35:04 +0200

    Basic unit test for pose loss is written

--- examples/efficientpose/efficientpose_test.py
+++ examples/efficientpose/efficientpose_test.py
@@ -28,21 +28,26 @@ def test_multi_pose_loss_zero_condition(dataset_path):
 
 @pytest.mark.parametrize(('num_anchors, loss'),
                          [
-                            (10,   1207.7871),
-                            (100,  1207.7871),
-                            (1000, 1207.7871),
-                            (500,  1207.7871),
-                            (5000, 1207.7871),
+                            (10,   1207.7874),
+                            (100,  1178.727),
+                            (1000, 1111.5929),
+                            (500,  1111.9001),
+                            (5000, 1094.8196),
                          ])
 def test_multi_pose_loss_non_zero_condition(dataset_path, num_anchors, loss):
     model = EFFICIENTPOSEA()
     pose_loss = MultiPoseLoss('08', model.translation_priors[:num_anchors, :],
                               dataset_path)
-    target_shape = (1, num_anchors, 11)
     prediction_shape = (1, num_anchors, 6)
-    y_true = tf.ones(target_shape, dtype=np.float32)
+    rotation = tf.ones((num_anchors, 3), dtype=tf.float32)
+    is_symmetric = tf.zeros((num_anchors, 1), dtype=tf.float32)
+    flag = tf.ones((num_anchors, 1), dtype=tf.float32)
+    y_true = tf.concat([rotation, is_symmetric, is_symmetric,
+                        flag, rotation, flag, flag], axis=1)
+    y_true = tf.expand_dims(y_true, axis=0)
     y_pred = tf.ones(prediction_shape, dtype=np.float32)
     loss = pose_loss.compute_loss(y_true, y_pred)
+
     assert tf.is_tensor(loss), 'Incorrect loss datatype'
     assert loss.shape == [], 'Incorrect loss shape'
     assert not tf.math.reduce_any(tf.math.is_nan(loss)), 'Loss has NaN values'
@@ -63,11 +68,14 @@ def test_loss_gradients(dataset_path, num_anchors):
     model = EFFICIENTPOSEA()
     pose_loss = MultiPoseLoss('08', model.translation_priors[:num_anchors, :],
                               dataset_path)
-    target_shape = (1, num_anchors, 11)
     prediction_shape = (1, num_anchors, 6)
-    y_true = tf.ones(target_shape, dtype=np.float32)
+    rotation = tf.ones((num_anchors, 3), dtype=tf.float32)
+    is_symmetric = tf.zeros((num_anchors, 1), dtype=tf.float32)
+    flag = tf.ones((num_anchors, 1), dtype=tf.float32)
+    y_true = tf.concat([rotation, is_symmetric, is_symmetric,
+                        flag, rotation, flag, flag], axis=1)
+    y_true = tf.expand_dims(y_true, axis=0)
     y_pred = tf.ones(prediction_shape, dtype=np.float32)
-    loss = pose_loss.compute_loss(y_true, y_pred)
 
     with tf.GradientTape(persistent=True) as g:
         g.watch(y_pred)
