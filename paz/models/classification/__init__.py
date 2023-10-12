07317f69a3964f764a4ae8d6087354b3e0b6ba3a
Cedric Carl-Franek Kränzle <cedric_carl-franek.kraenzle@dfki.de>
2023-10-12 14:02:41 +0200

    First tests

--- paz/models/classification/__init__.py
+++ paz/models/classification/__init__.py
@@ -2,3 +2,6 @@ from .xception import build_xception
 from .xception import MiniXception
 from .protonet import ProtoEmbedding
 from .protonet import ProtoNet
+from .MoViNet import MoViNetManuall
+from .MoViNet import MoViNet
+from .MoViNet import MoViNetTut
