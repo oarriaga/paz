0bb48030c589a65d55a86c04571fd169c2ce6f70
ManojKumarMuru <manummk95@gmail.com>
2023-10-22 23:56:30 +0200

    Docstrings added

--- examples/efficientpose/processors.py
+++ examples/efficientpose/processors.py
@@ -336,6 +336,14 @@ def draw_pose6D(image, pose6D, points3D, intrinsics, thickness, color):
 
 
 class MatchPoses(Processor):
+    """Match prior boxes with ground truth poses.
+
+    # Arguments
+        prior_boxes: Numpy array of shape (num_boxes, 4).
+        iou: Float in [0, 1]. Intersection over union in which prior boxes
+            will be considered positive. A positive box is box with a class
+            different than `background`.
+    """
     def __init__(self, prior_boxes, iou=.5):
         self.prior_boxes = prior_boxes
         self.iou = iou
