f97267e6d36817529d9349d69c3a7273d7c96d4b
Cedric Carl-Franek Kränzle <cedric_carl-franek.kraenzle@dfki.de>
2023-11-06 13:39:54 +0100

    Fixed Import

--- paz/models/classification/__init__.py
+++ paz/models/classification/__init__.py
@@ -3,3 +3,6 @@ from .xception import MiniXception
 from .protonet import ProtoEmbedding
 from .protonet import ProtoNet
 from .cnn2Plus1 import CNN2Plus1D
+from .cnn2Plus1 import CNN2Plus1D_Filters
+from .cnn2Plus1 import CNN2Plus1D_Light
+from .cnn2Plus1 import CNN2Plus1D_Layers
