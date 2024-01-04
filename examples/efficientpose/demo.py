4644169f4f7bb3793fb7b575b046a6c57021abd3
Manoj Kumar Murugan <manummk95@gmail.com>
2024-01-04 21:46:31 +0100

    Minor refactoring

--- examples/efficientpose/demo.py
+++ examples/efficientpose/demo.py
@@ -2,12 +2,12 @@ from paz.backend.image import load_image, show_image
 from pose import EFFICIENTPOSEALINEMODDRILLER
 
 IMAGE_PATH = ('/home/manummk95/Desktop/paz/paz/examples/efficientpose/'
-              'Linemod_preprocessed/data/08/rgb/0079.png')
+              'Linemod_preprocessed/data/08/rgb/0002.png')
 
 
 detect = EFFICIENTPOSEALINEMODDRILLER(score_thresh=0.60, nms_thresh=0.45,
                                       show_boxes2D=True, show_poses6D=True)
-detect.model.load_weights('weights.671-2.53.hdf5')
+detect.model.load_weights('weights.4583-1.55.hdf5')
 image = load_image(IMAGE_PATH)
 inferences = detect(image)
 show_image(inferences['image'])
--- examples/efficientpose/processors.py
+++ examples/efficientpose/processors.py
@@ -5,7 +5,12 @@ import paz.processors as pr
 from paz.processors.draw import (quaternion_to_rotation_matrix,
                                  project_to_image, draw_cube)
 from paz.backend.boxes import compute_ious, to_corner_form
-from pose import LINEMOD_CAMERA_MATRIX
+
+LINEMOD_CAMERA_MATRIX = np.array([
+    [572.41140, 000.00000, 325.26110],
+    [000.00000, 573.57043, 242.04899],
+    [000.00000, 000.00000, 001.00000]],
+    dtype=np.float32)
 
 
 class ComputeResizingShape(Processor):
