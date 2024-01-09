2e17c46fd0230d7049e7df4993dbf5a4496e4316
Manoj Kumar Murugan <manummk95@gmail.com>
2024-01-09 19:28:36 +0100

    Docstrings added

--- examples/efficientpose/linemod.py
+++ examples/efficientpose/linemod.py
@@ -6,7 +6,7 @@ from pose import get_class_names
 
 
 class Linemod(Loader):
-    """ Dataset loader for the Linemod dataset.
+    """Dataset loader for the Linemod dataset.
 
     # Arguments
         path: Str, data path to Linemod annotations.
@@ -19,11 +19,12 @@ class Linemod(Loader):
             with values equal to the input size of the model.
 
     # Return
-        data: List of dictionaries with keys corresponding to the image
+        List of dictionaries with keys corresponding to the image
             paths and values numpy arrays of shape
             ``[num_objects, 4 + 1]`` where the ``+ 1`` contains the
             ``class_arg`` and ``num_objects`` refers to the amount of
-            boxes in the image.
+            boxes in the image, pose transformation of shape
+            ``[num_bixes, 11]`` and path to object mask image.
     """
     def __init__(self, path=None, object_id='08', split='train',
                  name='Linemod', input_size=(512, 512)):
@@ -43,6 +44,17 @@ class Linemod(Loader):
 
 
 def compute_class_names(object_id, object_id_to_class_arg):
+    """Creates a list containing class names. The list includes
+    `background` class and the object class e.g. `driller` as strings.
+
+    # Arguments:
+        object_id: Str, ID of the object to train.
+        object_id_to_class_arg: Dict, containing mapping
+            from object IDs to class arguments.
+
+    # Return:
+        List: Containing background and object class names as strings.
+    """
     class_arg = object_id_to_class_arg[int(object_id)]
     class_names_all = get_class_names('Linemod')
     foreground_class = class_names_all[class_arg]
@@ -51,26 +63,27 @@ def compute_class_names(object_id, object_id_to_class_arg):
 
 
 class LinemodParser(object):
-    """ Preprocess the Linemod yaml annotations data.
+    """Preprocess the Linemod yaml annotations data.
 
     # Arguments
-        dataset_name: Str, or list indicating with dataset or datasets
-            to load. e.g. ``VOC2007`` or ``[''VOC2007'', VOC2012]``.
         split: Str, determining the data split to load.
             e.g. `train`, `val` or `test`
         dataset_path: Str, data path to Linemod annotations.
         object_id: Str, ID of the object to train.
         input_size: Dict, containing keys 'width' and 'height'
             with values equal to the input size of the model.
+        data_path: Str, containing path to the Linemod data folder.
         ground_truth_file: Str, name of the file
             containing ground truths.
         info_file: Str, name of the file containing info.
-        data: Str, name of the directory containing object data.
+        image_path: Str, containing path to the RGB images.
+        mask_path: Str, containing path to the object mask images.
+        class_arg: Int, class argument of object class.
 
     # Return
-        data: Dict, with keys correspond to the image names and values
-            are numpy arrays for boxes, rotation, translation
-            and integer for class.
+        Dict, with keys correspond to the image and mask image
+            names and values are numpy arrays for boxes, rotation,
+            translation and integer for class.
     """
     def __init__(self, split='train', dataset_path='/Linemod_preprocessed/',
                  object_id='08', input_size=(512, 512), data_path='data/',
@@ -96,6 +109,28 @@ class LinemodParser(object):
 
 def load(dataset_path, data_path, object_id, ground_truth_file, info_file,
          split, image_path, input_size, class_arg, mask_path):
+    """Preprocess the Linemod yaml annotations data.
+
+    # Arguments
+        dataset_path: Str, data path to Linemod annotations.
+        data_path: Str, containing path to the Linemod data folder.
+        object_id: Str, ID of the object to train.
+        ground_truth_file: Str, name of the file
+            containing ground truths.
+        info_file: Str, name of the file containing info.
+        split: Str, determining the data split to load.
+            e.g. `train`, `val` or `test`
+        image_path: Str, containing path to the RGB images.
+        input_size: Dict, containing keys 'width' and 'height'
+            with values equal to the input size of the model.
+        class_arg: Int, class argument of object class.
+        mask_path: Str, containing path to the object mask images.        
+
+    # Return
+        data: Dict, with keys correspond to the image and mask image
+            names and values are numpy arrays for boxes, rotation,
+            translation and integer for class.
+    """
     root_path = make_root_path(dataset_path, data_path, object_id)
     files = load_linemod_filenames(root_path, ground_truth_file,
                                    info_file, split)
@@ -122,10 +157,36 @@ def load(dataset_path, data_path, object_id, ground_truth_file, info_file,
 
 
 def make_root_path(dataset_path, data_path, object_id):
+    """Composes root path as a string from `dataset_path`,
+    `data_path` and `object_id`.
+
+    # Arguments
+        dataset_path: Str, data path to Linemod annotations.
+        data_path: Str, containing path to the Linemod data folder.
+        object_id: Str, ID of the object to train.
+
+    # Return
+        Str, root directory path to Linemod dataset.
+    """
     return os.path.join(dataset_path, data_path, object_id)
 
 
 def load_linemod_filenames(root_path, ground_truth_file, info_file, split):
+    """Composes path to ground truth file, info file and split file
+    of Linemod dataset.
+
+    # Arguments
+        root_path: Str, root directory path to Linemod dataset.
+        ground_truth_file: Str, name of the file
+            containing ground truths.
+        info_file: Str, name of the file containing info.
+        split: Str, determining the data split to load.
+            e.g. `train`, `val` or `test`
+
+    # Return
+        List: containing path to ground truth file, info file
+            and split file.
+    """
     ground_truth_file = '{}.{}'.format(ground_truth_file, 'yml')
     info_file = '{}.{}'.format(info_file, 'yml')
     split_file = '{}.{}'.format(split, 'txt')
@@ -135,6 +196,14 @@ def load_linemod_filenames(root_path, ground_truth_file, info_file, split):
 
 
 def open_file(file):
+    """Opens a file given by a file handle.
+
+    # Arguments
+        file: Str, name of the file to be opened.
+
+    # Return
+        file_contents: List of strings containing file contents.
+    """
     file_to_parser = {'.txt': parse_txt,
                       '.yml': parse_yml}
     file_name, file_extension = os.path.splitext(file)
@@ -146,25 +215,71 @@ def open_file(file):
 
 
 def parse_txt(file_handle):
+    """Parses given text file.
+
+    # Arguments
+        file_handle: Filehandle, of the file to be parsed.
+
+    # Return
+        List of strings containing file contents.
+    """
     return [line.strip() for line in file_handle.readlines()]
 
 
 def parse_yml(file_handle):
+    """Parses given yaml file.
+
+    # Arguments
+        file_handle: Filehandle, of the file to be parsed.
+
+    # Return
+        Dictionary containing file contents.
+    """
     return yaml.safe_load(file_handle)
 
 
 def make_image_path(root_path, image_path, split_data, image_extension='png'):
+    """Composes path to image specified by `image_path`.
+
+    # Arguments
+        root_path: Str, root directory path to Linemod dataset.
+        image_path: Str, containing path to the RGB image.
+        split_data: Str, name of the image file.
+        image_extension: Str, file extension of the image.
+
+    # Return
+        Str: containing path to the image.
+    """
     file_name = '{}.{}'.format(split_data, image_extension)
     return os.path.join(root_path, image_path, file_name)
 
 
 def get_data(file_id, data, key):
+    """Fetches data from file contents indexed by dictionary keys.
+
+    # Arguments
+        file_id: Str, containing path to the RGB image.
+        data: Dictionary containing file contents.
+        key: Str, key name of data to be fetched.
+
+    # Return
+        Array: containing the fetched data.
+    """
     file_key = int(file_id)
     data = np.asarray(data[file_key][0][key])
     return np.expand_dims(data, axis=0)
 
 
 def linemod_to_corner_form(box):
+    """Converts bounding box from Linemod form to corner form.
+    The Linemod form of bounding box is `[x_min, y_min, W, H]`.
+
+    # Arguments
+        box: Array, of shape `[1, 4]`.
+
+    # Return
+        Array: of shape `[1, 4]` with box in corner form.
+    """
     x_min, y_min, W, H = box[0][0], box[0][1], box[0][2], box[0][3]
     x_max = x_min + W
     y_max = y_min + H
@@ -172,6 +287,16 @@ def linemod_to_corner_form(box):
 
 
 def normalize_box_input_size(box, input_size):
+    """Normalizes bounding box to model's input size.
+
+    # Arguments
+        box: Array, of shape `[1, 4]`
+        input_size: List, containing wdth and height
+            of input layer of model.
+
+    # Return
+        Array: of shape `[1, 4]` containing normalized box coordinates.
+    """
     x_min, y_min = box[0][0], box[0][1]
     x_max, y_max = box[0][2], box[0][3]
     input_W, input_H = input_size
@@ -184,4 +309,14 @@ def normalize_box_input_size(box, input_size):
 
 
 def append_class_to_box(box, class_arg=1):
+    """Appends class information to bounding box information.
+    In other words appends `class_arg` to `box` coordinates.
+
+    # Arguments
+        box: Array, of shape `[1, 4]`
+        class_arg: Int, class argument of object class.
+
+    # Return
+        Array: of shape `[1, 5]` containing box and class information.
+    """
     return np.concatenate((box, np.array([[class_arg]])), axis=-1)
