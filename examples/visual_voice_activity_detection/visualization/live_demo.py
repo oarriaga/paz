9167d7be8013ab10b384dd998cb0d3de37b8ff1c
Cedric Carl-Franek Kränzle <cedric_carl-franek.kraenzle@dfki.de>
2024-01-17 00:03:43 +0100

    Cleaned up demo files

--- examples/visual_voice_activity_detection/visualization/live_demo.py
+++ examples/visual_voice_activity_detection/visualization/live_demo.py
@@ -8,10 +8,7 @@ parser.add_argument('-c', '--camera_id', type=int, default=0,
                     help='Camera device ID')
 args = parser.parse_args()
 
-# model = pretrained_models.getFaceImageModel()
-
 pipeline = dt.DetectVVAD()
 camera = Camera(args.camera_id)
 player = VideoPlayer((640, 480), pipeline, camera)
-# player = VideoPlayer((640, 640), pipeline, camera)
 player.run()
