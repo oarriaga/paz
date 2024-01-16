59aeedef789c3b8f8634f8763dcd68471930ef4e
Cedric Carl-Franek Kränzle <cedric_carl-franek.kraenzle@dfki.de>
2024-01-16 17:43:03 +0100

    Removed imports of deleted classes

--- paz/models/classification/__init__.py
+++ paz/models/classification/__init__.py
@@ -3,7 +3,3 @@ from .xception import MiniXception
 from .protonet import ProtoEmbedding
 from .protonet import ProtoNet
 from .cnn2Plus1 import CNN2Plus1D
-from .cnn2Plus1 import CNN2Plus1D_Light
-from .cnn2Plus1 import CNN2Plus1D_Layers
-from .cnn2Plus1 import CNN2Plus1D_Filters
-from .cnn2Plus1 import CNN2Plus1D_18
