7b7467f6f14c172ff12caa706886fdd8dd90de1b
kraenzle <kraenzle@uni-bremen.de>
2024-02-10 16:46:22 +0100

    Added argpass for file input path

--- examples/visual_voice_activity_detection/recorded_demo.py
+++ examples/visual_voice_activity_detection/recorded_demo.py
@@ -1,8 +1,15 @@
+import argparse
+
 from paz.backend.camera import VideoPlayer
 import paz.pipelines.detection as dt
+
+parser = argparse.ArgumentParser(description='Visual Voice Activity Detection Recorded Demonstration')
+parser.add_argument('-p', '--path', type=str, default="./demo_video.mp4",
+                    help='Path to the video file to be used as input for the VVAD Pipeline.')
+args = parser.parse_args()
 
 pipeline = dt.DetectVVAD()
 player = VideoPlayer((640, 480), pipeline, None)
 
-player.record_from_file(video_file_path="./demo_video.mp4",
+player.record_from_file(video_file_path="args.path",
                         name="./demo_video_labeled.avi", fps=25)
