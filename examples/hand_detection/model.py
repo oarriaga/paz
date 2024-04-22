4a788106de6148c9e668d359f1ae3f182c51cca3
proneetsharma <sproneet@gmail.com>
2024-04-22 16:06:04 +0200

    Add custom model for SSD512

---
+++ examples/hand_detection/model.py
@@ -0,0 +1,21 @@
+from tensorflow.keras import Model
+from paz.models import SSD512
+from paz.models.detection.utils import create_multibox_head
+
+
+def SSD512Custom(num_classes, num_priors=[4, 6, 6, 6, 6, 4, 4], l2_loss=5e-4,
+                 trainable_base=False):
+    base_model = SSD512(base_weights='COCO', head_weights='COCO',
+                        trainable_base=trainable_base)
+    branch_names = ['branch_1', 'branch_2', 'branch_3', 'branch_4',
+                    'branch_5', 'branch_6', 'branch_7']
+    branch_tensors = []
+    for branch_name in branch_names:
+        branch_layer = base_model.get_layer(branch_name)
+        branch_tensors.append(branch_layer.output)
+
+    output_tensor = create_multibox_head(
+        branch_tensors, num_classes, num_priors, l2_loss)
+    model = Model(base_model.input, output_tensor, name='SSD512Custom')
+    model.prior_boxes = base_model.prior_boxes
+    return model
