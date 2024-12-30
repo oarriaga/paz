a07d0e292e3930082699efbb93db1628d89d161b
Octavio Arriaga <arriaga.camargo@gmail.com>
2024-12-30 12:57:46 +0100

    Add non maximum suppression

--- paz/backend/detection.py
+++ paz/backend/detection.py
@@ -1,3 +1,4 @@
+import jax.numpy as jp
 import paz
 
 
@@ -5,3 +6,58 @@ def random_flip_left_right(image, boxes):
     boxes = paz.boxes.flip_left_right(boxes, image.shape[1])
     image = paz.image.flip_left_right(image)
     return image, boxes
+
+
+def non_max_suppression(boxes, scores, iou_thresh=.45, top_k=200):
+    selected_indices = jp.zeros(shape=len(scores))
+    if boxes is None or len(boxes) == 0:
+        return selected_indices
+    x_min = boxes[:, 0]
+    y_min = boxes[:, 1]
+    x_max = boxes[:, 2]
+    y_max = boxes[:, 3]
+    areas = (x_max - x_min) * (y_max - y_min)
+    remaining_sorted_box_indices = jp.argsort(scores)
+    remaining_sorted_box_indices = remaining_sorted_box_indices[-top_k:]
+
+    num_selected_boxes = 0
+    while len(remaining_sorted_box_indices) > 0:
+        best_score_args = remaining_sorted_box_indices[-1]
+        selected_indices[num_selected_boxes] = best_score_args
+        num_selected_boxes = num_selected_boxes + 1
+        if len(remaining_sorted_box_indices) == 1:
+            break
+
+        remaining_sorted_box_indices = remaining_sorted_box_indices[:-1]
+
+        best_x_min = x_min[best_score_args]
+        best_y_min = y_min[best_score_args]
+        best_x_max = x_max[best_score_args]
+        best_y_max = y_max[best_score_args]
+
+        remaining_x_min = x_min[remaining_sorted_box_indices]
+        remaining_y_min = y_min[remaining_sorted_box_indices]
+        remaining_x_max = x_max[remaining_sorted_box_indices]
+        remaining_y_max = y_max[remaining_sorted_box_indices]
+
+        inner_x_min = jp.maximum(remaining_x_min, best_x_min)
+        inner_y_min = jp.maximum(remaining_y_min, best_y_min)
+        inner_x_max = jp.minimum(remaining_x_max, best_x_max)
+        inner_y_max = jp.minimum(remaining_y_max, best_y_max)
+
+        inner_box_widths = inner_x_max - inner_x_min
+        inner_box_heights = inner_y_max - inner_y_min
+
+        inner_box_widths = jp.maximum(inner_box_widths, 0.0)
+        inner_box_heights = jp.maximum(inner_box_heights, 0.0)
+
+        intersections = inner_box_widths * inner_box_heights
+        remaining_box_areas = areas[remaining_sorted_box_indices]
+        best_area = areas[best_score_args]
+        unions = remaining_box_areas + best_area - intersections
+        intersec_over_union = intersections / unions
+        intersec_over_union_mask = intersec_over_union <= iou_thresh
+        remaining_sorted_box_indices = remaining_sorted_box_indices[
+            intersec_over_union_mask]
+
+    return selected_indices.astype(int), num_selected_boxes
