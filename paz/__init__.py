5d80b648b0862a6000ea81ce1e1a4f74b8444c0c
Octavio Arriaga <arriaga.camargo@gmail.com>
2024-12-31 14:42:50 +0100

    Add cast to public module function

--- paz/__init__.py
+++ paz/__init__.py
@@ -3,4 +3,4 @@ from paz.backend import boxes
 from paz.backend import image
 from paz.backend import classes
 from paz.backend import detection
-from paz.backend.standard import lock, merge_dicts
+from paz.backend.standard import lock, merge_dicts, cast
