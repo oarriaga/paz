33e8fb505d46499a2ca34d66d3c6b8c552915b9e
Octavio Arriaga <arriaga.camargo@gmail.com>
2025-01-06 15:29:30 +0100

    Add basic image load and resize

--- paz/backend/image.py
+++ paz/backend/image.py
@@ -1,3 +1,13 @@
+import cv2
+import jax
+import jax.numpy as jp
+
+# import numpy as np
+
+
+BILINEAR = cv2.INTER_LINEAR
+
+
 def flip_left_right(image):
     """Flips an image left and right.
 
@@ -8,3 +18,13 @@ def flip_left_right(image):
         Flipped image array.
     """
     return image[:, ::-1]
+
+
+def load(filepath):
+    image_BGR = cv2.imread(filepath)
+    image_RGB = image_BGR[..., ::-1]
+    return jp.array(image_RGB)
+
+
+def resize(image, shape):
+    return jax.image.resize(image, (*shape, 3), "bilinear")
