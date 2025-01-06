200ef8b1efedd68e64e8a61f4a377a990d257268
Octavio Arriaga <arriaga.camargo@gmail.com>
2025-01-06 15:27:30 +0100

    Add modules

--- paz/__init__.py
+++ paz/__init__.py
@@ -2,5 +2,11 @@ from paz import datasets
 from paz.backend import boxes
 from paz.backend import image
 from paz.backend import classes
+from paz.backend import pinhole
+from paz.backend import mask
+from paz.backend import pointcloud
 from paz.backend import detection
-from paz.backend.standard import lock, merge_dicts, cast
+from paz.backend.lie import SE3
+from paz.backend.lie import SO3
+from paz.backend.lie import quaternion
+from paz.backend.standard import lock, merge_dicts, cast, to_jax, to_numpy
