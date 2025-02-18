9c73f8677b304999336132bdd9227a3d1f00f5cd
Octavio Arriaga <arriaga.camargo@gmail.com>
2025-02-18 12:53:26 +0100

    Add Haar cascade detector

--- paz/models/detection/haar_cascade.py
+++ paz/models/detection/haar_cascade.py
@@ -1,9 +1,11 @@
 import cv2
 import numpy as np
-from tensorflow.keras.utils import get_file
+from keras.utils import get_file
 
-WEIGHT_PATH = ('https://raw.githubusercontent.com/opencv/opencv/'
-               'master/data/haarcascades/')
+WEIGHT_PATH = (
+    "https://raw.githubusercontent.com/opencv/opencv/"
+    "master/data/haarcascades/"
+)
 
 
 class HaarCascadeDetector(object):
@@ -21,19 +23,24 @@ class HaarCascadeDetector(object):
             Cascades](https://github.com/opencv/opencv/tree/master/data/haarcascades)
     """
 
-    def __init__(self, weights='frontalface_default', class_arg=None,
-                 scale=1.3, neighbors=5):
+    def __init__(
+        self,
+        weights="frontalface_default",
+        class_arg=None,
+        scale=1.3,
+        neighbors=5,
+    ):
         self.weights = weights
-        self.name = 'haarcascade_' + weights + '.xml'
+        self.name = "haarcascade_" + weights + ".xml"
         self.url = WEIGHT_PATH + self.name
-        self.path = get_file(self.name, self.url, cache_subdir='paz/models')
+        self.path = get_file(self.name, self.url, cache_subdir="paz/models")
         self.model = cv2.CascadeClassifier(self.path)
         self.class_arg = class_arg
         self.scale = scale
         self.neighbors = neighbors
 
     def __call__(self, gray_image):
-        """ Detects faces from gray images.
+        """Detects faces from gray images.
 
         # Arguments
             gray_image: Numpy array of shape ``(H, W, 2)``.
@@ -42,7 +49,7 @@ class HaarCascadeDetector(object):
             Numpy array of shape ``(num_boxes, 4)``.
         """
         if len(gray_image.shape) != 2:
-            raise ValueError('Invalid gray image shape:', gray_image.shape)
+            raise ValueError("Invalid gray image shape:", gray_image.shape)
         args = (gray_image, self.scale, self.neighbors)
         boxes = self.model.detectMultiScale(*args)
         boxes_point_form = np.zeros_like(boxes)
@@ -55,4 +62,4 @@ class HaarCascadeDetector(object):
                 class_args = np.ones((len(boxes_point_form), 1))
                 class_args = class_args * self.class_arg
                 boxes_point_form = np.hstack((boxes_point_form, class_args))
-        return boxes_point_form.astype('int')
+        return boxes_point_form.astype("int")
