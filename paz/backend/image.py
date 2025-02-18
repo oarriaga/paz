e8b9d2cb83ed067d7b50854e74108167ee0c5b02
Octavio Arriaga <arriaga.camargo@gmail.com>
2025-02-18 12:40:25 +0100

    Add show image

--- paz/backend/image.py
+++ paz/backend/image.py
@@ -1,8 +1,9 @@
 import cv2
+import numpy as np
 import jax
 import jax.numpy as jp
 
-# import numpy as np
+import paz
 
 
 BILINEAR = cv2.INTER_LINEAR
@@ -24,9 +25,34 @@ def BGR_to_RGB(image_BGR):
     return image_BGR[..., ::-1]
 
 
+def RGB_to_BGR(image_RGB):
+    return image_RGB[..., ::-1]
+
+
 def load(filepath):
     return jp.array(BGR_to_RGB(cv2.imread(filepath)))
 
 
 def resize(image, shape):
     return jax.image.resize(image, (*shape, 3), "bilinear")
+
+
+def show(image, name="image", wait=True):
+    """Shows RGB image in an external window.
+
+    # Arguments
+        image: Numpy array
+        name: String indicating the window name.
+        wait: Boolean. If ''True'' window stays open until user presses a key.
+            If ''False'' windows closes immediately.
+    """
+    image = paz.to_numpy(image)
+    if image.dtype != np.uint8:
+        raise ValueError("``image`` must be of type ``uint8``")
+    image = RGB_to_BGR(image)  # openCV default color space is BGR
+    cv2.imshow(name, image)
+    if wait:
+        while True:
+            if cv2.waitKey(0) & 0xFF == ord("q"):
+                break
+        cv2.destroyAllWindows()
