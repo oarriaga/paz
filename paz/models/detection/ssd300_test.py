e0aa73c3daa1a611d5586b289c65b3f64cb62780
Mohamed Ebrahim <mohamedamrfarouk99@gmail.com>
2025-02-24 21:23:38 +0100

    Increase test coverage by adding more unit tests for SSD300, SSD512, and EfficientDet.

--- paz/models/detection/ssd300_test.py
+++ paz/models/detection/ssd300_test.py
@@ -38,5 +38,61 @@ def test_incompatible_num_classes_with_weights():
         SSD300(num_classes=10, head_weights="VOC")
 
 
+def test_SSD300_VOC_VOC():
+    """Test SSD300 with VOC-VOC weights."""
+    try:
+        SSD300(num_classes=21, base_weights="VOC", head_weights="VOC")
+    except ValueError as valuerror:
+        pytest.fail(f"SSD VOC-VOC loading failed: {valuerror}")
+
+
+def test_SSD300_FAT_FAT():
+    """Test SSD300 with FAT-FAT weights."""
+    try:
+        SSD300(num_classes=22, base_weights="FAT", head_weights="FAT")
+    except OSError as e:
+        pytest.fail(f"SSD FAT-FAT loading failed: {e}")
+
+
+def test_SSD300_None_VGG():
+    """Test SSD300 with None-VGG weights."""
+    try:
+        SSD300(num_classes=21, base_weights="VGG", head_weights=None)
+    except ValueError as valuerror:
+        pytest.fail(f"SSD None-VGG loading failed: {valuerror}")
+
+
+def test_SSD300_None_VOC():
+    """Test SSD300 with None-VOC weights."""
+    try:
+        SSD300(num_classes=21, base_weights="VOC", head_weights=None)
+    except ValueError as valuerror:
+        pytest.fail(f"SSD None-VOC loading failed: {valuerror}")
+
+
+def test_SSD300_VOC_VGG():
+    """Test SSD300 with VOC-VGG weights."""
+    with pytest.raises(NotImplementedError):
+        SSD300(num_classes=21, base_weights="VGG", head_weights="VOC")
+
+
+def test_SSD300_VOC_None():
+    """Test SSD300 with VOC-None weights."""
+    with pytest.raises(NotImplementedError):
+        SSD300(num_classes=2, base_weights=None, head_weights="VOC")
+
+
+def test_SSD300_FAT_VGG():
+    """Test SSD300 with FAT-VGG weights."""
+    with pytest.raises(NotImplementedError):
+        SSD300(num_classes=21, base_weights="VGG", head_weights="FAT")
+
+
+def test_SSD300_FAT_None():
+    """Test SSD300 with FAT-None weights."""
+    with pytest.raises(NotImplementedError):
+        SSD300(num_classes=2, base_weights=None, head_weights="FAT")
+
+
 if __name__ == "__main__":
     pytest.main([__file__])
