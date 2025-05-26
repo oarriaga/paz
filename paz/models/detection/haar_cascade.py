42c3222acb5dbe1cedad5580b066f7cb801bbc60
Octavio Arriaga <arriaga.camargo@gmail.com>
2025-05-26 10:55:06 +0200

    Add fix output shape to detector

--- paz/models/detection/haar_cascade.py
+++ paz/models/detection/haar_cascade.py
@@ -1,11 +1,11 @@
 from keras.utils import get_file
+import numpy as np
 import cv2
 import paz
-import jax.numpy as jp
+import jax
 
 
 def download(label):
-    # URL = "https://raw.githubusercontent.com/opencv/opencv/refs/heads/4.x/data/haarcascades/"
     URL = (
         "https://raw.githubusercontent.com/opencv/opencv/"
         "master/data/haarcascades/"
@@ -13,54 +13,63 @@ def download(label):
     filename = "haarcascade_" + label + ".xml"
     filepath = get_file(filename, URL + filename, cache_subdir="paz/models")
     model = cv2.CascadeClassifier(filepath)
-    return model.detectMultiScale
+    return model
 
 
-def get_empty_boxes():
-    return jp.full((1, 5), -1)
-
-
-def postprocess(boxes, class_arg):
-    boxes = paz.boxes.xywh_to_xyxy(boxes)
-    boxes = paz.boxes.append_class(boxes, class_arg).astype(int)
-    return boxes
-
-
-def HaarCascadeDetector(label, scale, neighbors, class_arg, draw=None):
+def HaarCascadeDetector(label, scale, neighbors, class_arg, max_boxes, draw):
     """Haar cascade detector."""
-    detect = download(label)
+    detector = download(label)
 
-    def call(RGB_image):
-        GRAY_image = paz.image.RGB_to_GRAY(RGB_image)
-        boxes = detect(paz.to_numpy(GRAY_image), scale, neighbors)
-        if len(boxes) == 0:
-            boxes = get_empty_boxes()
-        else:
-            boxes = postprocess(boxes, class_arg)
-        return paz.message.Detections(RGB_image, boxes)
+    def pad(boxes, size, value=-1):
+        boxes = boxes[:size]
+        padding = ((0, size - len(boxes)), (0, 0))
+        return np.pad(boxes, padding, "constant", constant_values=value)
 
-    def call_and_draw(RGB_image):
-        boxes = call(RGB_image).boxes
-        return paz.message.Detections(draw(RGB_image, boxes), boxes)
+    def model(GRAY_image):
+        GRAY_image = paz.to_numpy(GRAY_image)
+        boxes = detector.detectMultiScale(GRAY_image, scale, neighbors)
+        boxes = np.full((max_boxes, 4), -1) if len(boxes) == 0 else boxes
+        boxes = pad(boxes, max_boxes)
+        return paz.to_jax(boxes)
 
-    return call if draw is None else call_and_draw
+    @jax.jit
+    def postprocess(boxes):
+        boxes = paz.boxes.xywh_to_xyxy(boxes)
+        boxes = paz.boxes.append_class(boxes, class_arg)
+        boxes = paz.cast(boxes, "int32")
+        return boxes
+
+    @jax.jit
+    def preprocess(image):
+        return paz.image.RGB_to_GRAY(image)
+
+    def call(image):
+        return postprocess(model(preprocess(image)))
+
+    def call_and_draw(image):
+        boxes = call(image)
+        return boxes, draw(image, boxes)
+
+    return call_and_draw if callable(draw) else call
 
 
 def HaarCascadeFrontalFaceDetector(
     scale=1.3,
     neighbors=5,
     class_arg=0,
+    max_boxes=100,
     draw=paz.lock(paz.draw.boxes, paz.draw.GREEN, 2),
 ):
-    return HaarCascadeDetector(
-        "frontalface_default", scale, neighbors, class_arg, draw
-    )
+    args = ("frontalface_default", scale, neighbors, class_arg, max_boxes, draw)
+    return HaarCascadeDetector(*args)
 
 
 def HaarCascadeEyeDetector(
     scale=1.3,
     neighbors=5,
     class_arg=0,
+    max_boxes=100,
     draw=paz.lock(paz.draw.boxes, paz.draw.GREEN, 2),
 ):
-    return HaarCascadeDetector("eye", scale, neighbors, class_arg, draw)
+    args = ("eye", scale, neighbors, class_arg, max_boxes, draw)
+    return HaarCascadeDetector(*args)
