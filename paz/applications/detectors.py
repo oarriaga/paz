fe9a8210266ba8dff9d696ae515ade899a54a7bf
Octavio Arriaga <arriaga.camargo@gmail.com>
2025-05-27 16:12:19 +0200

    Refactor SSD generic function class

--- paz/applications/detectors.py
+++ paz/applications/detectors.py
@@ -1,89 +1,69 @@
+# TODO sort and remove boxes before sending to NMS
 import jax.numpy as jp
 import numpy as np
 import jax
 import paz
-import cv2
 
 
-def resize(image, size, method=cv2.INTER_LINEAR):
-    return cv2.resize(image, size[::-1], interpolation=method)
+def SSD(model, score_thresh, prior_boxes, variances, apply_NMS, draw):
 
-
-def SSD(
-    image,
-    model,
-    prior_boxes,
-    class_names,
-    score_thresh,
-    IOU_thresh,
-    top_k,
-    variances,
-):
-    model_input_size = model.input_shape[1:3]
-    image_size = paz.image.get_size(image)
-
+    @jax.jit
     def preprocess(image, mean=paz.image.BGR_IMAGENET_MEAN):
         """Single-shot Multi Box Detector preprocessing function."""
-        # image = paz.image.resize(image, model_input_size, "linear", False)
-        # image = paz.to_jax(resize(paz.to_numpy(image), model_input_size))
+        image = paz.image.resize_opencv(image, paz.image.get_input_size(model))
         image = paz.image.RGB_to_BGR(image)
         image = paz.image.subtract_mean(image, jp.array(mean))
         image = paz.cast(image, "float32")
-        image = jp.expand_dims(image, axis=0)
-        return image
+        return jp.expand_dims(image, axis=0)
 
-    def postprocess(detections):
+    def postprocess(detections, image_size):
         """Single-shot Multi Box Detector postprocessing function."""
         detections = jp.squeeze(detections, axis=0)
         detections = paz.detection.decode(detections, prior_boxes, variances)
         detections = paz.detection.remove_class(detections, 0)
-        NMS_args = (len(class_names), IOU_thresh, top_k, 0.01)
-        detections = paz.detection.apply_per_class_NMS(detections, *NMS_args)
+        detections = paz.time(apply_NMS)(detections)
+        detections = jp.reshape(detections, (-1, 5))
         detections = paz.detection.filter_by_score(detections, score_thresh, -1)
+        detections = paz.detection.denormalize(detections, *image_size)
         return detections
 
-    # image = paz.to_jax(resize(paz.to_numpy(image), model_input_size))
-    image = jax.jit(
-        paz.lock(paz.image.resize, model_input_size, "linear", False)
-    )(image)
-    image = jax.jit(preprocess)(image)
-    # predictions = jax.jit(model)(image)
-    predictions = model(image)
-    detections = jax.jit(postprocess, device=jax.devices("cpu")[0])(predictions)
-    detections = paz.detection.remove_invalid(detections)
-    detections = paz.detection.denormalize(detections, *image_size)
-    boxes, class_args, scores = paz.detection.to_boxes2D(detections)
-    return boxes, class_args, scores
+    def apply(image):
+        image_size = paz.image.get_size(image)
+        detections = postprocess(model(preprocess(image)), image_size)
+        detections = paz.detection.remove_invalid(detections)
+        return paz.detection.to_boxes2D(detections)
+
+    return lambda x: (y := apply(x), draw(x, *y)) if callable(draw) else apply
 
 
 def SSD300VOC(score_thresh=0.60, IOU_thresh=0.45, top_k=200, draw=None):
     model = paz.models.detection.SSD300(21, "VOC", "VOC", (300, 300, 3))
     boxes = paz.models.detection.utils.create_prior_boxes("VOC")
     names = paz.datasets.labels("VOC")
+    label_colors = paz.draw.lincolor(len(names))
     if draw is None:
-        colors = paz.draw.lincolor(len(names))
-        draw = paz.partial(paz.draw.boxes2D, names=names, colors=colors)
+        draw = paz.partial(paz.draw.boxes2D, names=names, colors=label_colors)
     variances = [0.1, 0.1, 0.2, 0.2]
-    args = (model, boxes, names, score_thresh, IOU_thresh, top_k, variances)
-    detect = paz.lock(SSD, *args)
-    return lambda x: draw(x, *detect(x)) if callable(draw) else detect
+    apply_NMS = (len(names), IOU_thresh, top_k)
+    apply_NMS = paz.lock(paz.detection.apply_per_class_NMS, *apply_NMS)
+    return SSD(model, score_thresh, boxes, variances, apply_NMS, draw)
 
 
 def SSD512COCO(score_thresh=0.60, IOU_thresh=0.45, top_k=200, draw=None):
     model = paz.models.detection.SSD512(81, "COCO", "COCO", (512, 512, 3))
     boxes = paz.models.detection.utils.create_prior_boxes("COCO")
     names = paz.datasets.labels("COCO")
+    label_colors = paz.draw.lincolor(len(names))
     if draw is None:
-        colors = paz.draw.lincolor(len(names))
-        draw = paz.partial(paz.draw.boxes2D, names=names, colors=colors)
+        draw = paz.partial(paz.draw.boxes2D, names=names, colors=label_colors)
     variances = [0.1, 0.1, 0.2, 0.2]
-    args = (model, boxes, names, score_thresh, IOU_thresh, top_k, variances)
-    detect = paz.lock(SSD, *args)
-    return lambda x: draw(x, *detect(x)) if callable(draw) else detect
+    apply_NMS = (len(names), IOU_thresh, top_k)
+    apply_NMS = paz.lock(paz.detection.apply_per_class_NMS, *apply_NMS)
+    return SSD(model, score_thresh, boxes, variances, apply_NMS, draw)
 
 
 def DetectMiniXceptionFER(box_scale=1.2):
-    # TODO add original buffer window prediction
+    # TODO add buffer window prediction
     detect = paz.models.HaarCascadeFrontalFaceDetector(draw=None)
     classify = paz.applications.ClassifyMiniXceptionFER()
     names = paz.datasets.labels("FER")
