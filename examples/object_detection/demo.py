71f2ebd164784447d6e045765ed370a61b66609c
Octavio Arriaga <arriaga.camargo@gmail.com>
2025-05-27 16:11:51 +0200

    Add SSD512 to demo

--- examples/object_detection/demo.py
+++ examples/object_detection/demo.py
@@ -6,6 +6,7 @@ import paz
 
 camera = 0
 pipeline = paz.applications.SSD300VOC()
+# pipeline = paz.applications.SSD512COCO()
 camera = paz.Camera(identifier=camera)
 player = paz.VideoPlayer((480, 640), pipeline, camera)
 player.run()
