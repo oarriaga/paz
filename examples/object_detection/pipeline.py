4dc38c2c2e0292f007e1a2b3bb57e199eab8a430
Octavio Arriaga <arriaga.camargo@gmail.com>
2025-06-09 07:31:08 +0200

    Start building training pipeline for object detection

--- examples/object_detection/pipeline.py
+++ examples/object_detection/pipeline.py
@@ -2,12 +2,20 @@ import jax.numpy as jp
 import paz
 
 
-def augment(key, image, detections, mean, augment=True):
+def augment(key, image, detections, mean, prior_boxes, match_IOU=0.5):
     image = paz.image.RGB_to_BGR(image)
-    if augment:
-        image = paz.image.augment_color(key, image)
+    image = paz.image.augment_color(key, image)
     image = paz.image.subtract_mean(image, mean)
     # paz.detection.pad
-    # paz.boxes.pad
+
+    detections = paz.detection.to_image_coordinates(detections)
+    detections = paz.detection.expand(detections, mean)
+    detections = paz.detection.random_sample_crop(key, detections, 1.0)
+    detections = paz.detection.random_flip_left_right(image, detections)
+    detections = paz.detection.normalized_box_coordinates(
+        detections, image.shape
+    )
+
+    detections = paz.detection.match(detections, prior_boxes, match_IOU)
     detections = paz.detection.encode(detections)
     detections = paz.detection.to_one_hot(detections)
