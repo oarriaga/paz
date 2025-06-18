b92854aae0630893b1fb4de425820f4a6c846ef0
Octavio Arriaga <arriaga.camargo@gmail.com>
2025-06-18 14:44:47 +0200

    Add original encoding function

--- examples/object_detection/pipeline.py
+++ examples/object_detection/pipeline.py
@@ -23,17 +23,66 @@ def add_background_class(detections):
     return detections
 
 
+def encode(matched, priors, variances=[0.1, 0.1, 0.2, 0.2]):
+    """Encode the variances from the priorbox layers into the ground truth
+    boxes we have matched (based on jaccard overlap) with the prior boxes.
+
+    # Arguments
+        matched: Numpy array of shape `(num_priors, 4)` with boxes in
+            point-form.
+        priors: Numpy array of shape `(num_priors, 4)` with boxes in
+            center-form.
+        variances: (list[float]) Variances of priorboxes
+
+    # Returns
+        encoded boxes: Numpy array of shape `(num_priors, 4)`.
+    """
+
+    def to_center_form(boxes):
+        """Transform from corner coordinates to center coordinates.
+
+        # Arguments
+            boxes: Numpy array with shape `(num_boxes, 4)`.
+
+        # Returns
+            Numpy array with shape `(num_boxes, 4)`.
+        """
+        x_min, y_min = boxes[:, 0:1], boxes[:, 1:2]
+        x_max, y_max = boxes[:, 2:3], boxes[:, 3:4]
+        center_x = (x_max + x_min) / 2.0
+        center_y = (y_max + y_min) / 2.0
+        W = x_max - x_min
+        H = y_max - y_min
+        return np.concatenate([center_x, center_y, W, H], axis=1)
+
+    boxes = matched[:, :4]
+    boxes = to_center_form(boxes)
+    center_difference_x = boxes[:, 0:1] - priors[:, 0:1]
+    encoded_center_x = center_difference_x / priors[:, 2:3]
+    center_difference_y = boxes[:, 1:2] - priors[:, 1:2]
+    encoded_center_y = center_difference_y / priors[:, 3:4]
+    encoded_center_x = encoded_center_x / variances[0]
+    encoded_center_y = encoded_center_y / variances[1]
+    encoded_W = np.log((boxes[:, 2:3] / priors[:, 2:3]) + 1e-8)
+    encoded_H = np.log((boxes[:, 3:4] / priors[:, 3:4]) + 1e-8)
+    encoded_W = encoded_W / variances[2]
+    encoded_H = encoded_H / variances[3]
+    encoded_boxes = [encoded_center_x, encoded_center_y, encoded_W, encoded_H]
+    return np.concatenate(encoded_boxes + [matched[:, 4:]], axis=1)
+
+
 def preprocess(detections, prior_boxes, num_classes, IOU, variances, H, W):
     # TODO change name.
     # TODO remove use of background class.
     detections = paz.detection.normalize(detections, H, W)
     detections = add_background_class(detections)
-    detections = jp.array(
-        paz.detection.match_np(np.array(detections), np.array(prior_boxes), IOU)
+    detections = paz.detection.match_np(
+        np.array(detections), np.array(prior_boxes), IOU
     )
-    detections = paz.detection.encode(detections, prior_boxes, variances)
+    # detections = paz.detection.encode(detections, prior_boxes, variances)
+    detections = encode(detections, np.array(prior_boxes), variances)
     # internally increase number of classes by 1 to account for background class
-    detections = paz.detection.to_one_hot(detections, num_classes + 1)
+    detections = paz.detection.to_one_hot(jp.array(detections), num_classes + 1)
     return detections
 
 
