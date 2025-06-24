afee95111e9f24bdbefd3e29b4736100c2d6f97d
Octavio Arriaga <arriaga.camargo@gmail.com>
2025-06-24 11:57:48 +0200

    Add to demo fish detection algorithm

--- examples/object_detection/demo.py
+++ examples/object_detection/demo.py
@@ -10,6 +10,7 @@ camera = 4
 
 def SSDCustom(path, score_thresh=0.30, IOU_thresh=0.45, top_k=200, draw=None):
     model = keras.saving.load_model(path)
+    num_classes = model.output_shape[-1] - 5
     # model = keras.saving.load_model(
     #     path,
     #     custom_objects={
@@ -23,8 +24,9 @@ def SSDCustom(path, score_thresh=0.30, IOU_thresh=0.45, top_k=200, draw=None):
 
     # model.load_weights(path)
     boxes = paz.models.detection.single_shot_detector.build_prior_boxes("VOC")
-    names = paz.datasets.labels("VOC")
-    label_colors = paz.draw.lincolor(len(names))
+    # names = paz.datasets.labels("VOC")
+    names = ["fish"]
+    label_colors = paz.draw.lincolor(num_classes)
     if draw is None:
         draw = paz.partial(paz.draw.boxes2D, names=names, colors=label_colors)
     variances = [0.1, 0.1, 0.2, 0.2]
@@ -35,11 +37,18 @@ def SSDCustom(path, score_thresh=0.30, IOU_thresh=0.45, top_k=200, draw=None):
     )
 
 
-# pipeline = paz.applications.SSD300VOC()
-# pipeline = SSDCustom("experiments/19-06-2025_12-27-06_SSD300/SSD300.weights.h5")
-# pipeline = SSDCustom("experiments/19-06-2025_11-09-34_SSD300/SSD300.keras")
-pipeline = SSDCustom("experiments/20-06-2025_17-13-52_SSD300/SSD300.keras")
+pipeline = SSDCustom("experiments/24-06-2025_11-16-56_SSD300/SSD300.keras")
+images, y_true = paz.datasets.deepfish.load("validation")
+for image_arg, image in enumerate(images):
+    y_pred, y_pred_image = pipeline(paz.image.load(image))
+    # paz.image.show(y_pred_image)
+    paz.image.write(f"detections/y_pred_{image_arg}.jpg", y_pred_image)
+
+# path = "experiments/24-06-2025_11-16-56_SSD300/SSD300.keras"
+# model = keras.saving.load_model(path)
+
 # pipeline = paz.applications.SSD512COCO()
-camera = paz.Camera(identifier=camera)
-player = paz.VideoPlayer((480, 640), pipeline, camera)
-player.run()
+# pipeline = paz.applications.SSD300VOC()
+# camera = paz.Camera(identifier=camera)
+# player = paz.VideoPlayer((480, 640), pipeline, camera)
+# player.run()
