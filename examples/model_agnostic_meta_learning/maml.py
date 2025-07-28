9b18abf7caad93474f68c9101690bb03848a9d73
Octavio Arriaga <arriaga.camargo@gmail.com>
2025-07-28 10:20:44 +0200

    Refactored MAML with functional approach

---
+++ examples/model_agnostic_meta_learning/maml.py
@@ -0,0 +1,68 @@
+from functools import partial
+import jax
+import jax.numpy as jp
+
+
+def call(model, parameters, x):
+    y_pred, _ = model.stateless_call(*parameters, x)
+    return y_pred
+
+
+def compute_task_loss(model, loss_fn, variables, static_parameters, x, y):
+    parameters = (variables, static_parameters)
+    y_pred = call(model, parameters, x)
+    return loss_fn(y, y_pred)
+
+
+def gradient_step(step_size, parameters, gradients):
+
+    def _gradient_step(parameters, gradients):
+        return parameters - step_size * gradients
+
+    variables, static_parameters = parameters
+    variables = jax.tree.map(_gradient_step, variables, gradients)
+    return variables, static_parameters
+
+
+def adapt(model, loss_fn, step_size, num_steps, parameters, support_data):
+    compute_gradients = jax.grad(partial(compute_task_loss, model, loss_fn))
+
+    def step(step, parameters):
+        gradients = compute_gradients(*parameters, *support_data)
+        parameters = gradient_step(step_size, parameters, gradients)
+        return parameters
+
+    return jax.lax.fori_loop(0, num_steps, step, parameters)
+
+
+def train_step(model, loss_fn, optimizer, step_size, num_steps, state, data):
+    _compute_task_loss = partial(compute_task_loss, model, loss_fn)
+    _predict = partial(adapt, model, loss_fn, step_size, num_steps)
+
+    def compute_meta_loss(
+        variables, static_parameters, x_support, y_support, x_queries, y_queries
+    ):
+        parameters = (variables, static_parameters)
+        params = _predict(parameters, (x_support, y_support))
+        meta_loss = _compute_task_loss(*params, x_queries, y_queries)
+        return meta_loss
+
+    compute_meta_gradients = jax.vmap(
+        jax.value_and_grad(compute_meta_loss),
+        in_axes=(None, None, 0, 0, 0, 0),
+        out_axes=0,
+    )
+
+    parameters, optimizer_state = state
+    variables, static_parameters = parameters
+    loss, gradients = compute_meta_gradients(*parameters, *data)
+    gradients = jax.tree.map(lambda gradient: jp.mean(gradient, 0), gradients)
+    optimizer_args = (optimizer_state, gradients, variables)
+    variables, optimizer_state = optimizer.stateless_apply(*optimizer_args)
+    return jp.mean(loss), ((variables, static_parameters), optimizer_state)
+
+
+def predict(model, loss_fn, step_size, num_steps, parameters, support_data, x):
+    adapt_args = (model, loss_fn, step_size, num_steps)
+    parameters = adapt(*adapt_args, parameters, support_data)
+    return call(model, parameters, x)
--- examples/model_agnostic_meta_learning/train.py
+++ examples/model_agnostic_meta_learning/train.py
@@ -1,4 +1,5 @@
 import os
+from functools import partial
 
 os.environ["KERAS_BACKEND"] = "jax"
 
@@ -8,6 +9,7 @@ import paz
 import jax.numpy as jp
 import matplotlib.pyplot as plt
 import sinusoidal
+import maml
 
 
 def MLP(hidden_dimensions=[40, 40]):
@@ -18,41 +20,12 @@ def MLP(hidden_dimensions=[40, 40]):
     return keras.Model(inputs, outputs)
 
 
-def meta_step(state, data, model, compute_loss, optimizer, fast_learning_rate):
-
-    def compute_task_loss(theta, theta_static, x, y):
-        y_pred, _ = model.stateless_call(theta, theta_static, x)
-        return compute_loss(y, y_pred)
-
-    def gradient_step(theta, gradients):
-        return theta - fast_learning_rate * gradients
-
-    def compute_meta_loss(theta, theta_static, s_x, s_y, q_x, q_y):
-        gradients = jax.grad(compute_task_loss)(theta, theta_static, s_x, s_y)
-        fast_weights = jax.tree.map(gradient_step, theta, gradients)
-        meta_loss = compute_task_loss(fast_weights, theta_static, q_x, q_y)
-        return meta_loss
-
-    compute_meta_gradients = jax.vmap(
-        jax.value_and_grad(compute_meta_loss),
-        in_axes=(None, None, 0, 0, 0, 0),
-        out_axes=0,
-    )
-
-    theta, theta_static, theta_optimizer = state
-    loss, gradients = compute_meta_gradients(theta, theta_static, *data)
-    gradients = jax.tree.map(lambda gradient: jp.mean(gradient, 0), gradients)
-    theta, optimizer_theta = optimizer.stateless_apply(
-        theta_optimizer, gradients, theta
-    )
-    return jp.mean(loss), (theta, theta_static, optimizer_theta)
-
-
 META_LEARNING_RATE = 1e-3
 FAST_LR = 0.01
 TRAIN_STEPS = 20_000
 TASKS_PER_BATCH = 4
 SHOTS_PER_TASK = 20
+num_steps = 1
 seed = 777
 min_x = -5.0
 max_x = 5.0
@@ -61,17 +34,16 @@ max_y = 5.0
 
 key = jax.random.PRNGKey(seed)
 model = MLP()
-compute_loss = keras.losses.MeanSquaredError()
+loss_fn = keras.losses.MeanSquaredError()
 optimizer = keras.optimizers.Adam(learning_rate=META_LEARNING_RATE)
 optimizer.build(model.trainable_variables)
 
-state = (
-    model.trainable_variables,
-    model.non_trainable_variables,
-    optimizer.variables,
-)
+parameters = (model.trainable_variables, model.non_trainable_variables)
+state = (parameters, optimizer.variables)
 
-step = jax.jit(paz.lock(meta_step, model, compute_loss, optimizer, FAST_LR))
+train_step_args = (model, loss_fn, optimizer, FAST_LR, num_steps)
+train_step = jax.jit(partial(maml.train_step, *train_step_args))
+
 batch = jax.jit(
     paz.lock(
         sinusoidal.sample_batch,
@@ -86,51 +58,20 @@ batch = jax.jit(
 
 losses, progress_bar = [], keras.utils.Progbar(TRAIN_STEPS)
 for step_arg, step_key in enumerate(jax.random.split(key, TRAIN_STEPS)):
-    loss, state = step(state, batch(step_key)[0])
-    losses.append(loss)
+    loss, state = train_step(state, batch(step_key)[0])
     progress_bar.update(step_arg + 1, [("loss", float(loss))])
+    losses.append(loss)
 
 plt.plot(losses)
 plt.show()
 
-theta, theta_static, optimizer_variables = state
-for variable, value in zip(model.trainable_variables, theta):
+(variables, static_parameters), optimizer_variables = state
+for variable, value in zip(model.trainable_variables, variables):
     variable.assign(value)
 
 
 print("\nVisualizing model adaptation on new, unseen tasks...")
-
-
-def forward_pass(theta, theta_static, x, model):
-    y_pred, _ = model.stateless_call(theta, theta_static, x)
-    return y_pred
-
-
-def adapt_step(state, support_data, model, compute_loss, fast_learning_rate):
-    theta, theta_static = state
-    support_x, support_y = support_data
-
-    def compute_task_loss(theta, theta_static, x, y):
-        y_pred, _ = model.stateless_call(theta, theta_static, x)
-        return compute_loss(y, y_pred)
-
-    gradients = jax.grad(compute_task_loss)(
-        theta, theta_static, support_x, support_y
-    )
-
-    fast_weights = jax.tree.map(
-        lambda t, g: t - fast_learning_rate * g, theta, gradients
-    )
-    return fast_weights
-
-
-jitted_forward_pass = jax.jit(forward_pass, static_argnames=("model",))
-jitted_adapt_step = jax.jit(
-    adapt_step, static_argnames=("model", "compute_loss", "fast_learning_rate")
-)
-
-trained_theta, trained_theta_static, _ = state
-trained_state = (trained_theta, trained_theta_static)
+parameters, _ = state
 
 key, viz_key = jax.random.split(key)
 (s_x, s_y, q_x, q_y), (amplitudes, phases) = sinusoidal.sample_batch(
@@ -143,27 +84,13 @@ figure.suptitle("MAML Adaptation to New Sinusoid Tasks", fontsize=18)
 x_curve = jp.linspace(min_x, max_x, 200).reshape(-1, 1)
 
 for i, axis in enumerate(axes.flat):
-    # Select data for the i-th task
     support_data = (s_x[i], s_y[i])
     query_data = (q_x[i], q_y[i])
 
-    # --- Pre-Update Prediction (using meta-parameters) ---
-    y_pred_pre_update = jitted_forward_pass(
-        trained_theta, trained_theta_static, x_curve, model
+    y_pred_pre_update = maml.call(model, parameters, x_curve)
+    y_pred_post_update = maml.predict(
+        model, loss_fn, FAST_LR, num_steps, parameters, support_data, x_curve
     )
-
-    # --- Adaptation Step ---
-    fast_weights = jitted_adapt_step(
-        trained_state, support_data, model, compute_loss, FAST_LR
-    )
-
-    # --- Post-Update Prediction (using fast weights) ---
-    y_pred_post_update = jitted_forward_pass(
-        fast_weights, trained_theta_static, x_curve, model
-    )
-
-    # --- Plotting ---
-    # Plot ground truth and data points
     y_curve_true = amplitudes[i] * jp.sin(x_curve - phases[i])
     axis.plot(
         x_curve,
