d87a4c599931d652af88ed0f33428aed9736fb6a
Octavio Arriaga <arriaga.camargo@gmail.com>
2025-07-28 10:36:22 +0200

    Refactor visualization

--- examples/model_agnostic_meta_learning/train.py
+++ examples/model_agnostic_meta_learning/train.py
@@ -5,7 +5,6 @@ os.environ["KERAS_BACKEND"] = "jax"
 
 import jax
 import keras
-import paz
 import jax.numpy as jp
 import matplotlib.pyplot as plt
 import sinusoidal
@@ -45,14 +44,14 @@ train_step_args = (model, loss_fn, optimizer, FAST_LR, num_steps)
 train_step = jax.jit(partial(maml.train_step, *train_step_args))
 
 batch = jax.jit(
-    paz.lock(
+    partial(
         sinusoidal.sample_batch,
-        TASKS_PER_BATCH,
-        SHOTS_PER_TASK,
-        min_x,
-        max_x,
-        min_y,
-        max_y,
+        batch_size=TASKS_PER_BATCH,
+        num_shots=SHOTS_PER_TASK,
+        min_x=min_x,
+        max_x=max_x,
+        min_y=min_y,
+        max_y=max_y,
     )
 )
 
@@ -72,65 +71,39 @@ for variable, value in zip(model.trainable_variables, variables):
 
 print("\nVisualizing model adaptation on new, unseen tasks...")
 parameters, _ = state
-
-key, viz_key = jax.random.split(key)
-(s_x, s_y, q_x, q_y), (amplitudes, phases) = sinusoidal.sample_batch(
-    viz_key, TASKS_PER_BATCH, SHOTS_PER_TASK, min_x, max_x, min_y, max_y
-)
-
+key, subkey = jax.random.split(key)
+data_args = subkey, TASKS_PER_BATCH, SHOTS_PER_TASK, min_x, max_x, min_y, max_y
+data, (amplitudes, phases) = sinusoidal.sample_batch(*data_args)
 figure, axes = plt.subplots(2, 2, figsize=(16, 14))
 figure.suptitle("MAML Adaptation to New Sinusoid Tasks", fontsize=18)
-
-x_curve = jp.linspace(min_x, max_x, 200).reshape(-1, 1)
-
-for i, axis in enumerate(axes.flat):
-    support_data = (s_x[i], s_y[i])
-    query_data = (q_x[i], q_y[i])
-
-    y_pred_pre_update = maml.call(model, parameters, x_curve)
+x = jp.linspace(min_x, max_x, 200).reshape(-1, 1)
+for task_arg, axis in enumerate(axes.flat):
+    support_data = data[0][task_arg], data[1][task_arg]
+    queries_data = data[2][task_arg], data[3][task_arg]
+    y_pred_pre_update = maml.call(model, parameters, x)
     y_pred_post_update = maml.predict(
-        model, loss_fn, FAST_LR, num_steps, parameters, support_data, x_curve
+        model, loss_fn, FAST_LR, num_steps, parameters, support_data, x
     )
-    y_curve_true = amplitudes[i] * jp.sin(x_curve - phases[i])
-    axis.plot(
-        x_curve,
-        y_curve_true,
-        label="Ground Truth",
-        color="gray",
-        linestyle="--",
-    )
+    y = amplitudes[task_arg] * jp.sin(x - phases[task_arg])
+    axis.plot(x, y, label="Ground Truth", color="gray", linestyle="--")
+    axis.scatter(*support_data, s=80, label="Support Set (for adaptation)")
     axis.scatter(
-        support_data[0],
-        support_data[1],
-        s=80,
-        label="Support Set (for adaptation)",
-    )
-    axis.scatter(
-        query_data[0],
-        query_data[1],
-        s=60,
-        label="Query Set (for evaluation)",
-        marker="x",
-    )
-
-    # Plot model predictions
-    axis.plot(
-        x_curve, y_pred_pre_update, label="Pre-Update Prediction", linestyle=":"
+        *queries_data, s=60, label="Query Set (for evaluation)", marker="x"
     )
     axis.plot(
-        x_curve,
+        x, y_pred_pre_update, label="Pre-Update Prediction", linestyle=":"
+    )
+    axis.plot(
+        x,
         y_pred_post_update,
         label="Post-Update Prediction (1 step)",
         linewidth=2.5,
     )
-
-    # Make it pretty
-    axis.set_title(f"Task {i + 1}", fontsize=12)
+    axis.set_title(f"Task {task_arg + 1}", fontsize=12)
     axis.set_xlabel("x")
     axis.set_ylabel("y")
     axis.legend()
     axis.grid(True, linestyle=":", alpha=0.6)
     axis.set_ylim(min_y - max_y - 1, max_y + 1)
-
 plt.tight_layout(rect=[0, 0, 1, 0.96])
 plt.show()
