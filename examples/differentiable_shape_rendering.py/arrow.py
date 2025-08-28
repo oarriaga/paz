8ef854a6326880a46427dabc2fdc1738eff156b7
Octavio Arriaga <arriaga.camargo@gmail.com>
2025-08-28 18:15:53 +0200

    Start building arrow

--- examples/differentiable_shape_rendering.py/arrow.py
+++ examples/differentiable_shape_rendering.py/arrow.py
@@ -1,3 +1,4 @@
+# TODO put another cylinder next to it to make sure is of length 1 total
 # TODO should group has its own pose?
 # TODO make logic to check that parent array has the same length as shapes
 # should be an inherit property of the shapes?
@@ -16,22 +17,34 @@ G_material = paz.graphics.Material(paz.graphics.GREEN, *material)
 B_material = paz.graphics.Material(paz.graphics.BLUE, *material)
 O_material = paz.graphics.Material(jp.array([0.75, 0.75, 0.75]), *material)
 
-origin = paz.graphics.Sphere(paz.SE3.scaling(jp.full((3,), 1.0)), O_material)
-
-line_scale = paz.SE3.scaling(jp.array([0.25, 2.0, 0.25]))
-line_shift = paz.SE3.translation(jp.array([0.0, 2.0, 0.0]))
+line_width = 0.05
+line_scale = paz.SE3.scaling(jp.array([line_width, 1.0, line_width]))
+line_shift = paz.SE3.translation(jp.array([0.0, 1.0, 0.0]))
 line_transform = line_shift @ line_scale
 
-head_shift = paz.SE3.translation(jp.array([0.0, 5.0, 0.0]))
-head_scale = paz.SE3.scaling(jp.array([0.5, 1.0, 0.5]))
+
+head_scale = paz.SE3.scaling(
+    jp.array([2 * line_width, 2 * line_width, 2 * line_width])
+)
+head_shift = paz.SE3.translation(jp.array([0.0, 0.0, 0.0]))
 head_transform = head_shift @ head_scale
+
+origin_scale = paz.SE3.scaling(jp.full((3,), line_width * 2.0))
+origin = paz.graphics.Sphere(origin_scale, O_material)
 
 line = paz.graphics.Cylinder(line_transform, G_material)
 head = paz.graphics.Cone(head_transform, G_material)
-arrow = paz.graphics.Group([line, head], jp.array([-1, 0]))
+arrow = paz.graphics.Group([line, head], jp.array([-1, -1]))
+
+arrow = [
+    # paz.graphics.Cylinder(line_transform, G_material),
+    arrow,
+    origin,
+    # paz.graphics.Plane(),
+]
 
 camera_pose = paz.SE3.view_transform(
-    camera_origin=jp.array([0.0, 0.0, 10.0]),
+    camera_origin=jp.array([0.0, 2.0, 5.0]),
     target_origin=jp.array([0.0, 0.0, 0.0]),
     world_up=jp.array([0.0, 1.0, 0.0]),
 )
