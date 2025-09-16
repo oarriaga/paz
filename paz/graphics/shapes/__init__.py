9145e97462efcfaf56f28a6b6aae313d5c6f8efb
Octavio Arriaga <arriaga.camargo@gmail.com>
2025-09-16 15:55:56 +0200

    Add basic functions to shapes

--- paz/graphics/shapes/__init__.py
+++ paz/graphics/shapes/__init__.py
@@ -1,6 +1,9 @@
+from collections import defaultdict
 import jax
-import jax.numpy as jp
 import paz
+import jax.numpy as jp
+from paz.backend.algebra import transform_points
+from paz.graphics.geometry import transform_rays, compute_points3D
 
 from .sphere import intersect_canonical_sphere
 from .cube import intersect_canonical_cube
@@ -13,6 +16,8 @@ from .cube import compute_canonical_normals_cube
 from .cylinder import compute_canonical_normals_cylinder
 from .cone import compute_canonical_normals_cone
 from .plane import compute_canonical_normals_plane
+
+from ..constants import EPSILON
 
 
 intersection_cases = [
@@ -77,3 +82,48 @@ def split(shape):
     """Splits a batched Shape object into a list of N shapes with batch 1."""
     num = shape.transform.shape[0]
     return [jax.tree.map(lambda x: x[i : (i + 1)], shape) for i in range(num)]
+
+
+def intersect(shape, ray_origins, ray_directions):
+    world_to_shape = jp.linalg.inv(shape.transform)
+    rays_shape = transform_rays(world_to_shape, ray_origins, ray_directions)
+    intersections = jax.lax.switch(shape.type, intersection_cases, *rays_shape)
+    hit_mask, sorted_depths, depth = intersections
+    # transform world points
+    world_points = compute_points3D(ray_origins, ray_directions, depth)
+    world_to_shape = jp.linalg.inv(shape.transform)
+    shape_points = transform_points(world_to_shape, world_points)
+    # transform world normals
+    shape_normals = jax.lax.switch(shape.type, normal_cases, shape_points)
+    world_normals = transform_points(world_to_shape.T, shape_normals)
+    world_normals = paz.algebra.normalize(world_normals)
+    # postprocess normals
+    eyes = compute_eyes(ray_directions)
+    world_normals = invert_inside_normals(eyes, world_normals)
+    world_points = move_toward_normals(world_points, world_normals)
+    return hit_mask, depth, world_points, world_normals, eyes
+
+
+def compute_eyes(ray_directions):
+    eyes = -ray_directions
+    return eyes
+
+
+def invert_inside_normals(eyes, normals):
+    inside_mask = paz.algebra.dot(normals, eyes) < 0
+    inside_mask = jp.expand_dims(inside_mask, 1)
+    normals = jp.where(inside_mask, -normals, normals)
+    return normals
+
+
+def move_toward_normals(points, normals):
+    return points + (normals * EPSILON)  # fix pattern noise
+
+
+def group_by_pattern_size(shapes_list):
+    """Groups a list of Shape objects by the size of their pattern's image."""
+    grouped_shapes = defaultdict(list)
+    for shape in shapes_list:
+        image_size_key = shape.pattern.image.shape[:2]
+        grouped_shapes[image_size_key].append(shape)
+    return dict(grouped_shapes)
