d66c3685b1c24df19bad76ceb53ddb423ddd7824
Octavio Arriaga <arriaga.camargo@gmail.com>
2025-10-27 15:02:29 +0100

    Add recursive function to flatten scene

--- paz/graphics/scene.py
+++ paz/graphics/scene.py
@@ -15,37 +15,17 @@ def prepare_lights(lights):
     return processed_lights
 
 
-def compute_num_shapes(scene):
-    total_shapes = 0
-    for shape_or_group in scene.nodes:
-        if isinstance(shape_or_group, paz.graphics.Shape):
-            total_shapes = total_shapes + 1
-        elif isinstance(shape_or_group, paz.graphics.Group):
-            total_shapes = total_shapes + len(shape_or_group.shapes)
-        else:
-            raise TypeError("Scene elements must be 'Shape' or 'Group' types.")
-    return total_shapes
-
-
-def prepare_mask(mask, scene):
+def prepare_mask(mask, num_shapes):
     """Prepares user mask to match flat scene."""
 
     if mask is None:
-        flat_mask = jp.ones(compute_num_shapes(scene), dtype=bool)
+        flat_mask = jp.ones(num_shapes, dtype=bool)
     else:
-        if len(mask) != len(scene.nodes):
+        if len(mask) != num_shapes:
             raise ValueError("Mask length must match top-level scene elements.")
-        flat_mask = expand_mask(mask, scene)
+        flat_mask = mask  # TODO expand_mask using recursion
 
     return flat_mask
-
-
-def validate_scene(scene):
-    if not isinstance(scene, paz.graphics.Scene):
-        raise TypeError("'scene' must be a paz.graphics.Scene type.")
-    for node in scene.nodes:
-        if not isinstance(node, (paz.graphics.Shape, paz.graphics.Group)):
-            raise TypeError("'scene' elements must be 'Shape' or 'Group' type.")
 
 
 def expand_mask(mask, scene):
@@ -62,71 +42,30 @@ def expand_mask(mask, scene):
     return jp.array(expanded_mask, dtype=bool)
 
 
-def get_groups(scene):
-    groups = []
-    for shape_or_group in scene.nodes:
-        if isinstance(shape_or_group, paz.graphics.Group):
-            groups.append(shape_or_group)
-    return groups
-
-
-def get_shapes(scene):
-    shapes = []
-    for shape_or_group in scene.nodes:
-        if isinstance(shape_or_group, paz.graphics.Shape):
-            shapes.append(shape_or_group)
-    return shapes
-
-
-def prepare_group(group):
-    if not isinstance(group.shapes, list):
-        raise TypeError("'group.shapes' must be a list of Shapes.")
-
-    transformed_shapes = []
-    for shape in group.shapes:
-        transform = group.transform @ shape.transform
-        transformed_shapes.append(shape._replace(transform=transform))
-
-    if not transformed_shapes:
-        return None
-
-    return prepare_shapes(transformed_shapes)
-
-
-def prepare_shapes(shapes):
-    if isinstance(shapes, list):
-        if not all(isinstance(shape, paz.graphics.Shape) for shape in shapes):
-            raise TypeError("All elements must be Shape objects.")
+def flatten(node, accumulated_transform):
+    if isinstance(node, paz.graphics.Shape):
+        return [node._replace(transform=accumulated_transform @ node.transform)]
+    elif isinstance(node, paz.graphics.Group):
+        children, child_transform = [], accumulated_transform @ node.transform
+        for shape_or_group in node.shapes:
+            children.extend(flatten(shape_or_group, child_transform))
+        return children
     else:
-        raise TypeError("'shapes' must be a Shape or a list of Shapes.")
-
-    return shapes
+        raise ValueError(f"Invalid node type{type(node)}.")
 
 
-def prepare_groups(groups):
-    return [prepare_group(group) for group in groups]
-
-
-def prepare_scene(scene):
-    validate_scene(scene)
+def flatten_scene(scene):
     flat_scene = []
-    for group in get_groups(scene):
-        shapes = prepare_group(group)
-        flat_scene.extend(shapes)
-
-    for shape in get_shapes(scene):
-        flat_scene.append(shape)
-
-    for flat_shape in flat_scene:
-        if not isinstance(flat_shape, paz.graphics.Shape):
-            raise TypeError("All elements in flat_scene must be Shape objects.")
-
+    for node in scene.nodes:
+        flat_scene.extend(flatten(node, jp.eye(4)))
     return flat_scene
 
 
 def compile(scene, lights, mask):
+    flat_scene = flatten_scene(scene)
     return (
-        prepare_scene(scene),
+        # prepare_scene(scene),
+        flat_scene,
         prepare_lights(lights),
-        prepare_mask(mask, scene),
+        prepare_mask(mask, len(flat_scene)),
     )
