d8e730cc3098260dc6615138fe1d257c32785126
Octavio Arriaga <arriaga.camargo@gmail.com>
2025-10-27 15:00:39 +0100

    Add hash table as direct import

--- paz/abstract/__init__.py
+++ paz/abstract/__init__.py
@@ -2,3 +2,4 @@ from paz.abstract.model import Model
 from paz.abstract.node import Node, Input
 from paz.abstract.sequential import Sequential
 from paz.abstract.tree import Tree
+from paz.abstract import hash_table
