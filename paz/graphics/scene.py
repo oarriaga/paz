8c1c981b31a49d43df0bb64c6cff20fd8e967d9e
Octavio Arriaga <arriaga.camargo@gmail.com>
2025-11-25 14:11:02 +0100

    Add print for scene hierarchy

--- paz/graphics/scene.py
+++ paz/graphics/scene.py
@@ -1,6 +1,69 @@
 import jax.numpy as jp
 import paz
-from paz.graphics import PointLight
+from paz.graphics import PointLight, Shape
+from paz.graphics import SPHERE, CYLINDER, CONE, CUBE, PLANE
+
+
+def _build_node_data_string(node, prefix, is_last):
+    t1, t2, t3 = node.transform[:3, 3]
+    t1 = f" {t1:.1f}" if t1 >= 0.0 else f"{t1:.1f}"
+    t2 = f" {t2:.1f}" if t2 >= 0.0 else f"{t2:.1f}"
+    t3 = f" {t3:.1f}" if t3 >= 0.0 else f"{t3:.1f}"
+    BOLD, RED, RESET = "\033[1m", "\033[91m", "\033[0m"
+    return f" {BOLD}{RED}t:{RESET}[{t1}{RED},{RESET} {t2}{RED},{RESET} {t3}]"
+
+
+def _get_node_name(node):
+    types = [SPHERE, CYLINDER, CONE, CUBE, PLANE]
+    names = ["Sphere  ", "Cylinder", "Cone    ", "Cube    ", "Plane   "]
+    type_to_name = dict(zip(types, names))
+    return type_to_name[node.type] if isinstance(node, Shape) else "Group   "
+
+
+def get_tree_data(node, prefix, is_last):
+    BOLD, GREEN, BLUE, RESET = "\033[1m", "\033[92m", "\033[94m", "\033[0m"
+    name = _get_node_name(node)
+    if isinstance(node, paz.graphics.Group):
+        label = f"{BOLD}{BLUE}{name}{RESET}"
+    else:
+        label = f"{GREEN}{name}{RESET}"
+    connector = "└── " if is_last else "├── "
+    main_string = f"{prefix}{connector}{label}"
+    data_string = _build_node_data_string(node, prefix, is_last)
+    now_total_lines = [main_string + data_string]
+    new_prefix = prefix + ("    " if is_last else "│   ")
+    now_lines, now_child_count = [], 0
+    if isinstance(node, paz.graphics.Group):
+        count = len(node.shapes)
+        for child_arg, child in enumerate(node.shapes):
+            is_child_last = child_arg == count - 1
+            child_data = get_tree_data(child, new_prefix, is_child_last)
+            new_lines, new_child_count = child_data
+            now_lines.extend(new_lines)
+            now_child_count = now_child_count + new_child_count
+
+    if isinstance(node, paz.graphics.Shape):
+        now_child_count = 1 + now_child_count
+    return now_total_lines + now_lines, now_child_count
+
+
+def show(scene):
+    all_lines, total_nodes, count = [], 0, len(scene.nodes)
+    for i, node in enumerate(scene.nodes):
+        is_last_node = i == count - 1
+        lines, node_count = get_tree_data(node, "", is_last_node)
+        all_lines.extend(lines)
+        total_nodes = total_nodes + node_count
+
+    print("\n" + "=" * 50)
+    BOLD, YELLOW, RESET = "\033[1m", "\033[93m", "\033[0m"
+    print(
+        f"      SCENE HIERARCHY ({BOLD}{YELLOW}Total Shapes:{RESET} {total_nodes})"
+    )
+    print("=" * 50)
+    for line in all_lines:
+        print(line)
+    print("=" * 50 + "\n")
 
 
 def prepare_lights(lights):
