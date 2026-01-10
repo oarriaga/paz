0569a66723b75f93c28ad94c0c66a338eaf0aa70
Octavio Arriaga <arriaga.camargo@gmail.com>
2026-01-10 09:40:56 +0100

    Add norm computation of depth image

--- paz/backend/depth.py
+++ paz/backend/depth.py
@@ -1,7 +1,6 @@
 import jax.numpy as jp
 import numpy as np
 import cv2
-import paz
 
 
 def write(depth, filepath, scale=1e4):
@@ -28,3 +27,9 @@ def to_RGB(depth):
     max_depth = jp.max(depth)
     normalized_depth = depth - min_depth / (max_depth - min_depth)
     return (255 * normalized_depth).astype("uint8")
+
+
+def compute_gradient_norm(depth):
+    gradient_x = jp.abs(jp.diff(depth, axis=1, prepend=depth[:, :1]))
+    gradient_y = jp.abs(jp.diff(depth, axis=0, prepend=depth[:1, :]))
+    return jp.sqrt(gradient_x**2 + gradient_y**2)
