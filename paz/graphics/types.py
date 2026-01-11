46bfc3a504b352a60970fefae7290f1edd8616ee
Octavio Arriaga <arriaga.camargo@gmail.com>
2026-01-11 15:51:52 +0100

    Refactor types to not trigger jax initialization

--- paz/graphics/types.py
+++ paz/graphics/types.py
@@ -17,26 +17,41 @@ from paz.graphics.constants import (
 PointLight = namedtuple("PointLight", ["intensity", "position"])
 
 
-Pattern = namedtuple(
+_Pattern = namedtuple(
     "Pattern",
     ["transform", "type", "image"],
-    defaults=(jp.eye(4), NO_PATTERN, jp.ones((1, 1, 3))),
+    defaults=(None, NO_PATTERN, None),
 )
 
 
-def SphericalPattern(image, transform=jp.eye(4)):
+class Pattern(_Pattern):
+    __slots__ = ()
+
+    def __new__(cls, transform=None, type=NO_PATTERN, image=None):
+        transform = jp.eye(4) if transform is None else jp.array(transform)
+        image = jp.ones((1, 1, 3)) if image is None else jp.array(image)
+        return super().__new__(cls, transform, type, image)
+
+
+def SphericalPattern(image, transform=None):
+    if transform is None:
+        transform = jp.eye(4)
     return Pattern(transform, SPHERICAL_PATTERN, image)
 
 
-def PlanarPattern(image, transform=jp.eye(4)):
+def PlanarPattern(image, transform=None):
+    if transform is None:
+        transform = jp.eye(4)
     return Pattern(transform, PLANAR_PATTERN, image)
 
 
-def CylindricalPattern(image, transform=jp.eye(4)):
+def CylindricalPattern(image, transform=None):
+    if transform is None:
+        transform = jp.eye(4)
     return Pattern(transform, CYLINDRICAL_PATTERN, image)
 
 
-Material = namedtuple(
+_Material = namedtuple(
     "Material",
     [
         "color",
@@ -48,12 +63,52 @@ Material = namedtuple(
         "transparency",
         "refractive_index",
     ],
-    defaults=(DEFAULT_COLOR, 0.1, 0.9, 0.9, 200.0, 0.0, 0.0, 1.0),
+    defaults=(None, 0.1, 0.9, 0.9, 200.0, 0.0, 0.0, 1.0),
 )
 
-Shape = namedtuple(
-    "Shape", ["transform", "type", "material", "pattern"], defaults=(Pattern(),)
+
+class Material(_Material):
+    __slots__ = ()
+
+    def __new__(
+        cls,
+        color=None,
+        ambient=0.1,
+        diffuse=0.9,
+        specular=0.9,
+        shininess=200.0,
+        reflective=0.0,
+        transparency=0.0,
+        refractive_index=1.0,
+    ):
+        color = jp.array(DEFAULT_COLOR) if color is None else jp.array(color)
+        return super().__new__(
+            cls,
+            color,
+            ambient,
+            diffuse,
+            specular,
+            shininess,
+            reflective,
+            transparency,
+            refractive_index,
+        )
+
+
+_Shape = namedtuple(
+    "Shape",
+    ["transform", "type", "material", "pattern"],
+    defaults=(None,),
 )
+
+
+class Shape(_Shape):
+    __slots__ = ()
+
+    def __new__(cls, transform, type, material, pattern=None):
+        transform = jp.array(transform)
+        pattern = Pattern() if pattern is None else pattern
+        return super().__new__(cls, transform, type, material, pattern)
 
 
 _SceneBase = namedtuple("Scene", ["nodes", "parent_array"])
@@ -80,37 +135,57 @@ class Scene(_SceneBase):
                     "Length of `nodes` and `parent_array` must equal."
                 )
 
-            # is_root = parent_array == -1
-            # is_valid_child = (parent_array >= 0) & (parent_array < len(nodes))
-            # if not jp.all(is_root | is_valid_child):
-            #     raise ValueError("`parent_array` contains invalid indices.")
+            is_root = parent_array == -1
+            is_valid_child = (parent_array >= 0) & (
+                parent_array < len(nodes)
+            )
+            if not jp.all(is_root | is_valid_child):
+                raise ValueError("`parent_array` contains invalid indices.")
 
         return super().__new__(cls, nodes, parent_array)
 
 
-Group = namedtuple("Group", ["shapes", "transform"], defaults=(jp.eye(4),))
+_Group = namedtuple("Group", ["shapes", "transform"], defaults=(None,))
+
+
+class Group(_Group):
+    __slots__ = ()
+
+    def __new__(cls, shapes, transform=None):
+        transform = jp.eye(4) if transform is None else jp.array(transform)
+        return super().__new__(cls, shapes, transform)
 
 CSG = namedtuple("CSG", ["shape_A", "shape_B", "operation"])
 
 
-def Sphere(transform=jp.eye(4), material=Material(), pattern=Pattern()):
+def Sphere(transform=None, material=Material(), pattern=Pattern()):
+    if transform is None:
+        transform = jp.eye(4)
     return Shape(transform, SPHERE, material, pattern)
 
 
-def Plane(transform=jp.eye(4), material=Material(), pattern=Pattern()):
+def Plane(transform=None, material=Material(), pattern=Pattern()):
+    if transform is None:
+        transform = jp.eye(4)
     return Shape(transform, PLANE, material, pattern)
 
 
-def Cube(transform=jp.eye(4), material=Material(), pattern=Pattern()):
+def Cube(transform=None, material=Material(), pattern=Pattern()):
+    if transform is None:
+        transform = jp.eye(4)
     return Shape(transform, CUBE, material, pattern)
 
 
-def Cone(transform=jp.eye(4), material=Material(), pattern=Pattern()):
+def Cone(transform=None, material=Material(), pattern=Pattern()):
+    if transform is None:
+        transform = jp.eye(4)
     canonical_scale = paz.SE3.scaling(jp.array([1.0, 2.0, 1.0]))
     canonical_shift = paz.SE3.translation(jp.array([0.0, 1.0, 0.0]))
     canonical_transform = canonical_shift @ canonical_scale
     return Shape(transform @ canonical_transform, CONE, material, pattern)
 
 
-def Cylinder(transform=jp.eye(4), material=Material(), pattern=Pattern()):
+def Cylinder(transform=None, material=Material(), pattern=Pattern()):
+    if transform is None:
+        transform = jp.eye(4)
     return Shape(transform, CYLINDER, material, pattern)
