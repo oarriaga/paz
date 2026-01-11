2fef2fb7594761951a48011c814083c0435d8d30
Octavio Arriaga <arriaga.camargo@gmail.com>
2026-01-11 15:49:23 +0100

    Modified colors to be numpy arrays to avoid jax initialization

--- paz/graphics/constants.py
+++ paz/graphics/constants.py
@@ -1,14 +1,14 @@
-import jax.numpy as jp
+import numpy as np
 
 FARAWAY = 1000.0
 EPSILON = 1e-4
 
-RED = jp.array([1.0, 0.0, 0.0])
-GREEN = jp.array([0.0, 1.0, 0.0])
-BLUE = jp.array([0.0, 0.0, 1.0])
-BLACK = jp.array([0.0, 0.0, 0.0])
-WHITE = jp.array([1.0, 1.0, 1.0])
-DEFAULT_COLOR = jp.array([1.0, 0.65, 0.0])
+RED = np.array([1.0, 0.0, 0.0])
+GREEN = np.array([0.0, 1.0, 0.0])
+BLUE = np.array([0.0, 0.0, 1.0])
+BLACK = np.array([0.0, 0.0, 0.0])
+WHITE = np.array([1.0, 1.0, 1.0])
+DEFAULT_COLOR = np.array([1.0, 0.65, 0.0])
 
 NO_PATTERN = 0
 SPHERICAL_PATTERN = 1
