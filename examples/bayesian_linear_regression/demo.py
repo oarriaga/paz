c2d8c4ce73ed08820cf29cd838ae5b0ae9c7d0bf
Octavio Arriaga <arriaga.camargo@gmail.com>
2026-01-12 17:49:31 +0100

    Comment out MAP

--- examples/bayesian_linear_regression/demo.py
+++ examples/bayesian_linear_regression/demo.py
@@ -41,38 +41,40 @@ parameters = line.sample_inverse(key)
 states = line.apply(parameters)
 
 
-def compute_loss(parameters):
-    return -line.apply(parameters).log_prob_sum
+# def compute_loss(parameters):
+#     return -line.apply(parameters).log_prob_sum
 
 
-learning_rate = 0.001
-optimizer = optax.adam(learning_rate)
-opt_state = optimizer.init(parameters)
+# learning_rate = 0.001
+# optimizer = optax.adam(learning_rate)
+# opt_state = optimizer.init(parameters)
 
 
-@jax.jit
-def update_step(parameters, opt_state):
-    loss, grads = jax.value_and_grad(compute_loss)(parameters)
-    updates, new_opt_state = optimizer.update(grads, opt_state)
-    new_params = optax.apply_updates(parameters, updates)
-    return new_params, new_opt_state, loss
+# @jax.jit
+# def update_step(parameters, opt_state):
+#     loss, grads = jax.value_and_grad(compute_loss)(parameters)
+#     updates, new_opt_state = optimizer.update(grads, opt_state)
+#     new_params = optax.apply_updates(parameters, updates)
+#     return new_params, new_opt_state, loss
 
 
-num_steps = 20_000
-for step in range(num_steps):
-    parameters, opt_state, loss = update_step(parameters, opt_state)
-    print(f"Step {step:3d}, Loss: {loss:.4f}")
+# num_steps = 20_000
+# for step in range(num_steps):
+#     parameters, opt_state, loss = update_step(parameters, opt_state)
+#     print(f"Step {step:3d}, Loss: {loss:.4f}")
 
-print(f"Final optimized parameters: {parameters}")
-state = line.apply(parameters)
+# print(f"Final optimized parameters: {parameters}")
+# state = line.apply(parameters)
 
-key = jax.random.PRNGKey(888)
-for key in jax.random.split(key, num_samples := 100):
-    plt.plot(X, state.sample.y_pred.sample(seed=key), color="blue", alpha=0.2)
-plt.plot(X, observations, color="red", alpha=0.8)
-plt.xlim(0, 1)
-plt.ylim(-4, 4)
-plt.show()
+# key = jax.random.PRNGKey(888)
+# for key in jax.random.split(key, num_samples := 100):
+#     plt.plot(X, state.sample.y_pred.sample(seed=key), color="blue", alpha=0.2)
+# plt.plot(X, observations, color="red", alpha=0.8)
+# plt.xlim(0, 1)
+# plt.ylim(-4, 4)
+# plt.show()
 
-
-paz.inference.metropolis_hastings
+# paz.inference.metropolis_hastings
+paz.inference.metropolis_hastings.sample(
+    key, log_density_fn, positions, sigma, num_samples, num_chains
+)
