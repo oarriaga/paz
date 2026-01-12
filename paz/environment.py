3da68658e1f97b1cf95ac7403f294837696ac1b0
Octavio Arriaga <arriaga.camargo@gmail.com>
2026-01-12 16:49:54 +0100

    Add environment function

--- paz/environment.py
+++ paz/environment.py
@@ -24,6 +24,7 @@ The functions below configure JAX behavior at runtime (after import):
 
 set_debug_nans(enabled): Enable/disable JAX NaN debugging
 set_platform(name): Set JAX platform ("cpu", "gpu", "tpu")
+set_compilation_cache(cache_dir): Enable JAX compilation caching
 """
 import jax
 
@@ -36,3 +37,10 @@ def set_debug_nans(enabled=True):
 def set_platform(name):
     jax.config.update("jax_platform_name", name)
     return name
+
+
+def set_compilation_cache(cache_dir="/tmp/jax_cache"):
+    jax.config.update("jax_compilation_cache_dir", cache_dir)
+    jax.config.update("jax_persistent_cache_min_compile_time_secs", 0)
+    jax.config.update("jax_persistent_cache_min_entry_size_bytes", -1)
+    return cache_dir
