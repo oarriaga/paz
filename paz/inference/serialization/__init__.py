1cb0b3349fc6d4390aea7734d1af53916eb97056
Octavio Arriaga <arriaga.camargo@gmail.com>
2026-01-20 12:14:41 +0100

    Fix init for serialization module

--- paz/inference/serialization/__init__.py
+++ paz/inference/serialization/__init__.py
@@ -1,3 +1,4 @@
 from .api import load, save
+from .serializable import build_distribution_fn, serializable
 
-__all__ = ["load", "save"]
+__all__ = ["load", "save", "serializable", "build_distribution_fn"]
