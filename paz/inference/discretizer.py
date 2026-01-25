25d10f9353795bb24963851bc2cdef0df7001a22
Octavio Arriaga <arriaga.camargo@gmail.com>
2026-01-25 19:32:43 +0100

    Add discretization functions with grid

--- paz/inference/discretizer.py
+++ paz/inference/discretizer.py
@@ -33,6 +33,31 @@ def get_grid_values(min_val, max_val, num_steps):
     return jp.linspace(min_val, max_val, num_steps)
 
 
+def discretize_to_grid(
+    distribution, min_val, max_val, num_steps, log_space=False
+):
+    """Discretize distribution by evaluating log_prob at grid points."""
+    # TODO most likely try to make the API fit to one single function
+    if log_space:
+        log_min = jp.log(min_val)
+        log_max = jp.log(max_val)
+        values = jp.exp(jp.linspace(log_min, log_max, num_steps))
+    else:
+        values = get_grid_values(min_val, max_val, num_steps)
+    log_probs = distribution.log_prob(values)
+    return values, log_probs
+
+
+def discretize_circular(
+    distribution, num_steps, min_angle=-jp.pi, max_angle=jp.pi
+):
+    """Discretize circular distribution (VonMises) over angle range."""
+    # TODO most likely try to make the API fit to one single function
+    angles = jp.linspace(min_angle, max_angle, num_steps, endpoint=False)
+    log_probs = distribution.log_prob(angles)
+    return angles, log_probs
+
+
 def indices_to_values(indices, min_val, max_val, num_steps):
     fraction = indices.astype(jp.float32) / (num_steps - 1)
     return min_val + fraction * (max_val - min_val)
