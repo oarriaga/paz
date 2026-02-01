6ac42b3e7b9190b20d33df9e07c12cbce73b5561
Octavio Arriaga <arriaga.camargo@gmail.com>
2026-02-01 21:55:28 +0100

    Delete old progress bar

--- paz/__init__.py
+++ paz/__init__.py
@@ -47,8 +47,6 @@ from paz import callbacks
 from paz import layers
 
 from paz import plot
-from paz.progress_bar import bar
-from paz import progress_bar
 from paz import progressbar
 from paz import inference
 from paz.inference import metropolis_hastings
