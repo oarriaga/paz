ccf91e8ddb789013f060e78b040b46b752bde686
Octavio Arriaga <arriaga.camargo@gmail.com>
2026-03-23 23:06:31 +0100

    Add module for keypoint models

---
+++ paz/models/keypoint/__init__.py
@@ -0,0 +1,6 @@
+from .detnet import DetNet
+from .iknet import IKNet
+from .keypointnet import FaceKeypointNet2D32
+from .keypointnet import KeypointNet
+from .keypointnet import KeypointNet2D
+from .keypointnet import KeypointNetShared
