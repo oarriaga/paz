da42ea91fa48f048a3987e25c78ae152dffcb8fe
Luis Octavio Arriaga Camargo <arriaga.camargo@gmail.com>
2026-03-31 16:14:09 +0200

    Add attribute to stopping functions for print debug

--- paz/optimization/stopping.py
+++ paz/optimization/stopping.py
@@ -9,6 +9,7 @@ def grad_norm_stop(tolerance):
     def stop_fn(_step_arg, _params, _loss, gradients):
         return otu.tree_norm(gradients) < tolerance
 
+    stop_fn.message = "stop=grad_norm"
     return stop_fn
 
 
@@ -16,6 +17,7 @@ def loss_stop(tolerance):
     def stop_fn(_step_arg, _params, loss, _gradients):
         return loss < tolerance
 
+    stop_fn.message = "stop=loss"
     return stop_fn
 
 
@@ -23,5 +25,12 @@ def _never_stop(_step_arg, _params, _loss, _gradients):
     return jp.array(False)
 
 
+_never_stop.message = None
+
+
 def _build_status(has_to_stop):
     return jp.where(has_to_stop, STOP_FN_MET, MAX_STEPS_REACHED)
+
+
+def _get_stop_message(stop_fn):
+    return getattr(stop_fn, "message", "stop=stop_fn")
