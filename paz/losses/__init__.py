4c9696c06ef9b482a1c8e7319cd9faa4d916a2de
Luis Octavio Arriaga Camargo <arriaga.camargo@gmail.com>
2026-03-31 07:27:20 +0200

    Expose loss functions to user

--- paz/losses/__init__.py
+++ paz/losses/__init__.py
@@ -1,1 +1,5 @@
 from paz.losses import multibox
+from paz.losses.standard import bce
+from paz.losses.standard import mae
+from paz.losses.standard import mse
+from paz.losses.standard import weight
