c09868fab83591bada932f906c83ed7e99febe28
Luis Octavio Arriaga Camargo <arriaga.camargo@gmail.com>
2026-03-31 16:08:13 +0200

    Add masked losses

--- paz/losses/standard.py
+++ paz/losses/standard.py
@@ -34,9 +34,57 @@ def mean_absolute_error(y_true, y_pred, axis=None, reduction="mean"):
     return result
 
 
+def _expand_masks(y_true, masks):
+    H, W = y_true.shape[:2]
+    masks = jp.array(masks)
+    if masks.ndim == 2:
+        masks = masks[None, ..., None]
+    elif masks.ndim == 3:
+        if masks.shape[:2] == (H, W):
+            masks = masks[None]
+        else:
+            masks = masks[..., None]
+    return masks.astype(bool)
+
+
+def masked(loss_fn, y_true, y_pred, masks, mask_value=0.0):
+    y_true = jp.array(y_true)
+    y_pred = jp.array(y_pred)
+    masks = _expand_masks(y_true, masks)
+    masked_true = jp.where(masks, y_true[None], mask_value)
+    masked_pred = jp.where(masks, y_pred[None], mask_value)
+    loss = loss_fn(masked_true, masked_pred, reduction="none")
+    axis = tuple(range(1, loss.ndim))
+    return loss.mean(axis=axis).sum()
+
+
+def masked_mean_absolute_error(y_true, y_pred, masks, mask_value=0.0):
+    return masked(mean_absolute_error, y_true, y_pred, masks, mask_value)
+
+
+def masked_mean_squared_error(y_true, y_pred, masks, mask_value=0.0):
+    return masked(mean_squared_error, y_true, y_pred, masks, mask_value)
+
+
+def masked_binary_cross_entropy(y_true, y_pred, masks, mask_value=0.0):
+    return masked(binary_cross_entropy, y_true, y_pred, masks, mask_value)
+
+
+def soft_box_barrier(
+    values, min_val, max_val, curvature, axis=None, reduction="sum"
+):
+    negative = curvature * (values - min_val)
+    positive = curvature * (values - max_val)
+    loss = jp.exp(-negative) + jp.exp(positive)
+    return _reduce_loss(loss, axis=axis, reduction=reduction)
+
+
 bce = binary_cross_entropy
 mse = mean_squared_error
 mae = mean_absolute_error
+masked_bce = masked_binary_cross_entropy
+masked_mse = masked_mean_squared_error
+masked_mae = masked_mean_absolute_error
 
 
 def weight(losses, weights):
