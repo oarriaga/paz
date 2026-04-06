20e53975843ebdb89851a7c81c455e83e44e572c
Luis Octavio Arriaga Camargo <arriaga.camargo@gmail.com>
2026-04-06 12:24:06 +0200

    Add tests for mask renderer

--- paz/graphics/mesh_test.py
+++ paz/graphics/mesh_test.py
@@ -26,6 +26,7 @@ from paz.graphics.mesh import (
     fill_bottom_with_last,
     fill_mesh,
     build_cube,
+    build_sphere,
     tile_render,
     tile_render_depth,
     assert_exact_tile_side,
@@ -235,6 +236,29 @@ def test_build_cube():
     assert edges.shape[1] == 2
     assert len(vertices) == 8
     assert len(faces) == 12
+
+
+def face_centers_and_normals(vertices, faces):
+    A = vertices[faces[:, 0]]
+    B = vertices[faces[:, 1]]
+    C = vertices[faces[:, 2]]
+    centers = (A + B + C) / 3.0
+    normals = jp.cross(B - A, C - A)
+    return centers, normals
+
+
+def test_build_cube_faces_point_outward():
+    vertices, faces, _ = build_cube(1.0)
+    centers, normals = face_centers_and_normals(vertices, faces)
+    dots = jp.sum(centers * normals, axis=1)
+    assert jp.all(dots > 0.0)
+
+
+def test_build_sphere_faces_point_outward():
+    vertices, faces, _ = build_sphere(1.0, 2)
+    centers, normals = face_centers_and_normals(vertices, faces)
+    dots = jp.sum(centers * normals, axis=1)
+    assert jp.all(dots > 0.0)
 
 
 def make_scene(image_shape=(20, 20)):
