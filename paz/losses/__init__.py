71d911598a56021c8dabcf3f68c744f8e58dc8b3
Luis Octavio Arriaga Camargo <arriaga.camargo@gmail.com>
2026-04-06 12:21:12 +0200

    Add mesh and depth losses

--- paz/losses/__init__.py
+++ paz/losses/__init__.py
@@ -1,4 +1,7 @@
+from paz.losses import depth
 from paz.losses import multibox
+from paz.losses.mesh import laplacian_smoothing
+from paz.losses.mesh import volume_matching
 from paz.losses.standard import bce
 from paz.losses.standard import mae
 from paz.losses.standard import mse
