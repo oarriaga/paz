dadb830ae2fd47db201240184f7f2b3646ecfc27
Luis Octavio Arriaga Camargo <arriaga.camargo@gmail.com>
2026-05-09 18:22:16 +0200

    Add Cook Torrance material type

--- paz/graphics/types.py
+++ paz/graphics/types.py
@@ -51,6 +51,21 @@ Material = namedtuple(
     defaults=(DEFAULT_COLOR, 0.1, 0.9, 0.9, 200.0, 0.0, 0.0, 1.0),
 )
 
+CookTorranceMaterial = namedtuple(
+    "CookTorranceMaterial",
+    [
+        "color",
+        "ambient",
+        "base_reflectance",
+        "roughness",
+        "metallic",
+        "reflective",
+        "transparency",
+        "refractive_index",
+    ],
+    defaults=(DEFAULT_COLOR, 0.1, 0.04, 0.5, 0.0, 0.0, 0.0, 1.0),
+)
+
 Shape = namedtuple(
     "Shape", ["transform", "type", "material", "pattern"], defaults=(Pattern(),)
 )
